bd503f12be1a772137e3ff41c22fabb74df079a6
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-07-16 18:18:03 +0100

    Connection, Invitation types

--- definitions/src/Simplex/Messaging/Types.hs
+++ definitions/src/Simplex/Messaging/Types.hs
@@ -6,62 +6,63 @@ module Simplex.Messaging.Types where
 
 import Data.Aeson
 import Data.String
+import Data.Text
 import GHC.Generics
 
-newtype CreateConnRequest
-  = CreateConnRequest
-      { recipientKey :: Key
-      }
+newtype CreateConnRequest = CreateConnRequest
+  { recipientKey :: Key
+  }
   deriving (Eq, Show, Generic, ToJSON, FromJSON)
 
 instance IsString CreateConnRequest where
   fromString = CreateConnRequest
 
-data CreateConnResponse
-  = CreateConnResponse
-      { recipientId :: String,
-        senderId :: String
-      }
+data CreateConnResponse = CreateConnResponse
+  { recipientId :: String,
+    senderId :: String
+  }
   deriving (Show, Generic, ToJSON, FromJSON)
 
-newtype SecureConnRequest
-  = SecureConnRequest
-      { senderKey :: Key
-      }
+newtype SecureConnRequest = SecureConnRequest
+  { senderKey :: Key
+  }
   deriving (Show, Generic, ToJSON, FromJSON)
 
 instance IsString SecureConnRequest where
   fromString = SecureConnRequest
 
-data Message
-  = Message
-      { msgId :: MessageId,
-        ts :: TimeStamp,
-        msg :: Encrypted -- TODO make it Text
-      }
+data Message = Message
+  { msgId :: MessageId,
+    ts :: TimeStamp,
+    msg :: Encrypted -- TODO make it Text
+  }
   deriving (Show, Generic, ToJSON, FromJSON)
 
-data MessagesResponse
-  = MessagesResponse
-      { messages :: [Message],
-        nextMessageId :: Maybe Base64EncodedString
-      }
+data MessagesResponse = MessagesResponse
+  { messages :: [Message],
+    nextMessageId :: Maybe Base64EncodedString
+  }
   deriving (Show, Generic, ToJSON, FromJSON)
 
-newtype SendMessageRequest
-  = SendMessageRequest
-      { msg :: Base64EncodedString
-      }
+newtype SendMessageRequest = SendMessageRequest
+  { msg :: Base64EncodedString
+  }
   deriving (Show, Generic, ToJSON, FromJSON)
 
 instance IsString SendMessageRequest where
   fromString = SendMessageRequest
 
-type Invitation = Base64EncodedString -- TODO define
+data Invitation = Invitation
+  { connId :: ConnId,
+    brokerUri :: Text,
+    encryptKey :: PublicKey
+  }
 
 type Key = Base64EncodedString -- deprecated, not to be used
 
 type PublicKey = Base64EncodedString
+
+type PrivateKey = Base64EncodedString
 
 type ConnId = Base64EncodedString
 
