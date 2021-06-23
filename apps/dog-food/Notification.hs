9c05422c0b1d50946c20b7f96ace1356135bc55b
Nikita Poberezkin <39944650+npoberezkin@users.noreply.github.com>
2021-06-23 16:01:53 +0300

    add support for windows/wsl notifications

--- apps/dog-food/Notification.hs
+++ apps/dog-food/Notification.hs
@@ -6,20 +6,29 @@ module Notification where
 
 import ChatTerminal.Core (safeDecodeUtf8)
 import Control.Monad (void)
-import DBus.Notify (connectSession, summary, body, appName, Body(Text), notify, blankNote)
+import Control.Monad.IO.Class (liftIO)
+import DBus.Notify (Body (Text), appName, blankNote, body, connectSession, notify, summary)
 import Data.ByteString.Char8 (ByteString)
 import Data.Char (toLower)
 import Data.List (isInfixOf)
 import Data.Text (Text)
 import qualified Data.Text as T
-import System.Directory (doesFileExist)
+import System.Directory (doesFileExist, getAppUserDataDirectory)
+import System.FilePath (combine)
 import System.Info (os)
 import System.Process (readCreateProcess, shell)
 
 data Notification = Notification {title :: ByteString, text :: ByteString}
 
-winPsScript :: FilePath
-winPsScript = combine (getAppUserDataDirectory "simplex") "win-toast-notify.ps1"
+-- winPsScript :: FilePath -> FilePath
+-- winPsScript dir = do
+--   appDir <- liftIO $ getAppUserDataDirectory "simplex"
+--   combine appDir "win-toast-notify.ps1"
+
+-- winPsScript :: IO FilePath
+-- winPsScript = do
+--   appDir <- getAppUserDataDirectory "simplex"
+--   return $ combine appDir "win-toast-notify.ps1"
 
 initializeNotifications :: IO (Notification -> IO ())
 initializeNotifications = case os of
@@ -36,15 +45,14 @@ initializeNotifications = case os of
   _ -> pure . const $ pure ()
 
 linuxNotify :: Notification -> IO ()
-linuxNotify Notification {title, text} =
-  do
-    client <- connectSession
-    let linuxNtf =
-          linuxNote
-            { summary = T.unpack $ safeDecodeUtf8 title,
-              body = Just $ Text (T.unpack $ safeDecodeUtf8 text)
-            }
-    void $ notify client linuxNtf
+linuxNotify Notification {title, text} = do
+  client <- connectSession
+  let linuxNtf =
+        linuxNote
+          { summary = T.unpack $ safeDecodeUtf8 title,
+            body = Just $ Text (T.unpack $ safeDecodeUtf8 text)
+          }
+  void $ notify client linuxNtf
   where
     linuxNote = blankNote {appName = "simplex-chat"}
 
@@ -59,28 +67,28 @@ initWinNotify :: IO (Notification -> IO ())
 initWinNotify = winNotify <$> savePowershellScript
 
 savePowershellScript :: IO FilePath
-savePowershellScript = writeFile winPsScript $
+savePowershellScript = do
+  appDir <- getAppUserDataDirectory "simplex"
+  let psScript = combine appDir "win-toast-notify.ps1"
+  writeFile psScript
     "[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null\n\
-    \$Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)\
-     
+    \$Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)\n\
     \$RawXml = [xml] $Template.GetXml()\n\
     \($RawXml.toast.visual.binding.text|where {$_.id -eq \"1\"}).AppendChild($RawXml.CreateTextNode($args[0])) > $null\n\
     \($RawXml.toast.visual.binding.text|where {$_.id -eq \"2\"}).AppendChild($RawXml.CreateTextNode($args[1])) > $null\n\
-    
     \$SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument\n\
     \$SerializedXml.LoadXml($RawXml.OuterXml)\n\
-    
     \$Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)\n\
     \$Toast.Tag = \"simplex-chat\"\n\
     \$Toast.Group = \"simplex-chat\"\n\
     \$Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)\n\
-    
     \$Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier(\"PowerShell\")\n\
     \$Notifier.Show($Toast);\n"
+  return psScript
 
 winNotify :: FilePath -> Notification -> IO ()
 winNotify path Notification {title, text} =
   void $ readCreateProcess (shell . T.unpack $ script) ""
   where
     script :: Text
-    script = "powershell.exe \"" <> winPsScript <> "\""
+    script = "powershell.exe \"" <> T.pack path <> " " <> safeDecodeUtf8 title <> " " <> safeDecodeUtf8 text <> "\""
