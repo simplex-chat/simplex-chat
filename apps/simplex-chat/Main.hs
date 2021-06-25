b76e2c78fe6cd5f95d3fa76775deee8ae9b89905
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-06-25 07:51:23 +0100

    rename app folder

--- apps/simplex-chat/Main.hs
+++ apps/simplex-chat/Main.hs
@@ -1,45 +1,92 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Control.Concurrent (threadDelay)
-import Control.Concurrent.STM (readTVarIO, retry)
-import Control.Monad (forever, void)
-import Simplex.Demo (chatLayoutDemo)
+import ChatOptions
+import Control.Concurrent.STM
+import Control.Logger.Simple
+import Control.Monad.IO.Unlift
+import Control.Monad.Reader
+import Simplex.Chat
+import Simplex.Chat.Controller
+import Simplex.Input
+import Simplex.Messaging.Agent (getSMPAgentClient)
+import Simplex.Messaging.Agent.Env.SQLite
+import Simplex.Messaging.Client (smpDefaultConfig)
 import Simplex.Store (createStore)
-import System.IO (hFlush, stdout)
-import System.Terminal (putStringLn, runTerminalT, withTerminal)
-import qualified System.Terminal as C
-import qualified System.Terminal.Internal as C
+import Simplex.Terminal
+import System.Directory (getAppUserDataDirectory)
+import UnliftIO.Async (race_)
 
-defaultSettings :: C.Size -> C.VirtualTerminalSettings
-defaultSettings size =
-  C.VirtualTerminalSettings
-    { C.virtualType = "xterm",
-      C.virtualWindowSize = pure size,
-      C.virtualEvent = retry,
-      C.virtualInterrupt = retry
+cfg :: AgentConfig
+cfg =
+  AgentConfig
+    { tcpPort = undefined, -- agent does not listen to TCP
+      smpServers = undefined, -- filled in from options
+      rsaKeySize = 2048 `div` 8,
+      connIdBytes = 12,
+      tbqSize = 16,
+      dbFile = "smp-chat.db",
+      smpCfg = smpDefaultConfig
     }
+
+logCfg :: LogConfig
+logCfg = LogConfig {lc_file = Nothing, lc_stderr = True}
 
 main :: IO ()
 main = do
+  ChatOpts {dbFile, smpServers} <- welcomeGetOpts
   void $ createStore "simplex-chat.db" 4
+  ct <- newChatTerminal
+  a <- getSMPAgentClient cfg {dbFile, smpServers}
+  cc <- atomically $ newChatController a ct $ tbqSize cfg
+  -- setLogLevel LogInfo -- LogError
+  -- withGlobalLogging logCfg $ do
+  runReaderT simplexChat cc
 
-  hFlush stdout
-  -- ChatTerminal {termSize} <- newChatTerminal
-  -- pos <- C.withVirtualTerminal (defaultSettings termSize) $
-  --   \t -> runTerminalT (C.setAlternateScreenBuffer True >> C.putString "a" >> C.flush >> C.getCursorPosition) t
-  -- print pos
-  -- race_ (printEvents t) (updateTerminal t)
-  void . withTerminal . runTerminalT $ chatLayoutDemo >> C.flush >> C.awaitEvent
+welcomeGetOpts :: IO ChatOpts
+welcomeGetOpts = do
+  appDir <- getAppUserDataDirectory "simplex"
+  opts@ChatOpts {dbFile} <- getChatOpts appDir
+  putStrLn "SimpleX chat prototype v0.3.1"
+  putStrLn $ "db: " <> dbFile
+  putStrLn "type \"/help\" or \"/h\" for usage info"
+  pure opts
 
-printEvents :: C.VirtualTerminal -> IO ()
-printEvents t = forever $ do
-  event <- withTerminal . runTerminalT $ C.flush >> C.awaitEvent
-  runTerminalT (putStringLn $ show event) t
+simplexChat :: (MonadUnliftIO m, MonadReader ChatController m) => m ()
+simplexChat = race_ runTerminalInput runChatController
 
-updateTerminal :: C.VirtualTerminal -> IO ()
-updateTerminal t = forever $ do
-  threadDelay 10000
-  win <- readTVarIO $ C.virtualWindow t
-  withTerminal . runTerminalT $ mapM_ C.putStringLn win >> C.flush
+-- defaultSettings :: C.Size -> C.VirtualTerminalSettings
+-- defaultSettings size =
+--   C.VirtualTerminalSettings
+--     { C.virtualType = "xterm",
+--       C.virtualWindowSize = pure size,
+--       C.virtualEvent = retry,
+--       C.virtualInterrupt = retry
+--     }
+
+-- main :: IO ()
+-- main = do
+--   void $ createStore "simplex-chat.db" 4
+
+--   hFlush stdout
+--   -- ChatTerminal {termSize} <- newChatTerminal
+--   -- pos <- C.withVirtualTerminal (defaultSettings termSize) $
+--   --   \t -> runTerminalT (C.setAlternateScreenBuffer True >> C.putString "a" >> C.flush >> C.getCursorPosition) t
+--   -- print pos
+--   -- race_ (printEvents t) (updateTerminal t)
+--   void . withTerminal . runTerminalT $ chatLayoutDemo >> C.flush >> C.awaitEvent
+
+-- printEvents :: C.VirtualTerminal -> IO ()
+-- printEvents t = forever $ do
+--   event <- withTerminal . runTerminalT $ C.flush >> C.awaitEvent
+--   runTerminalT (putStringLn $ show event) t
+
+-- updateTerminal :: C.VirtualTerminal -> IO ()
+-- updateTerminal t = forever $ do
+--   threadDelay 10000
+--   win <- readTVarIO $ C.virtualWindow t
+--   withTerminal . runTerminalT $ mapM_ C.putStringLn win >> C.flush
