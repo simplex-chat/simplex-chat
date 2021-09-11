46cf31440382ee7284cf3c68719eb8e05b341b40
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2021-09-11 22:45:22 +1000

    update default smp servers (#101)
    * update default smp servers
    
    * add smp3 server

--- src/Simplex/Chat/Options.hs
+++ src/Simplex/Chat/Options.hs
@@ -31,8 +31,17 @@ chatOpts appDir =
       ( long "server"
           <> short 's'
           <> metavar "SERVER"
-          <> help "SMP server(s) to use (smp1.simplex.im#pLdiGvm0jD1CMblnov6Edd/391OrYsShw+RgdfR0ChA=)"
-          <> value (L.fromList ["smp1.simplex.im#pLdiGvm0jD1CMblnov6Edd/391OrYsShw+RgdfR0ChA="])
+          <> help
+            ( "SMP server(s) to use"
+                <> "\n(smp2.simplex.im#z5W2QLQ1Br3Yd6CoWg7bIq1bHdwK7Y8bEiEXBs/WfAg=,"
+                <> "\nsmp3.simplex.im#nxc7HnrnM8dOKgkMp008ub/9o9LXJlxlMrMpR+mfMQw=)"
+            )
+          <> value
+            ( L.fromList
+                [ "smp2.simplex.im#z5W2QLQ1Br3Yd6CoWg7bIq1bHdwK7Y8bEiEXBs/WfAg=",
+                  "smp3.simplex.im#nxc7HnrnM8dOKgkMp008ub/9o9LXJlxlMrMpR+mfMQw="
+                ]
+            )
       )
   where
     defaultDbFilePath = combine appDir "simplex"
