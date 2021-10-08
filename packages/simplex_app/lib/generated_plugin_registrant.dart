1eaa306a70005fadc31b01e37e7310ff833f918c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-10-08 07:27:58 +0100

    revert README change

---
+++ packages/simplex_app/lib/generated_plugin_registrant.dart
@@ -0,0 +1,18 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  FilePickerWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
