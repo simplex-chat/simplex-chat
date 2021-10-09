493ea01f8585a77a86ad1272411f6a7823353cb4
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2021-10-09 22:32:22 +0500

    new screens added

--- packages/simplex_app/lib/generated_plugin_registrant.dart
+++ packages/simplex_app/lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -14,5 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FilePickerWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
