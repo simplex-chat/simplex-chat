963f49b76be6cf468e67110121d8fd70c78960f2
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-10-16 12:36:01 +0100

    encode/decode RSA keys using asn1lib library

--- packages/simplexmq/lib/src/rsa_keys.dart
+++ packages/simplexmq/lib/src/rsa_keys.dart
@@ -1,26 +1,108 @@
 import 'dart:typed_data';
 import 'package:asn1lib/asn1lib.dart';
-import 'package:pointycastle/asymmetric/api.dart';
-import 'package:ssh_key/ssh_key_bin.dart';
+import 'package:pointycastle/asymmetric/api.dart' as pc;
 
-const _rsaAlgorithmOid = '1.2.840.113549.1.1.1';
-final asn1Null = ASN1Null();
+const _rsaOid = '1.2.840.113549.1.1.1';
+final _asnRsaOid = ASN1ObjectIdentifier.fromComponentString(_rsaOid);
+final _asn1Null = ASN1Null();
 
-RSAPublicKey? decodeRsaPubKey(Uint8List binKey) {
-  // final source = PubTextSource('', 0, 0, PubKeyEncoding.x509spki);
-  final spki = SubjectPublicKeyInfo.decode(binKey, source: null);
-  if (spki.algorithmOid != _rsaAlgorithmOid ||
-      spki.algorithmParameters.length != 1 ||
-      spki.algorithmParameters.first is! ASN1Null) return null;
-  final pkcs1 = Pkcs1RsaPublicKey.decode(spki.data, null);
-  return RSAPublicKey(pkcs1.modulus, pkcs1.exponent);
+ASN1Sequence _asn1Sequence(List<ASN1Object> elements) {
+  final seq = ASN1Sequence()..elements = elements;
+  return seq;
 }
 
-Uint8List encodeRsaPubKey(RSAPublicKey key) {
-  final seq = ASN1Sequence()
-    ..add(ASN1Integer(key.modulus!))
-    ..add(ASN1Integer(key.publicExponent!));
-  final spki =
-      SubjectPublicKeyInfo(_rsaAlgorithmOid, [asn1Null], seq.encodedBytes);
-  return spki.encode();
+void _assertRsaAlgorithm(ASN1Sequence seq) {
+  if (seq.elements.isEmpty ||
+      seq.elements[0] is! ASN1ObjectIdentifier ||
+      (seq.elements[0] as ASN1ObjectIdentifier).identifier != _rsaOid) {
+    throw Exception('Invalid key algorithm identifier');
+  }
 }
+
+/// Decodes binary PKCS1 to [RSAPublicKey]
+pc.RSAPublicKey decodeRsaPubKeyPKCS1(Uint8List bytes) {
+  final els = ASN1Sequence.fromBytes(bytes).elements;
+  if (els.length != 2 || els[0] is! ASN1Integer || els[1] is! ASN1Integer) {
+    throw Exception('Invalid PKCS1 encoding');
+  }
+  return pc.RSAPublicKey(
+    (els[0] as ASN1Integer).valueAsBigInteger!,
+    (els[1] as ASN1Integer).valueAsBigInteger!,
+  );
+}
+
+/// Decodes binary SPKI to [RSAPublicKey]
+pc.RSAPublicKey decodeRsaPubKey(Uint8List bytes) {
+  final els = ASN1Sequence.fromBytes(bytes).elements;
+  if (els.length != 2 || els[1] is! ASN1BitString || els[0] is! ASN1Sequence) {
+    throw Exception('Invalid SPKI structure');
+  }
+  _assertRsaAlgorithm(els[0] as ASN1Sequence);
+  return decodeRsaPubKeyPKCS1(els[1].valueBytes().sublist(1));
+}
+
+/// Encodes [key] as binary PKCS1
+Uint8List encodeRsaPubKeyPKCS1(pc.RSAPublicKey key) =>
+    _asn1Sequence([ASN1Integer(key.modulus!), ASN1Integer(key.publicExponent!)])
+        .encodedBytes;
+
+/// Encodes [key] as binary SPKI
+Uint8List encodeRsaPubKey(pc.RSAPublicKey key) => _asn1Sequence([
+      _asn1Sequence([_asnRsaOid, _asn1Null]),
+      ASN1BitString(encodeRsaPubKeyPKCS1(key))
+    ]).encodedBytes;
+
+/// Decodes binary PKCS1 to [RSAPrivateKey]
+pc.RSAPrivateKey decodeRsaPrivKeyPKCS1(Uint8List bytes) {
+  final els = ASN1Sequence.fromBytes(bytes).elements;
+  if (els.length != 9 || els.any((el) => el is! ASN1Integer)) {
+    throw Exception('Invalid PKCS1 encoding');
+  }
+  return pc.RSAPrivateKey(
+      (els[1] as ASN1Integer).valueAsBigInteger!,
+      (els[3] as ASN1Integer).valueAsBigInteger!,
+      (els[4] as ASN1Integer).valueAsBigInteger!,
+      (els[5] as ASN1Integer).valueAsBigInteger!);
+}
+
+/// Decodes binary PKCS8 to [RSAPrivateKey]
+pc.RSAPrivateKey decodeRsaPrivKey(Uint8List bytes) {
+  final els = ASN1Sequence.fromBytes(bytes).elements;
+  if (els.length != 3 ||
+      els[1] is! ASN1Sequence ||
+      els[2] is! ASN1OctetString) {
+    throw Exception('Invalid PKCS8 structure');
+  }
+  _assertRsaAlgorithm(els[1] as ASN1Sequence);
+  return decodeRsaPrivKeyPKCS1(els[2].valueBytes());
+}
+
+final _asnZero = ASN1Integer(BigInt.from(0));
+
+/// Encodes [key] as PKCS1 binary
+Uint8List encodeRsaPrivKeyPKCS1(pc.RSAPrivateKey key) {
+  final d = key.privateExponent!;
+  final p = key.p!;
+  final q = key.q!;
+  final dModP = d % (p - BigInt.from(1));
+  final dModQ = d % (q - BigInt.from(1));
+  final coefficient = q.modInverse(p);
+  return _asn1Sequence([
+    _asnZero,
+    ASN1Integer(key.modulus!),
+    ASN1Integer(key.publicExponent!),
+    ASN1Integer(d),
+    ASN1Integer(p),
+    ASN1Integer(q),
+    ASN1Integer(dModP),
+    ASN1Integer(dModQ),
+    ASN1Integer(coefficient),
+  ]).encodedBytes;
+}
+
+/// Encodes [key] as PKCS8 binary
+Uint8List encodeRsaPrivKey(pc.RSAPrivateKey key) => _asn1Sequence([
+      _asnZero,
+      _asn1Sequence([_asnRsaOid, _asn1Null]),
+      ASN1OctetString(encodeRsaPrivKeyPKCS1(key))
+    ]).encodedBytes;
--- packages/simplexmq/test/rsa_keys_test.dart
+++ packages/simplexmq/test/rsa_keys_test.dart
@@ -5,12 +5,21 @@ import 'package:test/test.dart';
 
 void main() {
   group('RSA keys encoding', () {
-    test('encode/decode RSA public key', () {
+    test('SPKI encode/decode RSA public key', () {
       final keyStr =
-          'MIIBoDANBgkqhkiG9w0BAQEFAAOCAY0AMIIBiAKCAQEAsigXn8PsL+tyXVOXNhcIFuHmH/aU42ZZoaIEyt8/4MZrype9HD90o9s0bTObdiW4AAKoDG0hvwpjwQeLSTyMdIKqfntzw+yI/qrq+nsV35m+Xcy+HD91Q5eWcf3O6886N9cBTH2ThQ2an3+7z6T4yr58ybrUMs0sM7QKOKj6EOdEkUyRFJhqdhhuCyZPh+HPMEBj2qSJqkkBWEWRoEQULfnyDaEdQVZPCngQc9ixbZlajWDPoKXlKlfAVEIXMIWHiNAqqxOOuHx0+sv9DuWHxy+3WFuGRIPhNEz1oIYs4lqpkZ8wb2S+Og+wwsJbHgLxTsQErrnLuTEtIAa06u1q0wKBgHgT22txXS4nHmTQUEsUEa6In1AzNAsyTJwETBl/jYXHOtDUR4gH5sDVnzSpnawVG7ZerW7tBCXjMRMCzmv539sLbYvEslRUNXOaSFCGnImC1ImNIn5bjssGcMFSNEe1gNPnTTwWCuKoCQ3MqFduxJd+CT2fKAMTSMv0MsGtEf0Z';
+          'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2fdE2lndTnPi7QHS2OqP1YE6ZH6sGdf7Boji6jPgQVJB289aQAPZRSJlg6s+xHC52sa2isFiuZN2uFENNWznuZsOWXkHMthbo9Qkp7ZjOhomZURtsIsaRny9GTcaFOrd19rqbsrCRLyb3xtwbQjv/2HEGNZyP9YsGsZijTJaV0yQNEp/5Gt3jHebJ8mqLdBr/aDQBf3oSsmUDDvocGU4kL14GOuVYCKNlEUrFe1X1poSXLH0uu485GVfHB72XjKP/flS2rL91fguqMil1nkelL1K4WOyx1Z87LyyXT2Vh4GRLVHG/a9LyPpw7ovQlO5RIr6suODkXwbAUHq/8j5IDwIDAQAB';
       final key = decodeRsaPubKey(decode64(encodeAscii(keyStr))!);
       expect(key is RSAPublicKey, true);
-      final encKey = decodeAscii(encode64(encodeRsaPubKey(key!)));
+      final encKey = decodeAscii(encode64(encodeRsaPubKey(key)));
+      expect(encKey, keyStr);
+    });
+
+    test('PKCS8 encode/decode RSA private key', () {
+      final keyStr =
+          'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDZ90TaWd1Oc+LtAdLY6o/VgTpkfqwZ1/sGiOLqM+BBUkHbz1pAA9lFImWDqz7EcLnaxraKwWK5k3a4UQ01bOe5mw5ZeQcy2Fuj1CSntmM6GiZlRG2wixpGfL0ZNxoU6t3X2upuysJEvJvfG3BtCO//YcQY1nI/1iwaxmKNMlpXTJA0Sn/ka3eMd5snyaot0Gv9oNAF/ehKyZQMO+hwZTiQvXgY65VgIo2URSsV7VfWmhJcsfS67jzkZV8cHvZeMo/9+VLasv3V+C6oyKXWeR6UvUrhY7LHVnzsvLJdPZWHgZEtUcb9r0vI+nDui9CU7lEivqy44ORfBsBQer/yPkgPAgMBAAECggEAezFPgB4EgB/tpUk/k4xXiTPF/iC+QskYvyPFJNv3JtRIFuWGO+Iw/esn9xhlnH+d+/IOIDSXCQ44ropY7dZEzlm97YIDOJCikuEHaqciRCedheT8Hikwy6Aa/NJw8lug0SyRDdeZn2H+s0X98BJ6Gxx1yhgCcOQq/2MbNnS8LNQ0yNNHu9Ds2K5Weiwhb9nrLLuMvrF/k1z0QNi5mCzDZK8iDMr87UZycmKKue13/xppI1pddJm4Ta13/OmZtYe2d5UgK9FrLStFkl7yqWnIcDueCOZvqo4nIfxPlPVolQ9B9RXL2tctkYRVy6FBkZIJkSk4O1Vz5BuPgBy9McoRMQKBgQD5xmVKAnvBQ08BUGXw5HVy4qR1Oj/EmxXKNypZsTdaKpiMfjNB8GbegO0na9Ry7sRo5g15vXpjCS6LDwGx4fZ/5u4N5CP3845DscrZjebs0tS7u5USPMoMzZ/KYfddRsGdm7y9HMp3Z6O3ZGGqZ15VFHGYjRjzK8BYE91rvv1WlwKBgQDfZe0c8/aa25KHW2zLRtSOR8ze+pz79hxNmpCeOoyzfaVRzwDh3KUl78PEWqMbJ7EdEdokCisFU0yEpuuc29JD6l9YuQmYH2VGdyg52iPPCXJOP1PBO0VQ/D/cAmZd/75cmoiyC7kELHfiAWBqO/7xpWkNiEpZZcI33DbzReYBSQKBgBKGuaqUppM+J9UEHpuQhnmf/+zGBkbR7frSvqxqbZ2dfTUmgyzH5Qlp7K043UgtF5pkPemiuToxSyd7VHfaN8ti2JNlMZnJkerJfC9IzDESrj7CehshMSdj9Q8w1wUvI1tKWuR4Bzh2Enme03OtORz8aDSVep1GyHx/9LNyNh4/AoGAcpdk/nIB8ENrMTVrZAYsJ+OaqlIhTnla4U/EmPVtkPCFaaZmTHUS3ZfUcpcPjXFZv5CVteDlWnD1EiJRP3/epmnFiMw5qKeKGpAquSo1LhEpagu/2aGel8EcvK0ad2Mk8XlvXuz2dbads/eCzluCFdAESCW+BYdWDbNPGJClP8kCgYBoT+0res0efi1cn6H0fPx/q33Wmgf47txVrzQN0ZEWDFOOhnErvGpRan9AG+LGvp7TvWWHnW13qjFCXGocWcbaoqsLabkov961R8ij2MTeToz6V+7YfK0KBt/h2HHJ5t/CybNxE5iYFyUMI7GTlC2GzFrnvxH/UYwUma1AplkpEw==';
+      final key = decodeRsaPrivKey(decode64(encodeAscii(keyStr))!);
+      expect(key is RSAPrivateKey, true);
+      final encKey = decodeAscii(encode64(encodeRsaPrivKey(key)));
       expect(encKey, keyStr);
     });
   });
