095478f16690a264c2ec210fe64b4d958128d25a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-27 15:16:35 -0500

    :recycle: Move export to .profile

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 VERSION="v0.4.2"
 APP_NAME="simplex-chat"
+TARGET_DIR="$HOME/.local/bin"
 
-######################
 PLATFORM="$(uname)"
 
 if [ $PLATFORM == "Darwin" ]; then
@@ -13,35 +13,10 @@ else
 	exit 1
 fi
 
-PLATFORM_BIN="$APP_NAME-$PLATFORM"
-######################
-
-######################
-# Detect the shell and the file where to export the path
-FILE_TO_EXPORT=""
-
-if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
-	FILE_TO_EXPORT=".zshrc"
-elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
-	FILE_TO_EXPORT=".bashrc"
-else
-	echo "Your shell is not suported, try with bash/zsh."
-	exit 1
-fi
-######################
-
-# Check if the directory exists
-TARGET_DIR="$HOME/.local/bin"
 [ ! -d $TARGET_DIR ] && mkdir -p $TARGET_DIR
 
-# Build the url
-URL="https://github.com/$APP_NAME/$APP_NAME/releases/download/$VERSION/$PLATFORM"
+wget -O $TARGET_DIR/simplex-chat "https://github.com/$APP_NAME/$APP_NAME/releases/download/$VERSION/$PLATFORM" && chmod +x $TARGET_DIR/simplex-chat
 
-# Download the binary and make it executable
-wget -O $TARGET_DIR/simplex-chat $URL && chmod +x $TARGET_DIR/simplex-chat
+echo "export PATH=\$PATH:$TARGET_DIR" >>$HOME/.profile
 
-# Export the path to the binary
-echo "export PATH=\$PATH:$TARGET_DIR" >>$HOME/$FILE_TO_EXPORT
-
-# Exit message with instructions
-echo "simplex-chat is installed, source your $FILE_TO_EXPORT or open a new shell"
+echo "simplex-chat is installed, source your .profile or open a new shell"
