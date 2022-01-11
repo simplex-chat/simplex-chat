985a4434966758c3ad064823d4291f2bb9d5b4b6
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-01-11 11:15:47 +0000

    tests for JSON message encoding/decoding

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,11 +1,11 @@
 import ChatClient
 import ChatTests
 import MarkdownTests
--- import ProtocolTests
+import ProtocolTests
 import Test.Hspec
 
 main :: IO ()
 main = withSmpServer . hspec $ do
   describe "SimpleX chat markdown" markdownTests
-  -- describe "SimpleX chat protocol" protocolTests
+  describe "SimpleX chat protocol" protocolTests
   describe "SimpleX chat client" chatTests
