a32b5c7ed2a7f682d6f2473fc6157bc54c794412
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-01-17 08:31:11 +0000

    xcode project

---
+++ apps/ios/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Evgeny Poberezkin on 17/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
