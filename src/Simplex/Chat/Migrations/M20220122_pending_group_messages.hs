4084be102dd0a3f6337d1170fb0ca8113392c6bb
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-01-22 17:02:45 +0400

    comment

--- src/Simplex/Chat/Migrations/M20220122_pending_group_messages.hs
+++ src/Simplex/Chat/Migrations/M20220122_pending_group_messages.hs
@@ -8,6 +8,7 @@ import Database.SQLite.Simple.QQ (sql)
 m20220122_pending_group_messages :: Query
 m20220122_pending_group_messages =
   [sql|
+-- pending messages for announced but not yet connected group members (memberCurrent)
 CREATE TABLE pending_group_messages (
   pending_group_message_id INTEGER PRIMARY KEY,
   message_id INTEGER NOT NULL REFERENCES messages ON DELETE CASCADE,
