3d137995d8b1f6a522ed9b8028b724846846895f
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-02-05 14:24:23 +0000

    multiline message entry field (#270)

--- apps/ios/Shared/Views/Chat/SendMessageView.swift
+++ apps/ios/Shared/Views/Chat/SendMessageView.swift
@@ -11,36 +11,78 @@ import SwiftUI
 struct SendMessageView: View {
     var sendMessage: (String) -> Void
     var inProgress: Bool = false
-    @State var command: String = ""
+    @State private var message: String = "" //Lorem ipsum dolor sit amet, consectetur" // adipiscing elit, sed do eiusmod tempor incididunt ut labor7 et dolore magna aliqua. Ut enim ad minim veniam, quis"// nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."// Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    @Namespace var namespace
+    @State private var teHeight: CGFloat = 42
+    @State private var teFont: Font = .body
+    var maxHeight: CGFloat = 360
+    var minHeight: CGFloat = 37
 
     var body: some View {
-        HStack {
-            TextField("Message...", text: $command)
-                .textFieldStyle(.roundedBorder)
-                .textInputAutocapitalization(.never)
-                .disableAutocorrection(true)
-                .onSubmit(submit)
+        ZStack {
+            HStack(alignment: .bottom) {
+                ZStack(alignment: .leading) {
+                    Text(message)
+                        .font(teFont)
+                        .foregroundColor(.clear)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 8)
+                        .matchedGeometryEffect(id: "te", in: namespace)
+                        .background(GeometryReader(content: updateHeight))
+                    TextEditor(text: $message)
+                        .onSubmit(submit)
+                        .font(teFont)
+                        .textInputAutocapitalization(.never)
+                        .padding(.horizontal, 5)
+                        .allowsTightening(false)
+                        .frame(height: teHeight)
+                }
 
-            if (inProgress) {
-                ProgressView()
-                    .frame(width: 40, height: 20, alignment: .center)
-            } else {
-                Button("Send", action :submit)
-                    .disabled(command.isEmpty)
+                if (inProgress) {
+                    ProgressView()
+                        .scaleEffect(1.4)
+                        .frame(width: 31, height: 31, alignment: .center)
+                        .padding([.bottom, .trailing], 3)
+                } else {
+                    Button(action: submit) {
+                        Image(systemName: "arrow.up.circle.fill")
+                            .resizable()
+                            .foregroundColor(.accentColor)
+                    }
+                    .disabled(message.isEmpty)
+                    .frame(width: 29, height: 29)
+                    .padding([.bottom, .trailing], 4)
+                }
             }
+
+            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                .strokeBorder(.secondary, lineWidth: 0.3, antialiased: true)
+                .frame(height: teHeight)
         }
-        .frame(minHeight: 30)
-        .padding(12)
+        .padding(.horizontal, 12)
+        .padding(.vertical, 8)
     }
 
     func submit() {
-        sendMessage(command)
-        command = ""
+        sendMessage(message)
+        message = ""
+    }
+
+    func updateHeight(_ g: GeometryProxy) -> Color {
+        DispatchQueue.main.async {
+            teHeight = min(max(g.frame(in: .local).size.height, minHeight), maxHeight)
+            teFont = isShortEmoji(message) ? emojiFont : .body
+        }
+        return Color.clear
     }
 }
 
 struct SendMessageView_Previews: PreviewProvider {
     static var previews: some View {
-        SendMessageView(sendMessage: { print ($0) })
+        VStack {
+            Text("")
+            Spacer(minLength: 0)
+            SendMessageView(sendMessage: { print ($0) })
+        }
     }
 }
--- apps/ios/Shared/Views/ChatList/ContactRequestView.swift
+++ apps/ios/Shared/Views/ChatList/ContactRequestView.swift
@@ -12,28 +12,35 @@ struct ContactRequestView: View {
     var contactRequest: UserContactRequest
 
     var body: some View {
-        return VStack(alignment: .leading, spacing: 4) {
-            HStack(alignment: .top) {
-                Text(ChatInfo.contactRequest(contactRequest: contactRequest).chatViewName)
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
-                    .padding(.leading, 8)
-                    .padding(.top, 4)
-                    .frame(maxHeight: .infinity, alignment: .topLeading)
-                Spacer()
-                Text(getDateFormatter().string(from: contactRequest.createdAt))
-                    .font(.subheadline)
-                    .padding(.trailing, 28)
-                    .padding(.top, 4)
-                    .frame(minWidth: 60, alignment: .trailing)
-                    .foregroundColor(.secondary)
+        return HStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill")
+                .resizable()
+                .foregroundColor(Color(uiColor: .secondarySystemBackground))
+                .frame(width: 63, height: 63)
+                .padding(.leading, 4)
+            VStack(alignment: .leading, spacing: 4) {
+                HStack(alignment: .top) {
+                    Text(ChatInfo.contactRequest(contactRequest: contactRequest).chatViewName)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                        .padding(.leading, 8)
+                        .padding(.top, 4)
+                        .frame(maxHeight: .infinity, alignment: .topLeading)
+                    Spacer()
+                    Text(getDateFormatter().string(from: contactRequest.createdAt))
+                        .font(.subheadline)
+                        .padding(.trailing, 28)
+                        .padding(.top, 4)
+                        .frame(minWidth: 60, alignment: .trailing)
+                        .foregroundColor(.secondary)
+                }
+                Text("wants to connect to you!")
+                    .frame(minHeight: 44, maxHeight: 44, alignment: .topLeading)
+                    .padding([.leading, .trailing], 8)
+                    .padding(.bottom, 4)
+                    .padding(.top, 1)
             }
-            Text("wants to connect to you!")
-                .frame(minHeight: 44, maxHeight: 44, alignment: .topLeading)
-                .padding([.leading, .trailing], 8)
-                .padding(.bottom, 4)
-                .padding(.top, 1)
         }
     }
 }
