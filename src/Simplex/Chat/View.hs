92409820fb67e7f3ba81f629e418393256adfa55
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-02-11 12:03:34 +0400

    enable async commands (#290)
    * enable async
    
    * fix async command error response
    
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- src/Simplex/Chat/View.hs
+++ src/Simplex/Chat/View.hs
@@ -122,8 +122,8 @@ responseToView cmd testView = \case
   where
     r = (plain cmd :)
     -- this function should be `r` for "synchronous", `id` for "asynchronous" command responses
-    -- r' = id
-    r' = r
+    -- r' = r
+    r' = id
     testViewChats :: [AChat] -> [StyledString]
     testViewChats chats = [sShow $ map toChatView chats]
       where
