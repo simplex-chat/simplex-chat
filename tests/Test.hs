975ba8fc2c1248c2bdff30b57c68c2210e756893
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-02-15 19:12:46 +0400

    disable chat tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -10,4 +10,4 @@ main = withSmpServer . hspec $ do
   describe "SimpleX chat markdown" markdownTests
   describe "SimpleX chat protocol" protocolTests
   describe "Mobile API Tests" mobileTests
-  describe "SimpleX chat client" chatTests
+  -- describe "SimpleX chat client" chatTests
