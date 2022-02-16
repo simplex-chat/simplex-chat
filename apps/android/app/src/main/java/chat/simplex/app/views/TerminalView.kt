54bbe565e14250065a6ff77850e089dc1e1e2663
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-02-16 20:30:20 +0000

    update android api to call haskell off main thread

--- apps/android/app/src/main/java/chat/simplex/app/views/TerminalView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/TerminalView.kt
@@ -8,15 +8,16 @@ import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import chat.simplex.app.model.*
-import chat.simplex.app.ui.theme.SimpleXTheme
 import chat.simplex.app.views.chat.SendMsgView
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun TerminalPage(chatModel: ChatModel) {
@@ -30,7 +31,7 @@ fun TerminalPage(chatModel: ChatModel) {
           type = NavType.StringType
         }
       )
-    ) { entry -> DetailView( entry.arguments?.getString("_details"), navController)}
+    ) { entry -> DetailView( entry.arguments?.getString("_details"), navController) }
   }
 }
 
@@ -39,7 +40,13 @@ fun TerminalView(chatModel: ChatModel, navController: NavController) {
   Column {
     TerminalLog(chatModel.terminalItems, navController)
     SendMsgView(sendMessage = { cmd ->
-      chatModel.controller.sendCmd(CC.Console(cmd))
+      GlobalScope.launch {
+        withContext(Dispatchers.Main) {
+          // show "in progress"
+          chatModel.controller.sendCmd(CC.Console(cmd))
+          // hide "in progress"
+        }
+      }
     })
   }
 }
@@ -50,7 +57,7 @@ fun TerminalLog(terminalItems: List<TerminalItem>, navController: NavController)
     items(terminalItems) { item ->
       ClickableText(
         AnnotatedString(item.label),
-        onClick={navController.navigate("details/${item.details}")}
+        onClick = { navController.navigate("details/${item.details}") }
       )
     }
   }
@@ -60,11 +67,8 @@ fun TerminalLog(terminalItems: List<TerminalItem>, navController: NavController)
 fun DetailView(details: String?, navController: NavController){
   Column {
     Text("$details")
-
-    Button(
-      onClick={navController.popBackStack()}
-    )
-    {Text("Back")}
+    Button(onClick = { navController.popBackStack() }) {
+      Text("Back")
+    }
   }
 }
-
