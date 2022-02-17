069cd374905baa82db10aadef937b010f134c523
IanRDavies <ian_davies_@hotmail.co.uk>
2022-02-17 12:10:05 +0000

    add initial conditional routing -- create user not working

--- apps/android/app/src/main/java/chat/simplex/app/MainActivity.kt
+++ apps/android/app/src/main/java/chat/simplex/app/MainActivity.kt
@@ -11,15 +11,18 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import chat.simplex.app.views.TerminalView
+import chat.simplex.app.views.WelcomeView
 
 
 class MainActivity: ComponentActivity() {
   private val viewModel by viewModels<SimplexViewModel>()
   override fun onCreate(savedInstanceState: Bundle?) {
+    val route: String = if(viewModel.chatModel.currentUser == null) Pages.Welcome.route else Pages.Home.route
     super.onCreate(savedInstanceState)
     setContent {
       SimpleXTheme {
-        Navigation(viewModel = viewModel)
+        Navigation(viewModel=viewModel, initialRoute=route)
       }
     }
   }
@@ -30,16 +33,19 @@ class SimplexViewModel(application: Application) : AndroidViewModel(application)
 }
 
 @Composable
-fun Navigation(viewModel: SimplexViewModel) {
+fun Navigation(viewModel: SimplexViewModel, initialRoute: String) {
   val navController = rememberNavController()
 
-  NavHost(navController=navController, startDestination=Pages.Home.route){
+  NavHost(navController=navController, startDestination=initialRoute){
     composable(route=Pages.Home.route){
       MainPage(vm = viewModel)
     }
-//    composable(route=Pages.Welcome.route){
-//      WelcomeView(vm.)
-//    }
+    composable(route=Pages.Welcome.route){
+      WelcomeView(vm = viewModel) {navController.navigate(Pages.Home.route) { popUpTo(Pages.Home.route) { inclusive = true }}}
+    }
+    composable(route=Pages.Terminal.route){
+      TerminalView(chatModel = viewModel.chatModel, navController = navController)
+    }
   }
 }
 
