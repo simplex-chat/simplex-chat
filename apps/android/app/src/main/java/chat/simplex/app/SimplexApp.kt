f3fcc948e372dd98e5d64846b3a2a4e7a5d29c31
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-02-17 08:56:37 +0000

    2 spaces holy war

--- apps/android/app/src/main/java/chat/simplex/app/SimplexApp.kt
+++ apps/android/app/src/main/java/chat/simplex/app/SimplexApp.kt
@@ -27,76 +27,76 @@ external fun chatSendCmd(ctrl: ChatCtrl, msg: String) : String
 external fun chatRecvMsg(ctrl: ChatCtrl) : String
 
 class SimplexApp: Application() {
-    private lateinit var controller: ChatController
+  private lateinit var controller: ChatController
 
-    override fun onCreate() {
-        super.onCreate()
-        controller = ChatController(chatInit(applicationContext.filesDir.toString()))
-        GlobalScope.launch {
-            withContext(Dispatchers.Main) {
-                var user = controller.apiGetActiveUser()
-                if (user == null) {
-//            user =  controller.apiCreateActiveUser(Profile("android", "Android test"))
-                }
-                Log.d("SIMPLEX (user)", user.toString())
-                try {
-                    controller.apiStartChat()
-                    Log.d("SIMPLEX", "started chat")
-                } catch(e: Error) {
-                    Log.d("SIMPLEX", "failed starting chat $e")
-//            throw e
-                }
-            }
+  override fun onCreate() {
+    super.onCreate()
+    controller = ChatController(chatInit(applicationContext.filesDir.toString()))
+    GlobalScope.launch {
+      withContext(Dispatchers.Main) {
+        var user = controller.apiGetActiveUser()
+        if (user == null) {
+//      user =  controller.apiCreateActiveUser(Profile("android", "Android test"))
         }
-    }
-
-    val chatModel by lazy {
-        val m = ChatModel(controller)
-        controller.setModel(m)
-        controller.startReceiver()
-        m
-    }
-
-    companion object {
-        lateinit var weakActivity: WeakReference<MainActivity>
-        init {
-            val socketName = "local.socket.address.listen.native.cmd2"
-
-            val s = Semaphore(0)
-            thread(name="stdout/stderr pipe") {
-                Log.d("SIMPLEX", "starting server")
-                val server = LocalServerSocket(socketName)
-                Log.d("SIMPLEX", "started server")
-                s.release()
-                val receiver = server.accept()
-                Log.d("SIMPLEX", "started receiver")
-                val logbuffer = FifoQueue<String>(500)
-                if (receiver != null) {
-                    val inStream = receiver.inputStream
-                    val inStreamReader = InputStreamReader(inStream)
-                    val input = BufferedReader(inStreamReader)
-
-                    while(true) {
-                        val line = input.readLine() ?: break
-                        Log.d("SIMPLEX (stdout/stderr)", line)
-                        logbuffer.add(line)
-                    }
-                }
-            }
-
-            System.loadLibrary("app-lib")
-
-            s.acquire()
-            pipeStdOutToSocket(socketName)
-
-            initHS()
+        Log.d("SIMPLEX (user)", user.toString())
+        try {
+          controller.apiStartChat()
+          Log.d("SIMPLEX", "started chat")
+        } catch(e: Error) {
+          Log.d("SIMPLEX", "failed starting chat $e")
+//      throw e
         }
+      }
     }
+  }
+
+  val chatModel by lazy {
+    val m = ChatModel(controller)
+    controller.setModel(m)
+    controller.startReceiver()
+    m
+  }
+
+  companion object {
+    lateinit var weakActivity: WeakReference<MainActivity>
+    init {
+      val socketName = "local.socket.address.listen.native.cmd2"
+
+      val s = Semaphore(0)
+      thread(name="stdout/stderr pipe") {
+        Log.d("SIMPLEX", "starting server")
+        val server = LocalServerSocket(socketName)
+        Log.d("SIMPLEX", "started server")
+        s.release()
+        val receiver = server.accept()
+        Log.d("SIMPLEX", "started receiver")
+        val logbuffer = FifoQueue<String>(500)
+        if (receiver != null) {
+          val inStream = receiver.inputStream
+          val inStreamReader = InputStreamReader(inStream)
+          val input = BufferedReader(inStreamReader)
+
+          while(true) {
+            val line = input.readLine() ?: break
+            Log.d("SIMPLEX (stdout/stderr)", line)
+            logbuffer.add(line)
+          }
+        }
+      }
+
+      System.loadLibrary("app-lib")
+
+      s.acquire()
+      pipeStdOutToSocket(socketName)
+
+      initHS()
+    }
+  }
 }
 
 class FifoQueue<E>(private var capacity: Int) : LinkedList<E>() {
-    override fun add(element: E): Boolean {
-        if(size > capacity) removeFirst()
-        return super.add(element)
-    }
+  override fun add(element: E): Boolean {
+    if(size > capacity) removeFirst()
+    return super.add(element)
+  }
 }
