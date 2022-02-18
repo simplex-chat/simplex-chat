6daad1021054f9231596cb77e240c033411a27c8
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-02-18 09:00:21 +0000

    make condition depend on host os (#329)

--- sha256map.nix
+++ sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "git://github.com/simplex-chat/simplexmq.git"."c1f5f9d8461e254c0c67c65de36f0860306b3d72" = "1qbj4xgg27qwm67187vkn87jggd3q47v4vylh915kbxw7npilalj";
+  "git://github.com/simplex-chat/simplexmq.git"."dff5cad1bef67376e82c3dc15cccdb5ba9e675ab" = "06291v6vw7i00r0j13qx5apkz794jak68n1yr875gi32dxx5lhnp";
   "git://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "git://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "git://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
