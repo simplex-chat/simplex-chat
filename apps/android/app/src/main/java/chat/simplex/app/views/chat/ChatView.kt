f57a7009a3bb43a19f1d188125352cad434b5e09
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-02-19 17:22:52 +0400

    chat view layout (#335)
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/ChatView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/ChatView.kt
@@ -1,55 +1,87 @@
 package chat.simplex.app.views.chat
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material.Icon
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import chat.simplex.app.model.*
+import chat.simplex.app.ui.theme.SimpleXTheme
 import chat.simplex.app.views.chat.item.ChatItemView
-import chat.simplex.app.views.chatlist.ChatPreviewView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import chat.simplex.app.views.helpers.withApi
+import kotlinx.datetime.Clock
 
 @Composable
 fun ChatView(chatModel: ChatModel, nav: NavController) {
   if (chatModel.chatId.value != null && chatModel.chats.count() > 0) {
     val chat: Chat = chatModel.chats.first { chat -> chat.chatInfo.id == chatModel.chatId.value }
-    Column {
-      ChatInfoToolbar(chat, nav)
-      ChatItemsList(chatModel.chatItems)
-      SendMsgView(sendMessage = { msg ->
-        GlobalScope.launch {
-          withContext(Dispatchers.Main) {
-            // show "in progress"
-            val cInfo = chat.chatInfo
-            val newItem = chatModel.controller.apiSendMessage(
-              type = cInfo.chatType,
-              id = cInfo.apiId,
-              mc = MsgContent.MCText(msg)
-            )
-            // hide "in progress"
-            if (newItem != null) chatModel.addChatItem(cInfo, newItem.chatItem)
-          }
-        }
-      })
+    ChatLayout(chat, chatModel.chatItems, back = { nav.popBackStack() }, sendMessage = { msg ->
+      withApi {
+        // show "in progress"
+        val cInfo = chat.chatInfo
+        val newItem = chatModel.controller.apiSendMessage(
+          type = cInfo.chatType,
+          id = cInfo.apiId,
+          mc = MsgContent.MCText(msg)
+        )
+        // hide "in progress"
+        if (newItem != null) chatModel.addChatItem(cInfo, newItem.chatItem)
+      }
+    })
+  }
+}
+
+@Composable
+fun ChatLayout(
+  chat: Chat, chatItems: List<ChatItem>,
+  back: () -> Unit,
+  sendMessage: (String) -> Unit
+) {
+  Scaffold(
+    topBar = { ChatInfoToolbar(chat, back) },
+    bottomBar = { SendMsgView(sendMessage) }
+  ) { contentPadding ->
+    Box(
+      modifier = Modifier
+        .padding(contentPadding)
+        .fillMaxWidth()
+        .background(MaterialTheme.colors.background)
+    ) {
+      ChatItemsList(chatItems)
     }
   }
 }
 
 @Composable
-fun ChatInfoToolbar(chat: Chat, nav: NavController) {
-  Row {
-    Button(onClick = { nav.popBackStack() }) {
-      Text("Back")
-    }
-    Column {
+fun ChatInfoToolbar(chat: Chat, back: () -> Unit) {
+  Box(
+    modifier = Modifier.fillMaxWidth(),
+    contentAlignment = Alignment.CenterStart
+  ) {
+    Icon(
+      Icons.Outlined.ArrowBack,
+      "Back",
+      tint = MaterialTheme.colors.primary,
+      modifier = Modifier
+        .clickable(onClick = back)
+        .padding(start = 16.dp)
+    )
+    Column(
+      Modifier
+        .padding(horizontal = 40.dp)
+        .fillMaxWidth(),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
       Text(chat.chatInfo.displayName)
       Text(chat.chatInfo.fullName)
     }
@@ -62,5 +94,39 @@ fun ChatItemsList(chatItems: List<ChatItem>) {
     items(chatItems) { cItem ->
       ChatItemView(cItem)
     }
+  }
+}
+
+@Preview
+@Composable
+fun PreviewChatViewLayout() {
+  SimpleXTheme {
+    val chatItems = listOf(
+      ChatItem.getSampleData(
+        1, CIDirection.DirectSnd(), Clock.System.now(), "hello"
+      ),
+      ChatItem.getSampleData(
+        2, CIDirection.DirectRcv(), Clock.System.now(), "hello"
+      ),
+      ChatItem.getSampleData(
+        3, CIDirection.DirectSnd(), Clock.System.now(), "hello"
+      ),
+      ChatItem.getSampleData(
+        4, CIDirection.DirectSnd(), Clock.System.now(), "hello"
+      ),
+      ChatItem.getSampleData(
+        5, CIDirection.DirectRcv(), Clock.System.now(), "hello"
+      )
+    )
+    ChatLayout(
+      chat = Chat(
+        chatInfo = ChatInfo.Direct.sampleData,
+        chatItems = chatItems,
+        chatStats = Chat.ChatStats()
+      ),
+      chatItems = chatItems,
+      back = {},
+      sendMessage = {}
+    )
   }
 }
--- apps/android/app/src/main/java/chat/simplex/app/views/chat/SendMsgView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/SendMsgView.kt
@@ -1,31 +1,47 @@
 package chat.simplex.app.views.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowUpward
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import chat.simplex.app.ui.theme.SimpleXTheme
 
 @Composable
-fun SendMsgView (sendMessage: (String) -> Unit) {
+fun SendMsgView(sendMessage: (String) -> Unit) {
   var cmd by remember { mutableStateOf("") }
-  Row {
-    TextField(value = cmd, onValueChange = { cmd = it }, modifier = Modifier.height(60.dp))
-    Spacer(Modifier.height(10.dp))
-    Button(
-      onClick = {
-        sendMessage(cmd)
-        cmd = ""
-      },
-      modifier = Modifier.width(40.dp).height(60.dp),
-      enabled = cmd.isNotEmpty()
-    ) {
-      Text("Go")
-    }
+  Box(
+    modifier = Modifier.fillMaxWidth(),
+    contentAlignment = Alignment.CenterEnd
+  ) {
+    TextField(
+      value = cmd,
+      onValueChange = { cmd = it },
+      modifier = Modifier
+        .height(60.dp)
+        .fillMaxWidth()
+        .padding(end = 40.dp)
+        .background(MaterialTheme.colors.background)
+    )
+    val tint = if (cmd.isNotEmpty()) MaterialTheme.colors.primary else Color.Gray
+    Icon(
+      Icons.Outlined.ArrowUpward,
+      "Send Message",
+      tint = tint,
+      modifier = Modifier
+        .width(40.dp)
+        .clickable {
+          sendMessage(cmd)
+          cmd = ""
+        }
+    )
   }
 }
 
