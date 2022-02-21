2ef458fd7a6e346c69133c38eee735b3d4db55e8
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-02-21 18:02:07 +0400

    UserProfileView

--- apps/android/app/src/main/java/chat/simplex/app/views/usersettings/SettingsView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/usersettings/SettingsView.kt
@@ -13,55 +13,56 @@ import chat.simplex.app.model.ChatModel
 @Composable
 fun SettingsView(chatModel: ChatModel, nav: NavController) {
   val user = chatModel.currentUser.value
-  val profile = user!!.profile
-  Column() {
-    Button(
-      onClick = { nav.popBackStack() }
-    ) {
-      Text("Back")
-    }
-    Text("Your Settings")
-    Spacer(Modifier.height(4.dp))
-    Text("YOU", style = MaterialTheme.typography.h4)
-    Button(
-      onClick = { nav.navigate(Pages.UserProfile.route) }
-    ) {
-      Text(profile.displayName)
-    }
-    Button(
-      onClick = { println(profile.hashCode()) }
-    ) {
-      Text("Your SimpleX contact address", style = MaterialTheme.typography.body1)
-    }
-    Spacer(Modifier.height(10.dp))
-    Text("HELP", style = MaterialTheme.typography.h4)
-    Button(
-      onClick = { println("navigate to help") }
-    ) {
-      Text("How to use SimpleX Chat")
-    }
-    Button(
-      onClick = { println("start help chat") }
-    ) {
-      Text("Get help & advice via chat")
-    }
-    Button(
-      onClick = { println("navigate to email") }
-    ) {
-      Text("Ask questions via email")
-    }
-    Spacer(Modifier.height(10.dp))
-    Text("DEVELOP", style = MaterialTheme.typography.h4)
-    Button(
-      onClick = { println("navigate to console") }
-    ) {
-      Text("Chat console")
-    }
-    Button(
-      onClick = { println("navigate to github") }
-    ) {
-      Text("Install SimpleX for terminal")
+  if (user != null) {
+    val profile = user.profile
+    Column() {
+      Button(
+        onClick = { nav.popBackStack() }
+      ) {
+        Text("Back")
+      }
+      Text("Your Settings")
+      Spacer(Modifier.height(4.dp))
+      Text("YOU", style = MaterialTheme.typography.h4)
+      Button(
+        onClick = { nav.navigate(Pages.UserProfile.route) }
+      ) {
+        Text(profile.displayName)
+      }
+      Button(
+        onClick = { println(profile.hashCode()) }
+      ) {
+        Text("Your SimpleX contact address", style = MaterialTheme.typography.body1)
+      }
+      Spacer(Modifier.height(10.dp))
+      Text("HELP", style = MaterialTheme.typography.h4)
+      Button(
+        onClick = { println("navigate to help") }
+      ) {
+        Text("How to use SimpleX Chat")
+      }
+      Button(
+        onClick = { println("start help chat") }
+      ) {
+        Text("Get help & advice via chat")
+      }
+      Button(
+        onClick = { println("navigate to email") }
+      ) {
+        Text("Ask questions via email")
+      }
+      Spacer(Modifier.height(10.dp))
+      Text("DEVELOP", style = MaterialTheme.typography.h4)
+      Button(
+        onClick = { println("navigate to console") }
+      ) {
+        Text("Chat console")
+      }
+      Button(
+        onClick = { println("navigate to github") }
+      ) {
+        Text("Install SimpleX for terminal")
+      }
     }
   }
 }
-
--- apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserProfileView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserProfileView.kt
@@ -1,21 +1,60 @@
 package chat.simplex.app.views.usersettings
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.navigation.NavController
 import chat.simplex.app.model.ChatModel
+import chat.simplex.app.model.Profile
+import chat.simplex.app.views.helpers.withApi
 
 @Composable
 fun UserProfileView(chatModel: ChatModel, nav: NavController) {
   val user = chatModel.currentUser.value
-  val profile = user!!.profile
-  Column() {
-    Button(
-      onClick = { nav.popBackStack() }
-    ) {
-      Text("Back")
+  if (user != null) {
+    var editProfile by remember { mutableStateOf(false) }
+    var profile by remember { mutableStateOf(user.profile) }
+    Column {
+      Button(onClick = { nav.popBackStack() }) { Text("Back") }
+      Text(
+        "Your profile is stored on your device and shared only with your contacts.\n" +
+            "SimpleX servers cannot see your profile."
+      )
+      if (editProfile) {
+        var displayName by remember { mutableStateOf(profile.displayName) }
+        var fullName by remember { mutableStateOf(profile.fullName) }
+        Column {
+          TextField(value = displayName, onValueChange = { displayName = it })
+          TextField(value = fullName, onValueChange = { fullName = it })
+          Row {
+            Button(onClick = { editProfile = false }) { Text("Cancel") }
+            Button(
+              onClick = {
+                withApi {
+                  val newProfile = chatModel.controller.apiUpdateProfile(
+                    profile = Profile(displayName, fullName)
+                  )
+                  print("if (newProfile != null) {")
+                  if (newProfile != null) {
+                    chatModel.updateUserProfile(newProfile)
+                    print("profile = newProfile")
+                    profile = newProfile
+                  }
+                  editProfile = false
+                }
+              },
+              enabled = displayName.isNotEmpty()
+            ) { Text("Save (and notify contacts)") }
+          }
+        }
+      } else {
+        Column {
+          Text("Display name: ${profile.displayName}")
+          Text("Full name: ${profile.fullName}")
+          Button(onClick = { editProfile = true }) { Text("Edit") }
+        }
+      }
     }
   }
 }
