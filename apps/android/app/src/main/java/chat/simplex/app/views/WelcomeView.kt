582e938f4e4df85e1f0b03902ccb369301d0a570
IanRDavies <ian_davies_@hotmail.co.uk>
2022-02-22 18:56:59 +0000

    styling work round 1

--- apps/android/app/src/main/java/chat/simplex/app/views/WelcomeView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/WelcomeView.kt
@@ -2,10 +2,15 @@ package chat.simplex.app.views
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import chat.simplex.app.R
 import chat.simplex.app.model.ChatModel
@@ -16,27 +21,41 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 @DelicateCoroutinesApi
 @Composable
 fun WelcomeView(chatModel: ChatModel, routeHome: () -> Unit) {
-  Column(
+  Box(
     modifier = Modifier
-      .verticalScroll(rememberScrollState())
       .fillMaxSize()
       .background(color = MaterialTheme.colors.background)
-  ) {
-    Image(
-      painter = painterResource(R.drawable.logo), contentDescription = "Simplex Logo",
-    )
-    Text("You control your chat!", color = MaterialTheme.colors.onBackground)
-    Text(
-      "The messaging and application platform protecting your privacy and security.",
-      color = MaterialTheme.colors.onBackground
-    )
-    Spacer(Modifier.height(8.dp))
-    Text(
-      "We don't store any of your contacts or messages (once delivered) on the servers.",
-      color = MaterialTheme.colors.onBackground
-    )
-    Spacer(Modifier.height(24.dp))
-    CreateProfilePanel(chatModel, routeHome)
+  ){
+    Column(
+      verticalArrangement = Arrangement.SpaceBetween,
+      modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .fillMaxSize()
+        .background(color = MaterialTheme.colors.background)
+        .padding(12.dp)
+    ) {
+      Image(
+        painter = painterResource(R.drawable.logo), contentDescription = "Simplex Logo",
+      )
+      Text(
+        "You control your chat!",
+        style = MaterialTheme.typography.h4,
+        color = MaterialTheme.colors.onBackground
+      )
+      Text(
+        "The messaging and application platform protecting your privacy and security.",
+        style = MaterialTheme.typography.body1,
+        color = MaterialTheme.colors.onBackground
+      )
+      Spacer(Modifier.height(8.dp))
+      Text(
+        "We don't store any of your contacts or messages (once delivered) on the servers.",
+        style = MaterialTheme.typography.body1,
+        color = MaterialTheme.colors.onBackground
+      )
+      Spacer(Modifier.height(24.dp))
+      CreateProfilePanel(chatModel, routeHome)
+    }
   }
 }
 
@@ -49,16 +68,60 @@ fun CreateProfilePanel(chatModel: ChatModel, routeHome: () -> Unit) {
   Column(
     modifier=Modifier.fillMaxSize()
   ) {
-    Text("Create profile", color = MaterialTheme.colors.onBackground)
+    Text(
+      "Create profile",
+      style = MaterialTheme.typography.h4,
+      color = MaterialTheme.colors.onBackground,
+      modifier = Modifier.padding(vertical = 5.dp)
+    )
     Text(
       "Your profile is stored on your device and shared only with your contacts.",
+      style = MaterialTheme.typography.body1,
       color = MaterialTheme.colors.onBackground
     )
-    Text("Display Name", color = MaterialTheme.colors.onBackground)
-    TextField(value = displayName, onValueChange = { value -> displayName = value })
-    Text("Full Name (Optional)", color = MaterialTheme.colors.onBackground)
-    TextField(value = fullName, onValueChange = { fullName = it })
-    Button(onClick={
+    Spacer(Modifier.height(10.dp))
+    Text(
+      "Display Name",
+      style = MaterialTheme.typography.h6,
+      color = MaterialTheme.colors.onBackground
+    )
+    BasicTextField(
+      value = displayName,
+      onValueChange = { displayName = it },
+      modifier = Modifier
+        .fillMaxWidth()
+        .background(MaterialTheme.colors.secondary)
+        .height(40.dp)
+        .clip(RoundedCornerShape(3.dp)),
+      textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onBackground),
+      keyboardOptions = KeyboardOptions(
+        capitalization = KeyboardCapitalization.None,
+        autoCorrect = false
+      ),
+      singleLine = true
+    )
+    Spacer(Modifier.height(10.dp))
+    Text(
+      "Full Name (Optional)",
+      style = MaterialTheme.typography.h6,
+      color = MaterialTheme.colors.onBackground
+    )
+    BasicTextField(
+      value = fullName,
+      onValueChange = { fullName = it },
+      modifier = Modifier
+        .fillMaxWidth()
+        .background(MaterialTheme.colors.secondary)
+        .height(40.dp)
+        .clip(RoundedCornerShape(3.dp)),
+      textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onBackground),
+      keyboardOptions = KeyboardOptions(
+        capitalization = KeyboardCapitalization.None,
+        autoCorrect = false
+      ),
+      singleLine = true
+    )
+    Button(onClick = {
       withApi {
         val user = chatModel.controller.apiCreateActiveUser(
           Profile(displayName, fullName)
