4e375b664af3ba2bb646145aa57dd89b4f553fbf
IanRDavies <ian_davies_@hotmail.co.uk>
2022-02-25 16:38:30 +0000

    group notifications from the same chat

--- apps/android/app/src/main/java/chat/simplex/app/model/NtfManager.kt
+++ apps/android/app/src/main/java/chat/simplex/app/model/NtfManager.kt
@@ -9,8 +9,10 @@ import androidx.core.app.NotificationManagerCompat
 import chat.simplex.app.MainActivity
 import chat.simplex.app.R
 
-
 class NtfManager(val context: Context) {
+  private val manager: NotificationManager = (
+      context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    )
 
   fun createNotificationChannel(channelId: String) {
     val name = "SimpleX Chat"
@@ -19,7 +21,6 @@ class NtfManager(val context: Context) {
     val channel = NotificationChannel(channelId, name, importance).apply {
       description = desc
     }
-    val manager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     manager.createNotificationChannel(channel)
   }
 
@@ -44,8 +45,38 @@ class NtfManager(val context: Context) {
       title = cInfo.displayName,
       content = cItem.content.text,
       notificationId = cItem.hashCode(),
-      intent = intent
+      intent = intent,
+      group = cInfo.id
     )
+  }
+
+  private fun getMsgNotificationBuilder(
+    channelId: String,
+    title: String,
+    content: String,
+    group: String?,
+    priority: Int,
+    pendingIntent: PendingIntent,
+    groupNotificationText: String? = null,
+  ): NotificationCompat.Builder {
+    val builder = NotificationCompat.Builder(context, channelId)
+      .setSmallIcon(R.mipmap.icon)
+      .setContentTitle(title)
+      .setContentText(content)
+      .setPriority(priority)
+      .setGroup(group)
+      .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY)
+      .setAutoCancel(true)
+      .setContentIntent(pendingIntent)
+    if (groupNotificationText != null) {
+      builder.setStyle(NotificationCompat.InboxStyle().addLine(content))
+    }
+    else {
+      builder.setStyle(NotificationCompat.InboxStyle()
+        .setSummaryText(groupNotificationText)
+      )
+    }
+    return builder
   }
 
   private fun notify(
@@ -54,20 +85,34 @@ class NtfManager(val context: Context) {
     content: String,
     notificationId: Int,
     intent: Intent,
+    group: String? = null,
     priority: Int = NotificationCompat.PRIORITY_DEFAULT
   ) {
     val pendingIntent = TaskStackBuilder.create(context).run {
       addNextIntentWithParentStack(intent)
       getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE)
     }
-    val builder = NotificationCompat.Builder(context, channelId)
-      .setSmallIcon(R.mipmap.icon)
-      .setContentTitle(title)
-      .setContentText(content)
-      .setPriority(priority)
-      .setContentIntent(pendingIntent)
-      .setAutoCancel(true)
+    val builder = getMsgNotificationBuilder(channelId, title, content, group, priority, pendingIntent)
+
+    // Deal with notifications for the same chat
+    val notifications = manager.activeNotifications
+    val jointNotifications = notifications.filter { n -> (n.notification.group != null && n.notification.group == group) }
+    val rawCount = jointNotifications.count()
+    val msgCount = if (rawCount <= 1) rawCount + 1 else rawCount  // Avoid counting existing collective notification
+    val groupNotificationText = if (msgCount > 1) "$msgCount new messages" else "$msgCount new message"
+    val groupNotificationBuilder = getMsgNotificationBuilder(channelId, title, content, group, priority, pendingIntent, groupNotificationText)
+
+    // Send notifications
     with(NotificationManagerCompat.from(context)) {
+      // Cancel solo notification as it doesn't join group. Resend it where needed.
+      if (rawCount != 0) {
+        notify(group.hashCode(), groupNotificationBuilder.build())
+      }
+      if (rawCount == 1) {
+        val originalId = jointNotifications[0].id
+        manager.cancel(originalId)
+        notify(originalId, jointNotifications[0].notification.clone())
+      }
       notify(notificationId, builder.build())
     }
   }
