67d1fcf17916ee6d6ed09349024e88b57d828a55
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-02-25 10:45:57 +0000

    add help messages in summaries

--- src/Simplex/Chat/View.hs
+++ src/Simplex/Chat/View.hs
@@ -103,7 +103,7 @@ responseToView testView = \case
   CRContactSubscribed c -> [ttyContact' c <> ": connected to server"]
   CRContactSubError c e -> [ttyContact' c <> ": contact error " <> sShow e]
   CRContactSubSummary summary ->
-    (if null connected then [] else [sShow (length connected) <> " contacts connected"]) <> viewSummary errors " contact errors"
+    (if null connected then [] else [sShow (length connected) <> " contacts connected (use " <> highlight' "/cs" <> " for the list)"]) <> viewErrorsSummary errors " contact errors"
     where
       (errors, connected) = partition (isJust . contactError) summary
   CRGroupInvitation GroupInfo {localDisplayName = ldn, groupProfile = GroupProfile {fullName}} ->
@@ -120,7 +120,7 @@ responseToView testView = \case
   CRGroupRemoved g -> [ttyFullGroup g <> ": you are no longer a member or group deleted"]
   CRGroupDeleted g m -> [ttyGroup' g <> ": " <> ttyMember m <> " deleted the group", "use " <> highlight ("/d #" <> groupName' g) <> " to delete the local copy of the group"]
   CRMemberSubError g c e -> [ttyGroup' g <> " member " <> ttyContact c <> " error: " <> sShow e]
-  CRMemberSubErrors summary -> viewSummary summary " group member errors"
+  CRMemberSubErrors summary -> viewErrorsSummary summary " group member errors"
   CRGroupSubscribed g -> [ttyFullGroup g <> ": connected to server(s)"]
   CRSndFileSubError SndFileTransfer {fileId, fileName} e ->
     ["sent file " <> sShow fileId <> " (" <> plain fileName <> ") error: " <> sShow e]
@@ -146,8 +146,8 @@ responseToView testView = \case
       where
         toChatView :: CChatItem c -> (Int, Text)
         toChatView (CChatItem dir ChatItem {meta}) = (msgDirectionInt $ toMsgDirection dir, itemText meta)
-    viewSummary :: [a] -> StyledString -> [StyledString]
-    viewSummary summary s = if null summary then [] else [styled (colored Red) (T.pack . show $ length summary) <> s]
+    viewErrorsSummary :: [a] -> StyledString -> [StyledString]
+    viewErrorsSummary summary s = if null summary then [] else [styled (colored Red) (T.pack . show $ length summary) <> s <> " (run with -c option to show each error)"]
 
 viewChatItem :: ChatInfo c -> ChatItem c d -> [StyledString]
 viewChatItem chat (ChatItem cd meta content _) = case (chat, cd) of
