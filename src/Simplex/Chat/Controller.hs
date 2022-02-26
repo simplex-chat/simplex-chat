476bddabc461c8ee2ff9e918f04e60e9ce66d3a3
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-02-26 16:19:58 +0400

    prepare v1.2.2

--- src/Simplex/Chat/Controller.hs
+++ src/Simplex/Chat/Controller.hs
@@ -36,7 +36,7 @@ import System.IO (Handle)
 import UnliftIO.STM
 
 versionNumber :: String
-versionNumber = "1.2.1"
+versionNumber = "1.2.2"
 
 versionStr :: String
 versionStr = "SimpleX Chat v" <> versionNumber
