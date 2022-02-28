e9adcdbf5adcdfc915772bdd7acd2e19a7c8faa2
IanRDavies <ian_davies_@hotmail.co.uk>
2022-02-28 12:28:54 +0000

    Apply suggestions from code review
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- src/Simplex/Chat/Migrations/M20220228_avatars.hs
+++ src/Simplex/Chat/Migrations/M20220228_avatars.hs
@@ -5,9 +5,9 @@ module Simplex.Chat.Migrations.M20220228_avatars where
 import Database.SQLite.Simple (Query)
 import Database.SQLite.Simple.QQ (sql)
 
-m20220224_avatars :: Query
-m20220224_avatars =
+m20220228_avatars :: Query
+m20220228_avatars =
     [sql|
-    ALTER TABLE contact_profiles ADD COLUMN display_picture BLOB;
-    ALTER TABLE group_profiles ADD COLUMN display_picture BLOB;
+    ALTER TABLE contact_profiles ADD COLUMN image BLOB;
+    ALTER TABLE group_profiles ADD COLUMN image BLOB;
 |]
