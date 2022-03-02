5b4fd8b12b5202b25b893865e0246a9e9d04de21
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-03-02 18:20:14 +0400

    core: update simplexmq hash (remove SMPServer FromJSON)

--- sha256map.nix
+++ sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "git://github.com/simplex-chat/simplexmq.git"."f6198ebab00ce541b58e64c0ee77bba1e003a084" = "0dshf36r7l8pcvw10aqhd0mg3d1jdsh1pk3lx131fs3cqy4d8wr6";
+  "git://github.com/simplex-chat/simplexmq.git"."44e946ef747ffc8dd64f4827fc9ca2ec4f6d0134" = "0yznbjgyaad0s72sli1ibj1w88gkvjpcik6m3yyhs8v1f5awjp4w";
   "git://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "git://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "git://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
