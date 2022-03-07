3058b358926034ad3fc6b72001305a3c808c816c
IanRDavies <ian_davies_@hotmail.co.uk>
2022-03-07 17:38:55 +0000

    refactor edit profile screen

--- apps/android/app/src/main/java/chat/simplex/app/views/helpers/ImageGetter.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/helpers/ImageGetter.kt
@@ -11,17 +11,15 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.Collections
 import androidx.compose.material.icons.outlined.PhotoCamera
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import chat.simplex.app.views.newchat.ActionButton
@@ -36,30 +34,28 @@ fun bitmapToBase64(bitmap: Bitmap): String {
   return Base64.encodeToString(stream.toByteArray(), Base64.DEFAULT)
 }
 
-
 @Composable
-fun Base64ImageGetter(base64ImageString: MutableState<String?>) {
-  val isCameraSelected = remember { mutableStateOf (false) }
+fun GetImageOptions(
+  bottomSheetModalState: ModalBottomSheetState,
+  profileImageStr: MutableState<String?>
+) {
   val context = LocalContext.current
-  val bottomSheetModalState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+  val isCameraSelected = remember { mutableStateOf (false) }
   val coroutineScope = rememberCoroutineScope()
-
-
-
   val galleryLauncher = rememberLauncherForActivityResult(
     contract = ActivityResultContracts.GetContent()
   ) { uri: Uri? ->
     if (uri != null) {
       val source = ImageDecoder.createSource(context.contentResolver, uri)
       val bitmap = ImageDecoder.decodeBitmap(source)
-      base64ImageString.value = bitmapToBase64(bitmap)
+      profileImageStr.value = bitmapToBase64(bitmap)
     }
   }
 
   val cameraLauncher = rememberLauncherForActivityResult(
     contract = ActivityResultContracts.TakePicturePreview()
   ) { bitmap: Bitmap? ->
-    if (bitmap != null) base64ImageString.value = bitmapToBase64(bitmap)
+    if (bitmap != null) profileImageStr.value = bitmapToBase64(bitmap)
   }
 
   val permissionLauncher = rememberLauncherForActivityResult(
@@ -79,88 +75,56 @@ fun Base64ImageGetter(base64ImageString: MutableState<String?>) {
     }
   }
 
-  ModalBottomSheetLayout(
-    scrimColor=MaterialTheme.colors.onSurface.copy(alpha = 0.0f),
-    modifier = Modifier.fillMaxWidth(),
-    sheetContent = {
-      Box(
-        modifier = Modifier
-          .fillMaxWidth()
-          .wrapContentHeight()
-          .onFocusChanged { focusState ->
-            if (!focusState.hasFocus) { coroutineScope.launch { bottomSheetModalState.hide() } }
-    }
-      ) {
-          Row(
-            Modifier
-              .fillMaxWidth()
-              .padding(horizontal = 8.dp, vertical = 10.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-          ) {
 
-            ActionButton(null, "Use Camera", icon = Icons.Outlined.PhotoCamera) {
-              when (PackageManager.PERMISSION_GRANTED) {
-                ContextCompat.checkSelfPermission(
-                  context, Manifest.permission.CAMERA
-                ) -> {
-                  cameraLauncher.launch()
-                  coroutineScope.launch {
-                    bottomSheetModalState.hide()
-                  }
-                }
-                else -> {
-                  isCameraSelected.value = true
-                  permissionLauncher.launch(Manifest.permission.CAMERA)
-                }
-              }
-            }
-            ActionButton(null, "From Gallery", icon = Icons.Outlined.Collections) {
-              when (PackageManager.PERMISSION_GRANTED) {
-                ContextCompat.checkSelfPermission(
-                  context, Manifest.permission.READ_EXTERNAL_STORAGE
-                ) -> {
-                  galleryLauncher.launch("image/*")
-                  coroutineScope.launch {
-                    bottomSheetModalState.hide()
-                  }
-                }
-                else -> {
-                  isCameraSelected.value = false
-                  permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-                }
-              }
-            }
-            ActionButton(null, "Delete Image", icon = Icons.Filled.Delete) {
-              base64ImageString.value = null
-            }
-          }
-        }
-    },
-    sheetState = bottomSheetModalState,
+  Box(
+    modifier = Modifier
+      .fillMaxWidth()
+      .wrapContentHeight()
+      .onFocusChanged { focusState ->
+        if (!focusState.hasFocus) { coroutineScope.launch { bottomSheetModalState.hide() } }
+      }
   ) {
-    Box(
-      contentAlignment = Alignment.Center,
-      modifier = Modifier.fillMaxSize()
+    Row(
+      Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp, vertical = 10.dp),
+      horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-      Button(
-        onClick = {
-          coroutineScope.launch {
-            if (!bottomSheetModalState.isVisible) {
-              bottomSheetModalState.show()
-            } else {
+
+      ActionButton(null, "Use Camera", icon = Icons.Outlined.PhotoCamera) {
+        when (PackageManager.PERMISSION_GRANTED) {
+          ContextCompat.checkSelfPermission(
+            context, Manifest.permission.CAMERA
+          ) -> {
+            cameraLauncher.launch()
+            coroutineScope.launch {
               bottomSheetModalState.hide()
             }
           }
-        },
-        modifier = Modifier
-          .padding(16.dp)
-          .fillMaxWidth(),
-      ) {
-        Text(
-          text = "Update Profile Image",
-          modifier = Modifier.padding(8.dp),
-          textAlign = TextAlign.Center,
-        )
+          else -> {
+            isCameraSelected.value = true
+            permissionLauncher.launch(Manifest.permission.CAMERA)
+          }
+        }
+      }
+      ActionButton(null, "From Gallery", icon = Icons.Outlined.Collections) {
+        when (PackageManager.PERMISSION_GRANTED) {
+          ContextCompat.checkSelfPermission(
+            context, Manifest.permission.READ_EXTERNAL_STORAGE
+          ) -> {
+            galleryLauncher.launch("image/*")
+            coroutineScope.launch {
+              bottomSheetModalState.hide()
+            }
+          }
+          else -> {
+            isCameraSelected.value = false
+            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+          }
+        }
+      }
+      ActionButton(null, "Delete Image", icon = Icons.Filled.Delete) {
+        profileImageStr.value = null
       }
     }
   }
--- apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserProfileView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserProfileView.kt
@@ -1,24 +1,24 @@
 package chat.simplex.app.views.usersettings
 
 import android.content.res.Configuration
-import android.graphics.Bitmap
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import chat.simplex.app.model.ChatModel
 import chat.simplex.app.model.Profile
 import chat.simplex.app.ui.theme.SimpleXTheme
 import chat.simplex.app.views.helpers.*
+import kotlinx.coroutines.launch
 
 @Composable
 fun UserProfileView(chatModel: ChatModel) {
@@ -55,117 +55,169 @@ fun UserProfileLayout(
   editProfileOn: () -> Unit,
   saveProfile: (String, String) -> Unit,
 ) {
-  val profileImage = remember { mutableStateOf<Bitmap?>(null) }
-  Column(horizontalAlignment = Alignment.Start) {
-    Text(
-      "Your chat profile",
-      Modifier.padding(bottom = 24.dp),
-      style = MaterialTheme.typography.h1,
-      color = MaterialTheme.colors.onBackground
-    )
-    Text(
-      "Your profile is stored on your device and shared only with your contacts.\n" +
-          "SimpleX servers cannot see your profile.",
-      Modifier.padding(bottom = 24.dp),
-      color = MaterialTheme.colors.onBackground
-    )
-    if (editProfile) {
-      var displayName by remember { mutableStateOf(profile.displayName) }
-      var fullName by remember { mutableStateOf(profile.fullName) }
-      var profileImageStr = remember { mutableStateOf(profile.displayImage) }
-      Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Start
-      ) {
-        // TODO hints
-        ProfileImage(192.dp, profileImageStr.value)
-        BasicTextField(
-          value = displayName,
-          onValueChange = { displayName = it },
-          modifier = Modifier
-            .padding(bottom = 24.dp)
-            .fillMaxWidth(),
-          textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onBackground),
-          keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.None,
-            autoCorrect = false
-          ),
-          singleLine = true
-        )
-        BasicTextField(
-          value = fullName,
-          onValueChange = { fullName = it },
-          modifier = Modifier
-            .padding(bottom = 24.dp)
-            .fillMaxWidth(),
-          textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onBackground),
-          keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.None,
-            autoCorrect = false
-          ),
-          singleLine = true
-        )
-        Base64ImageGetter(profileImageStr)
-        Row {
-          Text(
-            "Cancel",
-            color = MaterialTheme.colors.primary,
-            modifier = Modifier
-              .clickable(onClick = editProfileOff),
-          )
-          Spacer(Modifier.padding(horizontal = 8.dp))
-          Text(
-            "Save (and notify contacts)",
-            color = MaterialTheme.colors.primary,
-            modifier = Modifier
-              .clickable(onClick = { saveProfile(displayName, fullName) })
-          )
+  val bottomSheetModalState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+  var displayName = remember { mutableStateOf(profile.displayName) }
+  var fullName = remember { mutableStateOf(profile.fullName) }
+  var profileImageStr = remember { mutableStateOf(profile.displayImage) }
+  val coroutineScope = rememberCoroutineScope()
+
+  ModalBottomSheetLayout(
+    scrimColor=MaterialTheme.colors.onSurface.copy(alpha = 0.0f),
+    modifier = Modifier.fillMaxWidth(),
+    sheetContent = {GetImageOptions(bottomSheetModalState, profileImageStr)},
+    sheetState = bottomSheetModalState,
+  ) {
+    Column(horizontalAlignment = Alignment.Start) {
+      Text(
+        "Your chat profile",
+        Modifier.padding(bottom = 24.dp),
+        style = MaterialTheme.typography.h1,
+        color = MaterialTheme.colors.onBackground
+      )
+      Text(
+        "Your profile is stored on your device and shared only with your contacts.\n" +
+            "SimpleX servers cannot see your profile.",
+        Modifier.padding(bottom = 24.dp),
+        color = MaterialTheme.colors.onBackground
+      )
+      // TODO hints
+      Row(modifier = Modifier.fillMaxWidth()) {
+        Column(
+          horizontalAlignment = Alignment.Start,
+          modifier = Modifier.padding(5.dp)
+        ) {
+          ProfileImage(80.dp, profileImageStr.value)
+        }
+        Column(horizontalAlignment = Alignment.Start) {
+          Row(
+            Modifier.padding(bottom = 24.dp)
+          ) {
+            EditableDisplayName(editProfile, profile, displayName)
+          }
+          Row(
+            Modifier.padding(bottom = 24.dp)
+          ) {
+            EditableFullName(editProfile, profile, fullName)
+          }
         }
       }
-    } else {
-      Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Start
-      ) {
-        ProfileImage(192.dp, profile.displayImage)
-        Row(
-          Modifier.padding(bottom = 24.dp)
-        ) {
-          Text(
-            "Display name:",
-            color = MaterialTheme.colors.onBackground
-          )
-          Spacer(Modifier.padding(horizontal = 4.dp))
-          Text(
-            profile.displayName,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colors.onBackground
-          )
+        if (editProfile) {
+
+          Button(
+            onClick = {
+              coroutineScope.launch {
+                if (!bottomSheetModalState.isVisible) {
+                  bottomSheetModalState.show()
+                } else {
+                  bottomSheetModalState.hide()
+                }
+              }
+            },
+            modifier = Modifier
+              .padding(8.dp)
+              .fillMaxWidth(),
+          ) {
+            Text(
+              text = "Update Profile Image",
+              modifier = Modifier.padding(8.dp),
+              textAlign = TextAlign.Center,
+            )
+          }
+
+          Row {
+            Text(
+              "Cancel",
+              color = MaterialTheme.colors.primary,
+              modifier = Modifier
+                .clickable(onClick = editProfileOff),
+            )
+            Spacer(Modifier.padding(horizontal = 8.dp))
+            Text(
+              "Save (and notify contacts)",
+              color = MaterialTheme.colors.primary,
+              modifier = Modifier
+                .clickable(onClick = { saveProfile(displayName.value, fullName.value) })
+            )
+          }
+        } else {
+          Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+          ) {
+            Text(
+              "Edit",
+              color = MaterialTheme.colors.primary,
+              modifier = Modifier
+                .clickable(onClick = editProfileOn)
+            )
+          }
         }
-        Row(
-          Modifier.padding(bottom = 24.dp)
-        ) {
-          Text(
-            "Full name:",
-            color = MaterialTheme.colors.onBackground
-          )
-          Spacer(Modifier.padding(horizontal = 4.dp))
-          Text(
-            profile.fullName,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colors.onBackground
-          )
-        }
-        Text(
-          "Edit",
-          color = MaterialTheme.colors.primary,
-          modifier = Modifier
-            .clickable(onClick = editProfileOn)
-        )
       }
     }
   }
+
+
+@Composable
+fun EditableDisplayName(editMode: Boolean, profile: Profile, displayName: MutableState<String>){
+  if (editMode) {
+    BasicTextField(
+      value = displayName.value,
+      onValueChange = { displayName.value = it },
+      modifier = Modifier
+        .fillMaxWidth(),
+      textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onBackground),
+      keyboardOptions = KeyboardOptions(
+        capitalization = KeyboardCapitalization.None,
+        autoCorrect = false
+      ),
+      singleLine = true
+    )
+  }
+  else {
+    Text(
+      "Display name:",
+      color = MaterialTheme.colors.onBackground
+    )
+    Spacer(Modifier.padding(horizontal = 4.dp))
+    Text(
+      profile.displayName,
+      fontWeight = FontWeight.Bold,
+      color = MaterialTheme.colors.onBackground
+    )
+  }
 }
+
+
+@Composable
+fun EditableFullName(editMode: Boolean, profile: Profile, fullName: MutableState<String>){
+  if (editMode) {
+    BasicTextField(
+      value = fullName.value,
+      onValueChange = { fullName.value = it },
+      modifier = Modifier
+        .fillMaxWidth(),
+      textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onBackground),
+      keyboardOptions = KeyboardOptions(
+        capitalization = KeyboardCapitalization.None,
+        autoCorrect = false
+      ),
+      singleLine = true
+    )
+  }
+  else {
+    Text(
+      "Full name:",
+      color = MaterialTheme.colors.onBackground
+    )
+    Spacer(Modifier.padding(horizontal = 4.dp))
+    Text(
+      profile.fullName,
+      fontWeight = FontWeight.Bold,
+      color = MaterialTheme.colors.onBackground
+    )
+  }
+}
+
 
 @Preview(showBackground = true)
 @Preview(
