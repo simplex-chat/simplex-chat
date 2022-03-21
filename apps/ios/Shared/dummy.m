1cfa0e02bfb198df068482d53779865f2d1b2a4c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-03-21 07:49:54 +0000

    ios: update dummy.m to work with x86 sim

--- apps/ios/Shared/dummy.m
+++ apps/ios/Shared/dummy.m
@@ -6,3 +6,14 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#import <dirent.h>
+
+int readdir_r$INODE64(DIR *restrict dirp, struct dirent *restrict entry,
+                      struct dirent **restrict result) {
+    return readdir_r(dirp, entry, result);
+}
+DIR *opendir$INODE64(const char *name) {
+    return opendir(name);
+}
+
