9c746e06c73ab4481b1805c101b93c7febe27926
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-04-03 19:10:16 +0100

    extract repl bot

--- apps/simplex-bot/Main.hs
+++ apps/simplex-bot/Main.hs
@@ -1,33 +1,22 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Control.Concurrent.Async
-import Control.Concurrent.STM
-import Control.Monad.Reader
-import qualified Data.ByteString.Char8 as B
-import qualified Data.Text as T
 import Simplex.Chat
 import Simplex.Chat.Bot
-import Simplex.Chat.Controller
-import Simplex.Chat.Messages
+import Simplex.Chat.Controller (versionNumber)
 import Simplex.Chat.Options
-import Simplex.Chat.Store
-import Simplex.Chat.Types
-import Simplex.Messaging.Encoding.String
 import System.Directory (getAppUserDataDirectory)
-import System.Exit (exitFailure)
 import Text.Read
 
 main :: IO ()
 main = do
   opts <- welcomeGetOpts
-  simplexChatBot defaultChatConfig opts myChatBot
+  simplexChatBot defaultChatConfig opts $
+    chatBotRepl "Hello! I am a simple squaring bot - if you send me a number, I will calculate its square" $ \msg ->
+      case readMaybe msg :: Maybe Integer of
+        Just n -> msg <> " * " <> msg <> " = " <> show (n * n)
+        _ -> "\"" <> msg <> "\" is not a number"
 
 welcomeGetOpts :: IO ChatOpts
 welcomeGetOpts = do
@@ -36,36 +25,3 @@ welcomeGetOpts = do
   putStrLn $ "SimpleX Chat Bot v" ++ versionNumber
   putStrLn $ "db: " <> dbFilePrefix <> "_chat.db, " <> dbFilePrefix <> "_agent.db"
   pure opts
-
-myChatBot :: User -> ChatController -> IO ()
-myChatBot User {} cc@ChatController {outputQ} = do
-  initializeBotAddress
-  race_ (forever $ void getLine) . forever $ do
-    (_, resp) <- atomically (readTBQueue outputQ)
-    case resp of
-      CRContactConnected contact -> do
-        contactConnected contact
-        void . sendMsg contact $ "Hello! I am a simple squaring bot - if you send me a number, I will calculate its square"
-      CRNewChatItem (AChatItem _ SMDRcv (DirectChat contact) ChatItem {content}) -> do
-        let msg = T.unpack $ ciContentToText content
-            number_ = readMaybe msg :: Maybe Integer
-        void . sendMsg contact $ case number_ of
-          Nothing -> "\"" <> msg <> "\" is not a number"
-          Just n -> msg <> " * " <> msg <> " = " <> show (n * n)
-      _ -> pure ()
-  where
-    initializeBotAddress = do
-      sendCmd "/show_address" >>= \case
-        CRUserContactLink uri _ -> showBotAddress uri
-        CRChatCmdError (ChatErrorStore SEUserContactLinkNotFound) -> do
-          putStrLn $ "No bot address, creating..."
-          sendCmd "/address" >>= \case
-            CRUserContactLinkCreated uri -> showBotAddress uri
-            _ -> putStrLn "can't create bot address" >> exitFailure
-        _ -> putStrLn "unexpected response" >> exitFailure
-    showBotAddress uri = do
-      putStrLn $ "Bot's contact address is: " <> B.unpack (strEncode uri)
-      void $ sendCmd "/auto_accept on"
-    sendCmd s = runReaderT (execChatCommand $ B.pack s) cc
-    sendMsg Contact {contactId} msg = sendCmd $ "/_send @" <> show contactId <> " text " <> msg
-    contactConnected Contact {localDisplayName} = putStrLn $ T.unpack localDisplayName <> " connected"
--- apps/simplex-chat/Main.hs
+++ apps/simplex-chat/Main.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
