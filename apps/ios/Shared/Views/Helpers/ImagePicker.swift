0ecaa59df6231c381e5d5a7d7f6c6dcd5786ed67
IanRDavies <ian_davies_@hotmail.co.uk>
2022-04-04 19:19:54 +0100

    ios: update image picker (#495)
    * switch to PHPicker for photos. TODO add back camera functionality. [rough]
    
    * add back camera selection option
    
    * remove force unwrap of optional
    
    * response to comments
    
    * rerun tests
    
    * refactor naming

--- apps/ios/Shared/Views/Helpers/ImagePicker.swift
+++ apps/ios/Shared/Views/Helpers/ImagePicker.swift
@@ -7,44 +7,102 @@
 //
 
 import SwiftUI
+import PhotosUI
 
-struct ImagePicker: UIViewControllerRepresentable {
-    @Environment(\.presentationMode) var presentationMode
-    var source: UIImagePickerController.SourceType
+enum ImageSource {
+    case imageLibrary
+    case camera
+}
+
+struct LibraryImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = PHPickerViewController
     @Binding var image: UIImage?
-    @Binding var imageUrl: URL?
-    
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: ImagePicker
-        
-        init(_ parent: ImagePicker) {
+    var didFinishPicking: (_ didSelectItems: Bool) -> Void
+
+    class Coordinator: PHPickerViewControllerDelegate {
+        let parent: LibraryImagePicker
+
+        init(_ parent: LibraryImagePicker) {
             self.parent = parent
         }
-        
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.didFinishPicking(!results.isEmpty)
+            guard !results.isEmpty else {
+                return
+            }
+
+            if let chosenImageProvider = results.first?.itemProvider {
+                if chosenImageProvider.canLoadObject(ofClass: UIImage.self) {
+                    chosenImageProvider.loadObject(ofClass: UIImage.self)  { [weak self] image, error in
+                        DispatchQueue.main.async {
+                            self?.loadImage(object: image, error: error)
+                        }
+                    }
+                }
+            }
+        }
+
+        func loadImage(object: Any?, error: Error? = nil) {
+            if let error = error {
+                logger.error("Couldn't load image with error: \(error.localizedDescription)")
+            }
+            parent.image = object as? UIImage
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+        let controller = PHPickerViewController(configuration: config)
+        controller.delegate = context.coordinator
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+
+    }
+}
+
+
+struct CameraImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraImagePicker
+
+        init(_ parent: CameraImagePicker) {
+            self.parent = parent
+        }
+
         func imagePickerController(_ picker: UIImagePickerController,
                                    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
             if let uiImage = info[.originalImage] as? UIImage {
-                parent.imageUrl = info[.imageURL] as? URL
                 parent.image = uiImage
             }
             parent.presentationMode.wrappedValue.dismiss()
         }
     }
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
 
-    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
-        picker.sourceType = source
+        picker.sourceType = .camera
         picker.allowsEditing = false
         picker.delegate = context.coordinator
         return picker
     }
 
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
-   
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraImagePicker>) {
+
     }
 }
