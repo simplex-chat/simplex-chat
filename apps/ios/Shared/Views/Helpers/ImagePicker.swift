d90c4261b8ce11f7251172f1ec82286b8001fc48
IanRDavies <ian_davies_@hotmail.co.uk>
2022-04-08 18:17:10 +0100

    ios: link previews (#503)
    * refactor image utils and initial link metadata tools
    
    * remove LPMetadata conversion as we will build our own view to avoid network calls
    
    * initial very basic preview outline, remove icon loading
    
    * connect preview view to compose view
    
    * v0.1 barely working
    
    * minor refactor
    
    * refactor
    
    * collect images effectively
    
    * link up to api for send/receive
    
    * rework async get metadata logic
    
    * show previews in chat
    
    * refactor resizing logic
    
    * checkpoint before view editing
    
    * ui changes
    
    * housekeeping
    
    * ui tweaks
    
    * typo
    
    * improve link preview design/logic
    
    * resize image to target data size
    
    * fix link preview state machine
    
    * tidy up
    
    * fix typo
    
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- apps/ios/Shared/Views/Helpers/ImagePicker.swift
+++ apps/ios/Shared/Views/Helpers/ImagePicker.swift
@@ -9,6 +9,64 @@
 import SwiftUI
 import PhotosUI
 
+func dropPrefix(_ s: String, _ prefix: String) -> String {
+    s.hasPrefix(prefix) ? String(s.dropFirst(prefix.count)) : s
+}
+
+func dropImagePrefix(_ s: String) -> String {
+    dropPrefix(dropPrefix(s, "data:image/png;base64,"), "data:image/jpg;base64,")
+}
+
+func resizeAndCrop(_ image: UIImage, to newSize: CGSize) -> UIImage {
+    let format = UIGraphicsImageRendererFormat()
+    format.scale = 1.0
+    format.opaque = true
+    return UIGraphicsImageRenderer(bounds: CGRect(origin: .zero, size: newSize), format: format).image { _ in
+        let size = image.size
+        let hScale = newSize.height / size.height
+        let vScale = newSize.width / size.width
+        let scale = max(hScale, vScale) // scaleToFill
+        let resizeSize = CGSize(width: size.width * scale, height: size.height * scale)
+        var middle = CGPoint.zero
+        if resizeSize.width > newSize.width {
+            middle.x -= (resizeSize.width - newSize.width) / 2
+        } else if resizeSize.height > newSize.height {
+            middle.y -= (resizeSize.height - newSize.height) / 2
+        }
+        image.draw(in: CGRect(origin: middle, size: resizeSize))
+    }
+}
+
+func cropToSquare(_ image: UIImage) -> UIImage {
+    let side = min(image.size.width, image.size.height)
+    return resizeAndCrop(image, to: CGSize(width: side, height: side))
+}
+
+func resizeImageToDataSize(_ image: UIImage, maxSize: Int) -> String? {
+    let size = image.size
+    var imageStr = compressImage(image)
+    var resized = image
+    var ratio: CGFloat = 1
+    var dataSize = imageStr?.count ?? 0
+    logger.debug("resizeImageToDataSize: initial size \(String(describing: size)), data size \(dataSize)")
+    while dataSize != 0 && dataSize > maxSize {
+        ratio *= sqrt(CGFloat(dataSize / maxSize) * 1.2)
+        resized = resizeAndCrop(resized, to: CGSize(width: size.width / ratio, height: size.height / ratio))
+        imageStr = compressImage(resized)
+        dataSize = imageStr?.count ?? 0
+        logger.debug("resizeImageToDataSize: ratio \(ratio)")
+    }
+    logger.debug("resizeImageToDataSize: final size \(String(describing: resized.size)), data size \(dataSize)")
+    return imageStr
+}
+
+func compressImage(_ image: UIImage, _ compressionQuality: CGFloat = 0.85) -> String? {
+    if let data = image.jpegData(compressionQuality: compressionQuality) {
+        return "data:image/jpg;base64,\(data.base64EncodedString())"
+    }
+    return nil
+}
+
 enum ImageSource {
     case imageLibrary
     case camera
