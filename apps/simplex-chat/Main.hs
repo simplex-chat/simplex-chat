edbd48eebde776ab6d3958732a2929e11cd71951
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-04-10 12:57:29 +0100

    terminal: option to execute a single chat command via command line

--- apps/simplex-chat/Main.hs
+++ apps/simplex-chat/Main.hs
@@ -2,24 +2,35 @@
 
 module Main where
 
+import Control.Concurrent (threadDelay)
 import Simplex.Chat
-import Simplex.Chat.Controller (versionNumber)
+import Simplex.Chat.Bot
+import Simplex.Chat.Controller (ChatConfig, versionNumber)
 import Simplex.Chat.Options
 import Simplex.Chat.Terminal
+import Simplex.Chat.View (serializeChatResponse)
 import System.Directory (getAppUserDataDirectory)
 import System.Terminal (withTerminal)
 
+cfg :: ChatConfig
+cfg = defaultChatConfig
+
 main :: IO ()
 main = do
-  opts <- welcomeGetOpts
-  t <- withTerminal pure
-  simplexChat defaultChatConfig opts t
-
-welcomeGetOpts :: IO ChatOpts
-welcomeGetOpts = do
   appDir <- getAppUserDataDirectory "simplex"
-  opts@ChatOpts {dbFilePrefix} <- getChatOpts appDir "simplex_v1"
+  opts@ChatOpts {chatCmd} <- getChatOpts appDir "simplex_v1"
+  if null chatCmd
+    then do
+      welcome opts
+      t <- withTerminal pure
+      simplexChat cfg opts t
+    else simplexChatBot cfg opts $ \_ cc -> do
+      r <- sendCmd cc chatCmd
+      putStrLn $ serializeChatResponse r
+      threadDelay $ chatCmdDelay opts * 1000000
+
+welcome :: ChatOpts -> IO ()
+welcome ChatOpts {dbFilePrefix} = do
   putStrLn $ "SimpleX Chat v" ++ versionNumber
   putStrLn $ "db: " <> dbFilePrefix <> "_chat.db, " <> dbFilePrefix <> "_agent.db"
   putStrLn "type \"/help\" or \"/h\" for usage info"
-  pure opts
