4cc3d4d921861553cd2f32ea9fb9561677eda195
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-04-11 15:52:21 +0100

    move nix to scripts

--- flake.nix
+++ flake.nix
@@ -18,10 +18,10 @@
           name = "simplex-chat";
           src = ./.;
         };
-        sha256map = import ./nix/sha256map.nix;
+        sha256map = import ./scripts/nix/sha256map.nix;
         modules = [{
-          packages.direct-sqlite.patches = [ ./nix/direct-sqlite-2.3.26.patch ];
-          packages.entropy.patches = [ ./nix/entropy.patch ];
+          packages.direct-sqlite.patches = [ ./scripts/nix/direct-sqlite-2.3.26.patch ];
+          packages.entropy.patches = [ ./scripts/nix/entropy.patch ];
         }
         ({ pkgs,lib, ... }: lib.mkIf (pkgs.stdenv.hostPlatform.isAndroid) {
           packages.simplex-chat.components.library.ghcOptions = [ "-pie" ];
@@ -329,7 +329,7 @@
           name = "update-sha256map";
           runtimeInputs = [ pkgs.nix-prefetch-git pkgs.jq pkgs.gawk ];
           text = ''
-            gawk -f ./nix/update-sha256.awk cabal.project > ./nix/sha256map.nix
+            gawk -f ./scripts/nix/update-sha256.awk cabal.project > ./scripts/nix/sha256map.nix
           '';
         }; in
 	pkgs.mkShell {
