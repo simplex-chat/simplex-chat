0a17f5c49193de36cbc8f997c0816ace83e52906
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-04-11 18:43:09 +0100

    ios: update package name in prepare script, update libs (#509)
    * ios: update package name in prepare script, update libs (not working yet)
    
    * ios: update/move prepare scripts

--- scripts/ios/prepare-x86_64.sh
+++ scripts/ios/prepare-x86_64.sh
@@ -3,8 +3,8 @@
 # the binaries folders should be in ~/Downloads folder
 rm -rf ./apps/ios/Libraries/mac-aarch64 ./apps/ios/Libraries/mac-x86_64 ./apps/ios/Libraries/ios ./apps/ios/Libraries/sim
 mkdir -p ./apps/ios/Libraries/mac-aarch64 ./apps/ios/Libraries/mac-x86_64 ./apps/ios/Libraries/ios ./apps/ios/Libraries/sim
-cp ~/Downloads/pkg-ios-aarch64/* ./apps/ios/Libraries/mac-aarch64
-cp ~/Downloads/pkg-ios-x86_64/* ./apps/ios/Libraries/mac-x86_64
+cp ~/Downloads/pkg-ios-aarch64-swift-json/* ./apps/ios/Libraries/mac-aarch64
+cp ~/Downloads/pkg-ios-x86_64-swift-json/* ./apps/ios/Libraries/mac-x86_64
 chmod +w ./apps/ios/Libraries/mac-aarch64/*
 chmod +w ./apps/ios/Libraries/mac-x86_64/*
 cp ./apps/ios/Libraries/mac-aarch64/* ./apps/ios/Libraries/ios
