4d81e473d84d8ab974cf75973c7a44f47cc54917
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-04-18 13:09:32 +0400

    android wider images

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ChatItemImageView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ChatItemImageView.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
 import chat.simplex.app.BuildConfig
 import chat.simplex.app.model.CIFile
@@ -17,7 +18,9 @@ import java.io.*
 
 @Composable
 fun ChatItemImageView(image: String, file: CIFile?) {
-  Column {
+  Column(
+    Modifier.width(300.dp)
+  ) {
     val imageBitmap = if (file?.filePath != null) {
       val context = LocalContext.current
       val filePath = getAppFilesDirectory(context) + "/" + file.filePath
