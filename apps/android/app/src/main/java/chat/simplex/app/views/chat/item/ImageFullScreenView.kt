47e0bc01436ac67af93e76f01cac6a6067fd35d1
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-04-19 10:33:51 +0400

    android: close image view on click

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ImageFullScreenView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ImageFullScreenView.kt
@@ -1,14 +1,12 @@
 import android.graphics.Bitmap
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import chat.simplex.app.views.helpers.CloseSheetBar
 
 @Composable
 fun ImageFullScreenView(imageBitmap: Bitmap, close: () -> Unit) {
@@ -17,6 +15,7 @@ fun ImageFullScreenView(imageBitmap: Bitmap, close: () -> Unit) {
     Modifier
       .fillMaxSize()
       .background(Color.Black)
+      .clickable(onClick = close)
   ) {
     Image(
       imageBitmap.asImageBitmap(),
