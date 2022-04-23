95f1de871bbe1ca7ac572ca54324924463e5430e
IanRDavies <ian_davies_@hotmail.co.uk>
2022-04-23 10:29:23 +0100

    first pass at pastebox in iOS

---
+++ apps/ios/Shared/Views/NewChat/PasteToConnectView.swift
@@ -0,0 +1,77 @@
+//
+//  PasteToConnectView.swift
+//  SimpleX (iOS)
+//
+//  Created by Ian Davies on 22/04/2022.
+//  Copyright © 2022 SimpleX Chat. All rights reserved.
+//
+
+import SwiftUI
+
+struct PasteToConnectView: View {
+    var connectViaLink: (String) -> Void
+    @State private var teHeight: CGFloat = 42
+    @State private var connectionLink: String = ""
+    @State private var submitEnabled: Bool = false
+    @Namespace var namespace
+    @FocusState.Binding var keyboardVisible: Bool
+    var maxHeight: CGFloat = 360
+    var minHeight: CGFloat = 37
+
+    var body: some View {
+        ZStack {
+            HStack(alignment: .bottom) {
+                ZStack(alignment: .leading) {
+                    Text(connectionLink)
+                        .font(.body)
+                        .foregroundColor(.clear)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 8)
+                        .matchedGeometryEffect(id: "te", in: namespace)
+                        .background(GeometryReader(content: updateHeight))
+                    TextEditor(text: $connectionLink)
+                        .onSubmit(submit)
+                        .focused($keyboardVisible)
+                        .font(.body)
+                        .textInputAutocapitalization(.never)
+                        .padding(.horizontal, 5)
+                        .allowsTightening(false)
+                        .frame(height: teHeight)
+                }
+                Button(action: submit) {
+                    Image(systemName: submitEnabled ? "checkmark.circle.fill" : "arrow.right.doc.on.clipboard")
+                        .resizable()
+                        .foregroundColor(.accentColor)
+                }
+                .frame(width: 29, height: 29)
+                .padding([.bottom, .trailing], 4)
+            }
+
+            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                .strokeBorder(.secondary, lineWidth: 0.3, antialiased: true)
+                .frame(height: teHeight)
+        }
+        .padding(.vertical, 8)
+    }
+    private func submit() {
+        if submitEnabled {
+            connectViaLink(connectionLink)
+            connectionLink = ""
+        } else {
+            connectionLink = UIPasteboard.general.string ?? ""
+        }
+    }
+    func updateHeight(_ g: GeometryProxy) -> Color {
+        DispatchQueue.main.async {
+            teHeight = min(max(g.frame(in: .local).size.height, minHeight), maxHeight)
+        }
+        return Color.clear
+    }
+}
+
+struct PasteToConnectView_Previews: PreviewProvider {
+    static var previews: some View {
+        @FocusState var keyboardVisible: Bool
+        return PasteToConnectView(connectViaLink: { print($0) }, keyboardVisible: $keyboardVisible)
+    }
+}
