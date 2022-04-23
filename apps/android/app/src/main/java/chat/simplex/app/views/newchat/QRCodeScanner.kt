f83da29d32e82c8ee7fab8a0a7807322c1bd2bed
IanRDavies <ian_davies_@hotmail.co.uk>
2022-04-23 17:59:02 +0100

    tidy up to make picking back up easier

--- apps/android/app/src/main/java/chat/simplex/app/views/newchat/QRCodeScanner.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/newchat/QRCodeScanner.kt
@@ -37,48 +37,56 @@ fun QRCodeScanner(onBarcode: (String) -> Unit) {
         )
         implementationMode = PreviewView.ImplementationMode.COMPATIBLE
       }
-    },
-    update = { previewView ->
-      val cameraSelector: CameraSelector = CameraSelector.Builder()
-        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-        .build()
-      val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
-      val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
-        ProcessCameraProvider.getInstance(context)
+    }
+  ) { previewView ->
+    val cameraSelector: CameraSelector = CameraSelector.Builder()
+      .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+      .build()
+    val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+    val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+      ProcessCameraProvider.getInstance(context)
 
-      cameraProviderFuture.addListener({
-        preview = Preview.Builder().build().also {
-          it.setSurfaceProvider(previewView.surfaceProvider)
+    cameraProviderFuture.addListener({
+      preview = Preview.Builder().build().also {
+        it.setSurfaceProvider(previewView.surfaceProvider)
+      }
+      val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+      val qrReader = QRCodeReader()
+      fun getQR(imageProxy: ImageProxy) {
+        val bitmap = imageProxyToBinaryBitmap(imageProxy)
+        val decodeHints = EnumMap<DecodeHintType, Any>(
+          DecodeHintType::class.java
+        )
+        decodeHints[DecodeHintType.TRY_HARDER] = true
+        decodeHints[DecodeHintType.POSSIBLE_FORMATS] = BarcodeFormat.QR_CODE
+        decodeHints[DecodeHintType.PURE_BARCODE] = false
+        try {
+          val result = qrReader.decode(bitmap, decodeHints)
+          println("QR RESULT: ${result.text}")
+//          onBarcode(result.text)
+        } catch (e: Exception) {
+          when (e) {
+            is FormatException, is ChecksumException, is NotFoundException -> {
+              // No QR found yet
+            }
+            else -> e.printStackTrace()
+          }
         }
-        val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-        val qrReader = QRCodeReader()
-        fun getQR(imageProxy: ImageProxy) {
-          val bitmap = imageProxyToBinaryBitmap(imageProxy)
-          val decodeHints = EnumMap<DecodeHintType, Any>(
-            DecodeHintType::class.java
-          )
-          decodeHints[DecodeHintType.TRY_HARDER] = true
-          decodeHints[DecodeHintType.POSSIBLE_FORMATS] = BarcodeFormat.QR_CODE
-          decodeHints[DecodeHintType.PURE_BARCODE] = false
-          // TODO(Breaks here need to downsample bitmap)
-          // val result = qrReader.decode(bitmap, decodeHints)
-          // println("QR RESULT: ${result.text}")
-          // onBarcode(result.text)
-        }
-        val imageAnalyzer = ImageAnalysis.Analyzer { proxy -> getQR(proxy)}
-        val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+      }
+
+      val imageAnalyzer = ImageAnalysis.Analyzer { proxy -> getQR(proxy) }
+      val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
         .build()
         .also { it.setAnalyzer(cameraExecutor, imageAnalyzer) }
-        try {
-          cameraProvider.unbindAll()
-          cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalysis)
-        } catch (e: Exception) {
-          Log.d(TAG, "CameraPreview: ${e.localizedMessage}")
-        }
-      }, ContextCompat.getMainExecutor(context))
-    }
-  )
+      try {
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalysis)
+      } catch (e: Exception) {
+        Log.d(TAG, "CameraPreview: ${e.localizedMessage}")
+      }
+    }, ContextCompat.getMainExecutor(context))
+  }
 }
 
 private fun imageProxyToBinaryBitmap(img: ImageProxy) : BinaryBitmap? {
