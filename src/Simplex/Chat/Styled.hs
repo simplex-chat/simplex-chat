a56fb95975ab9b1c744ada9846c016cfd9f1a699
Benjamin Swartley <reep236@gmail.com>
2022-04-22 21:10:42 -0400

    Reworked for snippets on lower layers

--- src/Simplex/Chat/Styled.hs
+++ src/Simplex/Chat/Styled.hs
@@ -29,31 +29,49 @@ instance Monoid StyledString where mempty = plain ""
 
 instance IsString StyledString where fromString = plain
 
+data SingleStyled = SStyled [SGR] String
+type MaybeSnippet = Either StyledString SingleStyled
+
 styleMarkdown :: Markdown -> StyledString
 styleMarkdown = \case 
-    Markdown Nothing s -> plain s 
-    Markdown (Just Snippet) s -> '`' `wrap` styled Snippet s -- TODO: This can't "undo" outer layers, meaning formatting still gets gorked if this is on the inside 
-    Markdown f s -> merge . (<> styleFormat f s) . styleMarkdown . parseMarkdown $ s 
-    (s1 :|: s2) -> styleMarkdown s1 <> styleMarkdown s2 
+    (a :|: b) -> styleMarkdown a <> styleMarkdown b 
+    Markdown f t -> inlineSnippets . styleSMarkdown $ FormattedText f t 
+    
+    where
 
-    where 
-        merge :: StyledString -> StyledString 
-        merge = \case 
-            (Styled f1 s :<>: Styled f2 _) -> Styled (f1 ++ f2) s 
-            (Styled f s :<>: a) -> merge . (<>) (Styled f s) . merge $ a 
-            sing -> sing
+        breakApart :: Markdown -> [FormattedText]
+        breakApart = \case 
+            Markdown f s -> [FormattedText f s]
+            a :|: b -> breakApart a ++ breakApart b
+
+        unify :: MaybeSnippet -> StyledString 
+        unify = \case 
+            Left a -> a
+            Right (SStyled a b) -> Styled a b
+
+        inlineSnippets :: [MaybeSnippet] -> StyledString 
+        inlineSnippets = foldr (\nxt acc -> (<> acc) . unify $ nxt) (plain "")
+
+        inherit :: [SGR] -> MaybeSnippet -> MaybeSnippet
+        inherit f1 = \case
+            Left snippet        -> Left snippet 
+            Right (SStyled f2 s) -> Right $ SStyled (f1 ++ f2) s 
+        
+        inheritAll :: [SGR] -> Markdown -> [MaybeSnippet]
+        inheritAll f = concatMap (map (inherit f) . styleSMarkdown) . breakApart 
+
+        styleSMarkdown :: FormattedText -> [MaybeSnippet]
+        styleSMarkdown = \case 
+            FormattedText (Just Snippet) s -> (: []) . Left $ '`' `wrap` styled Snippet s
+            FormattedText (Just Secret) s ->  (:) (Right $ SStyled [] "#") . (++ [Right $ SStyled [] "#"]) . inheritAll (sgr Secret) . parseMarkdown $ s   
+            FormattedText (Just f) s -> inheritAll (sgr f) . parseMarkdown $ s
+            FormattedText Nothing s -> (: []) . Right . SStyled [] . T.unpack $ s 
 
 styleMarkdownList :: MarkdownList -> StyledString
 styleMarkdownList = \case 
     [] -> plain ""
     [FormattedText f s] -> styleMarkdown (Markdown f s)
     (FormattedText f s : ts) -> (<>) (styleMarkdown (Markdown f s)) . styleMarkdownList $ ts
-
-styleFormat :: Maybe Format -> Text -> StyledString
-styleFormat (Just Snippet) s = '`' `wrap` styled Snippet s
-styleFormat (Just Secret) s = '#' `wrap` styled Secret s
-styleFormat (Just f) s = styled f s
-styleFormat Nothing s = plain s
 
 wrap :: Char -> StyledString -> StyledString
 wrap c s = plain [c] <> s <> plain [c]
