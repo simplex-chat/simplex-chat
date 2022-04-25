f64813a0bd7a9139ede9f241cd2097d0b17c6da2
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-04-25 12:22:32 +0400

    fix merge

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/ComposeView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/ComposeView.kt
@@ -2,6 +2,7 @@ package chat.simplex.app.views.chat
 
 import ComposeImageView
 import android.graphics.Bitmap
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
@@ -12,9 +13,13 @@ import androidx.compose.material.icons.filled.AttachFile
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import chat.simplex.app.R
 import chat.simplex.app.model.*
 import chat.simplex.app.views.helpers.ComposeLinkView
+import chat.simplex.app.views.helpers.generalGetString
 
 // TODO ComposeState
 @Composable
