45f5f33a6b02b848cf39c362873a975ce2ae7ea7
IanRDavies <ian_davies_@hotmail.co.uk>
2022-04-25 10:28:42 +0100

    update QR scanner to use boofcv

--- apps/android/app/src/main/java/chat/simplex/app/views/newchat/QRCodeScanner.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/newchat/QRCodeScanner.kt
@@ -1,6 +1,6 @@
 package chat.simplex.app.views.newchat
 
-import android.graphics.ImageFormat.*
+import android.annotation.SuppressLint
 import android.util.Log
 import android.view.ViewGroup
 import androidx.camera.core.*
@@ -11,15 +11,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import boofcv.abst.fiducial.QrCodeDetector
+import boofcv.alg.color.ColorFormat
+import boofcv.android.ConvertCameraImage
+import boofcv.factory.fiducial.FactoryFiducial
+import boofcv.struct.image.GrayU8
 import chat.simplex.app.TAG
 import com.google.common.util.concurrent.ListenableFuture
-import com.google.zxing.*
-import com.google.zxing.common.HybridBinarizer
-import com.google.zxing.qrcode.QRCodeReader
-import java.nio.ByteBuffer
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.util.concurrent.*
 
 @Composable
 fun QRCodeScanner(onBarcode: (String) -> Unit) {
@@ -53,43 +52,23 @@ fun QRCodeScanner(onBarcode: (String) -> Unit) {
         it.setSurfaceProvider(previewView.surfaceProvider)
       }
       val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-      val qrReader = QRCodeReader()
+      val detector: QrCodeDetector<GrayU8> = FactoryFiducial.qrcode(null, GrayU8::class.java)
       fun getQR(imageProxy: ImageProxy) {
         val currentTimeStamp = System.currentTimeMillis()
-        if (currentTimeStamp - lastAnalyzedTimeStamp >= 500) {
-          lastAnalyzedTimeStamp = currentTimeStamp
-          val bitmap = imageProxyToBinaryBitmap(imageProxy)
-          val decodeHints = EnumMap<DecodeHintType, Any>(
-            DecodeHintType::class.java
-          )
-          decodeHints[DecodeHintType.TRY_HARDER] = true
-          try {
-            val result = qrReader.decode(bitmap, decodeHints)
-            if (result.text != contactLink) {
+        if (currentTimeStamp - lastAnalyzedTimeStamp >= TimeUnit.SECONDS.toMillis(1)) {
+          detector.process(imageProxyToGrayU8(imageProxy))
+          val found = detector.detections
+          val qr = found.firstOrNull()
+          if (qr != null) {
+            if (qr.message != contactLink) {
               // Make sure link is new and not a repeat
-              contactLink = result.text
+              contactLink = qr.message
               onBarcode(contactLink)
             }
-            imageProxy.close()
-          } catch (nfe: NotFoundException) {
-            // Comment log for too much noise
-            // Log.d(TAG, "Failed to find QR code in current image.")
-            imageProxy.close()
-          } catch (fe: FormatException) {
-            Log.d(TAG, "Failed to find QR code of expected format. Possible mis-detection.")
-            imageProxy.close()
-          } catch (ce: ChecksumException) {
-            Log.d(TAG, "Invalid QR Code. Checksum failure.")
-            imageProxy.close()
-          } catch (e: Exception) {
-            Log.d(TAG, "QR code detection failed for unknown reason: ${e.localizedMessage}")
-            imageProxy.close()
           }
-        } else {
-          imageProxy.close()
         }
+        imageProxy.close()
       }
-
       val imageAnalyzer = ImageAnalysis.Analyzer { proxy -> getQR(proxy) }
       val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
@@ -106,23 +85,15 @@ fun QRCodeScanner(onBarcode: (String) -> Unit) {
   }
 }
 
-private fun imageProxyToBinaryBitmap(img: ImageProxy) : BinaryBitmap? {
-  if (img.format == YUV_420_888 || img.format == YUV_422_888 || img.format == YUV_444_888) {
-    val byteBuffer: ByteBuffer = img.planes[0].buffer
-    val imageData = ByteArray(byteBuffer.capacity())
-    try {
-      byteBuffer.get(imageData)
-      val source = PlanarYUVLuminanceSource(
-        imageData,
-        img.width, img.height,
-        0, 0,
-        img.width, img.height,
-        false
-      )
-      return BinaryBitmap(HybridBinarizer(source))
-    } catch (e: Exception) {
-      e.printStackTrace()
-    }
+
+
+@SuppressLint("UnsafeOptInUsageError")
+private fun imageProxyToGrayU8(img: ImageProxy) : GrayU8? {
+  val image = img.image
+  if (image != null) {
+    val out = GrayU8()
+    ConvertCameraImage.imageToBoof(image, ColorFormat.GRAY, out, null)
+    return out
   }
   return null
 }
