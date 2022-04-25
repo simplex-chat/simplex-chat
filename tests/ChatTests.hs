1d92d210ff0cf509888ac9a39aeb10897de67459
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-04-25 23:20:44 +0400

    test desc

--- tests/ChatTests.hs
+++ tests/ChatTests.hs
@@ -84,7 +84,7 @@ chatTests = do
   describe "async connection handshake" $ do
     it "connect when initiating client goes offline" testAsyncInitiatingOffline
     it "connect when accepting client goes offline" testAsyncAcceptingOffline
-    it "connect, fully asynchronous" testFullAsync
+    it "connect, fully asynchronous (when clients are never simultaneously online)" testFullAsync
   describe "async sending and receiving files" $ do
     it "send and receive file, fully asynchronous" testAsyncFileTransfer
     it "send and receive file to group, fully asynchronous" testAsyncGroupFileTransfer
