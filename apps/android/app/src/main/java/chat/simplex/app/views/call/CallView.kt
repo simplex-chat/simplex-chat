11e78df9430eecc42363c274ee3d12dbd77e9a65
IanRDavies <ian_davies_@hotmail.co.uk>
2022-04-26 11:49:09 +0100

    try to resolve permissions issues

--- apps/android/app/src/main/java/chat/simplex/app/views/call/CallView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/call/CallView.kt
@@ -1,52 +1,97 @@
 package chat.simplex.app.views.call
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.view.ViewGroup
 import android.webkit.*
 import androidx.activity.compose.BackHandler
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 @SuppressLint("JavascriptInterface")
 @Composable
 fun VideoCallView(close: () -> Unit) {
   BackHandler(onBack = close)
-  val assetLoader = WebViewAssetLoader.Builder()
-    .addPathHandler("/assets/www/", WebViewAssetLoader.AssetsPathHandler(LocalContext.current))
-    .build()
-  AndroidView(
-    factory = {
-        AndroidViewContext ->
-      WebView(AndroidViewContext).apply {
-        layoutParams = ViewGroup.LayoutParams(
-          ViewGroup.LayoutParams.MATCH_PARENT,
-          ViewGroup.LayoutParams.MATCH_PARENT,
-        )
-        this.webChromeClient = object: WebChromeClient() {
-          override fun onPermissionRequest(request: PermissionRequest?) {
-            request?.grant(request.resources)
-          }
-        }
-        this.webViewClient = LocalContentWebViewClient(assetLoader)
-        this.clearHistory()
-        this.clearCache(true)
-        this.addJavascriptInterface(JavascriptInterface(), "Android")
-        val webViewSettings = this.settings
-        webViewSettings.allowFileAccess = true
-        webViewSettings.allowContentAccess = true
-        webViewSettings.javaScriptEnabled = true
-        webViewSettings.mediaPlaybackRequiresUserGesture = false
-        webViewSettings.useWideViewPort = true
-        webViewSettings.cacheMode = WebSettings.LOAD_NO_CACHE
-        this.loadUrl("file:android_asset/www/call.html")
+  val permissionsState = rememberMultiplePermissionsState(
+    permissions = listOf(
+      Manifest.permission.CAMERA,
+      Manifest.permission.RECORD_AUDIO,
+      Manifest.permission.MODIFY_AUDIO_SETTINGS,
+      Manifest.permission.INTERNET
+    )
+  )
+  val lifecycleOwner = LocalLifecycleOwner.current
+  DisposableEffect(lifecycleOwner) {
+    val observer = LifecycleEventObserver { _, event ->
+      if (event == Lifecycle.Event.ON_RESUME || event == Lifecycle.Event.ON_START) {
+        permissionsState.launchMultiplePermissionRequest()
       }
     }
-  ) {
-//    webView -> webView.post { webView.evaluateJavascript("javascript:f();", null) }
+    lifecycleOwner.lifecycle.addObserver(observer)
+
+    onDispose { lifecycleOwner.lifecycle.removeObserver(observer) }
   }
+
+  val localContext = LocalContext.current
+  val assetLoader = WebViewAssetLoader.Builder()
+    .addPathHandler("/assets/www/", WebViewAssetLoader.AssetsPathHandler(localContext))
+    .build()
+  if (permissionsState.allPermissionsGranted) {
+    AndroidView(
+      factory = { AndroidViewContext ->
+        WebView(AndroidViewContext).apply {
+          layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+          )
+          this.webChromeClient = object: WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest) {
+              if (request.origin.toString().startsWith("file:/")) {
+                request.grant(request.resources)
+              } else {
+                println("DENIED")
+                request.deny()
+              }
+            }
+
+
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+              val rtnValue = super.onConsoleMessage(consoleMessage)
+              println("JS MESSAGE: ${consoleMessage?.message()}")
+              return rtnValue
+            }
+          }
+          this.webViewClient = LocalContentWebViewClient(assetLoader)
+          this.clearHistory()
+          this.clearCache(true)
+//        this.addJavascriptInterface(JavascriptInterface(), "Android")
+          val webViewSettings = this.settings
+          webViewSettings.allowFileAccess = true
+          webViewSettings.allowContentAccess = true
+          webViewSettings.javaScriptEnabled = true
+          webViewSettings.mediaPlaybackRequiresUserGesture = false
+          webViewSettings.cacheMode = WebSettings.LOAD_NO_CACHE
+          this.loadUrl("file:android_asset/www/call.html")
+        }
+      }
+    ) {
+//    webView -> webView.post { webView.evaluateJavascript("javascript:f();", null) }
+    }
+  } else {
+    Text("NEED PERMISSIONS")
+//    SideEffect {
+//      permissionsState.launchMultiplePermissionRequest()
+//    }
+  }
+
 }
 
 private class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
@@ -57,6 +102,6 @@ private class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoa
     return assetLoader.shouldInterceptRequest(request.url)
   }
   override fun onPageFinished(view: WebView?, url: String?) {
-    view?.post { view.evaluateJavascript("javascript:f();", null) }
+    view?.post { view.evaluateJavascript("startCall();", null) }
   }
 }
