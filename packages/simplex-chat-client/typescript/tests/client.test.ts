9b475e4015bb62107a4da129783bf4d17a49f6ed
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-05-13 13:27:39 +0100

    update test

--- packages/simplex-chat-client/typescript/tests/client.test.ts
+++ packages/simplex-chat-client/typescript/tests/client.test.ts
@@ -3,9 +3,12 @@ import {ChatClient} from "../src/index"
 import {ConnReqType} from "../src/client"
 import * as C from "../src/command"
 
-describe("ChatClient (expects SimpleX Chat server with a user, on localhost:5225)", () => {
+describe("ChatClient (expects SimpleX Chat server with a user, without contacts, on localhost:5225)", () => {
   test("connect, send message to themselves, delete contact", async () => {
     const c = await ChatClient.create("ws://localhost:5225")
+    assert.strictEqual((await c.msgQ.dequeue()).type, "contactSubSummary")
+    assert.strictEqual((await c.msgQ.dequeue()).type, "memberSubErrors")
+    assert.strictEqual((await c.msgQ.dequeue()).type, "pendingSubSummary")
     const user = await c.apiGetActiveUser()
     assert.strictEqual(typeof user?.localDisplayName, "string")
     const connReq = await c.apiCreateLink()
@@ -24,9 +27,20 @@ describe("ChatClient (expects SimpleX Chat server with a user, on localhost:5225
     // const contact2 = (r2 as C.CRContactConnected).contact
     const r3 = await c.apiSendTextMessage(C.ChatType.CTDirect, contact1.contactId, "hello")
     assert(r3.chatItem.content.type === "sndMsgContent" && r3.chatItem.content.msgContent.text === "hello")
-    for await (const r of c.msgQ) {
-      console.log(r)
-    }
+    const r4 = await c.msgQ.dequeue()
+    assert(isItemSent(r4) || isNewRcvItem(r4))
     await c.disconnect()
-  })
+
+    function isItemSent(r: C.ChatResponse): boolean {
+      return r.type === "chatItemStatusUpdated" && r.chatItem.chatItem.meta.itemStatus.type === "sndSent"
+    }
+
+    function isNewRcvItem(r: C.ChatResponse): boolean {
+      return (
+        r.type === "newChatItem" &&
+        r.chatItem.chatItem.content.type === "rcvMsgContent" &&
+        r.chatItem.chatItem.content.msgContent.text === "hello"
+      )
+    }
+  }, 20000)
 })
