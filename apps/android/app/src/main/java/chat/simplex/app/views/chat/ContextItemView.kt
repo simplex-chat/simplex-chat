665d5fd0c4cb0a94518036a7bebe7837db5b576d
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-05-13 12:38:12 +0400

    remove obsolete imports

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/ContextItemView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/ContextItemView.kt
@@ -11,10 +11,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import chat.simplex.app.R
 import chat.simplex.app.model.CIDirection
 import chat.simplex.app.model.ChatItem
