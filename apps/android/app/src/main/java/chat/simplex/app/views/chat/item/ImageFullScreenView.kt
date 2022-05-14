7ad7f4f91a55cf9f609f5db2825093560d89b4fa
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-05-14 08:07:13 +0100

    mobile: zoomable fullscreen image view (#651)
    * ios: zoomable fullscreen image view
    
    * android: zoomable images

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ImageFullScreenView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ImageFullScreenView.kt
@@ -1,12 +1,14 @@
 import android.graphics.Bitmap
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import chat.simplex.app.R
@@ -20,11 +22,35 @@ fun ImageFullScreenView(imageBitmap: Bitmap, close: () -> Unit) {
       .background(Color.Black)
       .clickable(onClick = close)
   ) {
+    var scale by remember { mutableStateOf(1f) }
+    var translationX by remember { mutableStateOf(0f) }
+    var translationY by remember { mutableStateOf(0f) }
     Image(
       imageBitmap.asImageBitmap(),
       contentDescription = stringResource(R.string.image_descr),
-      modifier = Modifier.fillMaxSize(),
       contentScale = ContentScale.Fit,
+      modifier = Modifier
+        .graphicsLayer(
+          scaleX = scale,
+          scaleY = scale,
+          translationX = translationX,
+          translationY = translationY,
+        )
+        .pointerInput(Unit) {
+          detectTransformGestures(
+            onGesture = { _, pan, gestureZoom, _ ->
+              scale = (scale * gestureZoom).coerceIn(1f, 20f)
+              if (scale > 1) {
+                translationX += pan.x * scale
+                translationY += pan.y * scale
+              } else {
+                translationX = 0f
+                translationY = 0f
+              }
+            }
+          )
+        }
+        .fillMaxSize(),
     )
   }
 }
