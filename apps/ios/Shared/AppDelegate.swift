7e96da95f9743aeb6f115f137af20db68e137517
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-05-30 16:15:17 +0400

    ios: enable notifications (#713)

--- apps/ios/Shared/AppDelegate.swift
+++ apps/ios/Shared/AppDelegate.swift
@@ -21,17 +21,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         logger.debug("AppDelegate: didRegisterForRemoteNotificationsWithDeviceToken \(token)")
         let m = ChatModel.shared
         m.deviceToken = token
-        UserDefaults.standard.set(false, forKey: DEFAULT_USE_NOTIFICATIONS)
-//        let useNotifications = UserDefaults.standard.bool(forKey: "useNotifications")
-//        if useNotifications {
-//            Task {
-//                do {
-//                    m.tokenStatus = try await apiRegisterToken(token: token)
-//                } catch {
-//                    logger.error("apiRegisterToken error: \(responseError(error))")
-//                }
-//            }
-//        }
+        let useNotifications = UserDefaults.standard.bool(forKey: "useNotifications")
+        if useNotifications {
+            Task {
+                do {
+                    m.tokenStatus = try await apiRegisterToken(token: token)
+                } catch {
+                    logger.error("apiRegisterToken error: \(responseError(error))")
+                }
+            }
+        }
     }
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
