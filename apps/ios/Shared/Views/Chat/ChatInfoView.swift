15ddefe86b0577e18aff2082280bb2201e32464d
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-05-31 20:55:19 +0400

    mobile: close modal views (#715)

--- apps/ios/Shared/Views/Chat/ChatInfoView.swift
+++ apps/ios/Shared/Views/Chat/ChatInfoView.swift
@@ -12,7 +12,6 @@ struct ChatInfoView: View {
     @EnvironmentObject var chatModel: ChatModel
     @ObservedObject var alertManager = AlertManager.shared
     @ObservedObject var chat: Chat
-    @Binding var showChatInfo: Bool
     @State var alert: ChatInfoViewAlert? = nil
     @State var deletingContact: Contact?
 
@@ -99,7 +98,7 @@ struct ChatInfoView: View {
                         try await apiDeleteChat(type: .direct, id: contact.apiId)
                         DispatchQueue.main.async {
                             chatModel.removeChat(contact.id)
-                            showChatInfo = false
+                            chatModel.showChatInfo = false
                         }
                     } catch let error {
                         logger.error("ChatInfoView.deleteContactAlert apiDeleteChat error: \(error.localizedDescription)")
@@ -118,7 +117,7 @@ struct ChatInfoView: View {
                 Task {
                     await clearChat(chat)
                     DispatchQueue.main.async {
-                        showChatInfo = false
+                        chatModel.showChatInfo = false
                     }
                 }
             },
@@ -130,6 +129,6 @@ struct ChatInfoView: View {
 struct ChatInfoView_Previews: PreviewProvider {
     static var previews: some View {
         @State var showChatInfo = true
-        return ChatInfoView(chat: Chat(chatInfo: ChatInfo.sampleData.direct, chatItems: []), showChatInfo: $showChatInfo)
+        return ChatInfoView(chat: Chat(chatInfo: ChatInfo.sampleData.direct, chatItems: []))
     }
 }
