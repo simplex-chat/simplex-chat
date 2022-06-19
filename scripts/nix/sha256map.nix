4b140198e111dff5aadf9a4d5c363512c082f3d5
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-06-19 13:22:49 +0100

    update simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."823376fb33e1c8ae774100ee013351186db1f39c" = "1pv6x1f5kx5fh9xgdaq94wxxl1q1grb3zy6xwg3iniksw2ddkdi8";
+  "https://github.com/simplex-chat/simplexmq.git"."31c4088333938d5663a5e78aae37834e15574b9c" = "0pskp5ni8c0w71ck60laq7n2vf3sivaf6lvjbcv7rpkhibm6svqm";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "https://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
