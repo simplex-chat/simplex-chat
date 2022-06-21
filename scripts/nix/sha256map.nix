976b1c919fad34795c3303f997c4536d848227c1
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-06-21 19:27:30 +0100

    revert tls to 1.5.7 and nix config changes (#746)
    * Revert "nix: update nix for new tls version (#744)"
    
    This reverts commit 7df8c23c81758efb1c67cc24691bb99d6b1ee44f.
    
    * Revert "update flake.nix"
    
    This reverts commit 4b263510eef4e4d609b1a9550c71a259ea27e522.
    
    * Revert "update tls"
    
    This reverts commit 2e34ae3b1c535fe1db2157499c217c9a92a1f6b8.
    
    * update simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."31c78e516dbc4fa2ca7cb61dd1838a3ed2028daa" = "19fyq6570x4jmqyznnz4qx3nq6sfcpvil0m9iz9q1mi50nn6c6aj";
+  "https://github.com/simplex-chat/simplexmq.git"."5b61c6bccdce8563bca2041484054a28707d8e50" = "11bsndjrj0s2vwgz2lg6plj1c1hjg2wqrgwqfsy1w0hx19mv4c0m";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "https://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
