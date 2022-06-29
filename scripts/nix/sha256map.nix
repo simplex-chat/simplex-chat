c8a85f651dfd1f3048d5b3db074d52ee37e1b1e5
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-06-29 17:23:42 +0400

    update simplexmq (v3.0.0-beta.0)

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."c9501a23a0626bf441756c4342acf08a5900aad8" = "0rra32hd86yawr6k8vqv86jq97lffyyaqxr2n8gq34vz7c8kmg4d";
+  "https://github.com/simplex-chat/simplexmq.git"."0b25b57e3c561f12914ccff13169388397b815c3" = "1d65dwrfha1lbnkyvjkn3s9f6bhbmz3cdzxkvwq830mcqnr8jb1b";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "https://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
