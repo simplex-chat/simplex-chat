2b764f65927aa7332f661407f30cacf73a2a586b
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-07-03 23:30:52 +0100

    core: use NTF scheme for notification server address

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."4c0164c49effe8d1b3fd98aca184245a8b4cac93" = "1ni3phxal10rfrkgnhcih3ksh7nfnj5b7vkk0qs9k52hmc8g306b";
+  "https://github.com/simplex-chat/simplexmq.git"."d7eff2cdda1d880f16f467053e68501d19889d15" = "1zjbywfswq7bn6ckpliv1z58hl7jx3qv6cv9qbh7168yx0ch29xs";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "https://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
