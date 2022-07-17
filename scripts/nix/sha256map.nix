13fbb66a21303d2bbe2ed545ff9b36c415ec8197
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-07-17 15:51:17 +0100

    core: use batched subscriptions (#818)
    * core: use batched subscriptions
    
    * update simplexmq
    
    * remove comments
    
    * clean up
    
    * refactor
    
    * remove todo
    
    * revert change
    
    * revert change
    
    * remove comment
    
    * add delay to the async group test
    
    * add more delay in test

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."e75846aa38dd26fa70e3faa38ec780edf245e022" = "1mjr5bpjnz6pw9w4qy2r548xlgw89rxbmj36zb4vwq4jghj3gmcz";
+  "https://github.com/simplex-chat/simplexmq.git"."a6f401041ac82c1ba94a8fea21339acb33904ad0" = "1q4wah7mwrbp98gv5z7vvdyyf6sgw4af26b220yimvkjam6l5mx5";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "https://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
