f9c5c612fb7c174921aaa6f8fd1c312a8e889352
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-07-20 20:47:51 +0400

    mobile: auxiliary group items

---
+++ apps/ios/Shared/Views/Chat/ChatItem/CIGroupEventView.swift
@@ -0,0 +1,37 @@
+//
+//  CIGroupEventView.swift
+//  SimpleX (iOS)
+//
+//  Created by JRoberts on 20.07.2022.
+//  Copyright © 2022 SimpleX Chat. All rights reserved.
+//
+
+import SwiftUI
+import SimpleXChat
+
+struct CIGroupEventView: View {
+    var chatItem: ChatItem
+    var showMember = false
+
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            if showMember, let member = chatItem.memberDisplayName {
+                Text(member).fontWeight(.medium) + Text(": ")
+            }
+            Text(chatItem.content.text)
+                .foregroundColor(.secondary)
+                .italic()
+            CIMetaView(chatItem: chatItem)
+                .padding(.horizontal, 12)
+        }
+        .padding(.leading, 12)
+        .padding(.vertical, 6)
+        .textSelection(.disabled)
+    }
+}
+
+struct CIGroupEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        CIGroupEventView(chatItem: ChatItem.getGroupEventSample())
+    }
+}
