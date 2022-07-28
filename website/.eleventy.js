ea9cb272db5547f247d12e0f725c54fee99a2fdf
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-07-28 14:26:13 +0500

    add simplex web files

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,15 +1,19 @@
-const Card = require('./src/_includes/components/Card');
+// const Card = require('./src/_includes/components/Card');
 
 module.exports = function (eleventyConfig) {
     // Keeps the same directory structure.
     eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/contact");
+    eleventyConfig.addPassthroughCopy("src/app-demo");
 
     eleventyConfig.addWatchTarget("src/css");
     eleventyConfig.addWatchTarget("markdown/");
     eleventyConfig.addWatchTarget("components/Card.js");
 
-    eleventyConfig.addShortcode("Card",Card);
+    // eleventyConfig.addShortcode("Card",Card);
 
     return {
         dir: {
