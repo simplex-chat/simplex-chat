a482538738c54f91dd33505a6b25791da7af128e
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-07-30 21:03:13 +0400

    android: add group view image wip

--- apps/android/app/src/main/java/chat/simplex/app/views/newchat/AddGroupView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/newchat/AddGroupView.kt
@@ -1,7 +1,7 @@
 package chat.simplex.app.views.newchat
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import android.graphics.Bitmap
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -19,10 +19,14 @@ import chat.simplex.app.R
 import chat.simplex.app.model.*
 import chat.simplex.app.ui.theme.HighOrLowlight
 import chat.simplex.app.views.ProfileNameField
-import chat.simplex.app.views.helpers.ModalView
-import chat.simplex.app.views.helpers.withApi
+import chat.simplex.app.views.helpers.*
 import chat.simplex.app.views.isValidDisplayName
 import chat.simplex.app.views.onboarding.ReadableText
+import chat.simplex.app.views.usersettings.DeleteImageButton
+import chat.simplex.app.views.usersettings.EditImageButton
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddGroupView(chatModel: ChatModel, close: () -> Unit) {
@@ -43,77 +47,115 @@ fun AddGroupView(chatModel: ChatModel, close: () -> Unit) {
 
 @Composable
 fun AddGroupLayout(createGroup: (GroupProfile) -> Unit, close: () -> Unit) {
-  val image = remember { mutableStateOf(null) }
+  val bottomSheetModalState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+  val scope = rememberCoroutineScope()
   val displayName = remember { mutableStateOf("") }
   val fullName = remember { mutableStateOf("") }
+  val profileImage = remember { mutableStateOf<String?>(null) }
+  val chosenImage = remember { mutableStateOf<Bitmap?>(null) }
   val focusRequester = remember { FocusRequester() }
 
-  ModalView(close) {
-    Surface(Modifier.background(MaterialTheme.colors.onBackground)) {
-      Column(
-        modifier = Modifier.fillMaxSize()
-      ) {
-        Row(
-          Modifier.fillMaxWidth(),
-          horizontalArrangement = Arrangement.Center
-        ) {
-          Text(
-            stringResource(R.string.create_secret_group_title),
-            style = MaterialTheme.typography.h4,
-            modifier = Modifier.padding(vertical = 5.dp)
-          )
-        }
-        ReadableText(R.string.group_is_decentralized)
-        Spacer(Modifier.height(10.dp))
-        Text(
-          stringResource(R.string.group_display_name_field),
-          style = MaterialTheme.typography.h6,
-          modifier = Modifier.padding(bottom = 3.dp)
-        )
-        ProfileNameField(displayName, focusRequester)
-        val errorText = if (!isValidDisplayName(displayName.value)) stringResource(R.string.display_name_cannot_contain_whitespace) else ""
-        Text(
-          errorText,
-          fontSize = 15.sp,
-          color = MaterialTheme.colors.error
-        )
-        Spacer(Modifier.height(3.dp))
-        Text(
-          stringResource(R.string.group_full_name_field),
-          style = MaterialTheme.typography.h6,
-          modifier = Modifier.padding(bottom = 5.dp)
-        )
-        ProfileNameField(fullName)
-
-        Spacer(Modifier.fillMaxHeight().weight(1f))
-        val enabled = displayName.value.isNotEmpty() && isValidDisplayName(displayName.value)
-        val createModifier: Modifier
-        val createColor: Color
-        if (enabled) {
-          val groupProfile = GroupProfile(displayName.value, fullName.value)
-          createModifier = Modifier.clickable { createGroup(groupProfile) }.padding(8.dp)
-          createColor = MaterialTheme.colors.primary
-        } else {
-          createModifier = Modifier.padding(8.dp)
-          createColor = HighOrLowlight
-        }
-        Row(
-          Modifier.fillMaxWidth(),
-          horizontalArrangement = Arrangement.End
-        ) {
-          Surface(shape = RoundedCornerShape(20.dp)) {
+  ProvideWindowInsets(windowInsetsAnimationsEnabled = true) {
+    ModalBottomSheetLayout(
+      scrimColor = Color.Black.copy(alpha = 0.12F),
+      modifier = Modifier.navigationBarsWithImePadding(),
+      sheetContent = {
+        GetImageBottomSheet(
+          chosenImage,
+          onImageChange = { bitmap -> profileImage.value = resizeImageToStrSize(cropToSquare(bitmap), maxDataSize = 12500) },
+          hideBottomSheet = {
+            scope.launch { bottomSheetModalState.hide() }
+          })
+      },
+      sheetState = bottomSheetModalState,
+      sheetShape = RoundedCornerShape(topStart = 18.dp, topEnd = 18.dp)
+    ) {
+      ModalView(close) {
+        Surface(Modifier.background(MaterialTheme.colors.onBackground)) {
+          Column(
+            Modifier
+              .fillMaxSize()
+              .verticalScroll(rememberScrollState())
+          ) {
             Row(
-              createModifier,
-              verticalAlignment = Alignment.CenterVertically,
+              Modifier.fillMaxWidth(),
+              horizontalArrangement = Arrangement.Center
             ) {
-              Text(stringResource(R.string.create_profile_button), style = MaterialTheme.typography.caption, color = createColor)
-              Icon(Icons.Outlined.ArrowForwardIos, stringResource(R.string.create_profile_button), tint = createColor)
+              Text(
+                stringResource(R.string.create_secret_group_title),
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.padding(vertical = 5.dp)
+              )
+            }
+            ReadableText(R.string.group_is_decentralized)
+            Spacer(Modifier.height(10.dp))
+            Box(
+              Modifier
+                .fillMaxWidth()
+                .padding(bottom = 24.dp),
+              contentAlignment = Alignment.Center
+            ) {
+              Box(contentAlignment = Alignment.TopEnd) {
+                Box(contentAlignment = Alignment.Center) {
+                  ProfileImage(192.dp, null)
+                  EditImageButton { scope.launch { bottomSheetModalState.show() } }
+                }
+                if (profileImage.value != null) {
+                  DeleteImageButton { profileImage.value = null }
+                }
+              }
+              Text(
+                stringResource(R.string.group_display_name_field),
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(bottom = 3.dp)
+              )
+              ProfileNameField(displayName, focusRequester)
+              val errorText = if (!isValidDisplayName(displayName.value)) stringResource(R.string.display_name_cannot_contain_whitespace) else ""
+              Text(
+                errorText,
+                fontSize = 15.sp,
+                color = MaterialTheme.colors.error
+              )
+              Spacer(Modifier.height(3.dp))
+              Text(
+                stringResource(R.string.group_full_name_field),
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(bottom = 5.dp)
+              )
+              ProfileNameField(fullName)
+
+              Spacer(Modifier.fillMaxHeight())
+              val enabled = displayName.value.isNotEmpty() && isValidDisplayName(displayName.value)
+              val createModifier: Modifier
+              val createColor: Color
+              if (enabled) {
+                val groupProfile = GroupProfile(displayName.value, fullName.value, profileImage.value)
+                createModifier = Modifier.clickable { createGroup(groupProfile) }.padding(8.dp)
+                createColor = MaterialTheme.colors.primary
+              } else {
+                createModifier = Modifier.padding(8.dp)
+                createColor = HighOrLowlight
+              }
+              Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+              ) {
+                Surface(shape = RoundedCornerShape(20.dp)) {
+                  Row(
+                    createModifier,
+                    verticalAlignment = Alignment.CenterVertically,
+                  ) {
+                    Text(stringResource(R.string.create_profile_button), style = MaterialTheme.typography.caption, color = createColor)
+                    Icon(Icons.Outlined.ArrowForwardIos, stringResource(R.string.create_profile_button), tint = createColor)
+                  }
+                }
+              }
+
+              LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+              }
             }
           }
-        }
-
-        LaunchedEffect(Unit) {
-          focusRequester.requestFocus()
         }
       }
     }
