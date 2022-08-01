c9b2045fb9ca76bdfb3841e20418c707eec00aa4
Avently <7953703+avently@users.noreply.github.com>
2022-08-02 00:15:35 +0300

    Script for downloading and unpacking prebuilt aarch64 libs for Android

---
+++ scripts/android/download_libs_aarch64.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z ${1} ]; then
+    echo "Job repo is unset. Provide it via first argument like: $(realpath $0) https://something.com/job/something"
+    exit 1
+fi
+
+job_repo=$1
+root_dir="$(dirname $(dirname $(dirname $(realpath $0))))"
+output_dir="$root_dir/apps/android/app/src/main/cpp/libs/arm64-v8a/"
+
+mkdir -p "$output_dir" 2> /dev/null
+
+wget -O libsupport.zip $job_repo/simplex-chat-nix-android/aarch64-android:lib:support.x86_64-linux/latest/download/1 && \
+unzip -o libsupport.zip && \
+mv libsupport.so "$output_dir" && \
+rm libsupport.zip
+
+wget -O libsimplex.zip $job_repo/simplex-chat-nix-android/aarch64-android:lib:simplex-chat.x86_64-linux/latest/download/1 && \
+unzip -o libsimplex.zip && \
+mv libsimplex.so "$output_dir" && \
+rm libsimplex.zip
