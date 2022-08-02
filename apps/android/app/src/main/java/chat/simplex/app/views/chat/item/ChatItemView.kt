b596dc9838e63a6010b6bbad58a28069fdc198cb
Avently <7953703+avently@users.noreply.github.com>
2022-08-02 22:06:27 +0300

    Removed gesture interception while long clicking on a chat bubble with a link - allowed to skip motion event consuming based on touch offset - long clicking on a link copies it to a clipboard

--- apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ChatItemView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/chat/item/ChatItemView.kt
@@ -1,6 +1,8 @@
 package chat.simplex.app.views.chat.item
 
-import android.content.Context
+import android.content.*
+import android.content.ClipboardManager
+import android.widget.Toast
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,8 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.UriHandler
+import androidx.compose.ui.platform.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,6 +54,13 @@ fun ChatItemView(
       .fillMaxWidth(),
     contentAlignment = alignment,
   ) {
+    val onLinkLongClick = { link: String ->
+      val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+      val clip: ClipData = ClipData.newPlainText("link", link)
+      clipboard.setPrimaryClip(clip)
+      Toast.makeText(context, context.getString(R.string.copied_to_clipboard, link), Toast.LENGTH_SHORT).show()
+    }
+
     Column(
       Modifier
         .clip(RoundedCornerShape(18.dp))
@@ -62,7 +70,7 @@ fun ChatItemView(
         if (cItem.file == null && cItem.quotedItem == null && isShortEmoji(cItem.content.text)) {
           EmojiItemView(cItem)
         } else {
-          FramedItemView(user, cItem, uriHandler, showMember = showMember, showMenu, receiveFile)
+          FramedItemView(user, cItem, uriHandler, showMember = showMember, showMenu, receiveFile, onLinkLongClick)
         }
         DropdownMenu(
           expanded = showMenu.value,
