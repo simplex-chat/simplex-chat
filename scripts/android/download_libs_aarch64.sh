13a85f0c03a4d350b3462a6761c70cf049d23def
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-08-03 09:09:05 +0100

    set -e

--- scripts/android/download_libs_aarch64.sh
+++ scripts/android/download_libs_aarch64.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 if [ -z ${1} ]; then
     echo "Job repo is unset. Provide it via first argument like: $(realpath $0) https://something.com/job/something"
@@ -9,14 +11,16 @@ job_repo=$1
 root_dir="$(dirname $(dirname $(dirname $(realpath $0))))"
 output_dir="$root_dir/apps/android/app/src/main/cpp/libs/arm64-v8a/"
 
+echo "$output_dir"
+
 mkdir -p "$output_dir" 2> /dev/null
 
-wget -O libsupport.zip $job_repo/simplex-chat-nix-android/aarch64-android:lib:support.x86_64-linux/latest/download/1 && \
-unzip -o libsupport.zip && \
-mv libsupport.so "$output_dir" && \
+wget -O libsupport.zip $job_repo/simplex-chat-nix-android/aarch64-android:lib:support.x86_64-linux/latest/download/1
+unzip -o libsupport.zip
+mv libsupport.so "./$output_dir"
 rm libsupport.zip
 
-wget -O libsimplex.zip $job_repo/simplex-chat-nix-android/aarch64-android:lib:simplex-chat.x86_64-linux/latest/download/1 && \
-unzip -o libsimplex.zip && \
-mv libsimplex.so "$output_dir" && \
+wget -O libsimplex.zip $job_repo/simplex-chat-nix-android/aarch64-android:lib:simplex-chat.x86_64-linux/latest/download/1
+unzip -o libsimplex.zip
+mv libsimplex.so "./$output_dir"
 rm libsimplex.zip
