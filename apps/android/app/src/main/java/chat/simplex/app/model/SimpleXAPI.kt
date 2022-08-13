85e8be347e47fb55d1465e4985084ed3b90282ef
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-08-13 20:05:55 +0100

    Apply suggestions from code review

--- apps/android/app/src/main/java/chat/simplex/app/model/SimpleXAPI.kt
+++ apps/android/app/src/main/java/chat/simplex/app/model/SimpleXAPI.kt
@@ -1279,8 +1279,8 @@ sealed class ChatPagination {
 
   companion object {
     const val INITIAL_COUNT = 100
-    const val PRELOAD_COUNT = 50
-    const val UNTIL_PRELOAD_COUNT = 10
+    const val PRELOAD_COUNT = 100
+    const val UNTIL_PRELOAD_COUNT = 50
   }
 }
 
