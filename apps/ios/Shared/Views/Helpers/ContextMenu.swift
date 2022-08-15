6a40286128aae99b32eb33c08fb1bae7b5aae924
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-08-15 19:19:25 +0100

    move hostview to async block

--- apps/ios/Shared/Views/Helpers/ContextMenu.swift
+++ apps/ios/Shared/Views/Helpers/ContextMenu.swift
@@ -12,7 +12,7 @@ import SwiftUI
 
 extension View {
     func uiKitContextMenu(title: String = "", actions: [UIAction]) -> some View {
-        self.overlay(
+        self.hidden().overlay(
             InteractionView(content: self, menu: UIMenu(title: title, children: actions))
         )
     }
@@ -30,19 +30,20 @@ private struct InteractionView<Content: View>: UIViewRepresentable {
     func makeUIView(context: Context) -> UIView {
         let view = UIView()
         view.backgroundColor = .clear
-        let hostView = UIHostingController(rootView: content)
-        hostView.view.translatesAutoresizingMaskIntoConstraints = false
-        let constraints = [
-            hostView.view.topAnchor.constraint(equalTo: view.topAnchor),
-            hostView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            hostView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            hostView.view.widthAnchor.constraint(equalTo: view.widthAnchor),
-            hostView.view.heightAnchor.constraint(equalTo: view.heightAnchor)
-        ]
-        hostView.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(hostView.view)
-        view.addConstraints(constraints)
+        DispatchQueue.main.async {
+            let hostView = UIHostingController(rootView: content)
+            hostView.view.translatesAutoresizingMaskIntoConstraints = false
+            let constraints = [
+                hostView.view.topAnchor.constraint(equalTo: view.topAnchor),
+                hostView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                hostView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                hostView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                hostView.view.widthAnchor.constraint(equalTo: view.widthAnchor),
+                hostView.view.heightAnchor.constraint(equalTo: view.heightAnchor)
+            ]
+            view.addSubview(hostView.view)
+            view.addConstraints(constraints)
+        }
         let menuInteraction = UIContextMenuInteraction(delegate: context.coordinator)
         view.addInteraction(menuInteraction)
         return view
