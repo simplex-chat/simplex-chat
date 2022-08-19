dcb5e3bdab3d2de745621128686845311780e15f
Avently <7953703+avently@users.noreply.github.com>
2022-08-19 15:57:14 +0300

    Enhancements

--- scripts/android/compress-and-sign-apk.sh
+++ scripts/android/compress-and-sign-apk.sh
@@ -6,15 +6,14 @@ set -e
 level=$1
 apk_parent_dir=$2
 sdk_dir=$3
-build_type=$4
 
-store_file=$5
-store_password=$6
-key_alias=$7
-key_password=$8
+store_file=$4
+store_password=$5
+key_alias=$6
+key_password=$7
 
-if [ -z ${8} ]; then echo "You didn't enter all required params:
-compress-and-sign-apk.sh level apk_parent_dir sdk_dir build_type store_file store_password key_alias key_password"
+if [ -z ${7} ]; then echo "You didn't enter all required params:
+compress-and-sign-apk.sh level apk_parent_dir sdk_dir store_file store_password key_alias key_password"
 fi
 
 cd $apk_parent_dir
@@ -33,13 +32,13 @@ rm $ORIG_NAME
 ALL_TOOLS=($sdk_dir/build-tools/*/)
 BIN_DIR="${ALL_TOOLS[1]}"
 
-$BIN_DIR/zipalign -p -f 4 app-${build_type}.apk app-${build_type}2.apk
+$BIN_DIR/zipalign -p -f 4 $ORIG_NAME $ORIG_NAME-2
 
-mv app-${build_type}{2,}.apk
+mv $ORIG_NAME{-2,}
 
 $BIN_DIR/apksigner sign \
   --ks "$store_file" --ks-key-alias "$key_alias" --ks-pass "pass:$store_password" \
-  --key-pass "pass:$key_password" app-${build_type}.apk
+  --key-pass "pass:$key_password" $ORIG_NAME
 
 # cleanup
 rm -rf apk || true
