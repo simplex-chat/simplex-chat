970c83338585e46b18dab22aefc4bfc4be24146e
MSarmadQadeer <MSarmadQadeer@users.noreply.github.com>
2022-08-19 17:31:11 +0000

    deploy: f108f1ef4940f53370fc2d94b35983fa9d6cbf05

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// console.log(window.location.href)
+// if(window.location.href.includes('comparison')){
+//     console.log('Comparison');
+//     document.querySelector('.comparison-nav-btn').classList.add('nav-button-active');
+// }
+// else if(window.location.href.includes('blog')){
+//     document.querySelector('.blog-nav-btn').classList.add('nav-button-active');
+// }
