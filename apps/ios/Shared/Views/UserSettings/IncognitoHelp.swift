864f9b0836fefbe29e71043e935e6ef810bb1aa9
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-08-22 14:03:51 +0100

    the

--- apps/ios/Shared/Views/UserSettings/IncognitoHelp.swift
+++ apps/ios/Shared/Views/UserSettings/IncognitoHelp.swift
@@ -16,7 +16,7 @@ struct IncognitoHelp: View {
                 .padding(.vertical)
             ScrollView {
                 VStack(alignment: .leading, spacing: 24) {
-                    Text("Incognito mode protects privacy of your main profile name and image — instead, your new random profile is sent to contacts and groups.")
+                    Text("Incognito mode protects the privacy of your main profile name and image — instead, your new random profile is sent to contacts and groups.")
                     Text("It allows having many anonymous connections without any shared data between them in a single chat profile.")
                     Text("To find the profile used for an incognito connection, tap the contact or group name on top of the chat.")
                 }
