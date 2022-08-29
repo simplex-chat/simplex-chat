cf6bb59d01e7032d4664da01f0b892d088095a4d
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-08-29 13:31:26 +0400

    text

--- apps/ios/Shared/Views/UserSettings/IncognitoHelp.swift
+++ apps/ios/Shared/Views/UserSettings/IncognitoHelp.swift
@@ -19,7 +19,7 @@ struct IncognitoHelp: View {
                     Group {
                     Text("Incognito mode protects the privacy of your main profile name and image — for each new contact a new random profile is created.")
                     Text("It allows having many anonymous connections without any shared data between them in a single chat profile.")
-                    Text("When you shared an incognito profile with somebody, this profile will be used for the groups they invite you to.")
+                    Text("When you share an incognito profile with somebody, this profile will be used for the groups they invite you to.")
                     Text("To find the profile used for an incognito connection, tap the contact or group name on top of the chat.")
                     }
                     .padding(.bottom)
