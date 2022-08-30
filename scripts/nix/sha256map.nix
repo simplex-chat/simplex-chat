6233d61010773759b54ad8cd3eb03a6178eebc8c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-08-30 12:47:24 +0100

    update dependencies

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,7 +1,7 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."96b0a5b467081ec9c93aebc05d2e3ab7954c98a5" = "06fpz25i6186ix05v1jzvajcbk3xafsnn5ff4wzskclm8v42438j";
-  "https://github.com/simplex-chat/direct-sqlcipher.git"."355647880bede89afdeb5eb1ae237314db04a56a" = "1xiqid1344mwh3wnrczn6rxf59hml5g7kifah7skpd9javj4bb7s";
-  "https://github.com/simplex-chat/sqlcipher-simple.git"."cadb4d3ee4fa6ab134d432b646035d0199ffa4fb" = "18xcv31zjrfv5pzqag5pksn7c8zigvc4s9q5iisj5idl4c3mq9cc";
+  "https://github.com/simplex-chat/simplexmq.git"."e4b77ed9e68373e2bad48a7c825db3860a6ad4d6" = "07p8g0a0pl61wrai2jyn311ys238s9kl1i98kpxsjifqif1h9wc1";
+  "https://github.com/simplex-chat/direct-sqlcipher.git"."477955063df65a2776c2a958b656ff359b76374d" = "1xiqid1344mwh3wnrczn6rxf59hml5g7kifah7skpd9javj4bb7s";
+  "https://github.com/simplex-chat/sqlcipher-simple.git"."0738c7957e971b84a2a156d297596206b948c4f6" = "0lysvzx2qzjcxka9w5cb0bnzym3nrqh7r7q5dw9h6g46vybc5lyc";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
   "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
   "https://github.com/zw3rk/android-support.git"."3c3a5ab0b8b137a072c98d3d0937cbdc96918ddb" = "1r6jyxbim3dsvrmakqfyxbd6ms6miaghpbwyl0sr6dzwpgaprz97";
