7d1233adb1d508b5f34e6a182d436e370dc58c11
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-09-04 18:18:38 +0500

    update 11ty config

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/contact");
     eleventyConfig.addPassthroughCopy("src/app-demo");
+    eleventyConfig.addPassthroughCopy("src/blog/images");
 
     eleventyConfig.addCollection('blogs', function (collection) {
         return collection.getFilteredByGlob('src/blog/*.md').reverse();
