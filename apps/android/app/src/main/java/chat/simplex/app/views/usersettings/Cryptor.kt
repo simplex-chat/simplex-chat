b9fcdc0bb8ecc4c7145c0a288b96ccae6252539f
Avently <7953703+avently@users.noreply.github.com>
2022-09-05 23:40:48 +0300

    Don't regenerate key if it exists

--- apps/android/app/src/main/java/chat/simplex/app/views/usersettings/Cryptor.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/usersettings/Cryptor.kt
@@ -31,6 +31,8 @@ internal class Cryptor {
   }
 
   private fun createSecretKey(alias: String): SecretKey {
+    if (keyStore.containsAlias(alias))
+      return (keyStore.getEntry(alias, null) as KeyStore.SecretKeyEntry).secretKey
     val keyGenerator: KeyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM, "AndroidKeyStore")
     keyGenerator.init(
       KeyGenParameterSpec.Builder(alias, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
