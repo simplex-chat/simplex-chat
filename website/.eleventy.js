1811c7a83ec21de5b05d1c7ae1860fec2542df10
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-09-05 13:25:14 +0500

    resolve issue of special characters in header ids introduce slugify

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,35 +1,39 @@
 module.exports = function (eleventyConfig) {
-    // Keeps the same directory structure.
-    eleventyConfig.addPassthroughCopy("src/assets/");
-    eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/js");
-    eleventyConfig.addPassthroughCopy("src/contact");
-    eleventyConfig.addPassthroughCopy("src/app-demo");
-    eleventyConfig.addPassthroughCopy("src/blog/images");
+  // Keeps the same directory structure.
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/contact");
+  eleventyConfig.addPassthroughCopy("src/app-demo");
+  eleventyConfig.addPassthroughCopy("src/blog/images");
 
-    eleventyConfig.addCollection('blogs', function (collection) {
-        return collection.getFilteredByGlob('src/blog/*.md').reverse();
-    });
+  eleventyConfig.addCollection('blogs', function (collection) {
+    return collection.getFilteredByGlob('src/blog/*.md').reverse();
+  });
 
-    eleventyConfig.addWatchTarget("src/css");
-    eleventyConfig.addWatchTarget("markdown/");
-    eleventyConfig.addWatchTarget("components/Card.js");
+  eleventyConfig.addWatchTarget("src/css");
+  eleventyConfig.addWatchTarget("markdown/");
+  eleventyConfig.addWatchTarget("components/Card.js");
 
-    // this works because Eleventy also installs markdown-it 
-    const markdownIt = require("markdown-it");
+  const markdownIt = require("markdown-it");
+  const markdownItAnchor = require("markdown-it-anchor");
+  const slugify = require("slugify");
 
-    // create a new markdown-it instance with the plugin
-    const markdownItAnchor = require("markdown-it-anchor");
+  const markdownLib = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    slugify: (str) =>
+      slugify(str, {
+        lower: true,
+        strict: true,
+      })
+  });
 
-    const markdownLib = markdownIt({
-        html: true,
-        breaks: true,
-        linkify: true
-    }).use(markdownItAnchor);
-
-    // replace the default markdown-it instance
-    eleventyConfig.setLibrary("md", markdownLib);
+  // replace the default markdown-it instance
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     dir: {
