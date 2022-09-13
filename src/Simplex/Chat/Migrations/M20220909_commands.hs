8889d5feda90c6d6331104ecb8f2418df6d73da9
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-09-13 13:17:54 +0400

    clean up schema

--- src/Simplex/Chat/Migrations/M20220909_commands.hs
+++ src/Simplex/Chat/Migrations/M20220909_commands.hs
@@ -21,20 +21,4 @@ CREATE TABLE commands (
 ALTER TABLE msg_deliveries ADD COLUMN agent_ack_cmd_id INTEGER; -- correlation id
 
 ALTER TABLE connections ADD COLUMN conn_req_inv BLOB;
-
-CREATE TABLE intros_for_group_members (
-  intro_for_group_member_id INTEGER PRIMARY KEY,
-  group_id INTEGER DEFAULT NULL REFERENCES groups ON DELETE CASCADE, -- required for saving group_id of snd message when sending XGrpMemInv
-  group_member_id INTEGER NOT NULL REFERENCES group_members(group_member_id) ON DELETE CASCADE, -- required for sending in XGrpMemInv
-  host_connection_id INTEGER NOT NULL REFERENCES connections ON DELETE CASCADE, -- required for XGrpMemInv back to host
-  direct_connection_id INTEGER NOT NULL REFERENCES connections ON DELETE CASCADE,
-  direct_conn_req BLOB, -- initially NULL, populated on INV agent response in direct connection
-  group_connection_id INTEGER NOT NULL REFERENCES connections ON DELETE CASCADE,
-  group_conn_req BLOB, -- initially NULL, populated on INV agent response in direct connection
-  user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
-  created_at TEXT NOT NULL DEFAULT (datetime('now')),
-  updated_at TEXT NOT NULL DEFAULT (datetime('now')),
-  UNIQUE(direct_connection_id),
-  UNIQUE(group_connection_id)
-);
 |]
