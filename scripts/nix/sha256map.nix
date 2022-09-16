3351503744c0cced81f343c9a1e7084e9d3f540c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-09-17 00:30:35 +0100

    update simplexmq (fix stopping agent)

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."959ae34f16fe8df89022bade32bb876abb92aaed" = "05b1ql3bv0l0ydi0c1l33qkiv609gm6bkxkf2ykf4gs77gibsgha";
+  "https://github.com/simplex-chat/simplexmq.git"."13a9eee0cfede5056a7dfd990609afb09840d02f" = "1jb0hylyzh1ml4kvc76amaj8akrqy7yxcshbm2zif31sxphpm80i";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."34309410eb2069b029b8fc1872deb1e0db123294" = "0kwkmhyfsn2lixdlgl15smgr1h5gjk7fky6abzh8rng2h5ymnffd";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."5e154a2aeccc33ead6c243ec07195ab673137221" = "1d1gc5wax4vqg0801ajsmx1sbwvd9y7p7b8mmskvqsmpbwgbh0m0";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
