31e4d1e5a3550a41ec579ab734905b06a0b89f87
Avently <7953703+avently@users.noreply.github.com>
2022-09-21 01:45:55 +0300

    Unused code

--- apps/android/app/src/main/java/chat/simplex/app/views/helpers/RecAndPlay.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/helpers/RecAndPlay.kt
@@ -7,11 +7,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import chat.simplex.app.SimplexApp
 import chat.simplex.app.TAG
-import cn.org.hentai.acodec.AudioCodec
-import cn.org.hentai.acodec.CodecFactory
-import kotlinx.coroutines.*
 import java.io.*
-import java.nio.ByteBuffer
 
 interface Recorder {
   fun start(recordingInProgress: MutableState<Boolean>): String
@@ -56,89 +52,6 @@ class RecorderNative: Recorder {
   override fun cancel(filePath: String, recordingInProgress: MutableState<Boolean>) {
     stop(recordingInProgress)
     runCatching { File(filePath).delete() }.getOrElse { Log.d(TAG, "Unable to delete a file: ${it.stackTraceToString()}") }
-  }
-}
-
-class RecorderExternal: Recorder {
-  companion object {
-    private const val SAMPLE_RATE = 8000
-    private const val ENCODING = AudioFormat.ENCODING_PCM_16BIT
-    private val BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, ENCODING) * 2
-  }
-
-  private val recorder = AudioRecord(
-    MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, ENCODING, BUFFER_SIZE
-  )
-  private var job: Job? = null
-
-  override fun start(recordingInProgress: MutableState<Boolean>): String {
-    recordingInProgress.value = true
-    val filePath = getAppFilePath(SimplexApp.context, uniqueCombine(SimplexApp.context, getAppFilePath(SimplexApp.context, "voice.pcm")))
-    recorder.startRecording()
-    job = CoroutineScope(Dispatchers.Default).launch {
-      val file = File(filePath)
-      val buffer: ByteBuffer = ByteBuffer.allocateDirect(BUFFER_SIZE)
-
-
-      try {
-        withContext(Dispatchers.IO) {
-          FileOutputStream(file).use { outStream ->
-            while (recordingInProgress.value) {
-              val result: Int = recorder.read(buffer, BUFFER_SIZE)
-              if (result < 0) {
-                Log.d(TAG, "Can't read audio buffer: " + errorCodeToString(result))
-                break
-              }
-              outStream.write(buffer.array(), 0, BUFFER_SIZE)
-              buffer.clear()
-            }
-            encode(filePath)
-          }
-        }
-      } catch (e: IOException) {
-        throw RuntimeException("Can't write audio buffer", e)
-      }
-    }
-    return filePath
-  }
-
-  override fun stop(recordingInProgress: MutableState<Boolean>) {
-    runCatching {
-      recorder.stop()
-    }
-    recordingInProgress.value = false
-  }
-
-  override fun cancel(filePath: String, recordingInProgress: MutableState<Boolean>) {
-    stop(recordingInProgress)
-    runCatching { File(filePath).delete() }.getOrElse { Log.d(TAG, "Unable to delete a file: ${it.stackTraceToString()}") }
-  }
-
-  private fun encode(filePath: String) {
-    val codec: AudioCodec = CodecFactory.getCodec("g711a")
-    val pcmData: ByteArray = File(filePath).readBytes()
-    val audio = AudioTrack(
-      AudioManager.STREAM_MUSIC,
-      SAMPLE_RATE,
-      AudioFormat.CHANNEL_OUT_MONO,
-      AudioFormat.ENCODING_PCM_16BIT,
-      BUFFER_SIZE,
-      AudioTrack.MODE_STREAM
-    )
-    audio.play()
-    audio.write(pcmData, 0, pcmData.size)
-    val res = codec.fromPCM(pcmData)
-    File(filePath.replace(".pcm", ".g711pcm")).outputStream().use { it.write(res) }
-  }
-
-  private fun errorCodeToString(errorCode: Int): String {
-    return when (errorCode) {
-      AudioRecord.ERROR_INVALID_OPERATION -> "ERROR_INVALID_OPERATION"
-      AudioRecord.ERROR_BAD_VALUE -> "ERROR_BAD_VALUE"
-      AudioRecord.ERROR_DEAD_OBJECT -> "ERROR_DEAD_OBJECT"
-      AudioRecord.ERROR -> "ERROR"
-      else -> "Unknown ($errorCode)"
-    }
   }
 }
 
