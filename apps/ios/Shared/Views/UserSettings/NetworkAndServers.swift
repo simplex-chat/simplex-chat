b79cec5c8517f0fa5ae01a314141411394b4b53d
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-09-21 10:05:42 +0100

    update servers

--- apps/ios/Shared/Views/UserSettings/NetworkAndServers.swift
+++ apps/ios/Shared/Views/UserSettings/NetworkAndServers.swift
@@ -37,14 +37,7 @@ struct NetworkAndServers: View {
                         SMPServers()
                             .navigationTitle("Your SMP servers")
                     } label: {
-                        settingsRow("server.rack") { Text("SMP servers") }
-                    }
-
-                    NavigationLink {
-                        RTCServers()
-                            .navigationTitle("Your ICE servers")
-                    } label: {
-                        settingsRow("dot.radiowaves.left.and.right") { Text("WebRTC ICE servers") }
+                        Text("SMP servers")
                     }
 
                     Picker("Use .onion hosts", selection: $onionHosts) {
@@ -56,13 +49,22 @@ struct NetworkAndServers: View {
                             AdvancedNetworkSettings()
                                 .navigationTitle("Network settings")
                         } label: {
-                            settingsRow("app.connected.to.app.below.fill") { Text("Advanced network settings") }
+                            Text("Advanced network settings")
                         }
                     }
                 } header: {
-                    Text("")
+                    Text("Messages")
                 } footer: {
                     Text("Using .onion hosts requires compatible VPN provider.")
+                }
+
+                Section("Calls") {
+                    NavigationLink {
+                        RTCServers()
+                            .navigationTitle("Your ICE servers")
+                    } label: {
+                        Text("WebRTC ICE servers")
+                    }
                 }
             }
         }
