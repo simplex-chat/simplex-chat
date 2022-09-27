e4fe80b10174386dfb9993b0735750a94020812a
Avently <7953703+avently@users.noreply.github.com>
2022-09-27 15:09:21 +0300

    android: Playing call sound in different stream type

--- apps/android/app/src/main/java/chat/simplex/app/views/call/SoundPlayer.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/call/SoundPlayer.kt
@@ -1,27 +1,36 @@
 package chat.simplex.app.views.call
 
 import android.content.Context
-import android.media.MediaPlayer
+import android.media.*
+import android.net.Uri
 import android.os.VibrationEffect
 import android.os.Vibrator
 import androidx.core.content.ContextCompat
 import chat.simplex.app.R
+import chat.simplex.app.SimplexApp
 import chat.simplex.app.views.helpers.withScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 
 class SoundPlayer {
-  var player: MediaPlayer? = null
+  private var player: MediaPlayer = MediaPlayer().apply {
+    setAudioAttributes(
+      AudioAttributes.Builder()
+        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+        .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+        .build()
+    )
+    setDataSource(SimplexApp.context, Uri.parse("android.resource://" + SimplexApp.context.packageName + "/" + R.raw.ring_once))
+  }
   var playing = false
 
   fun start(cxt: Context, scope: CoroutineScope, sound: Boolean) {
-    if (sound) player = MediaPlayer.create(cxt, R.raw.ring_once)
     val vibrator = ContextCompat.getSystemService(cxt, Vibrator::class.java)
     val effect = VibrationEffect.createOneShot(250, VibrationEffect.DEFAULT_AMPLITUDE)
     playing = true
     withScope(scope) {
       while (playing) {
-        if (sound) player?.start()
+        if (sound) player.start()
         vibrator?.vibrate(effect)
         delay(3500)
       }
@@ -30,7 +39,7 @@ class SoundPlayer {
 
   fun stop() {
     playing = false
-    player?.stop()
+    player.stop()
   }
 
   companion object {
