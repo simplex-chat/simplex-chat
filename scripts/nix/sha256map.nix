248d373fb4d6851ef8cab6c4ba430efb4c02912b
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-10-04 14:41:21 +0100

    update simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."fbddc7cc520f7297c5a0fc634732f226379cc2b9" = "116np2msr1pvr5x132aqm5hi5vr2d0gw7y9gfp8q5bi7fb2rdzkh";
+  "https://github.com/simplex-chat/simplexmq.git"."0fa40fbfb0d578499679a2a5f21d7d9d2da20ee3" = "116np2msr1pvr5x132aqm5hi5vr2d0gw7y9gfp8q5bi7fb2rdzkh";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."34309410eb2069b029b8fc1872deb1e0db123294" = "0kwkmhyfsn2lixdlgl15smgr1h5gjk7fky6abzh8rng2h5ymnffd";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."5e154a2aeccc33ead6c243ec07195ab673137221" = "1d1gc5wax4vqg0801ajsmx1sbwvd9y7p7b8mmskvqsmpbwgbh0m0";
   "https://github.com/simplex-chat/aeson.git"."3eb66f9a68f103b5f1489382aad89f5712a64db7" = "0kilkx59fl6c3qy3kjczqvm8c3f4n3p0bdk9biyflf51ljnzp4yp";
