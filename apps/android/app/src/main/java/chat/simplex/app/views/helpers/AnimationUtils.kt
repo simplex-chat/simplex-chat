bca9ff808b765434e788859e65aa8e4b29c53329
Avently <7953703+avently@users.noreply.github.com>
2022-10-06 19:02:52 +0300

    Faster animation

--- apps/android/app/src/main/java/chat/simplex/app/views/helpers/AnimationUtils.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/helpers/AnimationUtils.kt
@@ -3,4 +3,4 @@ package chat.simplex.app.views.helpers
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 
-fun <T> newChatSheetAnimSpec(delayMillis: Int = 0) = tween<T>(200, delayMillis, LinearEasing)
+fun <T> newChatSheetAnimSpec(delayMillis: Int = 0) = tween<T>(100, delayMillis, LinearEasing)
