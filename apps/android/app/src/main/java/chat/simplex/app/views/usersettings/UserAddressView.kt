6a13f46bfa1547ceb623f9d9a81f1d423554e6f0
Avently <7953703+avently@users.noreply.github.com>
2022-10-24 13:43:05 +0300

    android: Settings to auto-accept contact requests

--- apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserAddressView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserAddressView.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,8 +18,7 @@ import androidx.compose.ui.unit.sp
 import chat.simplex.app.R
 import chat.simplex.app.model.ChatModel
 import chat.simplex.app.model.UserContactLinkRec
-import chat.simplex.app.ui.theme.SimpleButton
-import chat.simplex.app.ui.theme.SimpleXTheme
+import chat.simplex.app.ui.theme.*
 import chat.simplex.app.views.helpers.*
 import chat.simplex.app.views.newchat.QRCode
 
@@ -26,7 +26,7 @@ import chat.simplex.app.views.newchat.QRCode
 fun UserAddressView(chatModel: ChatModel) {
   val cxt = LocalContext.current
   UserAddressLayout(
-    userAddress = chatModel.userAddress.value,
+    userAddress = remember { chatModel.userAddress }.value,
     createAddress = {
       withApi {
         val connReqContact = chatModel.controller.apiCreateUserAddress()
@@ -36,6 +36,11 @@ fun UserAddressView(chatModel: ChatModel) {
       }
     },
     share = { userAddress: String -> shareText(cxt, userAddress) },
+    acceptRequests = {
+      chatModel.userAddress.value?.let { address ->
+        ModalManager.shared.showModal(settings = true) { AcceptRequestsView(chatModel, address) }
+      }
+    },
     deleteAddress = {
       AlertManager.shared.showAlertMsg(
         title = generalGetString(R.string.delete_address__question),
@@ -57,6 +62,7 @@ fun UserAddressLayout(
   userAddress: UserContactLinkRec?,
   createAddress: () -> Unit,
   share: (String) -> Unit,
+  acceptRequests: () -> Unit,
   deleteAddress: () -> Unit
 ) {
   Column(
@@ -97,13 +103,18 @@ fun UserAddressLayout(
             stringResource(R.string.share_link),
             icon = Icons.Outlined.Share,
             click = { share(userAddress.connReqContact) })
-          SimpleButton(
-            stringResource(R.string.delete_address),
-            icon = Icons.Outlined.Delete,
-            color = Color.Red,
-            click = deleteAddress
+          SimpleButtonIconEnded(
+            stringResource(R.string.accept_requests),
+            icon = Icons.Outlined.ChevronRight,
+            click = acceptRequests
           )
         }
+        SimpleButton(
+          stringResource(R.string.delete_address),
+          icon = Icons.Outlined.Delete,
+          color = Color.Red,
+          click = deleteAddress
+        )
       }
     }
   }
@@ -122,6 +133,7 @@ fun PreviewUserAddressLayoutNoAddress() {
       userAddress = null,
       createAddress = {},
       share = { _ -> },
+      acceptRequests = {},
       deleteAddress = {},
     )
   }
@@ -140,6 +152,7 @@ fun PreviewUserAddressLayoutAddressCreated() {
       userAddress = UserContactLinkRec("https://simplex.chat/contact#/?v=1&smp=smp%3A%2F%2FPQUV2eL0t7OStZOoAsPEV2QYWt4-xilbakvGUGOItUo%3D%40smp6.simplex.im%2FK1rslx-m5bpXVIdMZg9NLUZ_8JBm8xTt%23MCowBQYDK2VuAyEALDeVe-sG8mRY22LsXlPgiwTNs9dbiLrNuA7f3ZMAJ2w%3D"),
       createAddress = {},
       share = { _ -> },
+      acceptRequests = {},
       deleteAddress = {},
     )
   }
