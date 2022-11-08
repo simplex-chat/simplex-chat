ead56a573eff2772130731ec64e648a06b992979
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-11-08 16:36:01 +0500

    refactoring

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -1,3 +1,131 @@
+// switch theme
+var navBar = document.querySelector("nav");
+var landingPageHeader = document.querySelector('.landing-page-header');
+
+const sunIcon = document.querySelector('.sun');
+const moonIcon = document.querySelector('.moon');
+
+const userTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const iconToggle = () => {
+    sunIcon.classList.toggle('hidden');
+    moonIcon.classList.toggle('hidden');
+}
+
+const themeCheck = () => {
+    if(userTheme === 'dark' || (!userTheme && systemTheme)){
+        document.documentElement.classList.add('dark');
+        moonIcon.classList.add('hidden');
+    }
+    else{
+        sunIcon.classList.add('hidden');
+    }
+}
+
+const themeSwitch = () => {
+    if(document.documentElement.classList.contains('dark')){
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme','light');
+        iconToggle();
+    }
+    else{
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme','dark');
+        iconToggle();
+    }
+}
+
+const nav = document.querySelector('header nav');
+window.addEventListener('click',(e)=>{
+    if(e.target.closest('.nav-link')){
+        if(e.target.closest('.nav-link').classList.contains('active')){
+            e.target.closest('.nav-link').classList.remove('active');
+        }
+        else{
+            document.querySelectorAll('.nav-link').forEach(el => el.classList.remove('active'))
+            e.target.closest('.nav-link').classList.add('active');
+        }
+    }
+    else if(e.target.closest('.nav-toggle-btn')){
+        document.body.classList.toggle('lock-scroll');
+        if(nav.classList.contains('open')){
+            nav.classList.remove('open');
+            document.getElementById('hamburger').classList.remove('hidden');
+            document.getElementById('cross').classList.add('hidden');
+        }
+        else{
+            nav.classList.add('open');
+            document.getElementById('hamburger').classList.add('hidden');
+            document.getElementById('cross').classList.remove('hidden');
+        }
+    }
+    else if(e.target.closest('.theme-switch-btn')){
+        themeSwitch();
+    }
+})
+
+themeCheck();
+
+const changeHeaderBg = ()=>{
+    const header = document.querySelector('header')
+    const scrollValue = window.scrollY
+    if(scrollValue > 5){
+        header.classList.add('bg-primary-bg-light');
+        header.classList.add('dark:bg-primary-bg-dark');
+    }
+    else{
+        header.classList.remove('bg-primary-bg-light');
+        header.classList.remove('dark:bg-primary-bg-dark');
+    }
+}
+
+window.addEventListener('scroll',changeHeaderBg);
+
+
+
+window.addEventListener('load', function() {
+    window.addEventListener('click',e=>{
+        if(e.target.closest('.open-card-btn') || e.target.closest('.close-card-btn')){
+            e.target.closest('.simplex-unique-card').classList.toggle('active-card');
+            e.target.closest('.hide-show-btn').querySelector('.open-card-btn').classList.toggle('hidden');
+            e.target.closest('.hide-show-btn').querySelector('.open-card-btn').classList.toggle('flex');
+            e.target.closest('.hide-show-btn').querySelector('.close-card-btn').classList.toggle('hidden');
+            e.target.closest('.hide-show-btn').querySelector('.close-card-btn').classList.toggle('flex');
+        }
+    })
+});
+
+window.addEventListener("load", function () {
+    const simplexExplainedSwiper__bullets = document.querySelectorAll(".simplex-explained-swiper .swiper-pagination > span");
+    const simplexExplainedSwiper__tabs = document.querySelectorAll("#simplex-explained .tabs .tab-button");
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === "attributes" && mutation.attributeName === "class" && mutation.target.classList.contains("swiper-pagination")) {
+                simplexExplainedSwiper__bullets.forEach((el,index) => {
+                    if (el.classList.contains("swiper-pagination-bullet-active")) {
+                        simplexExplainedSwiper__tabs[index].classList.add("active");
+                    } else {
+                        simplexExplainedSwiper__tabs[index].classList.remove("active");
+                    }
+                });
+            }
+        });
+    }); 
+
+    const targetNode = document.querySelector(".simplex-explained-swiper .swiper-pagination");
+    observer.observe(targetNode, { attributes: true });
+
+    window.addEventListener('click',e=>{
+        if(e.target.closest("#simplex-explained .tabs .tab-button")){
+            const index = e.target.closest("#simplex-explained .tabs .tab-button").dataset.btnIndex;
+            simplexExplainedSwiper__bullets[index].click();
+        }
+    })
+});
+
+
 const uniqueSwiper = new Swiper('.unique-swiper', {
     slidesPerView: 1,
     spaceBetween: 80,
