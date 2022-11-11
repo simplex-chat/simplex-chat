c8b2bcb06459ee023430ff7f5a5a2a2c31bb742f
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2022-11-11 16:33:45 +0300

    android: Fix of StackoverflowError (#1322)
    * android: Fix of StackoverflowError
    
    * Break
    
    * Comment
    
    * Update apps/android/app/src/main/java/chat/simplex/app/model/SimpleXAPI.kt
    
    * Revert "Update apps/android/app/src/main/java/chat/simplex/app/model/SimpleXAPI.kt"
    
    This reverts commit ea8015e01d7e339042f5062e9db3420142c716c2.
    
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- apps/android/app/src/main/java/chat/simplex/app/model/SimpleXAPI.kt
+++ apps/android/app/src/main/java/chat/simplex/app/model/SimpleXAPI.kt
@@ -264,8 +264,20 @@ open class ChatController(var ctrl: ChatCtrl?, val ntfManager: NtfManager, val a
   private fun startReceiver() {
     Log.d(TAG, "ChatController startReceiver")
     if (receiverStarted) return
-    thread(name="receiver") {
-      GlobalScope.launch { withContext(Dispatchers.IO) { recvMspLoop() } }
+    receiverStarted = true
+    CoroutineScope(Dispatchers.IO).launch {
+      while (true) {
+        /** Global [ctrl] can be null. It's needed for having the same [ChatModel] that already made in [ChatController] without the need
+         * to change it everywhere in code after changing a database.
+         * Since it can be changed in background thread, making this check to prevent NullPointerException */
+        val ctrl = ctrl
+        if (ctrl == null) {
+          receiverStarted = false
+          break
+        }
+        val msg = recvMsg(ctrl)
+        if (msg != null) processReceivedMsg(msg)
+      }
     }
   }
 
@@ -291,24 +303,16 @@ open class ChatController(var ctrl: ChatCtrl?, val ntfManager: NtfManager, val a
     }
   }
 
-  private suspend fun recvMsg(ctrl: ChatCtrl): CR? {
-    return withContext(Dispatchers.IO) {
-      val json = chatRecvMsgWait(ctrl, MESSAGE_TIMEOUT)
-      if (json == "") {
-        null
-      } else {
-        val r = APIResponse.decodeStr(json).resp
-        Log.d(TAG, "chatRecvMsg: ${r.responseType}")
-        if (r is CR.Response || r is CR.Invalid) Log.d(TAG, "chatRecvMsg json: $json")
-        r
-      }
+  private fun recvMsg(ctrl: ChatCtrl): CR? {
+    val json = chatRecvMsgWait(ctrl, MESSAGE_TIMEOUT)
+    return if (json == "") {
+      null
+    } else {
+      val r = APIResponse.decodeStr(json).resp
+      Log.d(TAG, "chatRecvMsg: ${r.responseType}")
+      if (r is CR.Response || r is CR.Invalid) Log.d(TAG, "chatRecvMsg json: $json")
+      r
     }
-  }
-
-  private suspend fun recvMspLoop() {
-    val msg = recvMsg(ctrl ?: return)
-    if (msg != null) processReceivedMsg(msg)
-    recvMspLoop()
   }
 
   suspend fun apiGetActiveUser(): User? {
