2a1e90ba64883a6490c7d561c37a601b1614753d
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-11-11 11:37:18 +0400

    defensive checks

--- src/Simplex/Chat.hs
+++ src/Simplex/Chat.hs
@@ -2481,15 +2481,16 @@ processAgentMessage (Just user@User {userId}) corrId agentConnId agentMessage =
         forM_ r . uncurry $ probeMatch c1
 
     probeMatch :: Contact -> Contact -> Probe -> m ()
-    probeMatch c1@Contact {profile = p1} c2@Contact {profile = p2} probe =
+    probeMatch c1@Contact {contactId = ctId1, profile = p1} c2@Contact {contactId = ctId2, profile = p2} probe =
       when (fromLocalProfile p1 == fromLocalProfile p2) $ do
         void . sendDirectContactMessage c1 $ XInfoProbeOk probe
-        mergeContacts c1 c2
+        unless (ctId1 == ctId2) $ mergeContacts c1 c2
 
     xInfoProbeOk :: Contact -> Probe -> m ()
-    xInfoProbeOk c1 probe = do
+    xInfoProbeOk c1@Contact {contactId = ctId1} probe = do
       r <- withStore' $ \db -> matchSentProbe db userId c1 probe
-      forM_ r $ \c2 -> mergeContacts c1 c2
+      forM_ r $ \c2@Contact {contactId = ctId2} ->
+        unless (ctId1 == ctId2) $ mergeContacts c1 c2
 
     -- to party accepting call
     xCallInv :: Contact -> CallId -> CallInvitation -> RcvMessage -> MsgMeta -> m ()
