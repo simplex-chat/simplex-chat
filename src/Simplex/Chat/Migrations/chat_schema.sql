e14ab0fed0a9d40090ca248288bbbcc73a9f4831
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-11-14 08:04:11 +0000

    core: support SMP basic auth / server password (#1358)

--- src/Simplex/Chat/Migrations/chat_schema.sql
+++ src/Simplex/Chat/Migrations/chat_schema.sql
@@ -384,6 +384,7 @@ CREATE TABLE smp_servers(
   user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
   created_at TEXT NOT NULL DEFAULT(datetime('now')),
   updated_at TEXT NOT NULL DEFAULT(datetime('now')),
+  basic_auth TEXT,
   UNIQUE(host, port)
 );
 CREATE INDEX idx_messages_shared_msg_id ON messages(shared_msg_id);
