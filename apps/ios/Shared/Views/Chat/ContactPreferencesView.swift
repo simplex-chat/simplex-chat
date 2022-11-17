756a60f12645261186dcd286d4a6040d8fe2fe21
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-11-17 11:43:01 +0400

    pref disabled color

--- apps/ios/Shared/Views/Chat/ContactPreferencesView.swift
+++ apps/ios/Shared/Views/Chat/ContactPreferencesView.swift
@@ -48,7 +48,7 @@ struct ContactPreferencesView: View {
         } header: {
             HStack {
                 Image(systemName: "\(feature.icon).fill")
-                    .foregroundColor(enabled.forUser ? .green : enabled.forContact ? .yellow : .red)
+                    .foregroundColor(enabled.forUser ? .green : enabled.forContact ? .yellow : .secondary)
                 Text(feature.text)
             }
         } footer: {
