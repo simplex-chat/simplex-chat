029df78f08b1a0c3dd84737acbf41e2ab3da7fa9
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-11-29 15:48:49 +0000

    remove call notification category

--- apps/android/app/src/main/java/chat/simplex/app/model/NtfManager.kt
+++ apps/android/app/src/main/java/chat/simplex/app/model/NtfManager.kt
@@ -174,7 +174,6 @@ class NtfManager(val context: Context, private val appPreferences: AppPreference
         NotificationCompat.Builder(context, LockScreenCallChannel)
           .setFullScreenIntent(fullScreenPendingIntent, true)
           .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-          .setCategory(NotificationCompat.CATEGORY_CALL)
           .setSilent(false)
       } else {
         val soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.packageName + "/" + R.raw.ring_once)
