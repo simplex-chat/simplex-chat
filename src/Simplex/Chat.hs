91a39cae23ca883766e00abbb08562330fcf5462
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-01-16 17:25:06 +0000

    core: fix error handling (#1761)
    * core: fix error handling
    
    * fix tests

--- src/Simplex/Chat.hs
+++ src/Simplex/Chat.hs
@@ -254,11 +254,11 @@ stopChatController ChatController {smpAgent, agentAsync = s, sndFiles, rcvFiles,
       atomically $ writeTVar files M.empty
 
 execChatCommand :: (MonadUnliftIO m, MonadReader ChatController m) => ByteString -> m ChatResponse
-execChatCommand s = case parseChatCommand s of
-  Left e -> do
-    u <- readTVarIO =<< asks currentUser
-    pure $ chatCmdError u e
-  Right cmd -> either (CRChatCmdError Nothing) id <$> runExceptT (processChatCommand cmd)
+execChatCommand s = do
+  u <- readTVarIO =<< asks currentUser
+  case parseChatCommand s of
+    Left e -> pure $ chatCmdError u e
+    Right cmd -> either (CRChatCmdError u) id <$> runExceptT (processChatCommand cmd)
 
 parseChatCommand :: ByteString -> Either String ChatCommand
 parseChatCommand = A.parseOnly chatCommandP . B.dropWhileEnd isSpace
@@ -3792,7 +3792,9 @@ withUser' :: ChatMonad m => (User -> m ChatResponse) -> m ChatResponse
 withUser' action =
   asks currentUser
     >>= readTVarIO
-    >>= maybe (throwChatError CENoActiveUser) (\u -> action u `catchError` (pure . CRChatError (Just u)))
+    >>= maybe (throwChatError CENoActiveUser) run
+  where
+    run u = action u `catchError` (pure . CRChatCmdError (Just u))
 
 withUser :: ChatMonad m => (User -> m ChatResponse) -> m ChatResponse
 withUser action = withUser' $ \user ->
