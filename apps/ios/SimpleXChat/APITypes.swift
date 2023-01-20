d485640c157118102683be643a56b88af78b6601
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-01-20 09:13:59 +0000

    mobile: add PING count to network config, make advanced network config available without dev tools

--- apps/ios/SimpleXChat/APITypes.swift
+++ apps/ios/SimpleXChat/APITypes.swift
@@ -809,6 +809,7 @@ public struct NetCfg: Codable, Equatable {
     public var tcpTimeout: Int // microseconds
     public var tcpKeepAlive: KeepAliveOpts?
     public var smpPingInterval: Int // microseconds
+    public var smpPingCount: Int // times
     public var logTLSErrors: Bool
 
     public static let defaults: NetCfg = NetCfg(
@@ -816,7 +817,8 @@ public struct NetCfg: Codable, Equatable {
         tcpConnectTimeout: 10_000_000,
         tcpTimeout: 7_000_000,
         tcpKeepAlive: KeepAliveOpts.defaults,
-        smpPingInterval: 600_000_000,
+        smpPingInterval: 1200_000_000,
+        smpPingCount: 3,
         logTLSErrors: false
     )
 
@@ -825,7 +827,8 @@ public struct NetCfg: Codable, Equatable {
         tcpConnectTimeout: 20_000_000,
         tcpTimeout: 15_000_000,
         tcpKeepAlive: KeepAliveOpts.defaults,
-        smpPingInterval: 600_000_000,
+        smpPingInterval: 1200_000_000,
+        smpPingCount: 3,
         logTLSErrors: false
     )
 
