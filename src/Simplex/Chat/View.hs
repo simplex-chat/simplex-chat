7b7a9f1d547d86542e0a691f5362e1bf6fddec7f
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2023-01-24 19:47:22 +0400

    names

--- src/Simplex/Chat/View.hs
+++ src/Simplex/Chat/View.hs
@@ -217,15 +217,15 @@ responseToView user_ ChatConfig {logLevel, testView} liveItems ts = \case
     ]
   CRAgentStats stats -> map (plain . intercalate ",") stats
   CRConnectionDisabled entity -> viewConnectionEntityDisabled entity
-  CRAgentRcvQueueDeleted agentConnId server agentQueueId agentError_ ->
-    [ "completed deleting rcv queue, agent connection id: " <> sShow agentConnId
-        <> (", server: " <> sShow server)
-        <> (", agent queue id: " <> sShow agentQueueId)
-        <> maybe "" (\e -> ", error: " <> sShow e) agentError_
+  CRAgentRcvQueueDeleted acId srv aqId err_ ->
+    [ "completed deleting rcv queue, agent connection id: " <> sShow acId
+        <> (", server: " <> sShow srv)
+        <> (", agent queue id: " <> sShow aqId)
+        <> maybe "" (\e -> ", error: " <> sShow e) err_
       | logLevel <= CLLInfo
     ]
-  CRAgentConnDeleted agentConnId -> ["completed deleting connection, agent connection id: " <> sShow agentConnId | logLevel <= CLLInfo]
-  CRAgentUserDeleted agentUserId -> ["completed deleting user" <> if logLevel <= CLLInfo then ", agent user id: " <> sShow agentUserId else ""]
+  CRAgentConnDeleted acId -> ["completed deleting connection, agent connection id: " <> sShow acId | logLevel <= CLLInfo]
+  CRAgentUserDeleted auId -> ["completed deleting user" <> if logLevel <= CLLInfo then ", agent user id: " <> sShow auId else ""]
   CRMessageError u prefix err -> ttyUser u [plain prefix <> ": " <> plain err | prefix == "error" || logLevel <= CLLWarning]
   CRChatCmdError u e -> ttyUser' u $ viewChatError logLevel e
   CRChatError u e -> ttyUser' u $ viewChatError logLevel e
