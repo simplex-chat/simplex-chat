25161d66119d25bcb9a4b712bdd2aaa4f7e31d9f
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-01-30 17:29:39 +0000

    make delay more visible

--- tests/ChatClient.hs
+++ tests/ChatClient.hs
@@ -198,7 +198,7 @@ withTmpFiles :: IO () -> IO ()
 withTmpFiles =
   bracket_
     (createDirectoryIfMissing False "tests/tmp")
-    (removeDirectoryRecursive "tests/tmp" >> threadDelay 2000000)
+    (removeDirectoryRecursive "tests/tmp")
 
 testChatN :: HasCallStack => ChatConfig -> ChatOpts -> [Profile] -> (HasCallStack => [TestCC] -> IO ()) -> IO ()
 testChatN cfg opts ps test = do
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,6 +1,7 @@
 import ChatClient
 -- import Control.Logger.Simple
 import ChatTests
+import Control.Concurrent (threadDelay)
 import MarkdownTests
 import MobileTests
 import ProtocolTests
@@ -15,7 +16,7 @@ main = do
     describe "SimpleX chat markdown" markdownTests
     describe "SimpleX chat protocol" protocolTests
     describe "Schema dump" schemaDumpTest
-    around_ withTmpFiles . around_ withSmpServer $ do
+    after_ (threadDelay 2000000) . around_ withTmpFiles . around_ withSmpServer $ do
       describe "Mobile API Tests" mobileTests
       describe "SimpleX chat client" chatTests
 
