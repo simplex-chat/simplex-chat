3c493db613b063f902ca22e8c1e45a0729afbec4
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-02-04 15:44:39 +0000

    mobile: use TCP for ICE requests of WebRTC calls (#1888)
    * ios: support query string parameters in ICE server addresses
    
    * android: support query params in ICE server address, add transport=TCP to default servers

--- apps/ios/Shared/Views/Call/WebRTC.swift
+++ apps/ios/Shared/Views/Call/WebRTC.swift
@@ -396,8 +396,8 @@ struct RTCIceServer: Codable, Equatable {
 }
 
 // the servers are expected in this format:
-// stun:stun.simplex.im:443
-// turn:private:yleob6AVkiNI87hpR94Z@turn.simplex.im:443
+// stun:stun.simplex.im:443?transport=tcp
+// turn:private:yleob6AVkiNI87hpR94Z@turn.simplex.im:443?transport=tcp
 func parseRTCIceServer(_ str: String) -> RTCIceServer? {
     var s = replaceScheme(str, "stun:")
     s = replaceScheme(s, "turn:")
@@ -406,8 +406,9 @@ func parseRTCIceServer(_ str: String) -> RTCIceServer? {
        let host = u.host,
        let port = u.port,
        u.path == "" && (scheme == "stun" || scheme == "turn")  {
+        let query = u.query == nil || u.query == "" ? "" : "?" + (u.query ?? "")
         return RTCIceServer(
-            urls: ["\(scheme):\(host):\(port)"],
+            urls: ["\(scheme):\(host):\(port)\(query)"],
             username: u.user,
             credential: u.password
         )
