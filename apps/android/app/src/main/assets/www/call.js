3a777623f73b2ae2a90a2cedcb636d652b058fe8
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-02-05 12:20:04 +0000

    update JS

--- apps/android/app/src/main/assets/www/call.js
+++ apps/android/app/src/main/assets/www/call.js
@@ -25,7 +25,9 @@ let activeCall;
 let answerTimeout = 30000;
 const processCommand = (function () {
     const defaultIceServers = [
+        { urls: ["stun:stun.simplex.im:443"] },
         { urls: ["stun:stun.simplex.im:443?transport=tcp"] },
+        { urls: ["turn:turn.simplex.im:443"], username: "private", credential: "yleob6AVkiNI87hpR94Z" },
         { urls: ["turn:turn.simplex.im:443?transport=tcp"], username: "private", credential: "yleob6AVkiNI87hpR94Z" },
     ];
     function getCallConfig(encodedInsertableStreams, iceServers, relay) {
