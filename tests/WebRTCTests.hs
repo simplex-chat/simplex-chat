992a9ea91324223155b5e0913d5630c2d4d08ade
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-02-24 19:14:07 +0000

    core: update/fix webrtc frame encryption function to return error

--- tests/WebRTCTests.hs
+++ tests/WebRTCTests.hs
@@ -1,5 +1,6 @@
 module WebRTCTests where
 
+import Control.Monad.Except
 import Crypto.Random (getRandomBytes)
 import qualified Data.ByteString.Base64.URL as U
 import qualified Data.ByteString.Char8 as B
@@ -11,8 +12,7 @@ webRTCTests = describe "WebRTC crypto" $ do
   it "encrypts and decrypts media" $ do
     key <- U.encode <$> getRandomBytes 32
     frame <- getRandomBytes 1000
-    let reservedSize = authTagSize + ivSize
-    frame' <- chatEncryptMedia key $ frame <> B.replicate reservedSize '\NUL'
+    Right frame' <- runExceptT $ chatEncryptMedia key $ frame <> B.replicate reservedSize '\NUL'
     B.length frame' `shouldBe` B.length frame + reservedSize
-    frame'' <- chatDecryptMedia key frame'
+    Right frame'' <- runExceptT $ chatDecryptMedia key frame'
     frame'' `shouldBe` frame <> B.replicate reservedSize '\NUL'
