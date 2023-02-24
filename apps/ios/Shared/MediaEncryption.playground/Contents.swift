95fbf3d43c849e1080776bf173d2df3615599166
Avently <7953703+avently@users.noreply.github.com>
2023-02-24 21:40:41 +0300

    added MediaEncryption playground

---
+++ apps/ios/Shared/MediaEncryption.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+import SimpleXChat
+
+hs_init(0, nil)
+
+var base: UnsafeMutableRawPointer = malloc(200)
+for i in 0..<28 {
+    base.assumingMemoryBound(to: UInt8.self)[i] = UInt8(i)
+}
+let unencrypted = NSData(bytesNoCopy: base, length: 200)
+let aesKey = "PI-bV-FTgRqZM_lsDH9T21a0yRVMsvLFmvilJ9Ssk3g="
+
+if var key: [CChar] = aesKey.cString(using: .utf8),
+   let pointer: UnsafeMutableRawPointer = malloc(unencrypted.count + 28) {
+    debugPrint("AesKey \(aesKey), cString \(key)")
+    memcpy(pointer, (unencrypted as NSData).bytes, unencrypted.count)
+    //let raw: UInt8 = (unencrypted[0] as UInt8) | ((unencrypted[1] as UInt8) << 8) | ((unencrypted[2] as UInt8) << 16)
+    let isKeyFrame = unencrypted[0] & 1 == 1
+    debugPrint("Is key frame \(isKeyFrame)")
+    let clearTextBytesSize = isKeyFrame ? 10 : 3
+    for i in 0..<30 {
+        debugPrint("Before \(i)  \(unencrypted[i + clearTextBytesSize])")
+    }
+    chat_encrypt_media(&key, pointer.advanced(by: clearTextBytesSize), Int32(unencrypted.count + 28 - clearTextBytesSize))
+    for i in 0..<30 {
+        debugPrint("After \(i)  \(pointer.advanced(by: clearTextBytesSize).assumingMemoryBound(to: UInt8.self)[i])")
+    }
+    let res_ = Data(bytes: pointer, count: unencrypted.count + 28)
+}
