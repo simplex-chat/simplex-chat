cbadacf61edf42afcabc53a5e0991b4841f559fb
Avently <7953703+avently@users.noreply.github.com>
2023-02-24 21:45:50 +0300

    better playground

--- apps/ios/Shared/MediaEncryption.playground/Contents.swift
+++ apps/ios/Shared/MediaEncryption.playground/Contents.swift
@@ -3,15 +3,20 @@ import SimpleXChat
 
 hs_init(0, nil)
 
-var base: UnsafeMutableRawPointer = malloc(200)
+let totalBytes = 200
+let ivTagBytes = 28
+var base: UnsafeMutableRawPointer = malloc(totalBytes)
+
+let assume = base.assumingMemoryBound(to: UInt8.self)
+assume[0] = 1 // key frame
 for i in 0..<28 {
-    base.assumingMemoryBound(to: UInt8.self)[i] = UInt8(i)
+    assume[totalBytes - ivTagBytes + i] = UInt8(i)
 }
 let unencrypted = NSData(bytesNoCopy: base, length: 200)
 let aesKey = "PI-bV-FTgRqZM_lsDH9T21a0yRVMsvLFmvilJ9Ssk3g="
 
 if var key: [CChar] = aesKey.cString(using: .utf8),
-   let pointer: UnsafeMutableRawPointer = malloc(unencrypted.count + 28) {
+   let pointer: UnsafeMutableRawPointer = malloc(unencrypted.count + ivTagBytes) {
     debugPrint("AesKey \(aesKey), cString \(key)")
     memcpy(pointer, (unencrypted as NSData).bytes, unencrypted.count)
     //let raw: UInt8 = (unencrypted[0] as UInt8) | ((unencrypted[1] as UInt8) << 8) | ((unencrypted[2] as UInt8) << 16)
@@ -21,9 +26,9 @@ if var key: [CChar] = aesKey.cString(using: .utf8),
     for i in 0..<30 {
         debugPrint("Before \(i)  \(unencrypted[i + clearTextBytesSize])")
     }
-    chat_encrypt_media(&key, pointer.advanced(by: clearTextBytesSize), Int32(unencrypted.count + 28 - clearTextBytesSize))
+    chat_encrypt_media(&key, pointer.advanced(by: clearTextBytesSize), Int32(unencrypted.count + ivTagBytes - clearTextBytesSize))
     for i in 0..<30 {
         debugPrint("After \(i)  \(pointer.advanced(by: clearTextBytesSize).assumingMemoryBound(to: UInt8.self)[i])")
     }
-    let res_ = Data(bytes: pointer, count: unencrypted.count + 28)
+    let res_ = Data(bytes: pointer, count: unencrypted.count + ivTagBytes)
 }
