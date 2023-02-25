50b90c4814d39bda194024bd84a40497cc654cfa
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-02-25 17:52:23 +0000

    core: use 12 bytes IV for WebRTC frame encryption with AES-GCM (#1951)
    * core: use 12 bytes IV for WebRTC frame encryption with AES-GCM
    
    * refactor

--- tests/WebRTCTests.hs
+++ tests/WebRTCTests.hs
@@ -5,6 +5,7 @@ import Crypto.Random (getRandomBytes)
 import qualified Data.ByteString.Base64.URL as U
 import qualified Data.ByteString.Char8 as B
 import Simplex.Chat.Mobile.WebRTC
+import qualified Simplex.Messaging.Crypto as C
 import Test.Hspec
 
 webRTCTests :: Spec
@@ -19,8 +20,8 @@ webRTCTests = describe "WebRTC crypto" $ do
   it "should fail on invalid frame size" $ do
     key <- U.encode <$> getRandomBytes 32
     frame <- getRandomBytes 10
-    runExceptT (chatEncryptMedia key frame) `shouldReturn` Left "frame has no [reserved space] IV and/or auth tag"
-    runExceptT (chatDecryptMedia key frame) `shouldReturn` Left "frame has no [reserved space] IV and/or auth tag"
+    runExceptT (chatEncryptMedia key frame) `shouldReturn` Left "frame has no [reserved space for] IV and/or auth tag"
+    runExceptT (chatDecryptMedia key frame) `shouldReturn` Left "frame has no [reserved space for] IV and/or auth tag"
   it "should fail on invalid key" $ do
     let key = B.replicate 32 '#'
     frame <- (<> B.replicate reservedSize '\NUL') <$> getRandomBytes 100
@@ -32,7 +33,7 @@ webRTCTests = describe "WebRTC crypto" $ do
     Right frame' <- runExceptT $ chatEncryptMedia key $ frame <> B.replicate reservedSize '\NUL'
     Right frame'' <- runExceptT $ chatDecryptMedia key frame'
     frame'' `shouldBe` frame <> B.replicate reservedSize '\NUL'
-    let (rest, iv) = B.splitAt (B.length frame' - ivSize) frame
-        (encFrame, _tag) = B.splitAt (B.length rest - authTagSize) rest
-        badFrame = encFrame <> B.replicate authTagSize '\NUL' <> iv
+    let (encFrame, rest) = B.splitAt (B.length frame' - reservedSize) frame
+        (_tag, iv) = B.splitAt C.authTagSize rest
+        badFrame = encFrame <> B.replicate C.authTagSize '\NUL' <> iv
     runExceptT (chatDecryptMedia key badFrame) `shouldReturn` Left "AESDecryptError"
