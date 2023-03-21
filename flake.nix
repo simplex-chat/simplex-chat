eaceee62d7c17db213c986328cbe3db301b29273
shum <shum@liber.li>
2023-03-21 12:21:58 +0300

    flake.nix: remove local nixconf
    This change to flake.nix breaks build-android.sh script by forcing user
    to input y/n. AFAIK, this cannot be automated and I rather not include
    workarounds like piping "yes n | nix build ...".

--- flake.nix
+++ flake.nix
@@ -346,15 +346,4 @@
         };
       }
     );
-    # --- Flake Local Nix Configuration ----------------------------
-    nixConfig = {
-      extra-substituters = [
-        "https://cache.zw3rk.com"
-      ];
-      extra-trusted-public-keys = [
-        "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
-      ];
-      allow-import-from-derivation = "true";
-    };
-    # --------------------------------------------------------------
 }
