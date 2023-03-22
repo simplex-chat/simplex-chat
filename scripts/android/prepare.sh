66edb9e9e341740e972457dd32c9c928293e13a3
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-03-22 14:36:28 +0000

    update android/prepare script to use zip archives

--- scripts/android/prepare.sh
+++ scripts/android/prepare.sh
@@ -3,10 +3,10 @@
 # libsimplex.so and libsupport.so binaries should be in ~/Downloads folder in their directories based on archive name
 mkdir -p ./apps/android/app/src/main/cpp/libs/arm64-v8a/
 rm ./apps/android/app/src/main/cpp/libs/arm64-v8a/*
-cp ~/Downloads/pkg-aarch64-android-libsupport/libsupport.so ./apps/android/app/src/main/cpp/libs/arm64-v8a/
-cp ~/Downloads/pkg-aarch64-android-libsimplex/libsimplex.so ./apps/android/app/src/main/cpp/libs/arm64-v8a/
+unzip -o ~/Downloads/pkg-aarch64-android-libsupport.zip -d ./apps/android/app/src/main/cpp/libs/arm64-v8a
+unzip -o  ~/Downloads/pkg-aarch64-android-libsimplex.zip -d ./apps/android/app/src/main/cpp/libs/arm64-v8a/
 
 mkdir -p ./apps/android/app/src/main/cpp/libs/armeabi-v7a/
 rm ./apps/android/app/src/main/cpp/libs/armeabi-v7a/*
-cp ~/Downloads/pkg-armv7a-android-libsupport/libsupport.so ./apps/android/app/src/main/cpp/libs/armeabi-v7a/
-cp ~/Downloads/pkg-armv7a-android-libsimplex/libsimplex.so ./apps/android/app/src/main/cpp/libs/armeabi-v7a/
+unzip -o  ~/Downloads/pkg-armv7a-android-libsupport.zip -d ./apps/android/app/src/main/cpp/libs/armeabi-v7a/
+unzip -o  ~/Downloads/pkg-armv7a-android-libsimplex.zip -d ./apps/android/app/src/main/cpp/libs/armeabi-v7a/
