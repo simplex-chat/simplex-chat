21a49710a829c960ff23d23a02543b7356c4b7bb
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-04-01 14:28:12 +0100

    ios: scripts to import/export localizations

---
+++ scripts/ios/import-localizations.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/cs.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/de.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/es.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/fr.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/it.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/nl.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/ru.xcloc
+xcodebuild -importLocalizations -project ./apps/ios/SimpleX.xcodeproj -localizationPath  ./apps/ios/SimpleX\ Localizations/zh-Hans.xcloc
