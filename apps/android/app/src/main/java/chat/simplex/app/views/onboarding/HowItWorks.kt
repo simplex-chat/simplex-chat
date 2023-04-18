717857e71a84eee34e382521034151f7b2f9d17f
Avently <7953703+avently@users.noreply.github.com>
2023-04-18 11:49:27 +0300

    unused code

--- apps/android/app/src/main/java/chat/simplex/app/views/onboarding/HowItWorks.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/onboarding/HowItWorks.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
--- apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserProfileView.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/usersettings/UserProfileView.kt
@@ -5,8 +5,6 @@ import android.net.Uri
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
@@ -17,8 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
