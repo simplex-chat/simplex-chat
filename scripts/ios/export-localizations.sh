cdd2efd6139eea2d1f4f37a1e8356ec2de1b4371
Doomguy <1833568+doomguy@users.noreply.github.com>
2023-04-25 22:04:39 +0000

    shellcheck ios/export-localizations.sh

--- scripts/ios/export-localizations.sh
+++ scripts/ios/export-localizations.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-
-set -e
+#!/usr/bin/env bash
+# Safety measures
+set -euo pipefail
+trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
 
 langs=( en cs de es fr it nl pl ru zh-Hans )
 
@@ -12,6 +14,6 @@ for lang in "${langs[@]}"; do
   xcodebuild -exportLocalizations \
             -project ./apps/ios/SimpleX.xcodeproj \
             -localizationPath ./apps/ios/SimpleX\ Localizations \
-            -exportLanguage $lang
+            -exportLanguage "$lang"
   sleep 2
 done
