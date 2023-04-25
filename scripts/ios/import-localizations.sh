9ba9b20e533df083f84657b59e88a7ff6043a691
Doomguy <1833568+doomguy@users.noreply.github.com>
2023-04-25 22:06:24 +0000

    shellcheck ios/import-localizations.sh

--- scripts/ios/import-localizations.sh
+++ scripts/ios/import-localizations.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-
-set -e
+#!/usr/bin/env bash
+# Safety measures
+set -euo pipefail
+trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
 
 langs=( en cs de es fr it nl pl ru zh-Hans )
 
@@ -11,7 +13,7 @@ for lang in "${langs[@]}"; do
   echo "*** Importing $lang"
   xcodebuild -importLocalizations \
             -project ./apps/ios/SimpleX.xcodeproj \
-            -localizationPath ./apps/ios/SimpleX\ Localizations/$lang.xcloc \
+            -localizationPath ./apps/ios/SimpleX\ Localizations/"$lang".xcloc \
             -disableAutomaticPackageResolution \
             -skipPackageUpdates
   sleep 10
