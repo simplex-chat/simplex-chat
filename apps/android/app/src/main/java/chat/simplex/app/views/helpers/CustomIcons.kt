23467a2248b9c5cebd666003ce55af4d1e5adf3e
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2023-04-25 12:31:26 +0300

    android: lint fix and removed unused lib (#2235)

--- apps/android/app/src/main/java/chat/simplex/app/views/helpers/CustomIcons.kt
+++ apps/android/app/src/main/java/chat/simplex/app/views/helpers/CustomIcons.kt
@@ -1,62 +1,11 @@
 package chat.simplex.app.views.helpers
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.materialPath
-import androidx.compose.material.icons.outlined.PersonAdd
-import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.vector.*
-import androidx.compose.ui.unit.dp
-
-private inline fun materialIcon(
-  name: String,
-  block: ImageVector.Builder.() -> ImageVector.Builder
-): ImageVector = ImageVector.Builder(
-  name = name,
-  defaultWidth = MaterialIconDimension.dp,
-  defaultHeight = MaterialIconDimension.dp,
-  viewportWidth = MaterialIconDimension,
-  viewportHeight = MaterialIconDimension
-).block().build()
-
-/**
- * Adds a vector path to this icon with Material defaults.
- *
- * @param fillAlpha fill alpha for this path
- * @param strokeAlpha stroke alpha for this path
- * @param pathFillType [PathFillType] for this path
- * @param pathBuilder builder lambda to add commands to this path
- */
-private inline fun ImageVector.Builder.materialPath(
-  fillAlpha: Float = 1f,
-  strokeAlpha: Float = 1f,
-  pathFillType: PathFillType = DefaultFillType,
-  pathBuilder: PathBuilder.() -> Unit
-) =
-// TODO: b/146213225
-// Some of these defaults are already set when parsing from XML, but do not currently exist
-  // when added programmatically. We should unify these and simplify them where possible.
-  path(
-    fill = SolidColor(Color.Black),
-    fillAlpha = fillAlpha,
-    stroke = null,
-    strokeAlpha = strokeAlpha,
-    strokeLineWidth = 1f,
-    strokeLineCap = StrokeCap.Butt,
-    strokeLineJoin = StrokeJoin.Bevel,
-    strokeLineMiter = 1f,
-    pathFillType = pathFillType,
-    pathBuilder = pathBuilder
-  )
-
-// All Material icons (currently) are 24dp by 24dp, with a viewport size of 24 by 24.
-@PublishedApi
-internal const val MaterialIconDimension = 24f
-
 
 val AccountCircleFilled: ImageVector
   get() {
-    Icons.Filled.Bolt
     if (_accountCircleFilled != null) {
       return _accountCircleFilled!!
     }
