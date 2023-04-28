c12b5db9f3c27bf1fd00d1eb0169370c035c4b9f
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-04-28 12:03:11 +0400

    fix typo

--- apps/ios/Shared/Views/UserSettings/UserAddressLearnMore.swift
+++ apps/ios/Shared/Views/UserSettings/UserAddressLearnMore.swift
@@ -12,7 +12,7 @@ struct UserAddressLearnMore: View {
     var body: some View {
         List {
             VStack(alignment: .leading, spacing: 18) {
-                Text("You can share your address as a link or QR code - anybody can to connect to you.")
+                Text("You can share your address as a link or QR code - anybody can connect to you.")
                 Text("You won't lose your contacts if you later delete your address.")
                 Text("When people request to connect, you can accept or reject it.")
                 Text("Learn more in [User Guide](https://github.com/simplex-chat/simplex-chat/blob/stable/docs/guide/app-settings.md#your-simplex-contact-address).")
