4793756f3b6621678be70bee90d12e960b333cb4
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-05-26 16:22:36 +0400

    core: add connection to CEInvalidChatMessage

--- src/Simplex/Chat/View.hs
+++ src/Simplex/Chat/View.hs
@@ -1384,7 +1384,7 @@ viewChatError logLevel = \case
     CEChatNotStopped -> ["error: chat not stopped"]
     CEChatStoreChanged -> ["error: chat store changed, please restart chat"]
     CEInvalidConnReq -> viewInvalidConnReq
-    CEInvalidChatMessage msg e -> [plain $ "chat message error: " <> e <> " (" <> T.unpack (T.take 120 msg) <> ")"]
+    CEInvalidChatMessage Connection {connId} msg e -> [plain $ "chat message error: " <> e <> " (" <> T.unpack (T.take 120 msg) <> "), connection id: " <> show connId]
     CEContactNotReady c -> [ttyContact' c <> ": not ready"]
     CEContactDisabled Contact {localDisplayName = c} -> [ttyContact c <> ": disabled, to enable: " <> highlight ("/enable " <> c) <> ", to delete: " <> highlight ("/d " <> c)]
     CEConnectionDisabled Connection {connId, connType} -> [plain $ "connection " <> textEncode connType <> " (" <> tshow connId <> ") is disabled" | logLevel <= CLLWarning]
