591ff302d0a1b8ba4be60b2e6049617545ba6e14
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-05-29 13:28:32 +0400

    more indexes

--- src/Simplex/Chat/Migrations/M20230529_indexes.hs
+++ src/Simplex/Chat/Migrations/M20230529_indexes.hs
@@ -13,11 +13,15 @@ DROP INDEX idx_chat_items_timed_delete_at;
 CREATE INDEX idx_chat_items_timed_delete_at ON chat_items(user_id, timed_delete_at);
 
 CREATE INDEX idx_group_members_group_id ON group_members(user_id, group_id);
+
+CREATE INDEX idx_msg_deliveries_agent_ack_cmd_id ON msg_deliveries(connection_id, agent_ack_cmd_id);
 |]
 
 down_m20230529_indexes :: Query
 down_m20230529_indexes =
   [sql|
+DROP INDEX idx_msg_deliveries_agent_ack_cmd_id;
+
 DROP INDEX idx_group_members_group_id;
 
 DROP INDEX idx_chat_items_timed_delete_at;
