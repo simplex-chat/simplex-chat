94b71df4fc6877f15a6fe7eed60f6209d764e23b
Avently <7953703+avently@users.noreply.github.com>
2023-07-12 23:38:05 +0700

    desktop: do not show scan QR code screen

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/newchat/ConnectViaLinkView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/newchat/ConnectViaLinkView.kt
@@ -9,7 +9,8 @@ import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 import androidx.compose.ui.unit.sp
 import chat.simplex.common.model.ChatModel
-import chat.simplex.common.views.newchat.ScanToConnectView
+import chat.simplex.common.platform.AppPlatform
+import chat.simplex.common.platform.appPlatform
 import chat.simplex.res.MR
 
 enum class ConnectViaLinkTab {
@@ -18,12 +19,23 @@ enum class ConnectViaLinkTab {
 
 @Composable
 fun ConnectViaLinkView(m: ChatModel, close: () -> Unit) {
+  val pages = remember {
+    if (appPlatform.isAndroid) {
+      ConnectViaLinkTab.values().toList()
+    } else {
+      listOf(ConnectViaLinkTab.PASTE)
+    }
+  }
   val selection = remember {
     mutableStateOf(
-      runCatching { ConnectViaLinkTab.valueOf(m.controller.appPrefs.connectViaLinkTab.get()!!) }.getOrDefault(ConnectViaLinkTab.SCAN)
+      if (appPlatform.isAndroid) {
+        runCatching { ConnectViaLinkTab.valueOf(m.controller.appPrefs.connectViaLinkTab.get()!!) }.getOrDefault(ConnectViaLinkTab.SCAN)
+      } else {
+        ConnectViaLinkTab.PASTE
+      }
     )
   }
-  val tabTitles = ConnectViaLinkTab.values().map {
+  val tabTitles = pages.map {
     when (it) {
       ConnectViaLinkTab.SCAN -> stringResource(MR.strings.scan_QR_code)
       ConnectViaLinkTab.PASTE -> stringResource(MR.strings.paste_the_link_you_received)
@@ -43,28 +55,30 @@ fun ConnectViaLinkView(m: ChatModel, close: () -> Unit) {
         }
       }
     }
-    TabRow(
-      selectedTabIndex = selection.value.ordinal,
-      backgroundColor = Color.Transparent,
-      contentColor = MaterialTheme.colors.primary,
-    ) {
-      tabTitles.forEachIndexed { index, it ->
-        Tab(
-          selected = selection.value.ordinal == index,
-          onClick = {
-            selection.value = ConnectViaLinkTab.values()[index]
-            m.controller.appPrefs.connectViaLinkTab.set(selection.value .name)
-          },
-          text = { Text(it, fontSize = 13.sp) },
-          icon = {
-            Icon(
-              if (ConnectViaLinkTab.SCAN.ordinal == index) painterResource(MR.images.ic_qr_code) else painterResource(MR.images.ic_article),
-              it
-            )
-          },
-          selectedContentColor = MaterialTheme.colors.primary,
-          unselectedContentColor = MaterialTheme.colors.secondary,
-        )
+    if (pages.size > 1) {
+      TabRow(
+        selectedTabIndex = pages.indexOf(selection.value),
+        backgroundColor = Color.Transparent,
+        contentColor = MaterialTheme.colors.primary,
+      ) {
+        tabTitles.forEachIndexed { index, it ->
+          Tab(
+            selected = index == tabTitles.indexOf(it),
+            onClick = {
+              selection.value = pages[index]
+              m.controller.appPrefs.connectViaLinkTab.set(selection.value.name)
+            },
+            text = { Text(it, fontSize = 13.sp) },
+            icon = {
+              Icon(
+                if (ConnectViaLinkTab.SCAN == pages[index]) painterResource(MR.images.ic_qr_code) else painterResource(MR.images.ic_article),
+                it
+              )
+            },
+            selectedContentColor = MaterialTheme.colors.primary,
+            unselectedContentColor = MaterialTheme.colors.secondary,
+          )
+        }
       }
     }
   }
