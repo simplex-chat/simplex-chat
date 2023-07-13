515eacb64f802efa066c4ed7f60ffaff2b796617
Avently <7953703+avently@users.noreply.github.com>
2023-07-13 15:43:53 +0700

    changes in Linux script

--- scripts/desktop/build-lib-linux.sh
+++ scripts/desktop/build-lib-linux.sh
@@ -4,18 +4,22 @@ OS=linux
 ARCH=${1:-`uname -a | rev | cut -d' ' -f2 | rev`}
 GHC_VERSION=8.10.7
 
-rm -rf dist-newstyle/build/$ARCH-$OS/ghc-${GHC_VERSION}/simplex-chat-*
+BUILD_DIR=dist-newstyle/build/$ARCH-$OS/ghc-${GHC_VERSION}/simplex-chat-*
+
+rm -rf $BUILD_DIR
 cabal build lib:simplex-chat --ghc-options='-optl-Wl,-rpath,$ORIGIN' --ghc-options="-optl-L$(ghc --print-libdir)/rts -optl-Wl,--as-needed,-lHSrts_thr-ghc$GHC_VERSION"
-cd dist-newstyle/build/$ARCH-$OS/ghc-${GHC_VERSION}/simplex-chat-*/build
+cd $BUILD_DIR/build
 #patchelf --add-needed libHSrts_thr-ghc${GHC_VERSION}.so libHSsimplex-chat-*-inplace-ghc${GHC_VERSION}.so
 #patchelf --add-rpath '$ORIGIN' libHSsimplex-chat-*-inplace-ghc${GHC_VERSION}.so
 mkdir deps
 ldd libHSsimplex-chat-*-inplace-ghc${GHC_VERSION}.so | grep "ghc" | cut -d' ' -f 3 | xargs -I {} cp {} ./deps/
 
-rm -rf ../../../../../../apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
-rm -rf ../../../../../../apps/multiplatform/common/src/commonMain/resources/libs/$OS-$ARCH/
-rm -rf ../../../../../../apps/multiplatform/desktop/build/cmake
+cd -
 
-mkdir -p ../../../../../../apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
-cp -r deps ../../../../../../apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
-cp libHSsimplex-chat-*-inplace-ghc${GHC_VERSION}.so ../../../../../../apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
+rm -rf apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
+rm -rf apps/multiplatform/common/src/commonMain/resources/libs/$OS-$ARCH/
+rm -rf apps/multiplatform/desktop/build/cmake
+
+mkdir -p apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
+cp -r $BUILD_DIR/build/deps apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
+cp $BUILD_DIR/build/libHSsimplex-chat-*-inplace-ghc${GHC_VERSION}.so apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
