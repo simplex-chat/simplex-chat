e83454a558153b2ab2b6a91ddc7db425217bb7d3
Avently <7953703+avently@users.noreply.github.com>
2023-07-27 20:13:24 +0700

    changes in script

--- scripts/desktop/prepare-ubuntu.sh
+++ scripts/desktop/prepare-ubuntu.sh
@@ -5,6 +5,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 ghcup install ghc 8.10.7
 ghcup set ghc 8.10.7
 cabal update
+git clone https://github.com/simplex-chat/simplex-chat -b master simplex
+cd simplex
 echo "ignore-project: False"    >> cabal.project.local
 echo "package direct-sqlcipher" >> cabal.project.local
 echo "    flags: +openssl"      >> cabal.project.local
@@ -14,5 +16,5 @@ cd apps/multiplatform
 sed -i 's|":android", ||' settings.gradle.kts
 
 ./gradlew packageDeb
-sudo dpkg -i ./release/main/deb/simpl*.deb
+#sudo dpkg -i ./release/main/deb/simpl*.deb
 #sudo ln -s /opt/simplex/bin/simplex /usr/bin/simplex
