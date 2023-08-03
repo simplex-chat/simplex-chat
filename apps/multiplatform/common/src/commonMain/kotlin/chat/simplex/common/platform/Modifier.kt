65391756efabccd100410d44dbcc5800f01d85f4
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2023-08-03 18:24:43 +0300

    desktop: files Drag & Drop support (#2843)
    * desktop: files Drag&Drop support
    
    * reduce diff
    
    * move
    
    * move 2
    
    * move 3
    
    ---------
    
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/platform/Modifier.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/platform/Modifier.kt
@@ -2,6 +2,7 @@ package chat.simplex.common.platform
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 
 expect fun Modifier.navigationBarsWithImePadding(): Modifier
 
@@ -11,3 +12,11 @@ expect fun ProvideWindowInsets(
   windowInsetsAnimationsEnabled: Boolean = true,
   content: @Composable () -> Unit
 )
+
+@Composable
+expect fun Modifier.desktopOnExternalDrag(
+  enabled: Boolean = true,
+  onFiles: (List<String>) -> Unit = {},
+  onImage: (Painter) -> Unit = {},
+  onText: (String) -> Unit = {}
+): Modifier
