6f28a3fde5389a3e49b1b34d82ce44537a720fb4
pdavidow <pdavidow@fastmail.fm>
2023-08-14 02:23:18 -0400

    compiles

--- tests/MarkdownEditingTests.hs
+++ tests/MarkdownEditingTests.hs
@@ -6,6 +6,7 @@
 module MarkdownEditingTests where
 
 import Data.List.NonEmpty (NonEmpty)
+import qualified Data.Sequence as S
 import Data.Text (Text)
 import Simplex.Chat.Markdown
 import Simplex.Chat.MarkdownEditing
@@ -31,34 +32,34 @@ redEdited t added = EditedText (Just $ colored Red) t (Just added)
 
 formattedEditedTextTests :: Spec
 formattedEditedTextTests = describe "show edits using Myers Diff algorithm" do
-  it "no change" do
-    formattedEditedText [plainText "H"] [plainText "H"]
-      `shouldBe` 
-        [ EditedChar Nothing 'H' Nothing              
-        ] 
+  -- it "no change" do
+  --   formattedEditedText [plainText "H"] [plainText "H"]
+  --     `shouldBe` 
+  --       [ EditedChar Nothing 'H' Nothing              
+  --       ] 
 
-  it "add 1 char to empty" do
-    formattedEditedText [plainText ""] [plainText "H"]
-      `shouldBe` 
-        [ EditedChar Nothing 'H' $ Just Add              
-        ] 
+  -- it "add 1 char to empty" do
+  --   formattedEditedText [plainText ""] [plainText "H"]
+  --     `shouldBe` 
+  --       [ EditedChar Nothing 'H' $ Just Add              
+  --       ] 
 
-  it "del the one and only" do
-    formattedEditedText [plainText "H"] [plainText ""]
-      `shouldBe` 
-        [ EditedChar Nothing 'H' $ Just Delete              
-        ] 
+  -- it "del the one and only" do
+  --   formattedEditedText [plainText "H"] [plainText ""]
+  --     `shouldBe` 
+  --       [ EditedChar Nothing 'H' $ Just Delete              
+  --       ] 
 
-  it "one character change" do
-    formattedEditedText [plainText "Hrllo"] [plainText "Hello"]
-      `shouldBe` 
-        [ EditedChar Nothing 'H' Nothing
-        , EditedChar Nothing 'r' $ Just Delete
-        , EditedChar Nothing 'e' $ Just Add
-        , EditedChar Nothing 'l' Nothing
-        , EditedChar Nothing 'l' Nothing
-        , EditedChar Nothing 'o' Nothing                
-        ]
+  -- it "one character change" do
+  --   formattedEditedText [plainText "Hrllo"] [plainText "Hello"]
+  --     `shouldBe` 
+  --       [ EditedChar Nothing 'H' Nothing
+  --       , EditedChar Nothing 'r' $ Just Delete
+  --       , EditedChar Nothing 'e' $ Just Add
+  --       , EditedChar Nothing 'l' Nothing
+  --       , EditedChar Nothing 'l' Nothing
+  --       , EditedChar Nothing 'o' Nothing                
+  --       ]
         -- [ EditedChar Nothing 'H' Nothing
         -- , EditedChar Nothing 'e' $ Just Substitute
         -- , EditedChar Nothing 'l' Nothing
@@ -66,58 +67,60 @@ formattedEditedTextTests = describe "show edits using Myers Diff algorithm" do
         -- , EditedChar Nothing 'o' Nothing                
         -- ]        
 
-  it "more1" do
-    formattedEditedText [plainText "Hrllo"] [plainText "Helloxyz"]
-      `shouldBe` 
-        [ EditedChar Nothing 'H' Nothing
-        , EditedChar Nothing 'r' $ Just Delete
-        , EditedChar Nothing 'e' $ Just Add
-        , EditedChar Nothing 'l' Nothing
-        , EditedChar Nothing 'l' Nothing
-        , EditedChar Nothing 'o' Nothing       
-        , EditedChar Nothing 'x' $ Just Add
-        , EditedChar Nothing 'y' $ Just Add
-        , EditedChar Nothing 'z' $ Just Add                                 
-        ]        
+  -- it "more1" do
+  --   formattedEditedText [plainText "Hrllo"] [plainText "Helloxyz"]
+  --     `shouldBe` 
+  --       [ EditedChar Nothing 'H' Nothing
+  --       , EditedChar Nothing 'r' $ Just Delete
+  --       , EditedChar Nothing 'e' $ Just Add
+  --       , EditedChar Nothing 'l' Nothing
+  --       , EditedChar Nothing 'l' Nothing
+  --       , EditedChar Nothing 'o' Nothing       
+  --       , EditedChar Nothing 'x' $ Just Add
+  --       , EditedChar Nothing 'y' $ Just Add
+  --       , EditedChar Nothing 'z' $ Just Add                                 
+  --       ]        
 
-  it "more2" do
-    formattedEditedText [plainText "Hrllo"] [plainText "Hexyzo"]
-      `shouldBe` 
-        [ EditedChar Nothing 'H' Nothing
-        , EditedChar Nothing 'e' $ Just Add  
-        , EditedChar Nothing 'x' $ Just Add
-        , EditedChar Nothing 'y' $ Just Add
-        , EditedChar Nothing 'z' $ Just Add     
-        , EditedChar Nothing 'r' $ Just Delete
-        , EditedChar Nothing 'l' $ Just Delete
-        , EditedChar Nothing 'l' $ Just Delete        
-        , EditedChar Nothing 'o' Nothing                                      
-        ] 
+  -- it "more2" do
+  --   formattedEditedText [plainText "Hrllo"] [plainText "Hexyzo"]
+  --     `shouldBe` 
+  --       [ EditedChar Nothing 'H' Nothing
+  --       , EditedChar Nothing 'e' $ Just Add  
+  --       , EditedChar Nothing 'x' $ Just Add
+  --       , EditedChar Nothing 'y' $ Just Add
+  --       , EditedChar Nothing 'z' $ Just Add     
+  --       , EditedChar Nothing 'r' $ Just Delete
+  --       , EditedChar Nothing 'l' $ Just Delete
+  --       , EditedChar Nothing 'l' $ Just Delete        
+  --       , EditedChar Nothing 'o' Nothing                                      
+  --       ] 
 
   it "more3" do
-    diff 
-        [ EditedChar (Just Bold)              'H' Nothing
-        , EditedChar Nothing                  'r' Nothing
-        , EditedChar (Just Secret)            'l' Nothing
-        , EditedChar Nothing                  'l' Nothing
-        , EditedChar (Just $ colored Green)   'o' Nothing                                       
-        ]   
-        [ EditedChar (Just Italic)            'H' Nothing
-        , EditedChar (Just $ colored Cyan)    'e' Nothing
-        , EditedChar Nothing                  'x' Nothing
-        , EditedChar Nothing                  'y' Nothing
-        , EditedChar (Just Secret)            'z' Nothing        
-        , EditedChar (Just $ colored Blue)    'o' Nothing                                       
-        ]    
+    findDiffs 
+        (S.fromList
+          [ FormattedChar (Just Bold)             'H' 
+          , FormattedChar Nothing                 'r'
+          , FormattedChar (Just Secret)           'l' 
+          , FormattedChar Nothing                 'l' 
+          , FormattedChar (Just $ colored Green)  'o'                                     
+          ])   
+        (S.fromList
+          [ FormattedChar (Just Italic)           'H' 
+          , FormattedChar (Just $ colored Cyan)   'e' 
+          , FormattedChar Nothing                 'x' 
+          , FormattedChar Nothing                 'y'
+          , FormattedChar (Just Secret)           'z'        
+          , FormattedChar (Just $ colored Blue)   'o'                                       
+          ])  
 
-      `shouldBe` 
-        [ EditedChar (Just Italic)            'H' Nothing
-        , EditedChar Nothing                  'e' $ Just Add  
-        , EditedChar Nothing                  'x' $ Just Add
-        , EditedChar Nothing                  'y' $ Just Add
-        , EditedChar (Just Secret)            'z' $ Just Add     
-        , EditedChar Nothing                  'r' $ Just Delete
-        , EditedChar (Just Secret)            'l' $ Just Delete
-        , EditedChar Nothing                  'l' $ Just Delete        
-        , EditedChar (Just $ colored Blue)    'o' Nothing                                      
-        ] 
+      `shouldBe` S.fromList
+        [ DiffedChar (FormattedChar (Just Bold) 'H') (UnchangedTextually (ChangedFormat (Just Italic)))   
+        , DiffedChar (FormattedChar (Just $ colored Cyan) 'e') Inserted
+        , DiffedChar (FormattedChar Nothing 'x') Inserted
+        , DiffedChar (FormattedChar Nothing 'y') Inserted
+        , DiffedChar (FormattedChar (Just Secret) 'z') Inserted
+        , DiffedChar (FormattedChar Nothing 'r') Deleted
+        , DiffedChar (FormattedChar (Just Secret) 'l') Deleted  
+        , DiffedChar (FormattedChar Nothing 'l') Deleted  
+        , DiffedChar (FormattedChar (Just $ colored Green) 'o') (UnchangedTextually (ChangedFormat (Just $ colored Blue)))                                    
+        ]
