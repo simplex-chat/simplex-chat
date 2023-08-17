d005d79d54aab6d8f0f0a703e3bde1fe7ef53f8c
pdavidow <pdavidow@fastmail.fm>
2023-08-17 11:35:57 -0400

    cleanup

--- src/Simplex/Chat/MarkdownEditing.hs
+++ src/Simplex/Chat/MarkdownEditing.hs
@@ -130,7 +130,7 @@ findDiffs (LeftSide left) (RightSide right) = addInserts markDeletesAndUnchanged
             where x = if fL == fR then UnchangedFormat else ChangedToFormat fR
 
         g :: ((Int, FormattedChar), (Int, FormattedChar)) -> (Int, FormattedChar, FormattedChar)
-        g ((i,c), (_j,d)) = (i,c,d) -- peg on left with i          
+        g ((i,c), (_,d)) = (i,c,d) -- peg on left         
 
     markDeletesAndUnchangedChars :: Seq DiffedChar
     markDeletesAndUnchangedChars = S.mapWithIndex f left
