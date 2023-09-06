6e0e3d49309171b38bebb9f55f855db7b85836c5
Avently <7953703+avently@users.noreply.github.com>
2023-09-06 22:46:09 +0800

    test

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/platform/NtfManager.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/platform/NtfManager.kt
@@ -55,6 +55,7 @@ abstract class NtfManager {
 
   fun openChatAction(userId: Long?, chatId: ChatId) {
     withBGApi {
+      delay(5000L)
       awaitChatStartedIfNeeded(chatModel)
       if (userId != null && userId != chatModel.currentUser.value?.userId && chatModel.currentUser.value != null) {
         chatModel.controller.changeActiveUser(userId, null)
