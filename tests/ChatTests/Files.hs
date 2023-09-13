ae04172d5061138208dc52dab02ed5e229e05001
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-09-13 14:36:15 +0400

    core: show rcv file error

--- tests/ChatTests/Files.hs
+++ tests/ChatTests/Files.hs
@@ -1368,6 +1368,7 @@ testXFTPRcvError tmp = do
                "started receiving file 1 (test.pdf) from alice"
              ]
       bob <## "error receiving file 1 (test.pdf) from alice"
+      _ <- getTermLine bob
 
       bob ##> "/fs 1"
       bob <## "receiving file 1 (test.pdf) error"
