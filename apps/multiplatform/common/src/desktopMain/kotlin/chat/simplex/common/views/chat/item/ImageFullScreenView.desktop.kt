8709ad6ff3a3f44bec82bbbf93b0aaab5ade91d4
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2023-09-27 17:19:48 +0800

    desktop: enhanced video player + inline player (#3130)
    * desktop: enhanced video player + inline player
    
    * simplify
    
    * simplify
    
    * removed unused code
    
    * follow up
    
    ---------
    
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/views/chat/item/ImageFullScreenView.desktop.kt
+++ apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/views/chat/item/ImageFullScreenView.desktop.kt
@@ -6,17 +6,15 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import chat.simplex.common.platform.*
-import chat.simplex.common.simplexWindowState
 import chat.simplex.common.views.helpers.getBitmapFromByteArray
 import chat.simplex.res.MR
 import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
-import kotlinx.coroutines.delay
+import org.jetbrains.compose.videoplayer.SkiaBitmapVideoSurface
 import kotlin.math.max
 
 @Composable
@@ -28,30 +26,40 @@ actual fun FullScreenImageView(modifier: Modifier, data: ByteArray, imageBitmap:
     modifier = modifier,
   )
 }
+
 @Composable
 actual fun FullScreenVideoView(player: VideoPlayer, modifier: Modifier, close: () -> Unit) {
-  // Workaround. Without changing size of the window the screen flashes a lot even if it's not being recomposed
-  LaunchedEffect(Unit) {
-    simplexWindowState.windowState.size = simplexWindowState.windowState.size.copy(width = simplexWindowState.windowState.size.width + 1.dp)
-    delay(50)
-    player.play(true)
-    simplexWindowState.windowState.size = simplexWindowState.windowState.size.copy(width = simplexWindowState.windowState.size.width - 1.dp)
-  }
   Box {
     Box(Modifier.fillMaxSize().padding(bottom = 50.dp)) {
-      val factory = remember { { player.mediaPlayerComponent } }
-      SwingPanel(
-        background = Color.Transparent,
-        modifier = Modifier,
-        factory = factory
-      )
+      SurfaceFromPlayer(player, modifier)
+      IconButton(onClick = close, Modifier.padding(top = 5.dp)) {
+        Icon(painterResource(MR.images.ic_arrow_back_ios_new), null, Modifier.size(30.dp), tint = MaterialTheme.colors.primary)
+      }
     }
-    Controls(player, close)
+    Controls(player)
   }
 }
 
 @Composable
-private fun BoxScope.Controls(player: VideoPlayer, close: () -> Unit) {
+fun BoxScope.SurfaceFromPlayer(player: VideoPlayer, modifier: Modifier) {
+  val surface = remember {
+    SkiaBitmapVideoSurface().also {
+      player.player.videoSurface().set(it)
+    }
+  }
+  surface.bitmap.value?.let { bitmap ->
+    Image(
+      bitmap,
+      modifier = modifier.align(Alignment.Center),
+      contentDescription = null,
+      contentScale = ContentScale.Fit,
+      alignment = Alignment.Center,
+    )
+  }
+}
+
+@Composable
+private fun BoxScope.Controls(player: VideoPlayer) {
   val playing = remember(player) { player.videoPlaying }
   val progress = remember(player) { player.progress }
   val duration = remember(player) { player.duration }
@@ -62,10 +70,7 @@ private fun BoxScope.Controls(player: VideoPlayer, close: () -> Unit) {
     Slider(
       value = progress.value.toFloat() / max(0.0001f, duration.value.toFloat()),
       onValueChange = { player.player.seekTo((it * duration.value).toInt()) },
-      modifier = Modifier.fillMaxWidth().weight(1f)
+      modifier = Modifier.fillMaxWidth()
     )
-    IconButton(onClick = close,) {
-      Icon(painterResource(MR.images.ic_close), null, Modifier.size(30.dp), tint = MaterialTheme.colors.primary)
-    }
   }
 }
