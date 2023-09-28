769f7085eb36369bd29ad70b5dbe3c3744e66f10
avently <avently@local>
2023-09-29 01:12:31 +0300

    fix lib loading

--- scripts/desktop/download-lib-windows.sh
+++ scripts/desktop/download-lib-windows.sh
@@ -23,4 +23,5 @@ $WINDIR\\System32\\tar.exe -xf libsimplex.zip && \
 mv libsimplex.dll "$output_dir" && \
 mv libcrypto*.dll "$output_dir/deps" && \
 mv libffi*.dll "$output_dir/deps" && \
+mv libgmp*.dll "$output_dir/deps" && \
 rm libsimplex.zip
