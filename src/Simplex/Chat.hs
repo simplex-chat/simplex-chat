7679281fdaf30c1d48233f6b3e595ee8256e0b31
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-09-29 10:49:44 +0400

    refactor

--- src/Simplex/Chat.hs
+++ src/Simplex/Chat.hs
@@ -2580,10 +2580,8 @@ subscribeUserConnections onlyNeeded agentBatchSubscribe user@User {userId} = do
     getGroupMemberConns :: m ([Group], [ConnId], Map ConnId GroupMember)
     getGroupMemberConns = do
       gs <- withStore_ ("subscribeUserConnections " <> show userId <> ", getUserGroups") getUserGroups
-      let mPairs = concatMap (\(Group _ ms) -> mapMaybe mapMember ms) gs
+      let mPairs = concatMap (\(Group _ ms) -> mapMaybe (\m -> (,m) <$> memberConnId m) (filter (not . memberRemoved) ms)) gs
       pure (gs, map fst mPairs, M.fromList mPairs)
-      where
-        mapMember m = if memberRemoved m then Nothing else (,m) <$> memberConnId m
     getSndFileTransferConns :: m ([ConnId], Map ConnId SndFileTransfer)
     getSndFileTransferConns = do
       sfts <- withStore_ ("subscribeUserConnections " <> show userId <> ", getLiveSndFileTransfers") getLiveSndFileTransfers
