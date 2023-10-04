fba8162f7c6fc2b85481717c1d51338c5095fb97
Avently <7953703+avently@users.noreply.github.com>
2023-10-04 16:25:01 +0800

    unused lines

--- apps/multiplatform/desktop/build.gradle.kts
+++ apps/multiplatform/desktop/build.gradle.kts
@@ -1,7 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
-import java.nio.file.Files
-import java.nio.file.Path
 
 plugins {
   kotlin("multiplatform")
