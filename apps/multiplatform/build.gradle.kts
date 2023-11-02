c462dd37040ac4700cf0bdc41b60ad0e3139b5ad
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2023-11-03 04:59:16 +0800

    android, desktop: removed unused plugin (#3309)

--- apps/multiplatform/build.gradle.kts
+++ apps/multiplatform/build.gradle.kts
@@ -45,7 +45,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:${rootProject.extra["gradle.plugin.version"]}")
         classpath(kotlin("gradle-plugin", version = rootProject.extra["kotlin.version"] as String))
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.2")
         classpath("dev.icerock.moko:resources-generator:0.23.0")
 
         // NOTE: Do not place your application dependencies here; they belong
