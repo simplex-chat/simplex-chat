3f5f126bddbe218c596f395d01f4e7b26d7f48a6
Avently <7953703+avently@users.noreply.github.com>
2023-11-13 20:06:04 +0700

    connect existing device

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/remote/ConnectMobileView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/remote/ConnectMobileView.kt
@@ -14,17 +14,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import chat.simplex.common.model.*
-import chat.simplex.common.platform.appPlatform
+import chat.simplex.common.platform.*
 import chat.simplex.common.ui.theme.DEFAULT_PADDING
+import chat.simplex.common.ui.theme.DEFAULT_PADDING_HALF
 import chat.simplex.common.views.helpers.*
-import chat.simplex.common.views.usersettings.ProtocolServerView
+import chat.simplex.common.views.newchat.QRCode
 import chat.simplex.common.views.usersettings.SettingsActionItem
 import chat.simplex.res.MR
 import dev.icerock.moko.resources.compose.painterResource
@@ -35,6 +35,7 @@ fun ConnectMobileView(
   m: ChatModel
 ) {
   val connecting = remember { mutableStateOf(false) }
+  val invitation = remember { mutableStateOf<String?>(null) }
   val remoteHosts = remember { mutableStateListOf<RemoteHostInfo>() }
   val deviceName = m.controller.appPrefs.deviceNameForRemoteAccess
   LaunchedEffect(Unit) {
@@ -57,11 +58,55 @@ fun ConnectMobileView(
     },
     addMobileDevice = {
       ModalManager.start.showModalCloseable { close ->
-        AddMobileView(m, connecting, close)
+        ConnectMobileViewLayout(stringResource(MR.strings.add_mobile_device), invitation)
+        DisposableEffect(Unit) {
+          val oldRemoteHostId = m.currentRemoteHost.value?.remoteHostId
+          withBGApi {
+            val r = chatModel.controller.startRemoteHost(null)
+            if (r != null) {
+              connecting.value = true
+              invitation.value = r.second
+            }
+          }
+          onDispose {
+            if (m.currentRemoteHost.value?.remoteHostId == oldRemoteHostId) {
+              withBGApi {
+                chatController.stopRemoteHost(null)
+              }
+            }
+          }
+        }
       }
     },
     connectMobileDevice = {
-
+      ModalManager.start.showModalCloseable { close ->
+        ConnectMobileViewLayout(
+          title = stringResource(MR.strings.connect_mobile_device),
+          invitation = invitation,
+        )
+        val remoteHost = remember { mutableStateOf<RemoteHostInfo?>(null) }
+        LaunchedEffect(Unit) {
+          val r = m.controller.startRemoteHost(it.remoteHostId)
+          if (r != null) {
+            val (rh_, inv) = r
+            connecting.value = true
+            remoteHost.value = rh_
+            invitation.value = inv
+          }
+        }
+        DisposableEffect(remember { chatModel.currentRemoteHost }.value) {
+          if (remoteHost.value != null && chatModel.currentRemoteHost.value?.remoteHostId == remoteHost.value?.remoteHostId) {
+            close()
+          }
+          onDispose {
+            if (remoteHost.value != null && chatModel.currentRemoteHost.value?.remoteHostId != remoteHost.value?.remoteHostId) {
+              withBGApi {
+                chatController.stopRemoteHost(remoteHost.value?.remoteHostId)
+              }
+            }
+          }
+        }
+      }
     }
   )
 }
@@ -88,7 +133,7 @@ fun ConnectMobileLayout(
           Text(host.hostDeviceName)
         }
       }
-      SettingsActionItem(painterResource(MR.images.ic_smartphone), stringResource(MR.strings.add_mobile_device), addMobileDevice, disabled = connecting.value, extraPadding = true)
+      SettingsActionItem(painterResource(MR.images.ic_smartphone), stringResource(MR.strings.add_mobile_device), addMobileDevice, disabled = connecting.value, extraPadding = false)
     }
     SectionTextFooter(generalGetString(MR.strings.this_device_name_shared_with_mobile))
   }
@@ -148,4 +193,27 @@ private fun DeviceNameField(
       )
     }
   )
+}
+
+@Composable
+private fun ConnectMobileViewLayout(
+  title: String,
+  invitation: MutableState<String?>,
+) {
+  Column(
+    Modifier.fillMaxWidth().verticalScroll(rememberScrollState()),
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+  ) {
+    AppBarTitle(title)
+    SectionView {
+      val inv = invitation.value
+      if (inv != null) {
+        QRCode(
+          inv, Modifier
+            .padding(horizontal = DEFAULT_PADDING, vertical = DEFAULT_PADDING_HALF)
+            .aspectRatio(1f)
+        )
+      }
+    }
+  }
 }
