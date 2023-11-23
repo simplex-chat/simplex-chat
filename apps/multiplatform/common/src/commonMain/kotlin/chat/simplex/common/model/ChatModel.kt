b2dbb558f9a242f94ad5a7963ffebb34f0b3ea11
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2023-11-24 05:00:11 +0800

    android, desktop: connect remote desktop via multicast (#3442)
    * android, desktop: connect remote desktop via multicast
    
    * changes
    
    * string
    
    ---------
    
    Co-authored-by: Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/ChatModel.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/ChatModel.kt
@@ -2921,7 +2921,7 @@ enum class NotificationPreviewMode {
 }
 
 data class RemoteCtrlSession(
-  val ctrlAppInfo: CtrlAppInfo,
+  val ctrlAppInfo: CtrlAppInfo?,
   val appVersion: String,
   val sessionState: UIRemoteCtrlSessionState
 ) {
@@ -2939,6 +2939,7 @@ data class RemoteCtrlSession(
 @Serializable
 sealed class RemoteCtrlSessionState {
   @Serializable @SerialName("starting") object Starting: RemoteCtrlSessionState()
+  @Serializable @SerialName("searching") object Searching: RemoteCtrlSessionState()
   @Serializable @SerialName("connecting") object Connecting: RemoteCtrlSessionState()
   @Serializable @SerialName("pendingConfirmation") data class PendingConfirmation(val sessionCode: String): RemoteCtrlSessionState()
   @Serializable @SerialName("connected") data class Connected(val sessionCode: String): RemoteCtrlSessionState()
@@ -2946,6 +2947,8 @@ sealed class RemoteCtrlSessionState {
 
 sealed class UIRemoteCtrlSessionState {
   @Serializable @SerialName("starting") object Starting: UIRemoteCtrlSessionState()
+  @Serializable @SerialName("searching") object Searching: UIRemoteCtrlSessionState()
+  @Serializable @SerialName("found") data class Found(val remoteCtrl: RemoteCtrlInfo, val compatible: Boolean): UIRemoteCtrlSessionState()
   @Serializable @SerialName("connecting") data class Connecting(val remoteCtrl_: RemoteCtrlInfo? = null): UIRemoteCtrlSessionState()
   @Serializable @SerialName("pendingConfirmation") data class PendingConfirmation(val remoteCtrl_: RemoteCtrlInfo? = null, val sessionCode: String): UIRemoteCtrlSessionState()
   @Serializable @SerialName("connected") data class Connected(val remoteCtrl: RemoteCtrlInfo, val sessionCode: String): UIRemoteCtrlSessionState()
