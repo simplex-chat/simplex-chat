a798d904bfcc231c47be1cff3be5d9290c12bcd0
Jesse Horne <j.horne2796@gmail.com>
2023-11-26 12:51:32 -0500

    vars to vals

--- apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/StoreWindowState.kt
+++ apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/StoreWindowState.kt
@@ -8,10 +8,10 @@ import java.io.File
 
 @Serializable
 data class WindowPositionSize(
-  var width: Int = if (desktopPlatform.isLinux()) 1376 else 1366,
-  var height: Int = 768,
-  var x: Int = 0,
-  var y: Int = 0,
+  val width: Int = if (desktopPlatform.isLinux()) 1376 else 1366,
+  val height: Int = 768,
+  val x: Int = 0,
+  val y: Int = 0,
 )
 
 fun getStoredWindowState() : WindowPositionSize {
