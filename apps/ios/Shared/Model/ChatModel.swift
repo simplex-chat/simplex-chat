afe5c91bb208daedcb156e73671588b5bb6d404a
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-11-27 17:09:18 +0400

    refactor

--- apps/ios/Shared/Model/ChatModel.swift
+++ apps/ios/Shared/Model/ChatModel.swift
@@ -267,17 +267,19 @@ final class ChatModel: ObservableObject {
     func addChatItem(_ cInfo: ChatInfo, _ cItem: ChatItem) {
         // update previews
         if let i = getChatIndex(cInfo.id) {
-            switch cInfo {
+            chats[i].chatItems = switch cInfo {
             case .group:
                 if let currentPreviewItem = chats[i].chatItems.first {
                     if cItem.meta.itemTs >= currentPreviewItem.meta.itemTs {
-                        chats[i].chatItems = [cItem]
+                        [cItem]
+                    } else {
+                        [currentPreviewItem]
                     }
                 } else {
-                    chats[i].chatItems = [cItem]
+                    [cItem]
                 }
             default:
-                chats[i].chatItems = [cItem]
+                [cItem]
             }
             if case .rcvNew = cItem.meta.itemStatus {
                 chats[i].chatStats.unreadCount = chats[i].chatStats.unreadCount + 1
