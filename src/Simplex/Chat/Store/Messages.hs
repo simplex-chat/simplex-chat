a6759deddc86d44886d99180c0ff182058f89530
IC Rainbow <aenor.realm@gmail.com>
2023-12-03 21:23:18 +0200

    lift another condition to ids query

--- src/Simplex/Chat/Store/Messages.hs
+++ src/Simplex/Chat/Store/Messages.hs
@@ -528,6 +528,24 @@ getDirectChatPreviews_ db user@User {userId} filterTS_ search_ = do
         WHERE ct.user_id = :user_id
           AND ct.is_user = 0
           AND ct.deleted = 0
+          AND (
+            (
+              ((c.conn_level = 0 AND c.via_group_link = 0) OR ct.contact_used = 1)
+              AND c.connection_id = (
+                SELECT cc_connection_id FROM (
+                  SELECT
+                    cc.connection_id AS cc_connection_id,
+                    cc.created_at AS cc_created_at,
+                    (CASE WHEN cc.conn_status = :conn_ready OR cc.conn_status = :conn_snd_ready THEN 1 ELSE 0 END) AS cc_conn_status_ord
+                  FROM connections cc
+                  WHERE cc.user_id = ct.user_id AND cc.contact_id = ct.contact_id
+                  ORDER BY cc_conn_status_ord DESC, cc_created_at DESC
+                  LIMIT 1
+                )
+              )
+            )
+            OR c.connection_id IS NULL
+          )
           -- XXX: only for search
           -- AND (
           --   ct.local_display_name LIKE '%' || :search || '%'
@@ -543,7 +561,11 @@ getDirectChatPreviews_ db user@User {userId} filterTS_ search_ = do
         -- XXX: common part again
         ORDER BY i.item_ts DESC
       |]
-    params = [":user_id" := userId] <> [":search" := q | q <- maybeToList search_] -- <> [":ts" := ts | (_, ts) <- maybeToList filterTS_]
+    params =
+      [ ":user_id" := userId,
+        ":conn_ready" := ConnReady,
+        ":conn_snd_ready" := ConnSndReady
+      ] -- <> [":search" := q | q <- maybeToList search_] <> [":ts" := ts | (_, ts) <- maybeToList filterTS_]
 
 getDirectChatPreview :: DB.Connection -> User -> Int64 -> IO (Maybe AChat)
 getDirectChatPreview db user contactId = do
@@ -587,26 +609,8 @@ getDirectChatPreview db user contactId = do
         ) ChatStats ON ChatStats.contact_id = ct.contact_id
         LEFT JOIN chat_items ri ON ri.user_id = i.user_id AND ri.contact_id = i.contact_id AND ri.shared_msg_id = i.quoted_shared_msg_id
         WHERE ct.contact_id = ?
-          AND (
-            (
-              ((c.conn_level = 0 AND c.via_group_link = 0) OR ct.contact_used = 1)
-              AND c.connection_id = (
-                SELECT cc_connection_id FROM (
-                  SELECT
-                    cc.connection_id AS cc_connection_id,
-                    cc.created_at AS cc_created_at,
-                    (CASE WHEN cc.conn_status = ? OR cc.conn_status = ? THEN 1 ELSE 0 END) AS cc_conn_status_ord
-                  FROM connections cc
-                  WHERE cc.user_id = ct.user_id AND cc.contact_id = ct.contact_id
-                  ORDER BY cc_conn_status_ord DESC, cc_created_at DESC
-                  LIMIT 1
-                )
-              )
-            )
-            OR c.connection_id IS NULL
-          )
       |]
-      (CISRcvNew, contactId, ConnReady, ConnSndReady)
+      (CISRcvNew, contactId)
   where
     toDirectChatPreview :: UTCTime -> ContactRow :. MaybeConnectionRow :. ChatStatsRow :. MaybeChatItemRow :. QuoteRow -> AChat
     toDirectChatPreview currentTs (contactRow :. connRow :. statsRow :. ciRow_) =
