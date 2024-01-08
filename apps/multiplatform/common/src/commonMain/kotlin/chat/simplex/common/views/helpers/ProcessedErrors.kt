ea23fd8a59cbc857b374b3b0972f9a5023fb99e2
Avently <7953703+avently@users.noreply.github.com>
2024-01-09 00:10:37 +0700

    refactoring

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/helpers/ProcessedErrors.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/helpers/ProcessedErrors.kt
@@ -8,7 +8,6 @@ import chat.simplex.common.views.database.restartChatOrApp
 import chat.simplex.res.MR
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlin.math.max
 
 class ProcessedErrors <T: AgentErrorType>(val interval: Long) {
   private var lastShownTimestamp: Long = System.currentTimeMillis() - interval
@@ -18,44 +17,49 @@ class ProcessedErrors <T: AgentErrorType>(val interval: Long) {
   fun newError(error: T, offerRestart: Boolean) {
     timer.cancel()
     val job = withBGApi {
-      if (lastShownOfferRestart || !offerRestart) {
-        delay(max((lastShownTimestamp + interval) - System.currentTimeMillis(), 0))
+      val delayBeforeNext = (lastShownTimestamp + interval) - System.currentTimeMillis()
+      if ((lastShownOfferRestart || !offerRestart) && delayBeforeNext > 0) {
+        delay(delayBeforeNext)
       }
       lastShownTimestamp = System.currentTimeMillis()
       lastShownOfferRestart = offerRestart
       AlertManager.shared.hideAllAlerts()
-      when (error) {
-        is AgentErrorType.CRITICAL -> {
-          val title = generalGetString(MR.strings.agent_critical_error_title)
-          val text = generalGetString(MR.strings.agent_critical_error_desc).format(error.criticalErr)
-          try {
-            ntfManager.showMessage(title, text)
-          } catch (e: Throwable) {
-            Log.e(TAG, e.stackTraceToString())
-          }
-          if (offerRestart) {
-            AlertManager.shared.showAlertDialog(
-              title = title,
-              text = text,
-              confirmText = generalGetString(MR.strings.restart_chat_button),
-              onConfirm = {
-                withApi { restartChatOrApp() }
-              })
-          } else {
-            AlertManager.shared.showAlertMsg(
-              title = title,
-              text = text,
-            )
-          }
+      showMessage(error, offerRestart)
+    }
+    timer = job
+  }
+
+  private fun showMessage(error: T, offerRestart: Boolean) {
+    when (error) {
+      is AgentErrorType.CRITICAL -> {
+        val title = generalGetString(MR.strings.agent_critical_error_title)
+        val text = generalGetString(MR.strings.agent_critical_error_desc).format(error.criticalErr)
+        try {
+          ntfManager.showMessage(title, text)
+        } catch (e: Throwable) {
+          Log.e(TAG, e.stackTraceToString())
         }
-        is AgentErrorType.INTERNAL -> {
+        if (offerRestart) {
+          AlertManager.shared.showAlertDialog(
+            title = title,
+            text = text,
+            confirmText = generalGetString(MR.strings.restart_chat_button),
+            onConfirm = {
+              withApi { restartChatOrApp() }
+            })
+        } else {
           AlertManager.shared.showAlertMsg(
-            title = generalGetString(MR.strings.agent_internal_error_title),
-            text = generalGetString(MR.strings.agent_internal_error_desc).format(error.internalErr),
+            title = title,
+            text = text,
           )
         }
       }
+      is AgentErrorType.INTERNAL -> {
+        AlertManager.shared.showAlertMsg(
+          title = generalGetString(MR.strings.agent_internal_error_title),
+          text = generalGetString(MR.strings.agent_internal_error_desc).format(error.internalErr),
+        )
+      }
     }
-    timer = job
   }
 }
