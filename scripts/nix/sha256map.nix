f92354b866cf872d9547a25fa50a9e4ce4bd3444
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2024-01-14 15:53:44 +0000

    remove lazy

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."44c26880acb561b6159efaa031835d82eb6c28b2" = "0hk0r0d9z6k1qf8xng8wbh5q1l01x87cmj40wn6qhicnzj8fvdi5";
+  "https://github.com/simplex-chat/simplexmq.git"."191780a5fe2136fb7efe8efe01e45985147a0f70" = "19024j9s9z01lhfi349zrvgkff981s0qkca5apmwm8awcyrjk6d3";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
