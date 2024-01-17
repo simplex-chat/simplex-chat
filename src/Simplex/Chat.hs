c4af699bd59cab27f9c63e22255a640f7512d668
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-01-17 17:50:06 +0400

    refactor

--- src/Simplex/Chat.hs
+++ src/Simplex/Chat.hs
@@ -1733,28 +1733,31 @@ processChatCommand' vr = \case
                 ci <- saveSndChatItem user (CDGroupSnd gInfo) msg (CISndGroupEvent gEvent)
                 toView $ CRNewChatItem user (AChatItem SCTGroup SMDSnd (GroupChat gInfo) ci)
           pure CRMemberRoleUser {user, groupInfo = gInfo, member = m {memberRole = memRole}, fromRole = mRole, toRole = memRole}
-  APIBlockMemberForAll groupId gmId blocked -> withUser $ \user -> do
+  APIBlockMemberForAll groupId memberId blocked -> withUser $ \user -> do
     Group gInfo@GroupInfo {membership} members <- withStore $ \db -> getGroup db vr user groupId
-    let (blockedMember, remainingMembers) = partition ((== gmId) . groupMemberId') members
-    case blockedMember of
-      [bm] -> do
+    case findMember memberId members of
+      Just (bm, remainingMembers) -> do
         let GroupMember {memberId = bmMemberId, memberRole = bmRole, memberProfile = bmp, blockedByGroupMemberId} = bm
         assertUserGroupRole gInfo $ max GRAdmin bmRole
         withChatLock "blockForAll" . procCmd $
           if blocked /= isJust blockedByGroupMemberId
             then do
               (msg, _) <- sendGroupMessage' user gInfo remainingMembers $ XGrpMemBlock bmMemberId blocked
-              let ciContent = CISndGroupEvent $ SGEMemberBlocked gmId (fromLocalProfile bmp) blocked
+              let ciContent = CISndGroupEvent $ SGEMemberBlocked memberId (fromLocalProfile bmp) blocked
               ci <- saveSndChatItem user (CDGroupSnd gInfo) msg ciContent
               toView $ CRNewChatItem user (AChatItem SCTGroup SMDSnd (GroupChat gInfo) ci)
               bm' <- withStore $ \db -> do
                 let byGMId_ = if blocked then Just (groupMemberId' membership) else Nothing
-                liftIO $ updateGroupMemberBlocked db user groupId gmId byGMId_
-                getGroupMember db user groupId gmId
+                liftIO $ updateGroupMemberBlocked db user groupId memberId byGMId_
+                getGroupMember db user groupId memberId
               toggleNtf user bm' (not blocked)
               pure CRMemberBlockedForAllUser {user, groupInfo = gInfo, member = bm', blocked}
             else throwChatError $ CECommandError $ if blocked then "already blocked" else "already unblocked"
-      _ -> throwChatError $ CEException "expected to find a single blocked member"
+      Nothing -> throwChatError $ CEException "expected to find a single blocked member"
+      where
+        findMember mId ms = case break ((== mId) . groupMemberId') ms of
+          (_, []) -> Nothing
+          (ms1, bm : ms2) -> Just (bm, ms1 <> ms2)
   APIRemoveMember groupId memberId -> withUser $ \user -> do
     Group gInfo members <- withStore $ \db -> getGroup db vr user groupId
     case find ((== memberId) . groupMemberId') members of
