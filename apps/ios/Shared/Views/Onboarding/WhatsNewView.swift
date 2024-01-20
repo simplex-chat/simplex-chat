f188118643ea71746ad852eb6af1ca60bdb5bf5b
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-01-20 09:07:57 +0000

    ui: update whats new (#3716)

--- apps/ios/Shared/Views/Onboarding/WhatsNewView.swift
+++ apps/ios/Shared/Views/Onboarding/WhatsNewView.swift
@@ -334,11 +334,6 @@ private let versionDescriptions: [VersionDescription] = [
                 description: "Recent history and improved [directory bot](simplex:/contact#/?v=1-4&smp=smp%3A%2F%2Fu2dS9sG8nMNURyZwqASV4yROM28Er0luVTx5X1CsMrU%3D%40smp4.simplex.im%2FeXSPwqTkKyDO3px4fLf1wx3MvPdjdLW3%23%2F%3Fv%3D1-2%26dh%3DMCowBQYDK2VuAyEAaiv6MkMH44L2TcYrt_CsX3ZvM11WgbMEUn0hkIKTOho%253D%26srv%3Do5vmywmrnaxalvz6wi3zicyftgio6psuvyniis6gco6bp6ekl4cqj4id.onion)."
             ),
             FeatureDescription(
-                icon: "person.2",
-                title: "Safer groups",
-                description: "- block a member to stop spam for all.\n- contact address is not shared."
-            ),
-            FeatureDescription(
                 icon: "battery.50",
                 title: "Improved message delivery",
                 description: "With reduced battery usage."
