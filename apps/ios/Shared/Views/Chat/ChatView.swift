2404ba362b4eff613605d2ea3eac2c13bee91939
Avently <avently@local>
2024-02-13 18:24:50 +0700

    end active call button

--- apps/ios/Shared/Views/Chat/ChatView.swift
+++ apps/ios/Shared/Views/Chat/ChatView.swift
@@ -160,9 +160,13 @@ struct ChatView: View {
                 case let .direct(contact):
                     HStack {
                         let callsPrefEnabled = contact.mergedPreferences.calls.enabled.forUser
-                        if callsPrefEnabled && chatModel.activeCall == nil {
-                            callButton(contact, .audio, imageName: "phone")
-                                .disabled(!contact.ready || !contact.active)
+                        if callsPrefEnabled {
+                            if chatModel.activeCall == nil {
+                                callButton(contact, .audio, imageName: "phone")
+                                    .disabled(!contact.ready || !contact.active)
+                            } else if let call = chatModel.activeCall, call.contact.id == cInfo.id {
+                                endCallButton(call)
+                            }
                         }
                         Menu {
                             if callsPrefEnabled && chatModel.activeCall == nil {
@@ -422,7 +426,19 @@ struct ChatView: View {
             Image(systemName: imageName)
         }
     }
-    
+
+    private func endCallButton(_ call: Call) -> some View {
+        Button {
+            if let uuid = call.callkitUUID {
+                CallController.shared.endCall(callUUID: uuid)
+            } else {
+                CallController.shared.endCall(call: call) {}
+            }
+        } label: {
+            Image(systemName: "phone.down.fill").tint(.red)
+        }
+    }
+
     private func searchButton() -> some View {
         Button {
             searchMode = true
