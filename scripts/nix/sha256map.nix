edff527fdec5324c9f0917100b950a63c4943ba9
IC Rainbow <aenor.realm@gmail.com>
2024-02-16 18:02:17 +0200

    bump simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."6f62d7ff05b9d25bca4f822c04ee83a102c34e42" = "1d1wbfk1p7r5mxs0mrgzg1f6sy89r4vvqfn911vss3c02nhaj080";
+  "https://github.com/simplex-chat/simplexmq.git"."194a7bb58e59c6389a30b13f8865914bbfe88eb9" = "1mf118h619hjdnqnicrp99pkifvpg15a2wy5zs1n5j0r46f51sja";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
