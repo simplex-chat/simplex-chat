10a9c2eb81eb567e45a537afe7fda875cfa0f04e
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-03-21 11:58:17 +0400

    remove comment

--- apps/ios/Shared/Views/Chat/ChatView.swift
+++ apps/ios/Shared/Views/Chat/ChatView.swift
@@ -979,7 +979,6 @@ struct ChatView: View {
                 title: NSLocalizedString("Hide", comment: "chat item action"),
                 image: UIImage(systemName: "eye.slash")
             ) { _ in
-                // With animation it looks bad because of UIKit context menu involved
                 withAnimation {
                     revealed = false
                 }
@@ -1051,7 +1050,6 @@ struct ChatView: View {
                 title: NSLocalizedString("Reveal", comment: "chat item action"),
                 image: UIImage(systemName: "eye")
             ) { _ in
-                // With animation it looks bad because of UIKit context menu involved
                 withAnimation {
                     revealed = true
                 }
