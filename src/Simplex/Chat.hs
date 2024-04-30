d118958b3127a7334e56cce9796837d844af65c2
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-04-30 15:07:07 +0400

    refactor

--- src/Simplex/Chat.hs
+++ src/Simplex/Chat.hs
@@ -1084,17 +1084,12 @@ processChatCommand' vr = \case
     CTLocal -> pure $ chatCmdError (Just user) "not supported"
     CTContactRequest -> pure $ chatCmdError (Just user) "not supported"
   APIMarkConversationDeleted cRef@(ChatRef cType _chatId) deleted -> withUser $ \user -> case cType of
-    CTDirect
-      | deleted -> do
-        _ <- processChatCommand $ APIClearChat cRef
-        -- TODO ct <- db: set conversation_deleted to True
-        -- (can be unset on any new chat item (see updateChatTs), and on APIMarkConversationDeleted False)
-        -- pure $ CRContactConversationDeleted user ct deleted
-        ok_
-      | otherwise -> do
-        -- TODO ct <- db: set conversation_deleted to False
-        -- pure $ CRContactConversationDeleted user ct deleted
-        ok_
+    CTDirect -> do
+      when deleted $ void $ processChatCommand $ APIClearChat cRef
+      -- TODO ct <- db: set conversation_deleted to deleted
+      -- (can be unset on any new chat item (see updateChatTs), and on APIMarkConversationDeleted False)
+      -- pure $ CRContactConversationDeleted user ct deleted
+      ok_
     _ -> pure $ chatCmdError (Just user) "not supported"
   APIClearChat (ChatRef cType chatId) -> withUser $ \user@User {userId} -> case cType of
     CTDirect -> do
