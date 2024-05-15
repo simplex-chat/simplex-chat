02cb0e58111e81d2f3201a34d7cfe6829c7a73bb
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-05-15 21:04:50 +0400

    icon

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/NetworkAndServers.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/NetworkAndServers.kt
@@ -584,7 +584,7 @@ private fun SMPProxyFallbackPicker(
     generalGetString(MR.strings.network_smp_proxy_fallback_allow_downgrade),
     smpProxyFallback,
     values,
-    icon = painterResource(MR.images.ic_arrow_downward),
+    icon = painterResource(MR.images.ic_settings_ethernet),
     enabled = enabled,
     onSelected = {
       showModal {
