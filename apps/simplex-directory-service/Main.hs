63ca1351db6aaef1824f7d127038357636bf016c
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-05-22 21:11:05 +0100

    same config

--- apps/simplex-directory-service/Main.hs
+++ apps/simplex-directory-service/Main.hs
@@ -5,19 +5,11 @@ module Main where
 import Directory.Options
 import Directory.Service
 import Directory.Store
-import Simplex.Chat.Controller (ChatConfig (..))
 import Simplex.Chat.Core
 import Simplex.Chat.Terminal (terminalChatConfig)
-import Simplex.Messaging.Agent.Env.SQLite (AgentConfig (..))
-
-directoryCfg :: ChatConfig
-directoryCfg =
-  terminalChatConfig
-    { agentConfig = (agentConfig terminalChatConfig) {persistErrorInterval = 30}
-    }
 
 main :: IO ()
 main = do
   opts@DirectoryOpts {directoryLog} <- welcomeGetOpts
   st <- restoreDirectoryStore directoryLog
-  simplexChatCore directoryCfg (mkChatOpts opts) $ directoryService st opts
+  simplexChatCore terminalChatConfig (mkChatOpts opts) $ directoryService st opts
