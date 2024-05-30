7b5b747b19c16ffe00192a273e18e4a061e49ea4
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2024-05-30 23:07:58 +0700

    desktop: performant drawing of wallpaper (#4249)
    * desktop: low quality drawing of wallpaper
    
    * fast drawing
    
    * clean up
    
    * unused

--- apps/multiplatform/desktop/src/jvmMain/kotlin/chat/simplex/desktop/Main.kt
+++ apps/multiplatform/desktop/src/jvmMain/kotlin/chat/simplex/desktop/Main.kt
@@ -3,7 +3,6 @@ package chat.simplex.desktop
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.*
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -17,6 +16,8 @@ import kotlinx.coroutines.*
 import java.io.File
 
 fun main() {
+  // Disable hardware acceleration
+  //System.setProperty("skiko.renderApi", "SOFTWARE")
   initHaskell()
   runMigrations()
   initApp()
