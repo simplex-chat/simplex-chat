72b37aeff1e035b01e72441d5b44fe1dc6eb0d47
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-06-27 21:40:00 +0400

    upd

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."40973571bdcf0abbe2ca2e48748d20f639b48b8d" = "1kdfzfpym19iqjp7vzclc44wb53886gxaccpyd6q3j09zzz6jg7x";
+  "https://github.com/simplex-chat/simplexmq.git"."2a3a2c502a88e1c8ffb103c2352ac2a140ec4945" = "05n1im5yifyc5qmf4dpg2c42xzmci5szgd1y7mhgqcmg3rymnq9k";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
