5bcca0afe41894268699b568980acaf8e668328a
Avently <7953703+avently@users.noreply.github.com>
2024-07-11 15:56:57 +0700

    refactor

--- apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/views/helpers/AppUpdater.kt
+++ apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/views/helpers/AppUpdater.kt
@@ -49,17 +49,17 @@ data class GitHubAsset(
 )
 
 private var updateCheckerJob: Job = Job()
-fun setupUpdateChecker() = withBGApi {
+fun setupUpdateChecker() = withLongRunningApi {
   updateCheckerJob.cancel()
   if (appPrefs.appUpdateChannel.get() == AppUpdatesChannel.NONE) {
-    return@withBGApi
+    return@withLongRunningApi
   }
   checkForUpdate()
   createUpdateJob()
 }
 
 private fun createUpdateJob() {
-  updateCheckerJob = withBGApi {
+  updateCheckerJob = withLongRunningApi {
     delay(24 * 60 * 60 * 1000)
     checkForUpdate()
     createUpdateJob()
@@ -147,7 +147,7 @@ private fun skipRelease(release: GitHubRelease) {
 
 private fun downloadAsset(asset: GitHubAsset) {
   showToast(generalGetString(MR.strings.app_check_for_updates_download_started))
-  withBGApi {
+  withLongRunningApi {
     val client = setupHttpClient()
     try {
       val request = Request.Builder().url(asset.browserDownloadUrl).addHeader("User-agent", "curl").build()
@@ -187,4 +187,58 @@ private fun downloadAsset(asset: GitHubAsset) {
       Log.e(TAG, "Failed to download the asset from release: ${e.stackTraceToString()}")
     }
   }
+}
+
+
+private fun chooseGitHubReleaseAssets(release: GitHubRelease): List<GitHubAsset> {
+  val process = Runtime.getRuntime().exec("which dpkg").onExit().join()
+  val isDebianBased = process.exitValue() == 0
+  // Show all available .deb packages and user will choose the one that works on his system
+  val res = if (isDebianBased) {
+    release.assets.filter { it.name.lowercase().endsWith(".deb") }
+  } else {
+    release.assets.filter { it.name == desktopPlatform.githubAssetName }
+  }
+  return res
+}
+
+private fun installAppUpdate(file: File) {
+  when {
+    desktopPlatform.isLinux() -> {
+      val process = Runtime.getRuntime().exec("xdg-open ${file.absolutePath}").onExit().join()
+      val startedInstallation = process.exitValue() == 0 && process.children().count() > 0
+      if (!startedInstallation) {
+        Log.e(TAG, "Error starting installation: ${process.inputReader().use { it.readLines().joinToString("\n") }}${process.errorStream.use { String(it.readAllBytes()) }}")
+        // Failed to start installation. show directory with the file for manual installation
+        desktopOpenDir(file.parentFile)
+      }
+    }
+    desktopPlatform.isWindows() -> {
+      val process = Runtime.getRuntime().exec("msiexec /i ${file.absolutePath}"/* /qb */).onExit().join()
+      val startedInstallation = process.exitValue() == 0
+      if (!startedInstallation) {
+        Log.e(TAG, "Error starting installation: ${process.inputReader().use { it.readLines().joinToString("\n") }}${process.errorStream.use { String(it.readAllBytes()) }}")
+        // Failed to start installation. show directory with the file for manual installation
+        desktopOpenDir(file.parentFile)
+      }
+    }
+    desktopPlatform.isMac() -> {
+      val process = Runtime.getRuntime().exec("hdiutil mount ${file.absolutePath}").onExit().join()
+      val startedInstallation = process.exitValue() == 0
+      if (!startedInstallation) {
+        Log.e(TAG, "Error starting installation: ${process.inputReader().use { it.readLines().joinToString("\n") }}${process.errorStream.use { String(it.readAllBytes()) }}")
+        // Failed to start installation. show directory with the file for manual installation
+        desktopOpenDir(file.parentFile)
+        return
+      }
+      var process2 = Runtime.getRuntime().exec("cp -R /Volumes/SimpleX/SimpleX.app /Applications").onExit().join()
+      val copiedSuccessfully = process.exitValue() == 0
+      if (!copiedSuccessfully) {
+        Log.e(TAG, "Error copying the app: ${process.inputReader().use { it.readLines().joinToString("\n") }}${process.errorStream.use { String(it.readAllBytes()) }}")
+        // Failed to start installation. show directory with the file for manual installation
+        desktopOpenDir(file.parentFile)
+      }
+    }
+  }
+  //  file.delete()
 }
