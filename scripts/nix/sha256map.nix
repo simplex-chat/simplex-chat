a73abfe6420ad60aef0f8c638778d61472fe1108
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-07-11 19:29:56 +0400

    core: update simplexmq (proxy err encoding) (#4441)

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."017469b2de65c7e3ef1d680c2da466b320d1b061" = "1c6aap31sa4wdv7x771q2l8bs499qg58rrnx80blj33y2zl877c9";
+  "https://github.com/simplex-chat/simplexmq.git"."ff2b00a0299343e39bd8d56fc43d97f7a8fe214c" = "05h3mclw2qvdngvvjm0xsb39v1sycia69xhxrfmg6yj3a3240byn";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
