3260f13b44feaf6b93d7f64b1cfa4bb5fdfb78fd
Arturs Krumins <auth@levitatingpineapple.com>
2024-07-17 21:16:06 +0300

    ios: align compose background, row height and fallback images for share-sheet (#4467)
    Co-authored-by: Evgeny Poberezkin <evgeny@poberezkin.com>

--- apps/ios/SimpleXChat/ChatUtils.swift
+++ apps/ios/SimpleXChat/ChatUtils.swift
@@ -51,3 +51,12 @@ private func canForwardToChat(_ cInfo: ChatInfo) -> Bool {
     }
 }
 
+public func chatIconName(_ cInfo: ChatInfo) -> String {
+    switch cInfo {
+    case .direct: "person.crop.circle.fill"
+    case .group: "person.2.circle.fill"
+    case .local: "folder.circle.fill"
+    case .contactRequest: "person.crop.circle.fill"
+    default:  "circle.fill"
+    }
+}
