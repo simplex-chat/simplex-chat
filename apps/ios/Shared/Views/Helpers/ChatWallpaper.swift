f5b032ea77ddd97bced482ea609b9f8083eb62f3
Levitating Pineapple <noreply@levitatingpineapple.com>
2024-07-24 16:02:18 +0300

    ios: fix crash, when displaying wallpaper

--- apps/ios/Shared/Views/Helpers/ChatWallpaper.swift
+++ apps/ios/Shared/Views/Helpers/ChatWallpaper.swift
@@ -23,6 +23,8 @@ struct ChatViewBackground: ViewModifier {
                 var image = context.resolve(image)
                 let rect = CGRectMake(0, 0, size.width, size.height)
                 func repeatDraw(_ imageScale: CGFloat) {
+                    // Prevent range bounds crash and dividing by zero
+                    if [size.height, size.width, image.size.height, image.size.width].contains(.zero) { return }
                     image.shading = .color(tint)
                     let scale = imageScale * 1.57 // for some reason a wallpaper on iOS looks smaller than on Android
                     for h in 0 ... Int(size.height / image.size.height / scale) {
