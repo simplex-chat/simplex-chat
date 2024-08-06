f893ad15de934737f2f440bf66d71cb2f84f091c
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-08-06 12:03:25 +0400

    ui: only show subsription summary indicator after any chat is created; ios: fix servers summary sheet dismissal, screen protection (#4590)

--- apps/ios/Shared/Views/ChatList/ChatListView.swift
+++ apps/ios/Shared/Views/ChatList/ChatListView.swift
@@ -266,13 +266,19 @@ struct SubsStatusIndicator: View {
     @AppStorage(DEFAULT_SHOW_SUBSCRIPTION_PERCENTAGE) private var showSubscriptionPercentage = false
 
     var body: some View {
-        Button {
-            showServersSummary = true
-        } label: {
-            HStack(spacing: 4) {
-                SubscriptionStatusIndicatorView(subs: subs, hasSess: hasSess)
-                if showSubscriptionPercentage {
-                    SubscriptionStatusPercentageView(subs: subs, hasSess: hasSess)
+        ZStack {
+            if subs.total == 0 && !hasSess {
+                EmptyView()
+            } else {
+                Button {
+                    showServersSummary = true
+                } label: {
+                    HStack(spacing: 4) {
+                        SubscriptionStatusIndicatorView(subs: subs, hasSess: hasSess)
+                        if showSubscriptionPercentage {
+                            SubscriptionStatusPercentageView(subs: subs, hasSess: hasSess)
+                        }
+                    }
                 }
             }
         }
@@ -282,8 +288,9 @@ struct SubsStatusIndicator: View {
         .onDisappear {
             stopTimer()
         }
-        .sheet(isPresented: $showServersSummary) {
+        .appSheet(isPresented: $showServersSummary) {
             ServersSummaryView()
+                .environment(\EnvironmentValues.refresh as! WritableKeyPath<EnvironmentValues, RefreshAction?>, nil)
         }
     }
 
