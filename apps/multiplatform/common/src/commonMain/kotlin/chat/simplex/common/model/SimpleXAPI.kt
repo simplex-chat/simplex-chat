c34b1248d73ed20e623614b49ad3e5e1ac0a90b9
Diogo Cunha <diogofncunha@gmail.com>
2024-08-06 12:40:33 +0100

    one hand ui to be android default

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/SimpleXAPI.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/SimpleXAPI.kt
@@ -225,7 +225,7 @@ class AppPreferences {
   val iosCallKitEnabled = mkBoolPreference(SHARED_PREFS_IOS_CALL_KIT_ENABLED, true)
   val iosCallKitCallsInRecents = mkBoolPreference(SHARED_PREFS_IOS_CALL_KIT_CALLS_IN_RECENTS, false)
 
-  val oneHandUI = mkBoolPreference(SHARED_PREFS_ONE_HAND_UI, false)
+  val oneHandUI = mkBoolPreference(SHARED_PREFS_ONE_HAND_UI, if (appPlatform.isAndroid) true else false)
 
   private fun mkIntPreference(prefName: String, default: Int) =
     SharedPreference(
