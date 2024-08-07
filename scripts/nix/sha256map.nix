605498a2b02dc6d134e6c91fea69817a4ba05985
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-08-07 12:27:33 +0100

    core: stop stats when chat is stopped

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."a76e15fd77d68a09bc43820c1cf6bd1c71700100" = "07vrjpid03w8f1himhvaaljzspwxxkln814k5kx0yw9hhmabh7zy";
+  "https://github.com/simplex-chat/simplexmq.git"."df8e3552f2aec54e342b08dd4286bca5de0449c5" = "1ahwnkzkrvw1jlwgqmkay09m90zbdgzf1rzvn9370dy1kz943cgf";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
