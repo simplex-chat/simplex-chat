f0f68210f0ed68bf40918cb3fcac623be1f5db9a
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-08-08 20:29:00 +0400

    remove

--- apps/ios/Shared/Model/ChatModel.swift
+++ apps/ios/Shared/Model/ChatModel.swift
@@ -125,8 +125,6 @@ final class ChatModel: ObservableObject {
     @Published var pasteboardHasStrings: Bool = UIPasteboard.general.hasStrings
     @Published var networkInfo = UserNetworkInfo(networkType: .other, online: true)
 
-    @Published var scenePhaseActive = false
-
     var messageDelivery: Dictionary<Int64, () -> Void> = [:]
 
     var filesToDelete: Set<URL> = []
