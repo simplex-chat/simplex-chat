0a4ff2e35ff4701c4bac353a5a030df111bda3e4
Evgeny <evgeny@poberezkin.com>
2024-08-09 22:30:30 +0100

    core: reduce usage of STM transactions for better performance and memory usage (#4636)
    * core: reduce usage of STM transactions for better performance and memory usage
    
    * simplexmq
    
    * fix test crash
    
    * enable all tests
    
    * simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."7d8457263bb77c6d9cb1cd102c3c70177e75327f" = "1n95mp2qmx9nj25zzqibkgx7s23shw7c26mf42bw661aprx4v24m";
+  "https://github.com/simplex-chat/simplexmq.git"."47dd0c4c64142cf5106c5448429d5b30273e0f7e" = "1k5kqkij3y65grvphsb2hbg0f7991anxx1ks651bxv7cgq09qh38";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
