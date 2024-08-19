7c5b50100b53f128b94589324410db5dff3ade7f
Levitating Pineapple <noreply@levitatingpineapple.com>
2024-08-19 21:01:23 +0300

    cleanup

--- apps/ios/Tests iOS/Tests_iOS.swift
+++ apps/ios/Tests iOS/Tests_iOS.swift
@@ -49,26 +49,26 @@ class ArrayMapTests: XCTestCase {
 
     private var arrayMap: ArrayMap<Element> { ["a", "b", "c", "d"] }
 
-    func testArrayMapAccess() {
+    func testAccess() {
         XCTAssertEqual(arrayMap["a"], "a")
         XCTAssertEqual(arrayMap[1], "b")
     }
 
-    func testArrayMapAppend() {
+    func testAppend() {
         var am = arrayMap
         am.append("e")
         XCTAssertEqual(am.elements, ["a", "b", "c", "d", "e"])
         XCTAssertEqual(am.hashMap, ["a": 0, "b": 1, "c": 2, "d": 3, "e": 4])
     }
 
-    func testArrayMapInsert() {
+    func testInsert() {
         var am = arrayMap
         am.insert("c1", at: 3)
         XCTAssertEqual(am.elements, ["a", "b", "c", "c1", "d"])
         XCTAssertEqual(am.hashMap, ["a": 0, "b": 1, "c": 2, "c1": 3, "d": 4])
     }
 
-    func testArrayMapMove() {
+    func testMove() {
         var am = arrayMap
         // Move to start
         am.move("c")
@@ -84,14 +84,14 @@ class ArrayMapTests: XCTestCase {
         XCTAssertEqual(am.hashMap, ["a": 0, "b": 1, "c": 2, "d": 3])
     }
 
-    func testArrayMapRemoveById() {
+    func testRemoveById() {
         var am = arrayMap
         am.remove(id: "b")
         XCTAssertEqual(am.elements, ["a", "c", "d"])
         XCTAssertEqual(am.hashMap, ["a": 0, "c": 1, "d": 2])
     }
 
-    func testArrayMapRemoveByIndex() {
+    func testRemoveByIndex() {
         var am = arrayMap
         am.remove(at: 1)
         XCTAssertEqual(am.elements, ["a", "c", "d"])
