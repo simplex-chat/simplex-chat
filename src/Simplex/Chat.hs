64d97a903bf64327e1ed9d126b5ee275c4ef656a
Diogo <diogofncunha@gmail.com>
2024-08-20 16:35:40 +0100

    simplify and reduce vars scope

--- src/Simplex/Chat.hs
+++ src/Simplex/Chat.hs
@@ -1656,39 +1656,38 @@ processChatCommand' vr = \case
       Just conn' -> pure $ CRConnectionIncognitoUpdated user conn'
       Nothing -> throwChatError CEConnectionIncognitoChangeProhibited
   APIChangeConnectionUser connId newUserId -> withUser $ \user@User {userId} -> do
-    cfg <- asks config
-    newUser <- privateGetUser newUserId
     conn <- withFastStore $ \db -> getPendingContactConnection db userId connId
     let PendingContactConnection {pccConnStatus, connReqInv, customUserProfileId} = conn
 
     case (pccConnStatus, connReqInv) of
       (ConnNew, Just (CRInvitationUri crData _)) -> do
+        cfg <- asks config
+        newUser <- privateGetUser newUserId
         newUserServers <- L.map (\ServerCfg {server} -> protoServer server) . useServers cfg SPSMP <$> withFastStore' (`getProtocolServers` newUser)
         let ConnReqUriData {crSmpQueues = q :| _} = crData
             SMPQueueUri {queueAddress = SMPQueueAddress {smpServer}} = q
 
-        connId' <-
-          if smpServer `elem` newUserServers
-            then do
-              -- Safe to update connection
-              withAgent $ \a -> changeConnectionUser a (aUserId user) (aConnId' conn) (aUserId newUser)
-              _ <- withFastStore' $ \db -> do
-                _ <- updatePCCUser db userId conn newUserId
-                forM_ customUserProfileId $ \profileId ->
-                  deletePCCIncognitoProfile db user profileId
-              pure connId
-            else do
-              -- Servers do not match, requires creation of a new connection
-              subMode <- chatReadVar subscriptionMode
-              (agConnId, cReq) <- withAgent $ \a -> createConnection a (aUserId user) True SCMInvitation Nothing IKPQOn subMode
-              conn' <- withFastStore' $ \db -> do
-                deleteConnectionRecord db user connId
-                forM_ customUserProfileId $ \profileId ->
-                  deletePCCIncognitoProfile db user profileId
-                createDirectConnection db newUser agConnId cReq ConnNew Nothing subMode initialChatVersion PQSupportOn
-              deleteAgentConnectionAsync user (aConnId' conn)
-              let PendingContactConnection {pccConnId} = conn'
-              pure pccConnId
+        connId' <- case smpServer `elem` newUserServers of
+          True -> do
+            -- Safe to update connection
+            withAgent $ \a -> changeConnectionUser a (aUserId user) (aConnId' conn) (aUserId newUser)
+            _ <- withFastStore' $ \db -> do
+              _ <- updatePCCUser db userId conn newUserId
+              forM_ customUserProfileId $ \profileId ->
+                deletePCCIncognitoProfile db user profileId
+            pure connId
+          False -> do
+            -- Servers do not match, requires creation of a new connection
+            subMode <- chatReadVar subscriptionMode
+            (agConnId, cReq) <- withAgent $ \a -> createConnection a (aUserId user) True SCMInvitation Nothing IKPQOn subMode
+            conn' <- withFastStore' $ \db -> do
+              deleteConnectionRecord db user connId
+              forM_ customUserProfileId $ \profileId ->
+                deletePCCIncognitoProfile db user profileId
+              createDirectConnection db newUser agConnId cReq ConnNew Nothing subMode initialChatVersion PQSupportOn
+            deleteAgentConnectionAsync user (aConnId' conn)
+            let PendingContactConnection {pccConnId} = conn'
+            pure pccConnId
         conn' <- withFastStore $ \db -> getPendingContactConnection db newUserId connId'
         pure $ CRConnectionUserChanged user conn conn' newUser
       _ -> throwChatError CEInvalidConnReq
