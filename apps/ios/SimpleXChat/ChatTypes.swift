26c410aef7385f2ce7e488b90cdf7b483acd7123
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-08-27 17:27:13 +0100

    ios: show correct message times

--- apps/ios/SimpleXChat/ChatTypes.swift
+++ apps/ios/SimpleXChat/ChatTypes.swift
@@ -2765,26 +2765,8 @@ public struct CITimed: Decodable, Hashable {
     public var deleteAt: Date?
 }
 
-let msgTimeFormat = Date.FormatStyle.dateTime.hour().minute()
-let msgDateFormat = Date.FormatStyle.dateTime.day(.twoDigits).month(.twoDigits)
-
 public func formatTimestampText(_ date: Date) -> Text {
-    Text(verbatim: date.formatted(recent(date) ? msgTimeFormat : msgDateFormat))
-}
-
-private func recent(_ date: Date) -> Bool {
-    let now = Date()
-    let calendar = Calendar.current
-
-    guard let previousDay = calendar.date(byAdding: DateComponents(day: -1), to: now),
-          let previousDay18 = calendar.date(bySettingHour: 18, minute: 0, second: 0, of: previousDay),
-          let currentDay00 = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: now),
-          let currentDay12 = calendar.date(bySettingHour: 12, minute: 0, second: 0, of: now) else {
-        return false
-    }
-
-    let isSameDay = calendar.isDate(date, inSameDayAs: now)
-    return isSameDay || (now < currentDay12 && date >= previousDay18 && date < currentDay00)
+    Text(verbatim: date.formatted(date: .omitted, time: .shortened))
 }
 
 public enum CIStatus: Decodable, Hashable {
