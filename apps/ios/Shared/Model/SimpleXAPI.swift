dfe16991d01eda56c670d1f220c3fbb719411fd0
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2024-08-28 14:49:11 +0000

    ios: make CallKit calls fire in time after cold start (#4787)
    * ios: make CallKit calls fire in time after cold start
    
    * longer wait period
    
    * uncomment
    
    * change
    
    * change
    
    * removed commented code
    
    * ios: update core library
    
    ---------
    
    Co-authored-by: Evgeny Poberezkin <evgeny@poberezkin.com>

--- apps/ios/Shared/Model/SimpleXAPI.swift
+++ apps/ios/Shared/Model/SimpleXAPI.swift
@@ -2182,9 +2182,11 @@ func refreshCallInvitations() async throws {
     }
 }
 
-func justRefreshCallInvitations() throws {
+func justRefreshCallInvitations() async throws {
     let callInvitations = try apiGetCallInvitationsSync()
-    ChatModel.shared.callInvitations = callsByChat(callInvitations)
+    await MainActor.run {
+        ChatModel.shared.callInvitations = callsByChat(callInvitations)
+    }
 }
 
 private func callsByChat(_ callInvitations: [RcvCallInvitation]) -> [ChatId: RcvCallInvitation] {
@@ -2194,8 +2196,9 @@ private func callsByChat(_ callInvitations: [RcvCallInvitation]) -> [ChatId: Rcv
 }
 
 func activateCall(_ callInvitation: RcvCallInvitation) {
-    if !callInvitation.user.showNotifications { return }
     let m = ChatModel.shared
+    logger.debug("reportNewIncomingCall activeCallUUID \(String(describing: m.activeCall?.callUUID)) invitationUUID \(String(describing: callInvitation.callUUID))")
+    if !callInvitation.user.showNotifications || m.activeCall?.callUUID == callInvitation.callUUID { return }
     CallController.shared.reportNewIncomingCall(invitation: callInvitation) { error in
         if let error = error {
             DispatchQueue.main.async {
