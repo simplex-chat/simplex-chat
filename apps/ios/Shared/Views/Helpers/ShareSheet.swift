27ea1ecfba10921d2e483699a3d0b7cdd22bf956
Diogo <diogofncunha@gmail.com>
2024-09-03 09:42:12 +0100

    revert showShareSheet

--- apps/ios/Shared/Views/Helpers/ShareSheet.swift
+++ apps/ios/Shared/Views/Helpers/ShareSheet.swift
@@ -9,37 +9,14 @@
 import SwiftUI
 
 func showShareSheet(items: [Any], completed: (() -> Void)? = nil) {
-    if let rootViewController = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.rootViewController {
-        let topViewController = getTopViewController(from: rootViewController)
-        
+    let keyWindowScene = UIApplication.shared.connectedScenes.first { $0.activationState == .foregroundActive } as? UIWindowScene
+    if let keyWindow = keyWindowScene?.windows.filter(\.isKeyWindow).first,
+       let presentedViewController = keyWindow.rootViewController?.presentedViewController ?? keyWindow.rootViewController {
         let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
-        
         if let completed = completed {
-            let handler: UIActivityViewController.CompletionWithItemsHandler = { _, _, _, _ in completed() }
+            let handler: UIActivityViewController.CompletionWithItemsHandler = { _,_,_,_ in completed() }
             activityViewController.completionWithItemsHandler = handler
         }
-        
-        topViewController.present(activityViewController, animated: true)
+        presentedViewController.present(activityViewController, animated: true)
     }
-}
-
-private func getTopViewController(from rootViewController: UIViewController) -> UIViewController {
-    var topViewController = rootViewController
-    
-    // Navigate through presented view controllers
-    while let presentedViewController = topViewController.presentedViewController {
-        topViewController = presentedViewController
-    }
-    
-    // If it's a UINavigationController, get the visible view controller
-    if let navigationController = topViewController as? UINavigationController {
-        topViewController = navigationController.visibleViewController ?? topViewController
-    }
-    
-    // If it's a UITabBarController, get the selected view controller
-    if let tabBarController = topViewController as? UITabBarController {
-        topViewController = tabBarController.selectedViewController ?? topViewController
-    }
-    
-    return topViewController
 }
