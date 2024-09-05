624e49e41277ca9c491e22385cd39453028ba48d
Diogo <diogofncunha@gmail.com>
2024-09-05 11:35:13 +0100

    always show your chat profiles in desktop

--- apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/views/chatlist/UserPicker.desktop.kt
+++ apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/views/chatlist/UserPicker.desktop.kt
@@ -3,7 +3,9 @@ package chat.simplex.common.views.chatlist
 import androidx.compose.runtime.Composable
 import chat.simplex.common.model.User
 import chat.simplex.common.model.UserInfo
-
+import chat.simplex.res.MR
+import dev.icerock.moko.resources.compose.painterResource
+import dev.icerock.moko.resources.compose.stringResource
 
 @Composable
 actual fun UserPickerInactiveUsersSection(
@@ -12,4 +14,9 @@ actual fun UserPickerInactiveUsersSection(
   onShowAllProfilesClicked: () -> Unit,
   onUserClicked: (user: User) -> Unit,
 ) {
+  UserPickerOptionRow(
+    painterResource(MR.images.ic_manage_accounts),
+    stringResource(MR.strings.your_chat_profiles),
+    onShowAllProfilesClicked
+  )
 }
