0f301adc57c199a7d68961d8643fde9e8f5de2a7
Evgeny <evgeny@poberezkin.com>
2024-09-24 09:25:41 +0100

    core: xrcp encryption with forward secrecy (#4926)
    * core: xrcp encryption with forward secrecy (tests intermittently fail)
    
    * track and correlate keys
    
    * simplify
    
    * refactor
    
    * remove comment

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."bef11e4cbe0a3776f0910375f2adb60399043835" = "195hir9crv51iyli8yjk8sivk7wagxxjfmhzq6ahydvbkrpd3258";
+  "https://github.com/simplex-chat/simplexmq.git"."04da670d00e904149040b136ebdea112de71e218" = "0kbpd56b3b8dy72xicfdqmiywxabq000125523dgmn3a5pjagc08";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
