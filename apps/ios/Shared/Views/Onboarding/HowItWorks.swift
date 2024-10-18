da71ecdc283a28548f455a7be57b8b8044eaf860
Diogo <diogofncunha@gmail.com>
2024-10-18 11:25:22 +0100

    how it works

--- apps/ios/Shared/Views/Onboarding/HowItWorks.swift
+++ apps/ios/Shared/Views/Onboarding/HowItWorks.swift
@@ -13,38 +13,33 @@ struct HowItWorks: View {
     var onboarding: Bool
 
     var body: some View {
-        VStack(alignment: .leading) {
+        List {
             Text("How SimpleX works")
                 .font(.largeTitle)
+                .bold()
+                .fixedSize(horizontal: false, vertical: true)
                 .padding(.vertical)
-            ScrollView {
-                VStack(alignment: .leading) {
-                    Group {
-                        Text("Many people asked: *if SimpleX has no user identifiers, how can it deliver messages?*")
-                        Text("To protect privacy, instead of user IDs used by all other platforms, SimpleX has identifiers for message queues, separate for each of your contacts.")
-                        Text("You control through which server(s) **to receive** the messages, your contacts – the servers you use to message them.")
-                        Text("Only client devices store user profiles, contacts, groups, and messages sent with **2-layer end-to-end encryption**.")
-                        if onboarding {
-                            Text("Read more in our GitHub repository.")
-                        } else {
-                            Text("Read more in our [GitHub repository](https://github.com/simplex-chat/simplex-chat#readme).")
-                        }
-                    }
-                    .padding(.bottom)
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            VStack(alignment: .leading, spacing: 18) {
+                Text("Many people asked: *if SimpleX has no user identifiers, how can it deliver messages?*")
+                Text("To protect privacy, instead of user IDs used by all other platforms, SimpleX has identifiers for message queues, separate for each of your contacts.")
+                Text("You control through which server(s) **to receive** the messages, your contacts – the servers you use to message them.")
+                Text("Only client devices store user profiles, contacts, groups, and messages sent with **2-layer end-to-end encryption**.")
+                if onboarding {
+                    Text("Read more in our GitHub repository.")
+                } else {
+                    Text("Read more in our [GitHub repository](https://github.com/simplex-chat/simplex-chat#readme).")
                 }
             }
-
-            Spacer()
-
-            if onboarding {
-                OnboardingActionButton()
-                    .padding(.bottom, 8)
-            }
+            .listRowBackground(Color.clear)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
         .lineLimit(10)
-        .padding()
         .frame(maxHeight: .infinity, alignment: .top)
-        .modifier(ThemedBackground())
+        .modifier(ThemedBackground(grouped: true))
     }
 }
 
