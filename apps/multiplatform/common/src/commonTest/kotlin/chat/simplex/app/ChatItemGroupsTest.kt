3182ac5afd01b5089dc44855e019de02156e6808
Avently <7953703+avently@users.noreply.github.com>
2024-11-19 17:03:37 +0700

    fix tests

--- apps/multiplatform/common/src/commonTest/kotlin/chat/simplex/app/ChatItemGroupsTest.kt
+++ apps/multiplatform/common/src/commonTest/kotlin/chat/simplex/app/ChatItemGroupsTest.kt
@@ -15,7 +15,7 @@ class ChatItemGroupsTest {
     val oldItems = listOf(ChatItem.getSampleData(0), ChatItem.getSampleData(123L), ChatItem.getSampleData(124L), ChatItem.getSampleData(125L))
 
     val anchors1 = MutableStateFlow(listOf(123L))
-    val chatState1 = ActiveChatState(anchors = anchors1)
+    val chatState1 = ActiveChatState(splits = anchors1)
     val removed1 = listOf(oldItems[1])
     val newItems1 = oldItems - removed1
     val recalc1 = recalculateChatStatePositions(chatState1)
@@ -24,7 +24,7 @@ class ChatItemGroupsTest {
     assertEquals(124L, anchors1.value.first())
 
     val anchors2 = MutableStateFlow(listOf(123L))
-    val chatState2 = ActiveChatState(anchors = anchors2)
+    val chatState2 = ActiveChatState(splits = anchors2)
     val removed2 = listOf(oldItems[1], oldItems[2])
     val newItems2 = oldItems - removed2
     val recalc2 = recalculateChatStatePositions(chatState2)
@@ -33,7 +33,7 @@ class ChatItemGroupsTest {
     assertEquals(125L, anchors2.value.first())
 
     val anchors3 = MutableStateFlow(listOf(123L))
-    val chatState3 = ActiveChatState(anchors = anchors3)
+    val chatState3 = ActiveChatState(splits = anchors3)
     val removed3 = listOf(oldItems[1], oldItems[2], oldItems[3])
     val newItems3 = oldItems - removed3
     val recalc3 = recalculateChatStatePositions(chatState3)
@@ -41,7 +41,7 @@ class ChatItemGroupsTest {
     assertEquals(0, anchors3.value.size)
 
     val anchors4 = MutableStateFlow(listOf(123L))
-    val chatState4 = ActiveChatState(anchors = anchors4)
+    val chatState4 = ActiveChatState(splits = anchors4)
     val recalc4 = recalculateChatStatePositions(chatState4)
     recalc4.cleared()
     assertEquals(0, anchors4.value.size)
@@ -61,71 +61,95 @@ class ChatItemGroupsTest {
 
     val unreadCount = mutableStateOf(0)
     val chatState = ActiveChatState()
-    val groups1 = Sections.create(items, unreadCount, emptySet(), chatState)
+    val merged1 = MergedItems.create(items, unreadCount, emptySet(), chatState)
     assertEquals(
       listOf(
-        listOf(0, 0, false,
+        listOf(0, false,
           listOf(
             listOf(0, 100, CIMergeCategory.ChatFeature),
             listOf(1, 99, CIMergeCategory.ChatFeature)
           )
         ),
-        listOf(1, 2, false,
+        listOf(2, false,
           listOf(
             listOf(0, 98, CIMergeCategory.RcvItemDeleted),
             listOf(1, 97, CIMergeCategory.RcvItemDeleted)
           )
         ),
-        listOf(2, 4, true,
+        listOf(4, true,
           listOf(
             listOf(0, 96, null),
-            listOf(1, 95, null),
-            listOf(2, 94, null)
+          )
+        ),
+        listOf(5, true,
+          listOf(
+            listOf(0, 95, null),
+          )
+        ),
+        listOf(6, true,
+          listOf(
+            listOf(0, 94, null)
           )
         )
       ).toList().toString(),
-      groups1.sections.map {
+      merged1.items.map {
         listOf(
-          it.startIndexInParentItems,
           it.startIndexInReversedItems,
-          it.revealed.value,
-          it.items.mapIndexed { index, listItem ->
-            listOf(index, listItem.item.id, listItem.item.mergeCategory)
+          if (it is MergedItem.Grouped) it.revealed else true,
+          when (it) {
+            is MergedItem.Grouped -> it.items.mapIndexed { index, listItem ->
+              listOf(index, listItem.item.id, listItem.item.mergeCategory)
+            }
+            is MergedItem.Single -> listOf(listOf(0, it.item.item.id, it.item.item.mergeCategory))
           }
         )
       }.toString()
     )
 
-    val groups2 = Sections.create(items, unreadCount, setOf(98L, 97L), chatState)
+    val merged2 = MergedItems.create(items, unreadCount, setOf(98L, 97L), chatState)
     assertEquals(
       listOf(
-        listOf(0, 0, false,
+        listOf(0, false,
           listOf(
             listOf(0, 100, CIMergeCategory.ChatFeature),
             listOf(1, 99, CIMergeCategory.ChatFeature)
           )
         ),
-        listOf(1, 2, true,
+        listOf(2, true,
           listOf(
             listOf(0, 98, CIMergeCategory.RcvItemDeleted),
-            listOf(1, 97, CIMergeCategory.RcvItemDeleted)
           )
         ),
-        listOf(3, 4, true,
+        listOf(3, true,
+          listOf(
+            listOf(0, 97, CIMergeCategory.RcvItemDeleted)
+          )
+        ),
+        listOf(4, true,
           listOf(
             listOf(0, 96, null),
-            listOf(1, 95, null),
-            listOf(2, 94, null)
+          )
+        ),
+        listOf(5, true,
+          listOf(
+            listOf(0, 95, null),
+          )
+        ),
+        listOf(6, true,
+          listOf(
+            listOf(0, 94, null)
           )
         )
       ).toList().toString(),
-      groups2.sections.map {
+      merged2.items.map {
         listOf(
-          it.startIndexInParentItems,
           it.startIndexInReversedItems,
-          it.revealed.value,
-          it.items.mapIndexed { index, listItem ->
-            listOf(index, listItem.item.id, listItem.item.mergeCategory)
+          if (it is MergedItem.Grouped) it.revealed else true,
+          when (it) {
+            is MergedItem.Grouped -> it.items.mapIndexed { index, listItem ->
+              listOf(index, listItem.item.id, listItem.item.mergeCategory)
+            }
+            is MergedItem.Single -> listOf(listOf(0, it.item.item.id, it.item.item.mergeCategory))
           }
         )
       }.toString()
