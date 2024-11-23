d9d7ecbe742c1af3699acdcee0fcc6acf7368a9e
Avently <7953703+avently@users.noreply.github.com>
2024-11-23 15:00:41 +0700

    version

--- scripts/desktop/prepare-openssl-windows.sh
+++ scripts/desktop/prepare-openssl-windows.sh
@@ -9,7 +9,7 @@ root_dir="$(dirname "$(dirname "$(readlink "$0")")")"
 
 cd $root_dir
 
-if [ ! -f dist-newstyle/openssl-3.0.15/libcrypto-3_0-x64.dll ]; then
+if [ ! -f dist-newstyle/openssl-3.0.15/libcrypto-3-x64.dll ]; then
     mkdir dist-newstyle 2>/dev/null || true
     cd dist-newstyle
     curl --tlsv1.2 https://www.openssl.org/source/openssl-3.0.15.tar.gz -L -o openssl.tar.gz
