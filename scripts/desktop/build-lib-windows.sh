bda84b08a1dcc62143c826d59ef371eebf76d014
Stanislav Dmitrenko <7953703+avently@users.noreply.github.com>
2024-11-23 18:41:48 +0700

    ci: fix mac & Windows build (#5232)
    * core: 6.2.0.1 (simplexmq 6.2.0.4)
    
    * action: fix mac build
    
    * fix Windows
    
    * version
    
    * revert version change
    
    ---------
    
    Co-authored-by: Evgeny Poberezkin <evgeny@poberezkin.com>

--- scripts/desktop/build-lib-windows.sh
+++ scripts/desktop/build-lib-windows.sh
@@ -47,7 +47,7 @@ echo "    flags: +openssl" >> cabal.project.local
 echo "    extra-include-dirs: $openssl_windows_style_path\include" >> cabal.project.local
 echo "    extra-lib-dirs: $openssl_windows_style_path" >> cabal.project.local
 echo "package simplex-chat" >> cabal.project.local
-echo "    ghc-options: -shared -threaded -optl-L$openssl_windows_style_path -optl-lcrypto-1_1-x64 -o libsimplex.dll libsimplex.dll.def" >> cabal.project.local
+echo "    ghc-options: -shared -threaded -optl-L$openssl_windows_style_path -optl-lcrypto-3-x64 -o libsimplex.dll libsimplex.dll.def" >> cabal.project.local
 # Very important! Without it the build fails on linking step since the linker can't find exported symbols.
 # It looks like GHC bug because with such random path the build ends successfully
 sed -i "s/ld.lld.exe/abracadabra.exe/" `ghc --print-libdir`/settings
@@ -57,7 +57,7 @@ rm -rf apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
 rm -rf apps/multiplatform/desktop/build/cmake
 
 mkdir -p apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
-cp dist-newstyle/openssl-3.0.15/libcrypto-1_1-x64.dll apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
+cp dist-newstyle/openssl-3.0.15/libcrypto-3-x64.dll apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
 cp libsimplex.dll apps/multiplatform/common/src/commonMain/cpp/desktop/libs/$OS-$ARCH/
 
 scripts/desktop/prepare-vlc-windows.sh
