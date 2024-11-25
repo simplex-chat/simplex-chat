c6855a0b6a84144e081ffc9bfa598699733f3ec3
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-11-25 13:48:32 +0000

    ios: address or 1-time link

--- apps/ios/Shared/Views/UserSettings/UserAddressLearnMore.swift
+++ apps/ios/Shared/Views/UserSettings/UserAddressLearnMore.swift
@@ -15,21 +15,47 @@ struct UserAddressLearnMore: View {
     var body: some View {
         VStack {
             List {
-                VStack(alignment: .leading, spacing: 18) {
-                    Text("You can share your address as a link or QR code - anybody can connect to you.")
+                VStack(alignment: .leading, spacing: 16) {
+                    (Text(Image(systemName: "envelope")).foregroundColor(.secondary) + Text(" ") + Text("Share address publicly").bold().font(.title2))
+                    Text("Share SimpleX address on social media.")
+                    Text("Verify who connected, as users can use any name.")
                     Text("You won't lose your contacts if you later delete your address.")
-                    Text("When people request to connect, you can accept or reject it.")
-                    Text("Read more in [User Guide](https://simplex.chat/docs/guide/app-settings.html#your-simplex-contact-address).")
+
+                    (Text(Image(systemName: "link.badge.plus")).foregroundColor(.secondary) + Text(" ") + Text("Share 1-time link with a friend").font(.title2).bold())
+                    Text("1-time link can be used *with one contact only* - share in person or via any messenger.")
+                    Text("Connections via 1-time links are faster, more private and more secure.")
+                    Text("You can set connection name, to remember who the link was shared with.")
+
+                    if !showCreateAddressButton {
+                        (Text(Image(systemName: "shield")).foregroundColor(.secondary) + Text(" ") + Text("Connection security").font(.title2).bold())
+                        Text("SimpleX address and 1-time links are safe to share via any messenger.")
+                        Text("To protect against your link being replaced, you can compare contact security codes.")
+                        Text("Read more in [User Guide](https://simplex.chat/docs/guide/making-connections.html#comparison-of-1-time-invitation-links-and-simplex-contact-addresses).")
+                    }
+
                 }
                 .listRowBackground(Color.clear)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             }
-            .frame(maxHeight: .infinity)
+            .frame(maxHeight: .infinity, alignment: .top)
 
+            Spacer()
+            
             if showCreateAddressButton {
-                addressCreationButton()
-                    .padding()
+                VStack {
+                    addressCreationButton()
+                        .padding(.bottom)
+                    
+                    Button("Create 1-time link") {
+                        
+                    }
+                    .font(.footnote)
+                }
+                .padding()
             }
         }
+        .frame(maxHeight: .infinity, alignment: .top)
     }
     
     private func addressCreationButton() -> some View {
