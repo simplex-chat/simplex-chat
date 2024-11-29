1e326c69a6c133fd87452246ce40980646405e95
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-11-29 17:20:24 +0400

    remove

--- src/Simplex/Chat/Migrations/M20241128_business_chats.hs
+++ src/Simplex/Chat/Migrations/M20241128_business_chats.hs
@@ -10,7 +10,6 @@ m20241128_business_chats =
   [sql|
 ALTER TABLE user_contact_links ADD business INTEGER DEFAULT 0;
 ALTER TABLE connections ADD COLUMN business INTEGER DEFAULT 0;
-ALTER TABLE contacts ADD COLUMN business INTEGER DEFAULT 0;
 ALTER TABLE groups ADD COLUMN business TEXT NULL;
 ALTER TABLE groups ADD COLUMN business_group_member_id INTEGER NULL REFERENCES group_members(group_member_id);
 ALTER TABLE group_members ADD COLUMN business_member TEXT NULL;
@@ -25,7 +24,6 @@ DROP INDEX idx_groups_business_group_member_id;
 
 ALTER TABLE user_contact_links DROP COLUMN business;
 ALTER TABLE connections DROP COLUMN business;
-ALTER TABLE contacts DROP COLUMN business;
 ALTER TABLE groups DROP COLUMN business;
 ALTER TABLE groups DROP COLUMN business_group_member_id;
 ALTER TABLE group_members DROP COLUMN business_member;
