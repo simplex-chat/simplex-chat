8d36f749d0aad422ea1ba0c8ad8ff2f8b9cab630
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-12-11 13:03:50 +0000

    remove websockets from simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."e6e7f84d54f404f30412094aea4fa04f636e695f" = "0g4djr692fgsjk4xy0118zjw4lx8rcrj083xa5mgdcm8dzm1jhyl";
+  "https://github.com/simplex-chat/simplexmq.git"."e5c4a9e6100c6f1e7dd630eb4496fac9adf0acad" = "09z0y6w1c51szd9r90fl259inn76yahfbb164mawd34sr41ccp7g";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
