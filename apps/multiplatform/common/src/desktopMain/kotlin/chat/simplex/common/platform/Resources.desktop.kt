eb4290ef72b10e362e3f2163e4468fccff767752
Avently <7953703+avently@users.noreply.github.com>
2024-12-12 17:49:01 +0700

    enhancements

--- apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/platform/Resources.desktop.kt
+++ apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/platform/Resources.desktop.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.*
 import chat.simplex.common.simplexWindowState
 import chat.simplex.common.views.helpers.*
+import chat.simplex.res.MR
 import com.jthemedetecor.OsThemeDetector
 import com.russhwolf.settings.*
 import dev.icerock.moko.resources.ImageResource
@@ -69,19 +70,33 @@ private val settingsThemesProps =
   Properties()
     .also { props -> try { settingsThemesFile.reader().use { props.load(it) } } catch (e: Exception) { /**/ } }
 
-
-private val settingsWriterThread = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
-
-actual val settings: Settings = PropertiesSettings(settingsProps) { CoroutineScope(settingsWriterThread).launch {
-  createTmpFileAndDelete(settingsTmpDir) { tmpFile ->
-    tmpFile.writer().use { settingsProps.store(it, "") }
-    Files.move(tmpFile.toPath(), settingsFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
-  } } }
-actual val settingsThemes: Settings = PropertiesSettings(settingsThemesProps) { CoroutineScope(settingsWriterThread).launch {
-  createTmpFileAndDelete(settingsTmpDir) { tmpFile ->
-    tmpFile.writer().use { settingsThemesProps.store(it, "") }
-    Files.move(tmpFile.toPath(), settingsThemesFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
-  } } }
+private val lock = "settingsSaver"
+actual val settings: Settings = PropertiesSettings(settingsProps) {
+  synchronized(lock) {
+    try {
+      createTmpFileAndDelete(settingsTmpDir) { tmpFile ->
+        tmpFile.writer().use { settingsProps.store(it, "") }
+        Files.move(tmpFile.toPath(), settingsFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+      }
+    } catch (e: Exception) {
+      AlertManager.shared.showAlertMsg(generalGetString(MR.strings.prefs_error_saving_settings), e.stackTraceToString())
+      throw e
+    }
+  }
+}
+actual val settingsThemes: Settings = PropertiesSettings(settingsThemesProps) {
+  synchronized(lock) {
+    try {
+      createTmpFileAndDelete(settingsTmpDir) { tmpFile ->
+        tmpFile.writer().use { settingsThemesProps.store(it, "") }
+        Files.move(tmpFile.toPath(), settingsThemesFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+      }
+    } catch (e: Exception) {
+      AlertManager.shared.showAlertMsg(generalGetString(MR.strings.prefs_error_saving_settings), e.stackTraceToString())
+      throw e
+    }
+  }
+}
 
 actual fun windowOrientation(): WindowOrientation =
   if (simplexWindowState.windowState.size.width > simplexWindowState.windowState.size.height) {
