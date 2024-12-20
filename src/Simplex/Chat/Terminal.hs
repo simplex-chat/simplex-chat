00973d6e13c5b6782bebf064042bd6e367505d21
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-12-20 16:54:24 +0400

    core: split Chat.hs module (#5397)

--- src/Simplex/Chat/Terminal.hs
+++ src/Simplex/Chat/Terminal.hs
@@ -14,10 +14,11 @@ import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 import Database.SQLite.Simple (SQLError (..))
 import qualified Database.SQLite.Simple as DB
-import Simplex.Chat (_defaultNtfServers, defaultChatConfig, operatorSimpleXChat)
+import Simplex.Chat (defaultChatConfig, operatorSimpleXChat)
 import Simplex.Chat.Controller
 import Simplex.Chat.Core
 import Simplex.Chat.Help (chatWelcome)
+import Simplex.Chat.Library.Commands (_defaultNtfServers)
 import Simplex.Chat.Operators
 import Simplex.Chat.Options
 import Simplex.Chat.Terminal.Input
