38bb183d9054716563b235482daa83eccb76f8f6
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-12-20 21:03:51 +0400

    wip

--- src/Simplex/Chat/Store/Postgres/Migrations.hs
+++ src/Simplex/Chat/Store/Postgres/Migrations.hs
@@ -1,1 +1,16 @@
 module Simplex.Chat.Store.Postgres.Migrations where
+
+import Data.List (sortOn)
+import Data.Text (Text)
+import Simplex.Chat.Store.Postgres.Migrations.M20241220_initial
+
+schemaMigrations :: [(String, Text, Maybe Text)]
+schemaMigrations =
+  [ ("20241220_initial", m20241220_initial, Nothing)
+  ]
+
+-- | The list of migrations in ascending order by date
+migrations :: [Migration]
+migrations = sortOn name $ map migration schemaMigrations
+  where
+    migration (name, up, down) = Migration {name, up, down = down}
