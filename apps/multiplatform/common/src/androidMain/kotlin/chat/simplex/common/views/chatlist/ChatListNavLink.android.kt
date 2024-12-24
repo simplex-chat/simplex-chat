e0e1bb13ed4be850eff63088ac4a26ff40649c33
Avently <7953703+avently@users.noreply.github.com>
2024-12-24 16:55:21 +0700

    refactor

---
+++ apps/multiplatform/common/src/androidMain/kotlin/chat/simplex/common/views/chatlist/ChatListNavLink.android.kt
@@ -0,0 +1,42 @@
+package chat.simplex.common.views.chatlist
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import chat.simplex.common.platform.onRightClick
+import chat.simplex.common.views.helpers.*
+
+@Composable
+actual fun ChatListNavLinkLayout(
+  chatLinkPreview: @Composable () -> Unit,
+  click: () -> Unit,
+  dropdownMenuItems: (@Composable () -> Unit)?,
+  showMenu: MutableState<Boolean>,
+  disabled: Boolean,
+  selectedChat: State<Boolean>,
+  nextChatSelected: State<Boolean>,
+) {
+  var modifier = Modifier.fillMaxWidth()
+
+  if (!disabled) modifier = modifier
+    .combinedClickable(onClick = click, onLongClick = { showMenu.value = true })
+    .onRightClick { showMenu.value = true }
+  Box(modifier) {
+    Row(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 8.dp, top = 8.dp, end = 12.dp, bottom = 8.dp),
+      verticalAlignment = Alignment.Top
+    ) {
+      chatLinkPreview()
+    }
+    if (dropdownMenuItems != null) {
+      DefaultDropdownMenu(showMenu, dropdownMenuItems = dropdownMenuItems)
+    }
+  }
+  Divider(Modifier.padding(horizontal = 8.dp))
+}
--- apps/multiplatform/common/src/androidMain/kotlin/chat/simplex/common/views/chatlist/TagListEditor.android.kt
+++ apps/multiplatform/common/src/androidMain/kotlin/chat/simplex/common/views/chatlist/TagListEditor.android.kt
@@ -7,50 +7,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.emoji2.emojipicker.EmojiPickerView
 import chat.simplex.common.model.ChatController.appPrefs
-import chat.simplex.common.platform.*
 import chat.simplex.common.ui.theme.DEFAULT_PADDING_HALF
 import chat.simplex.common.views.chat.topPaddingToContent
 import chat.simplex.common.views.helpers.*
 import chat.simplex.res.MR
 import dev.icerock.moko.resources.compose.painterResource
-
-@Composable
-actual fun ChatListNavLinkLayout(
-  chatLinkPreview: @Composable () -> Unit,
-  click: () -> Unit,
-  dropdownMenuItems: (@Composable () -> Unit)?,
-  showMenu: MutableState<Boolean>,
-  disabled: Boolean,
-  selectedChat: State<Boolean>,
-  nextChatSelected: State<Boolean>,
-) {
-  var modifier = Modifier.fillMaxWidth()
-
-  if (!disabled) modifier = modifier
-    .combinedClickable(onClick = click, onLongClick = { showMenu.value = true })
-    .onRightClick { showMenu.value = true }
-  Box(modifier) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .padding(start = 8.dp, top = 8.dp, end = 12.dp, bottom = 8.dp),
-      verticalAlignment = Alignment.Top
-    ) {
-      chatLinkPreview()
-    }
-    if (dropdownMenuItems != null) {
-      DefaultDropdownMenu(showMenu, dropdownMenuItems = dropdownMenuItems)
-    }
-  }
-  Divider(Modifier.padding(horizontal = 8.dp))
-}
 
 @Composable
 actual fun ChatTagInput(name: MutableState<String>, showError: State<Boolean>, emoji: MutableState<String?>) {
