d27bebf08fbf8fea1c5435d23adc8af2d35a3a08
Avently <7953703+avently@users.noreply.github.com>
2024-12-25 02:29:13 -0800

    ios: open file as preview on click

--- apps/ios/Shared/Views/Chat/ChatView.swift
+++ apps/ios/Shared/Views/Chat/ChatView.swift
@@ -1494,7 +1494,7 @@ struct ChatView: View {
 
         func saveButton(file: CryptoFile) -> Button<some View> {
             Button {
-                saveCryptoFile(file)
+                saveOrOpenCryptoFile(file)
             } label: {
                 Label(
                     NSLocalizedString("Save", comment: "chat item action"),
