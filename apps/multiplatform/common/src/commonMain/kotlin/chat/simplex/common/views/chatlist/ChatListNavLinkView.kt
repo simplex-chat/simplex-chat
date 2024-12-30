db8674e19427591b7540479ff0b7d217cbb7fee4
Diogo <diogofncunha@gmail.com>
2024-12-30 21:47:49 +0000

    show change list in context menu if chat already had tag

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chatlist/ChatListNavLinkView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chatlist/ChatListNavLinkView.kt
@@ -351,7 +351,7 @@ fun TagListAction(
 ) {
   val userTags = remember { chatModel.userTags }
   ItemAction(
-    stringResource(MR.strings.add_to_list),
+    stringResource(if (chat.chatInfo.chatTags.isNullOrEmpty()) MR.strings.add_to_list else MR.strings.change_list),
     painterResource(MR.images.ic_label),
     onClick = {
       ModalManager.start.showModalCloseable { close ->
