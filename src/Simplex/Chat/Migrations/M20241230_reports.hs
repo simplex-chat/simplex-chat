97e8d454b7611026a0f96068153dd2a6cbd829bd
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-12-31 13:22:23 +0000

    migration

---
+++ src/Simplex/Chat/Migrations/M20241230_reports.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Simplex.Chat.Migrations.M20241230_reports where
+
+import Database.SQLite.Simple (Query)
+import Database.SQLite.Simple.QQ (sql)
+
+m20241230_reports :: Query
+m20241230_reports =
+  [sql|
+ALTER TABLE chat_items ADD COLUMN msg_content_tag TEXT;
+ALTER TABLE chat_items ADD COLUMN archived INTEGER NOT NULL DEFAULT 0;
+-- ALTER TABLE groups ADD COLUMN unread_count INTEGER;
+-- ALTER TABLE groups ADD COLUMN unread_chat_item_id INTEGER; -- the item that should be opened on initial load
+ALTER TABLE groups ADD COLUMN reports_count INTEGER NOT NULL DEFAULT 0;
+-- ALTER TABLE contacts ADD COLUMN unread_count INTEGER;
+-- ALTER TABLE contacts ADD COLUMN unread_chat_item_id INTEGER;
+
+-- TODO create indices
+|]
+
+down_m20241230_reports :: Query
+down_m20241230_reports =
+  [sql|
+ALTER TABLE chat_items DROP COLUMN msg_content_tag;
+ALTER TABLE chat_items DROP COLUMN archived;
+-- ALTER TABLE groups DROP COLUMN unread_count;
+-- ALTER TABLE groups DROP COLUMN unread_chat_item_id;
+ALTER TABLE groups DROP COLUMN reports_count;
+-- ALTER TABLE contacts DROP COLUMN unread_count INTEGER;
+-- ALTER TABLE contacts DROP COLUMN unread_chat_item_id INTEGER;
+
+-- TODO remove indices
+|]
