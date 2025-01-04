0ddd14900576dffb8f91216d20977fbe8218e320
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-01-04 10:53:17 +0000

    remove comment

--- src/Simplex/Chat/Migrations/M20241230_reports.hs
+++ src/Simplex/Chat/Migrations/M20241230_reports.hs
@@ -9,14 +9,10 @@
  m20241230_reports =
    [sql|
 ALTER TABLE chat_items ADD COLUMN msg_content_tag TEXT;
-
--- TODO create indices
 |]
 
  down_m20241230_reports :: Query
  down_m20241230_reports =
    [sql|
 ALTER TABLE chat_items DROP COLUMN msg_content_tag;
-
--- TODO remove indices
 |]
