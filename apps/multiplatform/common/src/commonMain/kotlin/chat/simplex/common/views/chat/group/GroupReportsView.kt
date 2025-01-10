7d2fe9d621ecab7baf16199288ad08799c01960c
Avently <7953703+avently@users.noreply.github.com>
2025-01-11 00:40:25 +0700

    show search button in bar without menu

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chat/group/GroupReportsView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chat/group/GroupReportsView.kt
@@ -1,19 +1,12 @@
 package chat.simplex.common.views.chat.group
 
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.*
 import chat.simplex.common.model.*
 import chat.simplex.common.model.ChatController.appPrefs
 import chat.simplex.common.platform.*
 import chat.simplex.common.views.chat.*
-import chat.simplex.common.views.chat.item.ItemAction
 import chat.simplex.common.views.chatlist.*
 import chat.simplex.common.views.helpers.*
 import chat.simplex.res.MR
@@ -49,7 +42,6 @@ fun GroupReportsAppBar(
   onSearchValueChanged: (String) -> Unit
 ) {
   val oneHandUI = remember { appPrefs.oneHandUI.state }
-  val showMenu = remember { mutableStateOf(false) }
   val showSearch = rememberSaveable { mutableStateOf(false) }
   val onBackClicked = {
     if (!showSearch.value) {
@@ -68,36 +60,11 @@ fun GroupReportsAppBar(
     showSearch = showSearch.value,
     onSearchValueChanged = onSearchValueChanged,
     buttons = {
-      IconButton({ showMenu.value = true }) {
-        Icon(MoreVertFilled, stringResource(MR.strings.icon_descr_more_button), tint = MaterialTheme.colors.primary)
+      IconButton({ showSearch.value = true }) {
+        Icon(painterResource(MR.images.ic_search), stringResource(MR.strings.search_verb), tint = MaterialTheme.colors.primary)
       }
     }
   )
-  Box(Modifier.fillMaxWidth().wrapContentSize(Alignment.TopEnd)) {
-    val density = LocalDensity.current
-    val width = remember { mutableStateOf(250.dp) }
-    val height = remember { mutableStateOf(0.dp) }
-
-    val onClosedAction = remember { mutableStateOf({}) }
-    DefaultDropdownMenu(
-      showMenu,
-      modifier = Modifier.onSizeChanged { with(density) {
-        width.value = it.width.toDp().coerceAtLeast(250.dp)
-        if (oneHandUI.value && (appPlatform.isDesktop || (platform.androidApiLevel ?: 0) >= 30)) height.value = it.height.toDp()
-      } },
-      offset = DpOffset(-width.value, if (oneHandUI.value) -height.value else AppBarHeight),
-      onClosed = onClosedAction
-    ) {
-      @Composable
-      fun Search() {
-        ItemAction(stringResource(MR.strings.search_verb), painterResource(MR.images.ic_search), onClick = {
-          showMenu.value = false
-          showSearch.value = true
-        })
-      }
-      Search()
-    }
-  }
   ItemsReload(groupReports)
 }
 
