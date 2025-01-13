27481116f014fe8ab5bc2eb2848ece1a2b45c018
Evgeny <evgeny@poberezkin.com>
2025-01-13 18:59:33 +0000

    core: 6.3.0.1 (simplexmq 6.3.0.1)

--- src/Simplex/Chat/Remote.hs
+++ src/Simplex/Chat/Remote.hs
@@ -75,11 +75,11 @@ remoteFilesFolder = "simplex_v1_files"
 
 -- when acting as host
 minRemoteCtrlVersion :: AppVersion
-minRemoteCtrlVersion = AppVersion [6, 3, 0, 0]
+minRemoteCtrlVersion = AppVersion [6, 3, 0, 1]
 
 -- when acting as controller
 minRemoteHostVersion :: AppVersion
-minRemoteHostVersion = AppVersion [6, 3, 0, 0]
+minRemoteHostVersion = AppVersion [6, 3, 0, 1]
 
 currentAppVersion :: AppVersion
 currentAppVersion = AppVersion SC.version
