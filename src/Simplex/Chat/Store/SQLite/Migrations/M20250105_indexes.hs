db8f33debe33a4eaf8208c690f0a85cbb3302907
Evgeny <evgeny@poberezkin.com>
2025-01-13 14:06:12 +0000

    core: add index to load chats faster (#5521)
    * core: add index to load chats faster
    
    * schema
    
    * revert query (sqlite)
    
    * Revert "revert query (sqlite)"
    
    This reverts commit 194a48d61fb4bbcab3137f95aad377edff82158b.
    
    ---------
    
    Co-authored-by: spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>

--- src/Simplex/Chat/Store/SQLite/Migrations/M20250105_indexes.hs
+++ src/Simplex/Chat/Store/SQLite/Migrations/M20250105_indexes.hs
@@ -9,10 +9,12 @@ m20250105_indexes :: Query
 m20250105_indexes =
   [sql|
 CREATE INDEX idx_chat_items_groups_msg_content_tag_item_ts ON chat_items(user_id, group_id, msg_content_tag, item_ts);
+CREATE INDEX idx_chat_items_groups_msg_content_tag_deleted ON chat_items(user_id, group_id, msg_content_tag, item_deleted, item_sent);
 |]
 
 down_m20250105_indexes :: Query
 down_m20250105_indexes =
   [sql|
 DROP INDEX idx_chat_items_groups_msg_content_tag_item_ts;
+DROP INDEX idx_chat_items_groups_msg_content_tag_deleted;
 |]
