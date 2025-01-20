6a84a80c909047cee764ab1ae8c2caf94fa69e69
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-01-20 20:42:29 +0400

    core: don't subscribe to deleted/left groups

--- src/Simplex/Chat/Library/Commands.hs
+++ src/Simplex/Chat/Library/Commands.hs
@@ -3347,8 +3347,9 @@ subscribeUserConnections vr onlyNeeded agentBatchSubscribe user = do
     getGroupMemberConns :: CM ([Group], [ConnId], Map ConnId GroupMember)
     getGroupMemberConns = do
       gs <- withStore_ (`getUserGroups` vr)
-      let mPairs = concatMap (\(Group _ ms) -> mapMaybe (\m -> (,m) <$> memberConnId m) (filter (not . memberRemoved) ms)) gs
-      pure (gs, map fst mPairs, M.fromList mPairs)
+      let gs' = filter (\Group {groupInfo} -> not . memberRemoved . membership $ groupInfo) gs
+          mPairs = concatMap (\(Group _ ms) -> mapMaybe (\m -> (,m) <$> memberConnId m) (filter (not . memberRemoved) ms)) gs'
+      pure (gs', map fst mPairs, M.fromList mPairs)
     getSndFileTransferConns :: CM ([ConnId], Map ConnId SndFileTransfer)
     getSndFileTransferConns = do
       sfts <- withStore_ getLiveSndFileTransfers
