ceaa10638a5a1d320e7fec4ca6e37fed69628523
Diogo <diogofncunha@gmail.com>
2025-01-29 16:31:39 +0000

    Revert "fix notifications"
    This reverts commit 59643c24725d3caee3c629df6732f4b5bc294f8f.

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chatlist/ChatListNavLinkView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chatlist/ChatListNavLinkView.kt
@@ -868,7 +868,7 @@ fun updateChatSettings(remoteHostId: Long?, chatInfo: ChatInfo, chatSettings: Ch
       withChats {
         updateChatInfo(remoteHostId, newChatInfo)
       }
-      if (chatSettings.enableNtfs != MsgFilter.All) {
+      if (chatSettings.enableNtfs == MsgFilter.None) {
         ntfManager.cancelNotificationsForChat(chatInfo.id)
       }
       val updatedChat = chatModel.getChat(chatInfo.id)
