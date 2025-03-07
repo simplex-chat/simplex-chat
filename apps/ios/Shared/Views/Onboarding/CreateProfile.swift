926a60412f34ca771da5505766f03a02d8f91166
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-03-07 17:04:53 +0400

    ios

--- apps/ios/Shared/Views/Onboarding/CreateProfile.swift
+++ apps/ios/Shared/Views/Onboarding/CreateProfile.swift
@@ -145,6 +145,7 @@ struct CreateFirstProfile: View {
                     TextField("Enter your name…", text: $displayName)
                         .focused($focusDisplayName)
                         .padding(.horizontal)
+                        .padding(.trailing, 20)
                         .padding(.vertical, 10)
                         .background(
                             RoundedRectangle(cornerRadius: 10, style: .continuous)
