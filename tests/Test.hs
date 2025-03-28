e2c510f4618d03f0a531e653559f91ed647ce03a
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-03-28 17:24:08 +0000

    fix directory, test

--- tests/Test.hs
+++ tests/Test.hs
@@ -69,7 +69,7 @@ main = do
 #endif
           describe "SimpleX chat client" chatTests
           xdescribe'' "SimpleX Broadcast bot" broadcastBotTests
-          fdescribe "SimpleX Directory service bot" directoryServiceTests
+          xdescribe'' "SimpleX Directory service bot" directoryServiceTests
           describe "Remote session" remoteTests
 #if !defined(dbPostgres)
           xdescribe'' "Save query plans" saveQueryPlans
