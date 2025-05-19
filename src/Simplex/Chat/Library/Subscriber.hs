98f35a38e84543acb9527035f9abe0c4298b0516
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-05-19 09:08:28 +0000

    core: fix member introduction when inviting admin accepts member; fix order of items for accepted member (#5924)

--- src/Simplex/Chat/Library/Subscriber.hs
+++ src/Simplex/Chat/Library/Subscriber.hs
@@ -2123,14 +2123,14 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
             membership' <- withStore' $ \db -> updateGroupMemberAccepted db user membership GSMemConnected role
             -- create item in both scopes
             let gInfo' = gInfo {membership = membership'}
-            createInternalChatItem user (CDGroupRcv gInfo' Nothing m) (CIRcvGroupEvent RGEUserAccepted) Nothing
-            let scopeInfo = Just $ GCSIMemberSupport {groupMember_ = Nothing}
-            createInternalChatItem user (CDGroupRcv gInfo' scopeInfo m) (CIRcvGroupEvent RGEUserAccepted) Nothing
-            toView $ CEvtUserJoinedGroup user gInfo' m
-            let cd = CDGroupRcv gInfo' Nothing m
+                cd = CDGroupRcv gInfo' Nothing m
             createInternalChatItem user cd (CIRcvGroupE2EEInfo E2EInfo {pqEnabled = PQEncOff}) Nothing
             createGroupFeatureItems user cd CIRcvGroupFeature gInfo'
             maybeCreateGroupDescrLocal gInfo' m
+            createInternalChatItem user cd (CIRcvGroupEvent RGEUserAccepted) Nothing
+            let scopeInfo = Just $ GCSIMemberSupport {groupMember_ = Nothing}
+            createInternalChatItem user (CDGroupRcv gInfo' scopeInfo m) (CIRcvGroupEvent RGEUserAccepted) Nothing
+            toView $ CEvtUserJoinedGroup user gInfo' m
           GAPendingReview -> do
             membership' <- withStore' $ \db -> updateGroupMemberAccepted db user membership GSMemPendingReview role
             let gInfo' = gInfo {membership = membership'}
