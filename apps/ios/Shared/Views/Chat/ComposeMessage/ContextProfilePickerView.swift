6f0c9b97c19858933a65bd7e36b00b63f4998bd4
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-06-13 14:04:49 +0400

    fix scroll flickering

--- apps/ios/Shared/Views/Chat/ComposeMessage/ContextProfilePickerView.swift
+++ apps/ios/Shared/Views/Chat/ComposeMessage/ContextProfilePickerView.swift
@@ -18,6 +18,7 @@ struct ContextProfilePickerView: View {
     @State var selectedProfile: User
     @State private var profiles: [User] = []
     @State private var listExpanded = false
+    @State private var expandedListReady = false
     @State private var showIncognitoSheet = false
 
     @UserDefault(DEFAULT_TOOLBAR_MATERIAL) private var toolbarMaterial = ToolbarMaterial.defaultMaterial
@@ -39,65 +40,85 @@ struct ContextProfilePickerView: View {
 
     @ViewBuilder private func viewBody() -> some View {
         if !listExpanded {
-            if incognitoDefault {
-                incognitoOption()
-            } else {
-                profilerPickerUserOption(selectedProfile)
-            }
+            currentSelection()
         } else {
             profilePicker()
         }
     }
 
+    @ViewBuilder private func currentSelection() -> some View {
+        if incognitoDefault {
+            incognitoOption()
+        } else {
+            profilerPickerUserOption(selectedProfile)
+        }
+    }
+
     private func profilePicker() -> some View {
         ScrollViewReader { proxy in
-            let scroll = ScrollView {
-                LazyVStack(spacing: 0) {
-                    let otherProfiles = profiles.filter { u in u.userId != selectedProfile.userId }
-                    ForEach(otherProfiles) { p in
-                        profilerPickerUserOption(p)
-                            .contentShape(Rectangle())
-                        Divider()
-                            .padding(.leading)
-                            .padding(.leading, 48)
+            Group {
+                if expandedListReady {
+                    let scroll = ScrollView {
+                        LazyVStack(spacing: 0) {
+                            let otherProfiles = profiles.filter { u in u.userId != selectedProfile.userId }
+                            ForEach(otherProfiles) { p in
+                                profilerPickerUserOption(p)
+                                    .contentShape(Rectangle())
+                                Divider()
+                                    .padding(.leading)
+                                    .padding(.leading, 48)
+                            }
+
+                            if incognitoDefault {
+                                profilerPickerUserOption(selectedProfile)
+                                    .contentShape(Rectangle())
+                                Divider()
+                                    .padding(.leading)
+                                    .padding(.leading, 48)
+
+                                incognitoOption()
+                                    .contentShape(Rectangle())
+                                    .id("BOTTOM_ANCHOR")
+                            } else {
+                                incognitoOption()
+                                    .contentShape(Rectangle())
+                                Divider()
+                                    .padding(.leading)
+                                    .padding(.leading, 48)
+
+                                profilerPickerUserOption(selectedProfile)
+                                    .contentShape(Rectangle())
+                                    .id("BOTTOM_ANCHOR")
+                            }
+                        }
                     }
+                        .frame(maxHeight: USER_ROW_SIZE * min(MAX_VISIBLE_USER_ROWS, CGFloat(profiles.count + 1))) // + 1 for incognito
+                        .onAppear {
+                            DispatchQueue.main.async {
+                                withAnimation(nil) {
+                                    proxy.scrollTo("BOTTOM_ANCHOR", anchor: .bottom)
+                                }
+                            }
+                        }
+                        .onDisappear {
+                            expandedListReady = false
+                        }
 
-                    if incognitoDefault {
-                        profilerPickerUserOption(selectedProfile)
-                            .contentShape(Rectangle())
-                        Divider()
-                            .padding(.leading)
-                            .padding(.leading, 48)
-
-                        incognitoOption()
-                            .contentShape(Rectangle())
-                            .id("BOTTOM_ANCHOR")
+                    if #available(iOS 16.0, *) {
+                        scroll.scrollDismissesKeyboard(.never)
                     } else {
-                        incognitoOption()
-                            .contentShape(Rectangle())
-                        Divider()
-                            .padding(.leading)
-                            .padding(.leading, 48)
-                        
-                        profilerPickerUserOption(selectedProfile)
-                            .contentShape(Rectangle())
-                            .id("BOTTOM_ANCHOR")
+                        scroll
                     }
+                } else {
+                    // Keep showing current selection to delay rendering picker and flickering of scroll to bottom
+                    currentSelection()
+                        .onAppear {
+                            // Delay rendering for 1 frame
+                            DispatchQueue.main.async {
+                                expandedListReady = true
+                            }
+                        }
                 }
-            }
-            .frame(maxHeight: USER_ROW_SIZE * min(MAX_VISIBLE_USER_ROWS, CGFloat(profiles.count + 1))) // + 1 for incognito
-            .onAppear {
-                DispatchQueue.main.async {
-                    withAnimation(nil) {
-                        proxy.scrollTo("BOTTOM_ANCHOR", anchor: .bottom)
-                    }
-                }
-            }
-
-            if #available(iOS 16.0, *) {
-                scroll.scrollDismissesKeyboard(.never)
-            } else {
-                scroll
             }
         }
     }
