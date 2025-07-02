60c2fb785915691c4fb684c39bc06f4138ff1bc3
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-07-02 20:19:02 +0400

    ios, postgres migration

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."e4375c4c38e7ea14fd3e698994f0719b1b0baf6b" = "1bcz172rw43kz5sg49mrcic0fs5hxf802chmyqmrbiz3giybsx2z";
+  "https://github.com/simplex-chat/simplexmq.git"."aadccbc02e83b18d3fda8de9f35c1a9be6c8811c" = "0yjp0bi069hzg3sp3r0b36cadlmriyza65mi6302xb7mi8zm78gm";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
