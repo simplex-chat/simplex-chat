b2d2a834dc1be2482b846a33383e5706a8b69573
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-07-22 14:18:29 +0400

    kotlin

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/Preferences.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/Preferences.kt
@@ -69,9 +69,18 @@ private fun PreferencesLayout(
   ColumnWithScrollBar {
     AppBarTitle(stringResource(MR.strings.your_preferences))
     val timedMessages = remember(preferences) { mutableStateOf(preferences.timedMessages.allow) }
-    TimedMessagesFeatureSection(timedMessages) {
-      applyPrefs(preferences.copy(timedMessages = TimedMessagesPreference(allow = if (it) FeatureAllowed.YES else FeatureAllowed.NO)))
+    val onTTLUpdated = { ttl: Int? ->
+      applyPrefs(preferences.copy(timedMessages = preferences.timedMessages.copy(ttl = ttl)))
     }
+    TimedMessagesFeatureSection(
+      preferences,
+      timedMessages,
+      onSelected = {
+        applyPrefs(preferences.copy(timedMessages = TimedMessagesPreference(allow = if (it) FeatureAllowed.YES else FeatureAllowed.NO)))
+      },
+      onTTLUpdated = onTTLUpdated
+    )
+
     SectionDividerSpaced(true, maxBottomPadding = false)
     val allowFullDeletion = remember(preferences) { mutableStateOf(preferences.fullDelete.allow) }
     FeatureSection(ChatFeature.FullDelete, allowFullDeletion) {
@@ -117,7 +126,12 @@ private fun FeatureSection(feature: ChatFeature, allowFeature: State<FeatureAllo
 }
 
 @Composable
-private fun TimedMessagesFeatureSection(allowFeature: State<FeatureAllowed>, onSelected: (Boolean) -> Unit) {
+private fun TimedMessagesFeatureSection(
+  preferences: FullChatPreferences,
+  allowFeature: State<FeatureAllowed>,
+  onSelected: (Boolean) -> Unit,
+  onTTLUpdated: (Int?) -> Unit
+) {
   SectionView {
     PreferenceToggleWithIcon(
       ChatFeature.TimedMessages.text,
@@ -127,6 +141,16 @@ private fun TimedMessagesFeatureSection(allowFeature: State<FeatureAllowed>, onS
       extraPadding = false,
       onChange = onSelected
     )
+    if (allowFeature.value == FeatureAllowed.ALWAYS || allowFeature.value == FeatureAllowed.YES) {
+      val ttl = rememberSaveable(preferences) { mutableStateOf(preferences.timedMessages.ttl) }
+      ExposedDropDownSettingRow(
+        generalGetString(MR.strings.delete_after),
+        TimedMessagesPreference.profileLevelTTLValues.map { v -> v to timeText(v) },
+        ttl,
+        icon = null,
+        onSelected = onTTLUpdated
+      )
+    }
   }
   SectionTextFooter(ChatFeature.TimedMessages.allowDescription(allowFeature.value))
 }
