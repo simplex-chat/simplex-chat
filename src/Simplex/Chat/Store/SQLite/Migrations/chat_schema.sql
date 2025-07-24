28be6488f22b681ee4de968cd6b5c39e71c443f2
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-07-24 19:21:28 +0400

    schema

--- src/Simplex/Chat/Store/SQLite/Migrations/chat_schema.sql
+++ src/Simplex/Chat/Store/SQLite/Migrations/chat_schema.sql
@@ -1076,3 +1076,12 @@ CREATE INDEX idx_contact_requests_xcontact_id ON contact_requests(
   user_id,
   xcontact_id
 );
+CREATE INDEX idx_chat_items_group_scope_stats_all ON chat_items(
+  user_id,
+  group_id,
+  group_scope_tag,
+  group_scope_group_member_id,
+  item_status,
+  chat_item_id,
+  user_mention
+);
