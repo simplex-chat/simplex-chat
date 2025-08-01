b9b0a9af9460ac4c0bb61efeb617502bb1ccaf90
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-08-01 17:52:44 +0400

    padding

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chat/ComposeContextGroupDirectInvitationActionsView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chat/ComposeContextGroupDirectInvitationActionsView.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import chat.simplex.common.model.*
 import chat.simplex.common.platform.chatModel
+import chat.simplex.common.ui.theme.DEFAULT_PADDING_HALF
 import chat.simplex.common.views.helpers.*
 import chat.simplex.res.MR
 import dev.icerock.moko.resources.compose.painterResource
@@ -59,7 +60,8 @@ fun ComposeContextMemberContactActionsView(
       if (groupDirectInv.memberRemoved) {
         Row(
           Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(horizontal = DEFAULT_PADDING_HALF),
           verticalAlignment = Alignment.CenterVertically,
           horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
         ) {
