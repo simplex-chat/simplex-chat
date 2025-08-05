39a1c94ebca2005adeb77315185ab0a2c4203767
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-08-05 12:50:16 +0100

    core: updated simplexmq (prevent connection error with incomplete addresses of pre-configured servers)

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."db325cb81f77652471a27f4331143982739f9f10" = "15q2zjcsmp40c65d9wgngjk7vs753nq9cxf5vd996ns1lkqq1ckb";
+  "https://github.com/simplex-chat/simplexmq.git"."79ba60e3ad415f07568151f6b7756f721212502c" = "1i8gw8fi9xslk7zgnl3xggwvqg5fdscpcfnjvf78d343nbq4c30k";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
