d669c1ee629fd423a1b52c01aa80e588bf5bbdb1
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-08-11 08:49:40 +0100

    remove non-total methods

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/ChatModel.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/ChatModel.kt
@@ -474,14 +474,17 @@ object ChatModel {
     }
 
     fun removeLastChatItems() {
-      val removed: Triple<Long, Int, Boolean>
+      val remIndex: Int
+      val rem: ChatItem?
       chatItems.value = SnapshotStateList<ChatItem>().apply {
         addAll(chatItems.value)
-        val remIndex = lastIndex
-        val rem = removeLast()
-        removed = Triple(rem.id, remIndex, rem.isRcvNew)
+        remIndex = lastIndex
+        rem = removeLastOrNull()
       }
-      chatState.itemsRemoved(listOf(removed), chatItems.value)
+      if (rem != null) {
+        val removed = Triple(rem.id, remIndex, rem.isRcvNew)
+        chatState.itemsRemoved(listOf(removed), chatItems.value)
+      }
     }
 
     suspend fun addChatItem(rhId: Long?, chatInfo: ChatInfo, cItem: ChatItem) {
