805d9377c8097265cea22bae33981fde39fae587
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-08-11 17:12:05 +0100

    android, desktop: core API event timeout setting

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/DeveloperView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/DeveloperView.kt
@@ -2,9 +2,11 @@ package chat.simplex.common.views.usersettings
 
 import SectionBottomSpacer
 import SectionDividerSpaced
+import SectionItemView
 import SectionTextFooter
 import SectionView
 import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.toInt
 import androidx.compose.ui.platform.LocalUriHandler
 import chat.simplex.common.model.ChatController.appPrefs
 import chat.simplex.common.platform.*
@@ -12,6 +14,7 @@ import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 import chat.simplex.common.views.TerminalView
 import chat.simplex.common.views.helpers.*
+import chat.simplex.common.views.usersettings.networkAndServers.TimeoutSettingRow
 import chat.simplex.res.MR
 
 @Composable
@@ -23,7 +26,11 @@ fun DeveloperView(withAuth: (title: String, desc: String, block: () -> Unit) -> 
     AppBarTitle(stringResource(MR.strings.settings_developer_tools))
     val developerTools = m.controller.appPrefs.developerTools
     val devTools = remember { developerTools.state }
+    val apiRecvTimeout = remember { mutableStateOf(chatModel.controller.appPrefs.apiRecvTimeout.get().toLong()) }
     val unchangedHints = mutableStateOf(unchangedHintPreferences())
+    LaunchedEffect(apiRecvTimeout.value) {
+      chatModel.controller.appPrefs.apiRecvTimeout.set(apiRecvTimeout.value.toInt())
+    }
     SectionView {
       InstallTerminalAppItem(uriHandler)
       ChatConsoleItem { withAuth(generalGetString(MR.strings.auth_open_chat_console), generalGetString(MR.strings.auth_log_in_using_credential)) { ModalManager.start.showModalCloseable { TerminalView(false) } } }
@@ -57,6 +64,12 @@ fun DeveloperView(withAuth: (title: String, desc: String, block: () -> Unit) -> 
         }
         SettingsPreferenceItem(painterResource(MR.images.ic_report), stringResource(MR.strings.show_internal_errors), appPreferences.showInternalErrors)
         SettingsPreferenceItem(painterResource(MR.images.ic_avg_pace), stringResource(MR.strings.show_slow_api_calls), appPreferences.showSlowApiCalls)
+        SectionItemView {
+          TimeoutSettingRow(
+            "Core API timeout", apiRecvTimeout,
+            listOf(15_000000, 60_000000, 180_000000, 600_000000, 1200_000000, 1800_000000), stringResource(MR.strings.network_option_seconds_label)
+          )
+        }
       }
     }
     SectionDividerSpaced(maxTopPadding = true)
