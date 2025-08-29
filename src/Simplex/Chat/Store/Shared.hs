f6c699f3a56ec60fda3be048308ccaeccd6636cd
Evgeny <evgeny@poberezkin.com>
2025-08-29 10:05:20 +0100

    core: update simplexmq (error handling) (#6231)
    * core: update simplexmq (error handling)
    
    * update simplexmq

--- src/Simplex/Chat/Store/Shared.hs
+++ src/Simplex/Chat/Store/Shared.hs
@@ -46,6 +46,7 @@ import Simplex.Messaging.Crypto.Ratchet (PQEncryption (..), PQSupport (..))
 import qualified Simplex.Messaging.Crypto.Ratchet as CR
 import Simplex.Messaging.Parsers (dropPrefix, sumTypeJSON)
 import Simplex.Messaging.Protocol (SubscriptionMode (..))
+import Simplex.Messaging.Util (AnyError (..))
 import Simplex.Messaging.Version
 import UnliftIO.STM
 #if defined(dbPostgres)
@@ -148,6 +149,10 @@ data StoreError
   | SEInvalidQuote
   | SEInvalidMention
   deriving (Show, Exception)
+
+instance AnyError StoreError where
+  fromSomeException = SEInternalError . show
+  {-# INLINE fromSomeException #-}
 
 $(J.deriveJSON (sumTypeJSON $ dropPrefix "SE") ''StoreError)
 
