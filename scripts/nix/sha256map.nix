a5233d23075b7c529efb06fb834c2c0246a574a1
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-09-07 15:06:14 +0100

    update simplexmq - JSON encoding for missing NetworkError

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."50ddd630be3727d2860e4ff006a3ba48fdd8c07d" = "1s7jv1646fpasiag6gskd4hcsw5d020wf4h7wkd3x1s523ccfrh9";
+  "https://github.com/simplex-chat/simplexmq.git"."2a61085f078417c6a0767d3e2d8162bac83c963c" = "1zqssgz9d3bydcrnqlfy6aj7hfi65wb1mjcjpl3q744s82lrj68w";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
