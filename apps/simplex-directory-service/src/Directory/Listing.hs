429ec9d21a0ab24ddd19240c52023f1b3bec3b43
Evgeny <evgeny@poberezkin.com>
2025-09-18 17:52:47 +0100

    directory: listings for web pages (#6281)
    * directory: listings for web pages
    
    * refactor
    
    * tests

---
+++ apps/simplex-directory-service/src/Directory/Listing.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -fno-warn-ambiguous-fields #-}
+
+module Directory.Listing where
+
+import Control.Applicative ((<|>))
+import Control.Concurrent.STM
+import Control.Monad
+import qualified Data.Aeson as J
+import qualified Data.Aeson.TH as JQ
+import Data.ByteString (ByteString)
+import Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as LB
+import Data.Maybe (fromMaybe)
+import qualified Data.Set as S
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
+import Directory.Store
+import Simplex.Chat.Types
+import Simplex.Messaging.Parsers (defaultJSON, dropPrefix, taggedObjectJSON)
+import System.Directory
+import System.FilePath
+
+listingFileName :: String
+listingFileName = "listing.json"
+
+promotedFileName :: String
+promotedFileName = "promoted.json"
+
+listingImageFolder :: String
+listingImageFolder = "images"
+
+data DirectoryEntryType = DETGroup
+  { admission :: Maybe GroupMemberAdmission,
+    summary :: GroupSummary
+  }
+
+$(JQ.deriveJSON (taggedObjectJSON $ dropPrefix "DET") ''DirectoryEntryType)
+
+data DirectoryEntry = DirectoryEntry
+  { entryType :: DirectoryEntryType,
+    displayName :: Text,
+    shortDescr :: Maybe Text,
+    welcomeMessage :: Maybe Text,
+    imageFile :: Maybe String
+  }
+
+$(JQ.deriveJSON defaultJSON ''DirectoryEntry)
+
+data DirectoryListing = DirectoryListing {entries :: [DirectoryEntry]}
+
+$(JQ.deriveJSON defaultJSON ''DirectoryListing)
+
+type ImageFileData = ByteString
+
+groupDirectoryEntry :: GroupInfoSummary -> (DirectoryEntry, Maybe (FilePath, ImageFileData))
+groupDirectoryEntry (GIS GroupInfo {groupId, groupProfile} summary) =
+  let GroupProfile {displayName, shortDescr, description, image, memberAdmission} = groupProfile
+      entryType = DETGroup memberAdmission summary
+      imgData = imgFileData =<< image
+   in (DirectoryEntry {entryType, displayName, shortDescr, welcomeMessage = description, imageFile = fst <$> imgData}, imgData)
+  where
+    imgFileData (ImageData img) =
+      let (img', imgExt) =
+            fromMaybe (img, ".jpg") $
+              (,".jpg") <$> T.stripPrefix "data:image/jpg;base64," img
+                <|> (,".png") <$> T.stripPrefix "data:image/png;base64," img
+          imgFile = listingImageFolder </> show groupId <> imgExt
+       in case B64.decode $ encodeUtf8 img' of
+            Right img'' -> Just (imgFile, img'')
+            Left _ -> Nothing
+
+generateListing :: DirectoryStore -> FilePath -> [GroupInfoSummary] -> IO ()
+generateListing st dir gs = do
+  gs' <- filterListedGroups st gs
+  removePathForcibly (dir </> listingImageFolder)
+  createDirectoryIfMissing True (dir </> listingImageFolder)
+  gs'' <- forM gs' $ \g@(GIS GroupInfo {groupId} _) -> do
+    let (g', img) = groupDirectoryEntry g
+    forM_ img $ \(imgFile, imgData) -> B.writeFile (dir </> imgFile) imgData
+    pure (groupId, g')
+  saveListing listingFileName gs''
+  saveListing promotedFileName =<< filterPromotedGroups st gs''
+  where
+    saveListing f = LB.writeFile (dir </> f) . J.encode . DirectoryListing . map snd
+
+filterPromotedGroups :: DirectoryStore -> [(GroupId, DirectoryEntry)] -> IO [(GroupId, DirectoryEntry)]
+filterPromotedGroups st gs = do
+  pgs <- readTVarIO $ promotedGroups st
+  pure $ filter (\g -> fst g `S.member` pgs) gs
