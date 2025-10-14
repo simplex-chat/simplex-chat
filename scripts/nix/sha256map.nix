24af49562f9f7ab8a4e384bbd8cd406b8784bd88
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-10-14 11:13:24 +0100

    update simplexmq

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."91fad98718e62fcb85c7a734f13a4c80f7d95573" = "0w0kaz28lqm6s4f3qpfay2gzl4mf882wb24xpbssr3gyq0zp889n";
+  "https://github.com/simplex-chat/simplexmq.git"."9c87ef139e9f7a5d95902d3578865820f2ff70fe" = "0zj352vrr7afs0pms3zd3y2fh711cr9lk5r33c6bjyavqxcjnm4a";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
