ee61074ead6d667d55c44922b748dc966226717c
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-10-23 17:48:56 +0400

    link to chat relays

--- src/Simplex/Chat/Store/SQLite/Migrations/M20251018_chat_relays.hs
+++ src/Simplex/Chat/Store/SQLite/Migrations/M20251018_chat_relays.hs
@@ -11,13 +11,16 @@ import Database.SQLite.Simple.QQ (sql)
 -- - group_relays - group owner's list of relays for a group
 -- - group_relays.relay_link - links for all relays of a group are included in GroupShortLinkData
 -- - group_relays.relay_status - group owner's status for each relay (GroupRelayStatus)
+-- - group_relays.chat_relay_id - associates group_relays record with a chat_relays record,
+--     chat_relays.deleted is to keep associated record if user removes chat relay from configuration,
+--     but has group relays using it
 -- - group_members.is_chat_relay - indicates that the member is a chat relay (to all group members)
 -- - group_members.group_relay_id - associates group_members record with a group_relays record for a group owner;
---     receiving event to member connection, owner can match it to the relay
+--     receiving event to member connection, owner can match it to the relay;
+--     TBC inverse association - from group_relays to group_members?
 -- - TBC also inverse link from group_relays to group_members? (group_relays.group_member_id)
--- - groups.relay_own_status - indicates for a relay client that it is chat relay for the group (GroupRelayStatus)
+-- - groups.relay_status_self - indicates for a relay client that it is chat relay for the group (GroupRelayStatus)
 -- - connections.group_member_id_messaging - secondary connection for a group member in relayed group
--- TODO [chat relays] link between group_relays and chat_relays
 m20251018_chat_relays :: Query
 m20251018_chat_relays =
   [sql|
@@ -30,6 +33,7 @@ CREATE TABLE chat_relays(
   tested INTEGER,
   enabled INTEGER NOT NULL DEFAULT 1,
   user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
+  deleted INTEGER NOT NULL DEFAULT 0,
   created_at TEXT NOT NULL DEFAULT(datetime('now')),
   updated_at TEXT NOT NULL DEFAULT(datetime('now')),
   UNIQUE(user_id, address),
@@ -42,19 +46,19 @@ ALTER TABLE users ADD COLUMN is_user_chat_relay INTEGER NOT NULL DEFAULT 0;
 CREATE TABLE group_relays(
   group_relay_id INTEGER PRIMARY KEY,
   group_id INTEGER NOT NULL REFERENCES groups ON DELETE CASCADE,
-  group_member_id INTEGER NOT NULL REFERENCES group_members ON DELETE CASCADE,
+  chat_relay_id INTEGER NOT NULL REFERENCES chat_relays ON DELETE CASCADE,
   relay_status TEXT NOT NULL,
   relay_link BLOB
 );
 CREATE INDEX idx_group_relays_group_id ON group_relays(group_id);
-CREATE INDEX idx_group_relays_group_member_id ON group_relays(group_member_id);
+CREATE INDEX idx_group_relays_chat_relay_id ON group_relays(chat_relay_id);
 
 ALTER TABLE group_members ADD COLUMN is_chat_relay INTEGER NOT NULL DEFAULT 0;
 
 ALTER TABLE group_members ADD COLUMN group_relay_id INTEGER REFERENCES group_relays ON DELETE SET NULL;
 CREATE INDEX idx_group_members_group_relay_id ON group_members(group_relay_id);
 
-ALTER TABLE groups ADD COLUMN relay_own_status TEXT;
+ALTER TABLE groups ADD COLUMN relay_status_self TEXT;
 
 ALTER TABLE connections ADD COLUMN group_member_id_messaging INTEGER REFERENCES group_members ON DELETE CASCADE;
 CREATE INDEX idx_connections_group_member_id_messaging ON connections(group_member_id_messaging);
@@ -69,7 +73,7 @@ DROP TABLE chat_relays;
 ALTER TABLE users DROP COLUMN is_user_chat_relay;
 
 DROP INDEX idx_group_relays_group_id;
-DROP INDEX idx_group_relays_group_member_id;
+DROP INDEX idx_group_relays_chat_relay_id;
 DROP TABLE group_relays;
 
 ALTER TABLE group_members DROP COLUMN is_chat_relay;
@@ -77,7 +81,7 @@ ALTER TABLE group_members DROP COLUMN is_chat_relay;
 DROP INDEX idx_group_members_group_relay_id;
 ALTER TABLE group_members DROP COLUMN group_relay_id;
 
-ALTER TABLE groups DROP COLUMN relay_own_status;
+ALTER TABLE groups DROP COLUMN relay_status_self;
 
 DROP INDEX idx_connections_group_member_id_messaging;
 ALTER TABLE connections DROP COLUMN group_member_id_messaging;
