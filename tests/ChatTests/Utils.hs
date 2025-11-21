d66e314509b085f95dbaa7239e96f78c9f7c8dea
shum <github.shum@liber.li>
2025-11-21 11:34:38 +0000

    tests: skip multicast discovery in Mac CI

--- tests/ChatTests/Utils.hs
+++ tests/ChatTests/Utils.hs
@@ -93,6 +93,12 @@ xit' = if os == "linux" then xit else it
 xit'' :: (HasCallStack, Example a) => String -> a -> SpecWith (Arg a)
 xit'' = ifCI xit Hspec.it
 
+xitMacCI :: HasCallStack => String -> (TestParams -> Expectation) -> SpecWith (Arg (TestParams -> Expectation))
+xitMacCI = ifCI (if os == "darwin" then xit else it) it
+
+xitMacCI'' :: (HasCallStack, Example a) => String -> a -> SpecWith (Arg a)
+xitMacCI'' = ifCI (if os == "darwin" then xit else Hspec.it) Hspec.it
+
 xdescribe'' :: HasCallStack => String -> SpecWith a -> SpecWith a
 xdescribe'' = ifCI xdescribe describe
 
