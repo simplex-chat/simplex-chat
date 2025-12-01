0fb5dccd285ae40d0f4b458f9400a0d36e7e433e
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-12-01 13:54:03 +0400

    migration

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."3016b929b48d3116f8ee60169c06383ca57f78e0" = "1gvv96w1ag8xh19an5j50isjw0yq1cbsywii5s1adaplr6fw34mb";
+  "https://github.com/simplex-chat/simplexmq.git"."0375f1d5be6a10acf28b9f990c804acb73354195" = "057pxl5xf0nmjqiclzppki5l8lrj3cg80lp98zicg67kjygqvkhi";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
--- src/Simplex/Chat/Store/SQLite/Migrations/M20251128_member_relations_vector_stage_2.hs
+++ src/Simplex/Chat/Store/SQLite/Migrations/M20251128_member_relations_vector_stage_2.hs
@@ -2,48 +2,21 @@
 
 module Simplex.Chat.Store.SQLite.Migrations.M20251128_member_relations_vector_stage_2 where
 
-import qualified Data.ByteString as B
-import Data.Int (Int64)
 import Database.SQLite.Simple (Query)
 import Database.SQLite.Simple.QQ (sql)
-import Database.SQLite3 (FuncArgs, FuncContext, funcArgInt64, funcResultBlob)
-import Simplex.Chat.Types.MemberRelations (MemberRelation (..), setRelations)
-
--- This migration uses custom aggregate function build_relations_vector(idx, relation).
--- Register it on DB open before running this migration:
---
--- > import Database.SQLite3 (createAggregate)
--- >
--- > createAggregate db "build_relations_vector" (Just 2) [] buildRelationsVectorStep buildRelationsVectorFinal
--- >   >>= either (throwIO . userError . show) pure
-
--- Functions below also to be moved near function registration code.
-
--- | Step function for build_relations_vector aggregate.
--- Accumulates (idx, relation) pairs.
-buildRelationsVectorStep :: FuncContext -> FuncArgs -> [(Int64, MemberRelation)] -> IO [(Int64, MemberRelation)]
-buildRelationsVectorStep _ args acc = do
-  idx <- funcArgInt64 args 0
-  relation <- toMemberRelation <$> funcArgInt64 args 1
-  pure $ (idx, relation) : acc
-
--- | Final function for build_relations_vector aggregate.
--- Builds the vector from accumulated pairs using setRelations.
-buildRelationsVectorFinal :: FuncContext -> [(Int64, MemberRelation)] -> IO ()
-buildRelationsVectorFinal ctx acc = funcResultBlob ctx $ setRelations acc B.empty
-
-toMemberRelation :: Int64 -> MemberRelation
-toMemberRelation = \case
-  1 -> MRIntroduced
-  2 -> MRIntroducedTo
-  3 -> MRConnected
-  _ -> MRNew
 
 -- TODO [relations vector] drop group_member_intros in the end of migration
-
--- Build member_relations_vector for all members that don't have it yet
--- Vector encoding: byte at index i = relation value for member with index_in_group = i
--- Values: 0 = MRNew, 1 = MRIntroduced, 2 = MRIntroducedTo, 3 = MRConnected
+-- Build member_relations_vector for all members that don't have it yet.
+-- Vector encoding: byte at index i = relation value for member with index_in_group = i.
+-- Values: 0 = MRNew, 1 = MRIntroduced, 2 = MRIntroducedTo, 3 = MRConnected.
+--
+-- Relations as mapped in union of introductions:
+-- - Introduced: current member is to_group_member_id, status not connected;
+-- - IntroducedTo: current member is re_group_member_id, status not connected;
+-- - Connected: current member is to_group_member_id/re_group_member_id, status connected.
+--
+-- This migration uses custom aggregate function build_relations_vector(idx, relation).
+-- It is registered on DB open in Simplex.Messaging.Agent.Store.SQLite `connectDB`.
 m20251128_member_relations_vector_stage_2 :: Query
 m20251128_member_relations_vector_stage_2 =
   [sql|
@@ -52,28 +25,24 @@ SET member_relations_vector = COALESCE(
   (
     SELECT build_relations_vector(idx, relation)
     FROM (
-      -- Introduced: current member is to_group_member_id, status not connected
       SELECT m.index_in_group AS idx, 1 AS relation
       FROM group_member_intros i
       JOIN group_members m ON m.group_member_id = i.re_group_member_id
       WHERE i.to_group_member_id = group_members.group_member_id
         AND i.intro_status NOT IN ('re-con', 'to-con', 'con')
       UNION ALL
-      -- IntroducedTo: current member is re_group_member_id, status not connected
       SELECT m.index_in_group AS idx, 2 AS relation
       FROM group_member_intros i
       JOIN group_members m ON m.group_member_id = i.to_group_member_id
       WHERE i.re_group_member_id = group_members.group_member_id
         AND i.intro_status NOT IN ('re-con', 'to-con', 'con')
       UNION ALL
-      -- Connected: current member is to_group_member_id, status connected
       SELECT m.index_in_group AS idx, 3 AS relation
       FROM group_member_intros i
       JOIN group_members m ON m.group_member_id = i.re_group_member_id
       WHERE i.to_group_member_id = group_members.group_member_id
         AND i.intro_status IN ('re-con', 'to-con', 'con')
       UNION ALL
-      -- Connected: current member is re_group_member_id, status connected
       SELECT m.index_in_group AS idx, 3 AS relation
       FROM group_member_intros i
       JOIN group_members m ON m.group_member_id = i.to_group_member_id
