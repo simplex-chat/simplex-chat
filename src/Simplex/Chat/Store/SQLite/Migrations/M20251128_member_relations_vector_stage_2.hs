30f3ceb003fecd5e606466c03d2444d6709b9874
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-12-03 20:04:17 +0400

    update

--- src/Simplex/Chat/Store/SQLite/Migrations/M20251128_member_relations_vector_stage_2.hs
+++ src/Simplex/Chat/Store/SQLite/Migrations/M20251128_member_relations_vector_stage_2.hs
@@ -22,29 +22,6 @@ import Database.SQLite.Simple.QQ (sql)
 -- - intro_status 're-con': if direction=0 then MRSubjectConnected (2), else MRReferencedConnected (3)
 -- - intro_status 'to-con': if direction=0 then MRReferencedConnected (3), else MRSubjectConnected (2)
 -- - intro_status 'con': MRConnected (4)
---
--- Aggregate function implementation in Simplex.Messaging.Agent.Store.SQLite `connectDB`:
---
--- createAggregate db' "migrate_relations_vector" (Just 3) [] migrateRelationsVectorStep migrateRelationsVectorFinal
---   >>= either (throwIO . userError . show) pure
---
--- migrateRelationsVectorStep :: FuncContext -> FuncArgs -> [(Int64, IntroductionDirection, MemberRelation)] -> IO [(Int64, IntroductionDirection, MemberRelation)]
--- migrateRelationsVectorStep _ args acc = do
---   idx <- funcArgInt64 args 0
---   direction <- fromIntroductionInt . fromIntegral <$> funcArgInt64 args 1
---   introStatus <- funcArgText args 2
---   let relation = introStatusToRelation direction introStatus
---   pure $ (idx, direction, relation) : acc
---   where
---     introStatusToRelation dir status = case status of
---       "re-con" -> if dir == IDSubjectIntroduced then MRSubjectConnected else MRReferencedConnected
---       "to-con" -> if dir == IDSubjectIntroduced then MRReferencedConnected else MRSubjectConnected
---       "con" -> MRConnected
---       _ -> MRIntroduced -- 'new', 'sent', 'rcv', 'fwd'
---
--- migrateRelationsVectorFinal :: FuncContext -> [(Int64, IntroductionDirection, MemberRelation)] -> IO ()
--- migrateRelationsVectorFinal ctx acc = funcResultBlob ctx $ setNewRelations acc B.empty
---
 m20251128_member_relations_vector_stage_2 :: Query
 m20251128_member_relations_vector_stage_2 =
   [sql|
@@ -53,13 +30,11 @@ SET member_relations_vector = COALESCE(
   (
     SELECT migrate_relations_vector(idx, direction, intro_status)
     FROM (
-      -- Subject is re_group_member_id, direction = 0 (IDSubjectIntroduced)
       SELECT m.index_in_group AS idx, 0 AS direction, i.intro_status
       FROM group_member_intros i
       JOIN group_members m ON m.group_member_id = i.to_group_member_id
       WHERE i.re_group_member_id = group_members.group_member_id
       UNION ALL
-      -- Subject is to_group_member_id, direction = 1 (IDReferencedIntroduced)
       SELECT m.index_in_group AS idx, 1 AS direction, i.intro_status
       FROM group_member_intros i
       JOIN group_members m ON m.group_member_id = i.re_group_member_id
