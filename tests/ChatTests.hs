defa7110142ed7b84c7c6e400da3bfd9edf67efe
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2025-12-09 12:57:34 +0400

    enable tests

--- tests/ChatTests.hs
+++ tests/ChatTests.hs
@@ -14,7 +14,7 @@ chatTests :: SpecWith TestParams
 chatTests = do
   describe "direct tests" chatDirectTests
   describe "forward tests" chatForwardTests
-  fdescribe "group tests" chatGroupTests
+  describe "group tests" chatGroupTests
   describe "local chats tests" chatLocalChatsTests
   describe "file tests" chatFileTests
   describe "profile tests" chatProfileTests
