37f8f2c00f2f83eae7c62dbba1d40ba95b36e2ea
shum <github.shum@liber.li>
2025-12-24 13:45:45 +0000

    strip ios like in android

--- flake.nix
+++ flake.nix
@@ -102,7 +102,7 @@
 
         sha256sum $out/_pkg/*.a
 
-        find "$out/_pkg" -type f \( -name "*.so" -o -name "*.a" -o -name "*.o" \) -exec sh -c '${pkgs.stdenv.cc.targetPrefix}strip --strip-unneeded "$1" || true' _ {} \;
+        find "$out/_pkg" -type f -name "*.a" -exec ${pkgs.stdenv.cc.targetPrefix}strip --strip-unneeded {} +
 
         (cd $out/_pkg; ${pkgs.zip}/bin/zip -r -9 $out/${bundleName}.zip *)
         rm -fR $out/_pkg
