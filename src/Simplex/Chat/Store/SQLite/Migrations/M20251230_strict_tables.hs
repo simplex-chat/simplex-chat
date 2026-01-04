918560cefaf0e19fe1a9abb96c1c538855100033
Evgeny Poberezkin <evgeny@poberezkin.com>
2026-01-04 20:59:13 +0000

    change types before strict

--- src/Simplex/Chat/Store/SQLite/Migrations/M20251230_strict_tables.hs
+++ src/Simplex/Chat/Store/SQLite/Migrations/M20251230_strict_tables.hs
@@ -19,20 +19,20 @@ UPDATE app_settings SET app_settings = CAST(app_settings as TEXT);
 PRAGMA writable_schema=1;
 
 UPDATE sqlite_master
-SET sql = CASE
-  WHEN LOWER(SUBSTR(sql, -15)) = ') without rowid' THEN sql || ', STRICT'
-  WHEN SUBSTR(sql, -1) = ')' THEN sql || ' STRICT'
-  ELSE sql
-END
-WHERE type = 'table' AND name != 'sqlite_sequence';
-
-UPDATE sqlite_master
 SET sql = replace(sql, 'call_state BLOB NOT NULL', 'call_state TEXT NOT NULL')
 WHERE type = 'table' AND name = 'calls';
 
 UPDATE sqlite_master
 SET sql = replace(sql, 'local_alias DEFAULT', 'local_alias TEXT DEFAULT')
 WHERE type = 'table' AND name = 'connections';
+
+UPDATE sqlite_master
+SET sql = CASE
+  WHEN LOWER(SUBSTR(sql, -15)) = ') without rowid' THEN sql || ', STRICT'
+  WHEN SUBSTR(sql, -1) = ')' THEN sql || ' STRICT'
+  ELSE sql
+END
+WHERE type = 'table' AND name != 'sqlite_sequence';
 
 PRAGMA writable_schema=0;
 |]
