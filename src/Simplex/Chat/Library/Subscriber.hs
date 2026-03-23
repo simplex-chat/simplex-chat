aa5956970118c1fb36d0ff12f703a898a356fb4c
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2026-03-23 17:01:58 +0400

    fixes

--- src/Simplex/Chat/Library/Subscriber.hs
+++ src/Simplex/Chat/Library/Subscriber.hs
@@ -1237,22 +1237,23 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
                     gInfo <- getGroupInfo db vr user groupId
                     gLink <- getGroupLink db user gInfo
                     relays <- liftIO $ getGroupRelays db gInfo
-                    (relays', changed) <- liftIO $ foldM (updateRelay db) ([], False) relays
+                    results <- liftIO $ mapM (updateRelay db) relays
+                    let relays' = map fst results
+                        changed = any snd results
                     liftIO $ setGroupInProgressDone db gInfo
                     pure (gInfo, gLink, relays', changed)
-                  -- TODO [relays] owner: "relays updated" chat item?
                   toView $ CEvtGroupLinkDataUpdated user gInfo gLink relays relaysChanged
                   where
                     -- TODO [relays] owner: on relay deletion (link absent from relayLinks)
                     -- TODO          move status RSActive to new "Removed" status / remove relay record
-                    updateRelay :: DB.Connection -> ([GroupRelay], Bool) -> GroupRelay -> IO ([GroupRelay], Bool)
-                    updateRelay db (acc, changed) relay@GroupRelay {relayLink, relayStatus} =
+                    updateRelay :: DB.Connection -> GroupRelay -> IO (GroupRelay, Bool)
+                    updateRelay db relay@GroupRelay {relayLink, relayStatus} =
                       case relayLink of
                         Just rLink
                           | rLink `elem` relayLinks && relayStatus == RSAccepted -> do
                               relay' <- updateRelayStatus db relay RSActive
-                              pure (acc <> [relay'], True)
-                        _ -> pure (acc <> [relay], changed)
+                              pure (relay', True)
+                        _ -> pure (relay, False)
                 _ -> throwChatError $ CECommandError "LINK event expected for a group link only"
             _ -> throwChatError $ CECommandError "unexpected cmdFunction"
       MERR _ err -> do
@@ -2766,9 +2767,9 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
                     then liftIO $ increaseGroupMembersRequireAttention db user gInfo
                     else pure gInfo
                 pure (updatedMember, gInfo')
-              toView $ CEvtUnknownMemberAnnounced user gInfo' m unknownMember updatedMember
-              memberAnnouncedToView updatedMember gInfo'
-              void $ updatePublicGroupData user gInfo'
+              gInfo'' <- updatePublicGroupData user gInfo'
+              toView $ CEvtUnknownMemberAnnounced user gInfo'' m unknownMember updatedMember
+              memberAnnouncedToView updatedMember gInfo''
               pure $ deliveryJobScope updatedMember
             Right _
               | useRelays' gInfo -> logInfo "x.grp.mem.new: member already created via another relay" $> Nothing
@@ -2781,8 +2782,8 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
                     then liftIO $ increaseGroupMembersRequireAttention db user gInfo
                     else pure gInfo
                 pure (newMember, gInfo')
-              memberAnnouncedToView newMember gInfo'
-              void $ updatePublicGroupData user gInfo'
+              gInfo'' <- updatePublicGroupData user gInfo'
+              memberAnnouncedToView newMember gInfo''
               pure $ deliveryJobScope newMember
       where
         initialStatus = case msgScope_ of
@@ -2989,12 +2990,12 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
                   Just (DJSMemberSupport _) | shouldForward -> updateMemberRecordDeleted user gInfo deletedMember GSMemRemoved
                   -- Undeleted "member connected" chat item will prevent deletion of member record.
                   _ -> deleteOrUpdateMemberRecord user gInfo deletedMember
-                gInfo' <- updatePublicGroupData user gInfo'
+                gInfo'' <- updatePublicGroupData user gInfo'
                 let wasDeleted = memberStatus == GSMemRemoved || memberStatus == GSMemLeft
                     deletedMember' = deletedMember {memberStatus = GSMemRemoved}
-                when withMessages $ deleteMessages gInfo' deletedMember' SMDRcv
-                unless wasDeleted $ deleteMemberItem gInfo' $ RGEMemberDeleted groupMemberId (fromLocalProfile memberProfile)
-                toView $ CEvtDeletedMember user gInfo' m deletedMember' withMessages msgSigned
+                when withMessages $ deleteMessages gInfo'' deletedMember' SMDRcv
+                unless wasDeleted $ deleteMemberItem gInfo'' $ RGEMemberDeleted groupMemberId (fromLocalProfile memberProfile)
+                toView $ CEvtDeletedMember user gInfo'' m deletedMember' withMessages msgSigned
                 pure deliveryScope
       where
         checkRole GroupMember {memberRole} a
@@ -3034,12 +3035,12 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
       deleteMemberConnection m
       -- member record is not deleted to allow creation of "member left" chat item
       gInfo' <- updateMemberRecordDeleted user gInfo m GSMemLeft
-      gInfo' <- updatePublicGroupData user gInfo'
-      unless (muteEventInChannel gInfo' m) $ do
-        (gInfo'', m', scopeInfo) <- mkGroupChatScope gInfo' m
-        (ci, cInfo) <- saveRcvChatItemNoParse user (CDGroupRcv gInfo'' scopeInfo m') msg brokerTs (CIRcvGroupEvent RGEMemberLeft)
+      gInfo'' <- updatePublicGroupData user gInfo'
+      unless (muteEventInChannel gInfo'' m) $ do
+        (gInfo''', m', scopeInfo) <- mkGroupChatScope gInfo'' m
+        (ci, cInfo) <- saveRcvChatItemNoParse user (CDGroupRcv gInfo''' scopeInfo m') msg brokerTs (CIRcvGroupEvent RGEMemberLeft)
         groupMsgToView cInfo ci
-        toView $ CEvtLeftMember user gInfo'' m' {memberStatus = GSMemLeft} msgSigned
+        toView $ CEvtLeftMember user gInfo''' m' {memberStatus = GSMemLeft} msgSigned
       pure $ memberEventDeliveryScope m
 
     xGrpDel :: GroupInfo -> GroupMember -> RcvMessage -> UTCTime -> CM ()
