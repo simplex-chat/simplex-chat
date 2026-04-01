b481a0194d1cc61c47ae5c90289316d25b334534
shum <github.shum@liber.li>
2026-04-01 14:40:21 +0000

    cli: decouple key reading from processing via TQueue

--- src/Simplex/Chat/Terminal.hs
+++ src/Simplex/Chat/Terminal.hs
@@ -8,6 +8,7 @@
 module Simplex.Chat.Terminal where
 
 import Control.Monad
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.List.NonEmpty as L
 import Simplex.Chat (defaultChatConfig)
 import Simplex.Chat.Controller
@@ -22,6 +23,8 @@ import Simplex.Chat.Terminal.Output
 import Simplex.FileTransfer.Client.Presets (defaultXFTPServers)
 import Simplex.Messaging.Client (NetworkConfig (..), SMPProxyFallback (..), SMPProxyMode (..), defaultNetworkConfig)
 import Simplex.Messaging.Util (raceAny_)
+import System.Terminal (Key, Modifiers)
+import UnliftIO.STM
 #if !defined(dbPostgres)
 import Control.Exception (handle, throwIO)
 import qualified Data.ByteArray as BA
@@ -99,4 +102,9 @@ simplexChatTerminal cfg options t = run options
 #endif
 
 runChatTerminal :: ChatTerminal -> ChatController -> ChatOpts -> IO ()
-runChatTerminal ct cc opts = raceAny_ [runTerminalInput ct cc, runTerminalOutput ct cc opts, runInputLoop ct cc]
+runChatTerminal ct cc opts = do
+  keyQ <- newTQueueIO
+  raceAny_ [runKeyReader ct keyQ, runTerminalInput ct cc keyQ, runTerminalOutput ct cc opts, runInputLoop ct cc]
+
+runKeyReader :: ChatTerminal -> TQueue (Key, Modifiers) -> IO ()
+runKeyReader ct q = withChatTerm ct $ forever $ getKey >>= liftIO . atomically . writeTQueue q
