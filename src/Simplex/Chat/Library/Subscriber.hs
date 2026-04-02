1f83f53b9c7e9fc41788789b014407ac2e66f05c
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2026-04-02 17:21:19 +0400

    fix

--- src/Simplex/Chat/Library/Subscriber.hs
+++ src/Simplex/Chat/Library/Subscriber.hs
@@ -1491,7 +1491,10 @@ processAgentMessageConn vr user@User {userId} corrId agentConnId agentMessage = 
               subMode <- chatReadVar subscriptionMode
               chatVR <- chatVersionRange
               let chatV = chatVR `peerConnChatVersion` chatVRange
-              void $ agentAcceptContactAsync user True invId msg subMode PQSupportOff chatV
+              (cmdId, acId) <- agentAcceptContactAsync user True invId msg subMode PQSupportOff chatV
+              withFastStore $ \db -> do
+                Connection {connId = testCId} <- createRelayTestConnection db vr user acId ConnAccepted chatV subMode
+                liftIO $ setCommandConnId db user cmdId testCId
         -- TODO [relays] owner, relays: TBC how to communicate member rejection rules from owner to relays
         -- TODO [relays] relay: TBC communicate rejection when memberId already exists (currently checked in createJoiningMember)
         memberJoinRequestViaRelay :: InvitationId -> VersionRangeChat -> Profile -> MemberId -> MemberKey -> CM ()
