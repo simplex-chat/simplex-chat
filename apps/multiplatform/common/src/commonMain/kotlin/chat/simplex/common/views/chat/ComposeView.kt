e911f5de565c0833596cb0f2d99658dee863f636
Narasimha-sc <166327228+Narasimha-sc@users.noreply.github.com>
2026-04-20 20:12:44 +0000

    desktop: fix voice message recording not stopping on chat switch

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chat/ComposeView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/chat/ComposeView.kt
@@ -1237,6 +1237,8 @@ fun ComposeView(
       composeState.value = cs.copy(inProgress = false, progressByTimeout = false)
     } else if (!cs.empty) {
       if (cs.preview is ComposePreview.VoicePreview && !cs.preview.finished) {
+        recState.value = RecordingState.NotStarted
+        RecorderInterface.stopRecording?.invoke()
         composeState.value = cs.copy(preview = cs.preview.copy(finished = true))
       }
       if (saveLastDraft) {
