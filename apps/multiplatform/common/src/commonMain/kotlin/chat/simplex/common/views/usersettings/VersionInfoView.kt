2bcbfdd35bb39c81308f15082e2237b63a4c66b0
Narasimha-sc <166327228+Narasimha-sc@users.noreply.github.com>
2026-06-06 10:35:20 +0000

    android, desktop: show app version info in its own card
    Wrap the version info block on the app version screen in a section card,
    matching iOS and the rest of the card-screen settings.

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/VersionInfoView.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/usersettings/VersionInfoView.kt
@@ -2,6 +2,8 @@ package chat.simplex.common.views.usersettings
 
 import SectionBottomSpacer
 import SectionDividerSpaced
+import SectionView
+import itemHPadding
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
@@ -14,7 +16,7 @@ import chat.simplex.common.model.CoreVersionInfo
 import chat.simplex.common.platform.ColumnWithScrollBar
 import chat.simplex.common.platform.appPlatform
 import chat.simplex.common.platform.chatModel
-import chat.simplex.common.ui.theme.DEFAULT_PADDING
+import chat.simplex.common.ui.theme.DEFAULT_PADDING_HALF
 import chat.simplex.common.views.helpers.AppBarTitle
 import chat.simplex.res.MR
 
@@ -29,18 +31,20 @@ fun VersionInfoView(
   }
   ColumnWithScrollBar {
     AppBarTitle(stringResource(MR.strings.app_version_title))
-    Column(Modifier.padding(horizontal = DEFAULT_PADDING)) {
-      if (appPlatform.isAndroid) {
-        Text(String.format(stringResource(MR.strings.app_version_name), BuildConfigCommon.ANDROID_VERSION_NAME))
-        Text(String.format(stringResource(MR.strings.app_version_code), BuildConfigCommon.ANDROID_VERSION_CODE))
-      } else {
-        Text(String.format(stringResource(MR.strings.app_version_name), BuildConfigCommon.DESKTOP_VERSION_NAME))
-        Text(String.format(stringResource(MR.strings.app_version_code), BuildConfigCommon.DESKTOP_VERSION_CODE))
-      }
-      versionInfo.value?.let { info ->
-        Text(String.format(stringResource(MR.strings.core_version), info.version))
-        val simplexmqCommit = if (info.simplexmqCommit.length >= 7) info.simplexmqCommit.substring(startIndex = 0, endIndex = 7) else info.simplexmqCommit
-        Text(String.format(stringResource(MR.strings.core_simplexmq_version), info.simplexmqVersion, simplexmqCommit))
+    SectionView {
+      Column(Modifier.padding(horizontal = itemHPadding, vertical = DEFAULT_PADDING_HALF)) {
+        if (appPlatform.isAndroid) {
+          Text(String.format(stringResource(MR.strings.app_version_name), BuildConfigCommon.ANDROID_VERSION_NAME))
+          Text(String.format(stringResource(MR.strings.app_version_code), BuildConfigCommon.ANDROID_VERSION_CODE))
+        } else {
+          Text(String.format(stringResource(MR.strings.app_version_name), BuildConfigCommon.DESKTOP_VERSION_NAME))
+          Text(String.format(stringResource(MR.strings.app_version_code), BuildConfigCommon.DESKTOP_VERSION_CODE))
+        }
+        versionInfo.value?.let { info ->
+          Text(String.format(stringResource(MR.strings.core_version), info.version))
+          val simplexmqCommit = if (info.simplexmqCommit.length >= 7) info.simplexmqCommit.substring(startIndex = 0, endIndex = 7) else info.simplexmqCommit
+          Text(String.format(stringResource(MR.strings.core_simplexmq_version), info.simplexmqVersion, simplexmqCommit))
+        }
       }
     }
     SectionDividerSpaced()
