67cbd4bd06395680d2ccd1e0b2aa828c6666eeae
shum <github.shum@liber.li>
2026-07-20 08:36:56 +0000

    chore(migration): date server_roles migration 20260720

--- src/Simplex/Chat/Store/Postgres/Migrations/M20260720_server_roles.hs
+++ src/Simplex/Chat/Store/Postgres/Migrations/M20260720_server_roles.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-module Simplex.Chat.Store.Postgres.Migrations.M20260716_server_roles where
+module Simplex.Chat.Store.Postgres.Migrations.M20260720_server_roles where
 
 import Data.Text (Text)
 import Text.RawString.QQ (r)
 
-m20260716_server_roles :: Text
-m20260716_server_roles =
+m20260720_server_roles :: Text
+m20260720_server_roles =
   [r|
 ALTER TABLE protocol_servers ADD COLUMN role_storage SMALLINT;
 ALTER TABLE protocol_servers ADD COLUMN role_proxy SMALLINT;
 ALTER TABLE protocol_servers ADD COLUMN role_names SMALLINT;
 |]
 
-down_m20260716_server_roles :: Text
-down_m20260716_server_roles =
+down_m20260720_server_roles :: Text
+down_m20260720_server_roles =
   [r|
 ALTER TABLE protocol_servers DROP COLUMN role_storage;
 ALTER TABLE protocol_servers DROP COLUMN role_proxy;
--- src/Simplex/Chat/Store/SQLite/Migrations/M20260720_server_roles.hs
+++ src/Simplex/Chat/Store/SQLite/Migrations/M20260720_server_roles.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE QuasiQuotes #-}
 
-module Simplex.Chat.Store.SQLite.Migrations.M20260716_server_roles where
+module Simplex.Chat.Store.SQLite.Migrations.M20260720_server_roles where
 
 import Database.SQLite.Simple (Query)
 import Database.SQLite.Simple.QQ (sql)
 
-m20260716_server_roles :: Query
-m20260716_server_roles =
+m20260720_server_roles :: Query
+m20260720_server_roles =
   [sql|
 ALTER TABLE protocol_servers ADD COLUMN role_storage INTEGER;
 ALTER TABLE protocol_servers ADD COLUMN role_proxy INTEGER;
 ALTER TABLE protocol_servers ADD COLUMN role_names INTEGER;
 |]
 
-down_m20260716_server_roles :: Query
-down_m20260716_server_roles =
+down_m20260720_server_roles :: Query
+down_m20260720_server_roles =
   [sql|
 ALTER TABLE protocol_servers DROP COLUMN role_storage;
 ALTER TABLE protocol_servers DROP COLUMN role_proxy;
