633e9cc909f02b7c6c8f51c38ad7e26b898f9267
Alain Brenzikofer <alain@integritee.network>
2026-08-05 17:31:23 +0200

    bump

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."8e0d24f379d634adb23e86b4ea6105c8c30facde" = "0y1yhigzdjja48njmridqjvxqsv2mvfrf13i708h7s30jl1hizpl";
+  "https://github.com/simplex-chat/simplexmq.git"."ff28e0ccaf885f631fb87fa2f6b5e06b0797e5e8" = "0pq6fvix1safk4c4qlflzv7l7jn9pv2vx1fnlw2vqm12jzn2jjcb";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
