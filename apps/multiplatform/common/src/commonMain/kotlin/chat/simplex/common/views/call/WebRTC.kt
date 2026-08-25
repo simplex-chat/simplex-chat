5d4bc4e591e0bf830fd4589bc59e11db1be1d38f
GuavaGoat <320862384+GuavaGoat@users.noreply.github.com>
2026-08-25 05:41:36 +0000

    Update WebRTC.kt

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/call/WebRTC.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/views/call/WebRTC.kt
@@ -56,6 +56,7 @@ enum class CallState {
   AnswerReceived,
   Negotiated,
   Connected,
+  Reconnecting,
   Ended;
 
   val text: String get() = when(this) {
@@ -67,6 +68,7 @@ enum class CallState {
     AnswerReceived -> generalGetString(MR.strings.callstate_received_confirmation)
     Negotiated -> generalGetString(MR.strings.callstate_connecting)
     Connected -> generalGetString(MR.strings.callstate_connected)
+    Reconnecting -> generalGetString(MR.strings.callstate_reconnecting)
     Ended -> generalGetString(MR.strings.callstate_ended)
   }
 }
@@ -173,6 +175,7 @@ enum class RTCIceCandidateType(val value: String) {
 enum class WebRTCCallStatus(val value: String) {
   @SerialName("connected") Connected("connected"),
   @SerialName("connecting") Connecting("connecting"),
+  @SerialName("reconnecting") Reconnecting("reconnecting"),
   @SerialName("disconnected") Disconnected("disconnected"),
   @SerialName("failed") Failed("failed")
 }
