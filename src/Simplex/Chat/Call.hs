0f734b7a502d588196f17be0f544ee0da1c7feaa
GuavaGoat <320862384+GuavaGoat@users.noreply.github.com>
2026-08-29 05:46:49 +0000

    Update Call.hs revert call status change
    minimization

--- src/Simplex/Chat/Call.hs
+++ src/Simplex/Chat/Call.hs
@@ -180,21 +180,19 @@ data WebRTCExtraInfo = WebRTCExtraInfo
   }
   deriving (Eq, Show)
 
-data WebRTCCallStatus = WCSConnecting | WCSConnected | WCSReconnecting | WCSDisconnected | WCSFailed
+data WebRTCCallStatus = WCSConnecting | WCSConnected | WCSDisconnected | WCSFailed
   deriving (Show)
 
 instance StrEncoding WebRTCCallStatus where
   strEncode = \case
     WCSConnecting -> "connecting"
     WCSConnected -> "connected"
-    WCSReconnecting -> "reconnecting"
     WCSDisconnected -> "disconnected"
     WCSFailed -> "failed"
   strP =
     A.takeTill (== ' ') >>= \case
       "connecting" -> pure WCSConnecting
       "connected" -> pure WCSConnected
-      "reconnecting" -> pure WCSReconnecting
       "disconnected" -> pure WCSDisconnected
       "failed" -> pure WCSFailed
       _ -> fail "bad WebRTCCallStatus"
