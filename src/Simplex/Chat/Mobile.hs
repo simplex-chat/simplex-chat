ca114a7a2ae2c33dc2863158baace88fee651a99
Evgeny <evgeny@poberezkin.com>
2026-09-13 12:23:39 +0100

    core: option to limit the max number of loaded chats (#7499)
    * core: option to limit the max number of loaded chats
    
    * update bot api
    
    ---------
    
    Co-authored-by: Evgeny @ SimpleX Chat <259188159+evgeny-simplex@users.noreply.github.com>

--- src/Simplex/Chat/Mobile.hs
+++ src/Simplex/Chat/Mobile.hs
@@ -259,6 +259,7 @@ mobileChatOpts dbOptions =
             logAgent = Nothing,
             logFile = Nothing,
             tbqSize = 4096,
+            maxChats = 5000,
             deviceName = Nothing,
             chatRelay = False,
             webPreviewConfig = Nothing,
