c4ad09be44d2c375acbcb52700b65927b9318e1d
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2026-09-14 21:20:24 +0400

    Revert "ios navigation"
    This reverts commit 90da57db5c785f314a1c6c439274c0ba18f259f7.

--- apps/ios/Shared/Views/Badges/BadgesRedeemCodeView.swift
+++ apps/ios/Shared/Views/Badges/BadgesRedeemCodeView.swift
@@ -37,7 +37,7 @@ struct BadgesRedeemCodeView: View {
     @EnvironmentObject var theme: AppTheme
     @EnvironmentObject var chatModel: ChatModel
     @AppStorage(DEFAULT_SUPPORTER_BANNER_SHOWN) private var supporterBannerShown = false
-    @Binding var active: Bool
+    @Environment(\.dismiss) var dismiss: DismissAction
     @State private var code = ""
     @State private var canonicalCode: String? = nil
     @State private var submitting = false
@@ -184,7 +184,7 @@ struct BadgesRedeemCodeView: View {
                         showAlert(NSLocalizedString("Cannot redeem code", comment: "alert title"), message: failureMessage(newBadge ? .badgeEnded : .codeUsed))
                     } else {
                         supporterBannerShown = true
-                        active = false
+                        dismiss()
                     }
                 }
             } catch let error {
@@ -222,7 +222,7 @@ struct BadgesRedeemCodeView: View {
 struct BadgesRedeemCodeView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            BadgesRedeemCodeView(active: .constant(true))
+            BadgesRedeemCodeView()
         }
     }
 }
