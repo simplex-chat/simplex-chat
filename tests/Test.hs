d02c5af067be3c2e8c9ebcd44f0281eb152d94de
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2026-09-15 20:26:54 +0400

    enable tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -63,7 +63,7 @@ main = do
 #endif
       around tmpBracket $ describe "WebRTC encryption" webRTCTests
 #endif
-      fdescribe "Supporter badges" badgeTests
+      describe "Supporter badges" badgeTests
       describe "SimpleX chat markdown" markdownTests
       describe "JSON Tests" jsonTests
       describe "Member relations" memberRelationsTests
@@ -90,7 +90,6 @@ main = do
           xdescribe'' "SimpleX Broadcast bot" broadcastBotTests
           xdescribe'' "SimpleX Directory service bot" directoryServiceTests
           xdescribe'' "SimpleX Badge service bot" badgeServiceTests
-          fdescribe "SimpleX Badge service bot" badgeServiceTests
           describe "Remote session" remoteTests
 #if !defined(dbPostgres)
           xdescribe'' "Save query plans" saveQueryPlans
