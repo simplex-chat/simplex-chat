3130a7c556d675131caf081c6ac4a5a270491839
Alain Brenzikofer <alain@encointer.org>
2026-09-25 08:34:31 +0200

    repin

--- scripts/nix/sha256map.nix
+++ scripts/nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/simplex-chat/simplexmq.git"."355cec2eb434fe29a8f947c8196e6936a1875932" = "0rfq20a1vvzw001zgw5df11xkdvh8qxwwanr08cz84g3xbi1ldvf";
+  "https://github.com/simplex-chat/simplexmq.git"."2bfc408a58d4a0bed084816f9d885527c499e6a4" = "1ccq3qvhd2ml1dwzw26j9akhpnxdp7yjdsjpkg7vhglzmhyvzzd4";
   "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
   "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
   "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
